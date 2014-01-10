61d1f0de4df4e6960e25639ebc4a51ab3f879fef
PrinterLUA <roberto_potrus@hotmail.com>
2014-01-10 20:15:17 +0100

    Real Tibia Rashid Travelling days.
    Now you can enable to make Rashid travel to citys as real tibia.
    http://tibia.wikia.com/wiki/Rashid
    Former-commit-id: efe70381f5f6bae4461e0b52b8ac29b36c5ece65

--- data/globalevents/scripts/others/RashidStartUp.lua
+++ data/globalevents/scripts/others/RashidStartUp.lua
@@ -1,4 +1,4 @@
---[[local config = {
+local config = {
     ["Monday"] = {pos = {x=32209, y=31158, z=7}}, -- DankWart - Svargrond
 
     ["Tuesday"] = {pos = {x=32290, y=32831, z=7}}, -- Lyonel - Liberty Bay
@@ -12,16 +12,21 @@
     ["Saturday"] = {pos = {x=33171, y=31810, z=6}}, -- Mirabell - Edron
 
     ["Sunday"] = {pos = {x=32326, y=31783, z=6}} -- Carlin Depot 2floor
-}]]--
+}
+
+local day = false --Should it spawn as real tibia?
  
 function onStartup()
-	--local day = config[os.date("%A")]
-	--doCreateNpc("Rashid", day.pos)
-	doCreateNpc("Rashid", {x=32209, y=31158, z=7})
-	doCreateNpc("Rashid", {x=32290, y=32831, z=7})
-	doCreateNpc("Rashid", {x=32579, y=32754, z=7})
-	doCreateNpc("Rashid", {x=33066, y=32880, z=6})
-	doCreateNpc("Rashid", {x=33239, y=32483, z=7})
-	doCreateNpc("Rashid", {x=33171, y=31810, z=6})
-	doCreateNpc("Rashid", {x=32326, y=31783, z=6})
+	if day then
+		doCreateNpc("Rashid", config[os.date("%A")].pos)
+	else
+		doCreateNpc("Rashid", config["Monday"].pos)
+        	doCreateNpc("Rashid", config["Tuesday"].pos)
+        	doCreateNpc("Rashid", config["Wednesday"].pos)
+        	doCreateNpc("Rashid", config["Thursday"].pos)
+        	doCreateNpc("Rashid", config["Friday"].pos)
+        	doCreateNpc("Rashid", config["Saturday"].pos)
+        	doCreateNpc("Rashid", config["Sunday"].pos)
+	end
+	return true
 end
