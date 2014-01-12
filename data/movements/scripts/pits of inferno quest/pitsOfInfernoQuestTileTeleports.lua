89ad21e5c1cee20fe277288e44c4c0f5527dcb66
Dogox22 <Dogox22@gmail.com>
2014-01-12 14:54:24 +0100

    Full PoI & Gravedigger Quest Fixed
    Former-commit-id: d1a479e2a277d9068302839bfda3c44683058e39

--- data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestTileTeleports.lua
+++ data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestTileTeleports.lua
@@ -1,18 +1,89 @@
---[[local teleports = {
-	[2100] = {x = , y = , z = },
-	[2101] = {x = , y = , z = },
-	[2102] = {x = , y = , z = },
-	[2103] = {x = , y = , z = },
-	[2104] = {x = , y = , z = },
-	[2105] = {x = , y = , z = },
-	[2106] = {x = , y = , z = },
-	[2107] = {x = , y = , z = },
-	[2108] = {x = , y = , z = },
-	[2109] = {x = , y = , z = }
-}]]--
+function onStepIn(cid, item, pos)
 
+local pole1 = {x=32827, y=32314, z=9}
+local pole2 = {x=32841, y=32323, z=9}
+local pole3 = {x=32855, y=32296, z=9}
+local pole4 = {x=32840, y=32317, z=9}
+local pole5 = {x=32854, y=32323, z=9}
+local pole6 = {x=32858, y=32296, z=9}
+local pole7 = {x=32856, y=32289, z=9}
+local pole8 = {x=32847, y=32287, z=9}
+local pole9 = {x=32861, y=32301, z=9}
+local pole10 = {x=32839, y=32321, z=9}
+local pole11 = {x=32827, y=32308, z=9}
+local pole12 = {x=32858, y=32304, z=9}
+local pole13 = {x=32851, y=32323, z=9}
+local pole14 = {x=32847, y=32287, z=9}
+local pole15 = {x=32855, y=32318, z=9}
+local pole16 = {x=32839, y=32321, z=9}
+local pole17 = {x=32861, y=32301, z=9}
 
-function onStepIn(cid, item, position, lastPosition)
-	doTeleportThing(cid, teleports[item.uid])
-	return true
+    if item.actionid == 28810 then
+		doTeleportThing(cid,pole1)
+        else
+		 if item.actionid == 28811 then
+		doTeleportThing(cid,pole2)
+		else
+		 if item.actionid == 28812 then
+		doTeleportThing(cid,pole3)
+		else
+		 if item.actionid == 28813 then
+		doTeleportThing(cid,pole4)
+		else
+		 if item.actionid == 28814 then
+		doTeleportThing(cid,pole5)
+		else
+		 if item.actionid == 28815 then
+		doTeleportThing(cid,pole6)
+		else
+		 if item.actionid == 28816 then
+		doTeleportThing(cid,pole7)
+		else
+		 if item.actionid == 28817 then
+		doTeleportThing(cid,pole8)
+		else
+		 if item.actionid == 28818 then
+		doTeleportThing(cid,pole9)
+		else
+		 if item.actionid == 28819 then
+		doTeleportThing(cid,pole10)
+		else
+		 if item.actionid == 28820 then
+		doTeleportThing(cid,pole11)
+		else
+		 if item.actionid == 28821 then
+		doTeleportThing(cid,pole12)
+		else
+		 if item.actionid == 28822 then
+		doTeleportThing(cid,pole13)
+		else
+		 if item.actionid == 28823 then
+		doTeleportThing(cid,pole14)
+		else
+		 if item.actionid == 28824 then
+		doTeleportThing(cid,pole15)
+		else
+		 if item.actionid == 28825 then
+		doTeleportThing(cid,pole16)
+		else
+		 if item.actionid == 28826 then
+		doTeleportThing(cid,pole17)
+		end
+end  
 end
+end
+end
+end
+end
+end
+end
+end
+end
+end
+end
+end
+end
+end
+end
+end
+
