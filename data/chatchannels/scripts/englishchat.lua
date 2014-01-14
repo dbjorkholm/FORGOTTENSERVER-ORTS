d9dec566342493bd2f4a7ad94f9b4215549f8a22
WibbenZ <wibbenz@gmail.com>
2014-01-14 06:28:30 +0100

    Update englishchat.lua
    Updated to the new functions.
    
    Update: Diden't see the cancelMessage at the start, fixed.

--- data/chatchannels/scripts/englishchat.lua
+++ data/chatchannels/scripts/englishchat.lua
@@ -1,21 +1,21 @@
 function onSpeak(cid, type, message)
-	if getPlayerLevel(cid) == 1 and getPlayerAccountType(cid) < ACCOUNT_TYPE_GAMEMASTER then
-		doPlayerSendCancel(cid, "You may not speak into channels as long as you are on level 1.")
+	if(Player(cid):getLevel() == 1 and Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER) then
+		Player(cid):sendCancelMessage("You may not speak into channels as long as you are on level 1.")
 		return false
 	end
 
-	if type == TALKTYPE_CHANNEL_Y then
-		if getPlayerAccountType(cid) >= ACCOUNT_TYPE_GAMEMASTER then
+	if(type == TALKTYPE_CHANNEL_Y) then
+		if(Player(cid):getAccountType() >= ACCOUNT_TYPE_GAMEMASTER) then
 			type = TALKTYPE_CHANNEL_O
 		end
-	elseif type == TALKTYPE_CHANNEL_O then
-		if getPlayerAccountType(cid) < ACCOUNT_TYPE_GAMEMASTER then
+	elseif(type == TALKTYPE_CHANNEL_O) then
+		if(Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER) then
 			type = TALKTYPE_CHANNEL_Y
 		end
-	elseif type == TALKTYPE_CHANNEL_R1 then
-		if getPlayerAccountType(cid) < ACCOUNT_TYPE_GAMEMASTER and not getPlayerFlagValue(cid, PlayerFlag_CanTalkRedChannel) then
+	elseif(type == TALKTYPE_CHANNEL_R1) then
+		if(Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER and not getPlayerFlagValue(cid, PlayerFlag_CanTalkRedChannel)) then
 			type = TALKTYPE_CHANNEL_Y
 		end
 	end
-	return type
+return type
 end
