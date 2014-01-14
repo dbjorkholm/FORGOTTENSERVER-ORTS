643edd1bcf7a8390812051e3fc94f4ac76b8b186
WibbenZ <wibbenz@gmail.com>
2014-01-14 23:12:30 +0100

    Update crusaderHelmetQuestRewards.lua
    New functions
    
    Former-commit-id: 1abfe33dc0effbfe17f205a5a359be70f7f78d12

--- data/actions/scripts/crusader helmet quest/crusaderHelmetQuestRewards.lua
+++ data/actions/scripts/crusader helmet quest/crusaderHelmetQuestRewards.lua
@@ -1,12 +1,12 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.uid == 2233) then
-		if(getPlayerStorageValue(cid, 328) < 1) then
-			setPlayerStorageValue(cid, 328, 1)
-			doPlayerAddItem(cid, 2497, 1)
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You've found a crusader helmet.")
-		else
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "The chest is empty.")
-		end
-	end
-	return true
+        if(item.uid == 2233) then
+                if(Player(cid):getStorageValue(328) < 1) then
+                        Player(cid):setStorageValue(328, 1)
+                        Player(cid):addItem(2497, 1)
+                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "You've found a crusader helmet.")
+                else
+                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+                end
+        end
+        return true
 end
