06bf23041b2662ce235bc80937f872888756feaa
WibbenZ <wibbenz@gmail.com>
2014-01-14 23:21:39 +0100

    Update demonHelmetQuestRewards.lua
    New functions
    
    Former-commit-id: 7efb9b97c2db42554dafc892df7faf1fe8ac7ec7

--- data/actions/scripts/demon helmet quest/demonHelmetQuestRewards.lua
+++ data/actions/scripts/demon helmet quest/demonHelmetQuestRewards.lua
@@ -1,37 +1,37 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if (getPlayerStorageValue(cid, 2217) < 1) then
-		doPlayerAddExp(cid, 250000, true, true)
-		setPlayerStorageValue(cid, 2217, 1)
-		if (getSex == 0) then
-			doPlayerAddOutfit(cid,542,1)
-		else
-			doPlayerAddOutfit(cid,541,1)
-		end
-	end
-	if(item.uid == 2212) then
-		if(getPlayerStorageValue(cid, 306) < 1) then
-			setPlayerStorageValue(cid, 306, 1)
-			doPlayerAddItem(cid, 2520, 1)
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You've found a demon shield.")
-		else
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "The chest is empty.")
-		end
-	elseif(item.uid == 2213) then
-		if(getPlayerStorageValue(cid, 305) < 1) then
-			setPlayerStorageValue(cid, 305, 1)
-			doPlayerAddItem(cid, 2493, 1)
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You've found a demon helmet.")
-		else
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "The chest is empty.")
-		end
-	elseif(item.uid == 2214) then
-		if(getPlayerStorageValue(cid, 304) < 1) then
-			setPlayerStorageValue(cid, 304, 1)
-			doPlayerAddItem(cid, 2645, 1)
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You've found a steel boots.")
-		else
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "The chest is empty.")
-		end
-	end
-	return true
+        if(Player(cid):getStorageValue(2217) < 1) then
+                Player(cid):addExperience(250000, true, true)
+                Player(cid):setStorageValue(2217, 1)
+                if(Player(cid):getSex() == 0) then
+                        Player(cid):addOutfitAddon(cid,542,1)
+                else
+                        Player(cid):addOutfitAddon(cid,541,1)
+                end
+        end
+        if(item.uid == 2212) then
+                if(Player(cid):getStorageValue(306) < 1) then
+                        Player(cid):setStorageValue(306, 1)
+                        Player(cid):addItem(2520, 1)
+                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "You've found a demon shield.")
+                else
+                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+                end
+        elseif(item.uid == 2213) then
+                if(Player(cid):getStorageValue(305) < 1) then
+                        Player(cid):setStorageValue(305, 1)
+                        Player(cid):addItem(2493, 1)
+                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "You've found a demon helmet.")
+                else
+                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+                end
+        elseif(item.uid == 2214) then
+                if(Player(cid):getStorageValue(304) < 1) then
+                        Player(cid):setStorageValue(304, 1)
+                        Player(cid):addItem(2645, 1)
+                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "You've found a steel boots.")
+                else
+                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+                end
+        end
+        return true
 end
