f598e8163f13a22701708aba78ca6265428e277b
WibbenZ <wibbenz@gmail.com>
2014-01-14 22:57:33 +0100

    Update annihilatorQuestDoor.lua
    New functions

--- data/actions/scripts/annihilator quest/annihilatorQuestDoor.lua
+++ data/actions/scripts/annihilator quest/annihilatorQuestDoor.lua
@@ -1,11 +1,11 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.uid == 2216) then
-		if(getPlayerStorageValue(cid, 2215) < 1) then
-			if(item.itemid == 5114) then
-				doTeleportThing(cid, toPosition, true)
-				doTransformItem(item.uid, 5115)
-			end
-		end
-	end
-	return true
+        if(item.uid == 2216) then
+                if(Player(cid):getStorageValue(2215) < 1) then
+                        if(item.itemid == 5114) then
+								Creature(uid):teleportTo(toPosition, true)
+                                Item(uid):transform(5115)
+                        end
+                end
+        end
+        return true
 end
