317f20826a58959c20db565897e45d1126d3296f
WibbenZ <wibbenz@gmail.com>
2014-01-14 23:36:06 +0100

    Update samOldBackpackQuestReward.lua
    New functions

--- data/actions/scripts/sam old backpack/samOldBackpackQuestReward.lua
+++ data/actions/scripts/sam old backpack/samOldBackpackQuestReward.lua
@@ -1,12 +1,12 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.uid == 2236) then
-		if(getPlayerStorageValue(cid, 330) == 2) then
-			setPlayerStorageValue(cid, 330, 3)
-			doPlayerAddItem(cid, 2503, 1)
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You've found a dwarven armor.")
-		else
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "The chest is empty.")
-		end
-	end
-	return true
+        if(item.uid == 2236) then
+                if(Player(cid):getStorageValue(330) == 2) then
+                        Player(cid):setStorageValue(330, 3)
+                        Player(cid):addItem(2503, 1)
+                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "You've found a dwarven armor.")
+                else
+                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+                end
+        end
+        return true
 end
