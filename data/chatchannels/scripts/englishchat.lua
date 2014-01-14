e1086f2e0c1fcc308926306a9006c8c989b13e72
PrinterLUA <mastero_90@hotmail.com>
2014-01-14 17:42:56 +0100

    Update englishchat.lua
    Had to add it myself, idk why i couldnt merge your pull request.
    https://github.com/PrinterLUA/FORGOTTENSERVER-ORTS/pull/54

--- data/chatchannels/scripts/englishchat.lua
+++ data/chatchannels/scripts/englishchat.lua
@@ -1,21 +1,21 @@
 function onSpeak(cid, type, message)
-        if(Player(cid):getLevel() == 1 and Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER) then
-                doPlayerSendCancel(cid, "You may not speak into channels as long as you are on level 1.")
-                return false
-        end
+	if(Player(cid):getLevel() == 1 and Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER) then
+		Player(cid):sendCancelMessage("You may not speak into channels as long as you are on level 1.")
+		return false
+	end
 
-        if(type == TALKTYPE_CHANNEL_Y) then
-                if(Player(cid):getAccountType() >= ACCOUNT_TYPE_GAMEMASTER) then
-                        type = TALKTYPE_CHANNEL_O
-                end
-        elseif(type == TALKTYPE_CHANNEL_O) then
-                if(Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER) then
-                        type = TALKTYPE_CHANNEL_Y
-                end
-        elseif(type == TALKTYPE_CHANNEL_R1) then
-                if(Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER and not getPlayerFlagValue(cid, PlayerFlag_CanTalkRedChannel)) then
-                        type = TALKTYPE_CHANNEL_Y
-                end
-        end
+	if(type == TALKTYPE_CHANNEL_Y) then
+		if(Player(cid):getAccountType() >= ACCOUNT_TYPE_GAMEMASTER) then
+			type = TALKTYPE_CHANNEL_O
+		end
+	elseif(type == TALKTYPE_CHANNEL_O) then
+		if(Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER) then
+			type = TALKTYPE_CHANNEL_Y
+		end
+	elseif(type == TALKTYPE_CHANNEL_R1) then
+		if(Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER and not getPlayerFlagValue(cid, PlayerFlag_CanTalkRedChannel)) then
+			type = TALKTYPE_CHANNEL_Y
+		end
+	end
 return type
 end
