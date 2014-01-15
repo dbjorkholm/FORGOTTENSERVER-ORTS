8a7bee42595939c288d3fc0e58f1c72b40cfe3d4
PrinterLUA <mastero_90@hotmail.com>
2014-01-15 15:33:23 +0100

    Update lifeRingQuestReward.lua
    Former-commit-id: c603b5881ba63c7be4694f5e05b5ca058f801387

--- data/actions/scripts/life ring quest/lifeRingQuestReward.lua
+++ data/actions/scripts/life ring quest/lifeRingQuestReward.lua
@@ -3,7 +3,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
                 if(Player(cid):getStorageValue(332) < 1) then
                         Player(cid):setStorageValue(332, 1)
                         bag = Player(cid):addItem(1987, 1)
-                        Container(bag.uid):addItemEx(2201, 1)
+                        Container(bag.uid):addItemEx(bag.uid, 2201, 1)
                         Container(bag.uid):addItemEx(2168, 1)
                         Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "You've found a bag.")
                 else
