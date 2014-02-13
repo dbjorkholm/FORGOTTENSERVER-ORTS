d09a679e45700e05db0e924b596cca27c2ac05f6
WibbenZ <wibbenz@gmail.com>
2014-02-13 13:48:15 +0100

    Optimized to speed it up :)
    Optimized to speed it up :)
    
    Former-commit-id: e98858253093a7b6d83b306c5597cbed5190e118

--- data/globalevents/scripts/others/RashidStartUp.lua
+++ data/globalevents/scripts/others/RashidStartUp.lua
@@ -1,32 +1,21 @@
 local config = {
-    ["Monday"] = {pos = {x=32209, y=31158, z=7}}, -- DankWart - Svargrond
-
-    ["Tuesday"] = {pos = {x=32290, y=32831, z=7}}, -- Lyonel - Liberty Bay
-
-    ["Wednesday"] = {pos = {x=32579, y=32754, z=7}}, -- Clyde - Port Hope
-
-    ["Thursday"] = {pos = {x=33066, y=32880, z=6}}, -- Arito - Ankrahmun
-
-    ["Friday"] = {pos = {x=33239, y=32483, z=7}}, -- Miraia - Darishia
-
-    ["Saturday"] = {pos = {x=33171, y=31810, z=6}}, -- Mirabell - Edron
-
-    ["Sunday"] = {pos = {x=32326, y=31783, z=6}} -- Carlin Depot 2floor
+    ["Monday"] = {pos = {x=32209, y=31158, z=7}},
+    ["Tuesday"] = {pos = {x=32290, y=32831, z=7}},
+    ["Wednesday"] = {pos = {x=32579, y=32754, z=7}},
+    ["Thursday"] = {pos = {x=33066, y=32880, z=6}},
+    ["Friday"] = {pos = {x=33239, y=32483, z=7}},
+    ["Saturday"] = {pos = {x=33171, y=31810, z=6}},
+    ["Sunday"] = {pos = {x=32326, y=31783, z=6}}
 }
 
 local day = false --Should it spawn as real tibia?
- 
 function onStartup()
-	if day then
+	if(day) then
 		doCreateNpc("Rashid", config[os.date("%A")].pos)
 	else
-		doCreateNpc("Rashid", config["Monday"].pos)
-        	doCreateNpc("Rashid", config["Tuesday"].pos)
-        	doCreateNpc("Rashid", config["Wednesday"].pos)
-        	doCreateNpc("Rashid", config["Thursday"].pos)
-        	doCreateNpc("Rashid", config["Friday"].pos)
-        	doCreateNpc("Rashid", config["Saturday"].pos)
-        	doCreateNpc("Rashid", config["Sunday"].pos)
+		for x, i in pairs(config) do
+			doCreateNpc("Rashid", i.pos)
+		end
 	end
-	return true
+return true
 end
