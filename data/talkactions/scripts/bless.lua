d365db0d9834f979d7cd221814e277c34214af5d
WibbenZ <wibbenz@gmail.com>
2014-02-14 12:24:48 +0100

    Update bless.lua

--- data/talkactions/scripts/bless.lua
+++ data/talkactions/scripts/bless.lua
@@ -1,21 +1,22 @@
 function onSay(cid, words, param)
-        local cost = getBlessingsCost(getPlayerLevel(cid))
-        if(not(isPlayerPzLocked(cid))) then
-                if(Player(cid):hasBlessing(1) and Player(cid):hasBlessing(2) and Player(cid):hasBlessing(3) and Player(cid):hasBlessing(4) and Player(cid):hasBlessing(5)) then
-                        Player(cid):sendCancelMessage("You have already been blessed by the gods.")
-                        return false
-                end
-                if(Player(cid):removeMoney(cost)) then
-                        for b = 1,5 do
-                            Player(cid):addBlessing(b)
-                        end
-                        Position(Player:getPosition()):sendMagicEffect(CONST_ME_HOLYAREA)
-                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "You have been blessed by the 5 gods!")
-                else
-                        Player(cid):sendCancelMessage("You need "..cost.." gold coins to buy all 5 bless.")
-                end
-        else
-                Player(cid):sendCancelMessage("You can't buy bless, when you are in a battle.")
-        end
-        return false
+	local p = Player(cid)
+	local cost = getBlessingsCost(getPlayerLevel(cid))
+	if(not(isPlayerPzLocked(cid))) then
+		if(p:hasBlessing(1) and p:hasBlessing(2) and p:hasBlessing(3) and p:hasBlessing(4) and p:hasBlessing(5)) then
+			p:sendCancelMessage("You have already been blessed by the gods.")
+			return false
+		end
+		if(p:removeMoney(cost)) then
+			for b = 1,5 do
+				p:addBlessing(b)
+			end
+			p:getPosition():sendMagicEffect(CONST_ME_HOLYAREA)
+			p:sendTextMessage(MESSAGE_INFO_DESCR, "You have been blessed by the 5 gods!")
+		else
+			p:sendCancelMessage("You need "..cost.." gold coins to buy all 5 bless.")
+		end
+	else
+		p:sendCancelMessage("You can't buy bless, when you are in a battle.")
+	end
+return false
 end
