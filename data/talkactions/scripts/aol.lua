2fe67e80fe897777190f128041bcce44d287860c
WibbenZ <wibbenz@gmail.com>
2014-02-14 20:20:03 +0100

    Update aol.lua

--- data/talkactions/scripts/aol.lua
+++ data/talkactions/scripts/aol.lua
@@ -1,15 +1,14 @@
-local cost = 50000
-
 function onSay(cid, words, param)
-        if(not(Player(cid):isPzLocked())) then
-                if(Player(cid):removeMoney(cost)) then
-                        Player(cid):addItem(2173, 1)
-                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "You have bought amulet of loss!")        
-                else
-                        Player(cid):sendCancelMessage("You need "..cost.." gold coins to buy amulet of loss.")
-                end
-        else
-                Player(cid):sendCancelMessage("You can't buy amulet of loss, when you are in a battle.")
-        end
-        return false
+	local cost, p = 50000, Player(cid)
+	if(not(p:isPzLocked())) then
+		if(p:removeMoney(cost)) then
+			p:addItem(2173, 1)
+			p:sendTextMessage(MESSAGE_INFO_DESCR, "You have bought amulet of loss!")        
+		else
+			p:sendCancelMessage("You need "..cost.." gold coins to buy amulet of loss.")
+		end
+	else
+		p:sendCancelMessage("You can't buy amulet of loss, when you are in a battle.")
+	end
+return false
 end
