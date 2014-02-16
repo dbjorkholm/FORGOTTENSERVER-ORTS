1c4cdd45b211b30f6ecdf945e87c63a98c2d1f86
WibbenZ <wibbenz@gmail.com>
2014-02-16 10:04:43 +0100

    Update belongings.lua

--- data/actions/scripts/other/belongings.lua
+++ data/actions/scripts/other/belongings.lua
@@ -1,10 +1,12 @@
 function onUse(cid, item, frompos, item2, topos)
-local items = {2237,2232,6577,2220,2227,5894,13508,13926,13925,5879,2114}
-local count = {1,1,1,1,1,1,10,10,10,1,1}
-present =math.random(#items)
+	local p = Player(cid)
+	local i = Item(item.uid)
+	local items = {2237,2232,6577,2220,2227,5894,13508,13926,13925,5879,2114}
+	local count = {1,1,1,1,1,1,10,10,10,1,1}
+	local present = math.random(#items)
 
-	doSendMagicEffect(getPlayerPosition(cid),27)
-	doPlayerAddItem(cid,items[present],count[present])
-	doRemoveItem(item.uid,1)
-	return true
+	p:getPosition():sndMagicEffect(27)
+	p:additem(items[present],count[present])
+	i:remove(1)
+return true
 end
