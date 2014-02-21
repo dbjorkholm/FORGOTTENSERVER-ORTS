02935df7da296fc4881be116fb4f342cdde568f3
WibbenZ <wibbenz@gmail.com>
2014-02-21 15:54:48 +0100

    Bugfix
    Bugfix
    
    Former-commit-id: f1369549a84fa404412169558293831e0a98e677

--- data/spells/scripts/healing/heal friend.lua
+++ data/spells/scripts/healing/heal friend.lua
@@ -5,17 +5,13 @@ setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 
 function onGetFormulaValues(cid, level, maglevel)
-min = ((level*0.2) + (maglevel*10))
-max = ((level*0.2) + (maglevel*14))
-return min, max
+	min = ((level / 5) + (maglevel * 6.3) + 45)
+	max = ((level / 5) + (maglevel * 14.4) + 90)
+	return min, max
 end
 
 setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
-	if getTileInfo(getThingPos(cid)).protection == false then
-		return doCombat(cid, combat, var)
-	else
-		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "You can't cast this spell in protection zone.")
-	end
+	return doCombat(cid, combat, var)
 end
