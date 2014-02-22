d52f2cfc0e0a433567802bf2a6f38ef6e150a1b0
Royalot <zapf-benny@web.de>
2014-02-22 13:08:11 +0100

    update fishing.lua add function waterelemental

--- data/actions/scripts/tools/fishing.lua
+++ data/actions/scripts/tools/fishing.lua
@@ -1,6 +1,7 @@
 local useWorms = false
 local waterIds = {493, 4608, 4609, 4610, 4611, 4612, 4613, 4614, 4615, 4616, 4617, 4618, 4619, 4620, 4621, 4622, 4623, 4624, 4625}
 local iceholeIds = {7236}
+local waterelementalcorpseIds = {10499}
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 local i = Item(itemEx.uid)
 local p = Player(cid)
@@ -15,7 +16,7 @@ local p = Player(cid)
 		end
 		toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
 		return true
-	elseif isInArray(iceholeIds, itemEx.itemid) == true then -- fish in icehole --
+	elseif isInArray(iceholeIds, itemEx.itemid) == true then -- fish in ice hole --
 		if useWorms == false or useWorms == true and p:removeItem(ITEM_WORM, 1) == true then
 			if math.random(1, (100 + (p:getSkillLevel(SKILL_FISHING) / 10))) <= p:getSkillLevel(SKILL_FISHING) then
 				local rand = math.random(1, 100)
@@ -35,6 +36,35 @@ local p = Player(cid)
 			toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
 			return true
 		end
+	elseif isInArray(waterelementalcorpseIds, itemEx.itemid) == true then -- fish Water elemental Corpse --
+		if math.random(1, (100 + (p:getSkillLevel(SKILL_FISHING) / 10))) <= p:getSkillLevel(SKILL_FISHING) then
+			local rand = math.random(1, 1000)
+			if(rand > 995) then
+				p:addItem(7632, 1) --  Giant Shimmering Pearl 0,5% --
+			elseif(rand > 983) then
+				p:addItem(2149, 1) -- small emerald 1,2% --
+			elseif(rand > 972) then
+				p:addItem(2167, 1) -- energy ring 1,1% --
+			elseif(rand > 962) then
+				p:addItem(7159, 1) -- Green Perch 1% --
+			elseif(rand > 952) then
+				p:addItem(2145, 1) -- Small Diamond 1% --
+			elseif(rand > 942) then
+				p:addItem(7158, 1) -- rainbow trout 1% --
+			elseif(rand > 933) then
+				p:addItem(2168, 1) -- life ring 0,9% --
+			elseif(rand > 924) then
+				p:addItem(2143, 1) -- white pearl 0,9% --
+			elseif(rand > 916) then
+				p:addItem(10220, 1) -- leviathan's amulet 0,8% --
+			elseif(rand > 904) then
+				p:addItem(2146, 1) -- small sapphire 1,2% --	
+			end
+			p:addSkillTries(SKILL_FISHING, 1)
+			i:transform(10500)
+		end
+		toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
+		return true
 	end
 	return false
 end
