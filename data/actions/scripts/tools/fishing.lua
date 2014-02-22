bd98dc8595a1eb283515b81648c8e95a1201fafd
Royalot <zapf-benny@web.de>
2014-02-22 12:05:55 +0100

    updated fishing.lua
    tested and workz now
    thanks to ninjalulz and wibbenz for help :)
    
    Former-commit-id: 07c843d46519e0eadee5b1b3f6a4674e7f973fb4

--- data/actions/scripts/tools/fishing.lua
+++ data/actions/scripts/tools/fishing.lua
@@ -6,33 +6,33 @@ local i = Item(itemEx.uid)
 local p = Player(cid)
 	if isInArray(waterIds, itemEx.itemid) == true then
 		if itemEx.itemid ~= 493 then
-			if useWorms == false or useWorms == true and p:doRemoveItem(ITEM_WORM, 1) == true then
-				if math.random(1, (100 + (p:getSkill(SKILL_FISHING) / 10))) <= p:getSkill(SKILL_FISHING) then
-					p:doAddItem(ITEM_FISH, 1)
+			if useWorms == false or useWorms == true and p:removeItem(ITEM_WORM, 1) == true then
+				if math.random(1, (100 + (p:getSkillLevel(SKILL_FISHING) / 10))) <= p:getSkillLevel(SKILL_FISHING) then
+					p:addItem(ITEM_FISH, 1)
 				end
-				p:doAddSkillTry(SKILL_FISHING, 1)
+				p:addSkillTries(SKILL_FISHING, 1)
 			end
 		end
-		toPosition:doSendMagicEffect(CONST_ME_LOSEENERGY)
+		toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
 		return true
 	elseif isInArray(iceholeIds, itemEx.itemid) == true then -- fish in icehole --
-		if useWorms == false or useWorms == true and p:doRemoveItem(ITEM_WORM, 1) == true then
-			if math.random(1, (100 + (p:getSkill(SKILL_FISHING) / 10))) <= p:getSkill(SKILL_FISHING) then
+		if useWorms == false or useWorms == true and p:removeItem(ITEM_WORM, 1) == true then
+			if math.random(1, (100 + (p:getSkillLevel(SKILL_FISHING) / 10))) <= p:getSkillLevel(SKILL_FISHING) then
 				local rand = math.random(1, 100)
 				if(rand > 98) then
-					p:doAddItem(7158, 1) -- rainbow trout 2% --
+					p:addItem(7158, 1) -- rainbow trout 2% --
 				elseif(rand > 95) then
-					p:doAddItem(2669, 1) -- Northern Pike 3% --
+					p:addItem(2669, 1) -- Northern Pike 3% --
 				elseif(rand > 89) then
-					p:doAddItem(7159, 1) -- Green Perch 6% --
+					p:addItem(7159, 1) -- Green Perch 6% --
 				else
-					p:doAddItem(ITEM_FISH, 1) -- normal fish --
+					p:addItem(ITEM_FISH, 1) -- normal fish --
 				end
-				p:doAddSkillTry(SKILL_FISHING, 1)
+				p:addSkillTries(SKILL_FISHING, 1)
 				i:transform(7237)
 				i:decay()
 			end
-			toPosition:doSendMagicEffect(CONST_ME_LOSEENERGY)
+			toPosition:sendMagicEffect(CONST_ME_LOSEENERGY)
 			return true
 		end
 	end
