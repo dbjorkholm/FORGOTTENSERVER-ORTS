f81941554ea24ca7f9893286cd75fbefd261b492
Royalot <zapf-benny@web.de>
2014-03-03 23:45:34 +0100

    Fixed and updated some NPCs
    - Alexander in Edron
    - Baxter in Thais
    - Beatrice in Edron
    
    
    Former-commit-id: 9e3a861134d6434b154b6814f56fda40f376e7f6

--- data/npc/scripts/Alexander.lua
+++ data/npc/scripts/Alexander.lua
@@ -2,14 +2,14 @@ local YELL = {
 	"Selling all sorts of magic equipment. Come and have a look."
 }
  
- 
-local keywordHandler = KeywordHandler:new()
+ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
+local talkState = {}
 
-yell_delay = 20
-frequency = 25
- 
+function onCreatureAppear(cid)				npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid) 			npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)			npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink() 
 	if ((os.time() - yell_delay) >= frequency) then
 		yell_delay = os.time()
@@ -17,3 +17,8 @@ function onThink()
 	end
 	npcHandler:onThink() 
 end
+npcHandler:addModule(FocusModule:new())
+
+yell_delay = 20
+frequency = 25
+ 
