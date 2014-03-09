65a9aa15a3d54b73c2285c0d2e41420e0f511f29
Vinicius Oliveira <vinioliveirasilva@hotmail.com>
2014-03-09 12:32:40 -0300

    fix bug on warlock pass and added some items on items.xml

--- data/movements/scripts/in service of yalahar quest/warlocksPass.lua
+++ data/movements/scripts/in service of yalahar quest/warlocksPass.lua
@@ -1,21 +1,31 @@
 function onStepIn(cid, item, position, lastPosition)
 	if(item.actionid == 7813) then
-		if(getPlayerItemCount(cid, 8299) >= 1 and lastPosition.y == 31081) then
-			doRemoveItem(cid, 8299, 1)
-			doSendMagicEffect(position, CONST_ME_MAGIC_RED)
+		if(lastPosition.y == 31081) then
+			if(getPlayerItemCount(cid, 8299) >= 1) then
+				doPlayerRemoveItem(cid, 8299, 1)
+				doSendMagicEffect(position, CONST_ME_MAGIC_RED)
+			else
+				doTeleportThing(cid, lastPosition)
+				doCreatureSay(cid, "You may not enter without a sacrifice of a glimmering soil.", TALKTYPE_ORANGE_1)
+				doSendMagicEffect(position, CONST_ME_ENERGY)
+			end
 		else
-			doTeleportThing(cid, lastPosition)
-			doCreatureSay(cid, "You may not enter without a sacrifice of a glimmering soil.", TALKTYPE_ORANGE_1)
-			doSendMagicEffect(position, CONST_ME_ENERGY)
+			doTeleportThing(cid, {x = lastPosition.x, y = lastPosition.y - 1, z = lastPosition.z})	
+			doSendMagicEffect({x = lastPosition.x, y = lastPosition.y - 1, z = lastPosition.z}, CONST_ME_ENERGY)
 		end
 	elseif(item.actionid == 7814) then
-		if(getPlayerItemCount(cid, 8303) >= 1 and lastPosition.y == 31080) then
-			doRemoveItem(cid, 8303, 1)
-			doSendMagicEffect(position, CONST_ME_MAGIC_BLUE)
+		if (lastPosition.y == 31080) then
+			if(getPlayerItemCount(cid, 8303) >= 1) then
+				doPlayerRemoveItem(cid, 8303, 1)
+				doSendMagicEffect(position, CONST_ME_MAGIC_BLUE)
+			else
+				doTeleportThing(cid, lastPosition)
+				doCreatureSay(cid, "You may not enter without a sacrifice of a energy soil.", TALKTYPE_ORANGE_1)
+				doSendMagicEffect(position, CONST_ME_ENERGY)
+			end
 		else
-			doTeleportThing(cid, lastPosition)
-			doCreatureSay(cid, "You may not enter without a sacrifice of a energy soil.", TALKTYPE_ORANGE_1)
-			doSendMagicEffect(position, CONST_ME_ENERGY)
+			doTeleportThing(cid, {x = lastPosition.x, y = lastPosition.y + 1, z = lastPosition.z})	
+			doSendMagicEffect({x = lastPosition.x, y = lastPosition.y + 1, z = lastPosition.z}, CONST_ME_ENERGY)			
 		end
 	end
 	return true
