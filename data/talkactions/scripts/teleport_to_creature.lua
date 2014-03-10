359273fe96471354d93694fcf5f8079391b00f09
Vinicius Oliveira <vinioliveirasilva@hotmail.com>
2014-03-10 09:53:25 -0300

    fix anhihilator door, lever and reward chest (could be moved) and revised monsters
    Former-commit-id: 2221dcd8cc0fc0ef237e2d7333165baeb6feca59

--- data/talkactions/scripts/teleport_to_creature.lua
+++ data/talkactions/scripts/teleport_to_creature.lua
@@ -1,15 +1,21 @@
 function onSay(cid, words, param)
+
+	local target = Creature(param)
+	local tile = string.split(param, ",")
+	local pos = {x = 0, y = 0, z = 0}
+	
 	local player = Player(cid)
 	if not player:getGroup():getAccess() then
 		return true
 	end
-
-	local target = Creature(param)
-	if target == nil then
-		player:sendCancelMessage("Creature not found.")
-		return false
+	
+	if target ~= nil then
+		player:teleportTo(target:getPosition())
+	elseif(tile[1] ~= 0 and tile[2] ~= 0) then
+		pos = {x = tile[1], y = tile[2], z = tile[3]}
+		player:teleportTo(pos)
+	else
+		player:sendCancelMessage("Cannot be Possible.")
 	end
-
-	player:teleportTo(target:getPosition())
-	return false
+	return true
 end
