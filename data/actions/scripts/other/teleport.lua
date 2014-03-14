76b31143a4276f6f831cd5071aecc6a328261524
Royalot <zapf-benny@web.de>
2014-03-14 16:32:15 +0100

    Revert "Fixed draw well teleport"
    This reverts commit d7fdc9615249cba3b7d0b6107f54816e1640e833 [formerly a51fbe9f605b626d82f6fb26f712a4e5b36d375c].
    
    
    Former-commit-id: 62ae3c238e9c3cdc42ffc4cf525e563c68982e5d

--- data/actions/scripts/other/teleport.lua
+++ data/actions/scripts/other/teleport.lua
@@ -3,12 +3,9 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if isInArray(upFloorIds, item.itemid) == TRUE then
 		fromPosition.y = fromPosition.y + 1
 		fromPosition.z = fromPosition.z - 1
-		doTeleportThing(cid, fromPosition, FALSE)
 	else
-		if(itemEx.actionid  == 1000 and itemEx.itemid == 1369) then
-			fromPosition.z = fromPosition.z + 1
-			doTeleportThing(cid, fromPosition, FALSE)
-		end
+		fromPosition.z = fromPosition.z + 1
 	end
+	doTeleportThing(cid, fromPosition, FALSE)
 	return TRUE
 end
