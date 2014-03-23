d1f5c8c51ccf2ecaa587830a7b42d3bbc5c02fc1
Vinicius Oliveira <vinioliveirasilva@hotmail.com>
2014-03-23 02:23:53 -0300

    fix djinn lamp function

--- data/actions/scripts/green djinn quest/greenDjinnQuestLamp.lua
+++ data/actions/scripts/green djinn quest/greenDjinnQuestLamp.lua
@@ -1,6 +1,6 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if(item.uid == 2285) then
-		if(getPlayerStorageValue(cid, 81) == 10 and getPlayerItemCount(cid, 2344, 1) > 0) then
+		if(getPlayerStorageValue(cid, 81) == 10 and getPlayerItemCount(cid, 2344) > 0) then
 			doPlayerRemoveItem(cid, 2344, 1)
 			setPlayerStorageValue(cid, 81, 11)
 			doSendMagicEffect(toPosition, CONST_ME_MAGIC_BLUE)
