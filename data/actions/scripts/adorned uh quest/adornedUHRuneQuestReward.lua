14237c78bc66293a7dc70b94accb04066b9aab9e
WibbenZ <wibbenz@gmail.com>
2014-03-27 00:50:02 +0100

    Update adornedUHRuneQuestReward.lua
    Former-commit-id: 06479b250c0fcc02ee14a145756a8a90297a8129

--- data/actions/scripts/adorned uh quest/adornedUHRuneQuestReward.lua
+++ data/actions/scripts/adorned uh quest/adornedUHRuneQuestReward.lua
@@ -1,12 +1,13 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-        if(item.uid == 2240) then
-                if(Player(cid):getStorageValue(335) < 1) then
-                        Player(cid):setStorageValue(335, 1)
-                        Player(cid):addItem(12559, 1)
-                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "You've found a silver rune emblem.")
-                else
-                        Player(cid):sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
-                end
-        end
-        return true
+	if(item.uid == 2240) then
+		local p = Player(cid)
+		if(p:getStorageValue(335) < 1) then
+			p:setStorageValue(335, 1)
+			p:addItem(12559, 1)
+			p:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a silver rune emblem.")
+		else
+			p:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+		end
+	end
+return true
 end
