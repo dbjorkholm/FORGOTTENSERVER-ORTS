07185a2cf0fb37d5954653454c5609e312a8176a
WibbenZ <wibbenz@gmail.com>
2014-03-27 17:10:58 +0100

    Bugfix

--- data/globalevents/scripts/others/RashidStartUp.lua
+++ data/globalevents/scripts/others/RashidStartUp.lua
@@ -1,20 +1,20 @@
 local config = {
-    ["Monday"] = {pos = {x=32209, y=31158, z=7}},
-    ["Tuesday"] = {pos = {x=32290, y=32831, z=7}},
-    ["Wednesday"] = {pos = {x=32579, y=32754, z=7}},
-    ["Thursday"] = {pos = {x=33066, y=32880, z=6}},
-    ["Friday"] = {pos = {x=33239, y=32483, z=7}},
-    ["Saturday"] = {pos = {x=33171, y=31810, z=6}},
-    ["Sunday"] = {pos = {x=32326, y=31783, z=6}}
+	["Monday"] = {{x=32209, y=31158, z=7}},
+	["Tuesday"] = {{x=32290, y=32831, z=7}},
+	["Wednesday"] = {{x=32579, y=32754, z=7}},
+	["Thursday"] = {{x=33066, y=32880, z=6}},
+	["Friday"] = {{x=33239, y=32483, z=7}},
+	["Saturday"] = {{x=33171, y=31810, z=6}},
+	["Sunday"] = {{x=32326, y=31783, z=6}}
 }
 
-local day = false --Should it spawn as real tibia?
+local day = true --Should it spawn as real tibia?
 function onStartup()
 	if(day) then
-		doCreateNpc("Rashid", config[os.date("%A")].pos)
+		doCreateNpc("Rashid", config[os.date("%A")])
 	else
-		for x, i in pairs(config) do
-			doCreateNpc("Rashid", i.pos)
+		for _, i in pairs(config) do
+			doCreateNpc("Rashid", i)
 		end
 	end
 return true
