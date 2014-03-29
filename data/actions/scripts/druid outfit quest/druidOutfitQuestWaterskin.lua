cd11c31c333b889d9aa3ec4672667994679bf2d5
Royalot <zapf-benny@web.de>
2014-03-29 21:21:49 +0100

    fixed Druid Outfit Quest
    - set for default start of Outfit and Addon Quests an Storage id
    - add to Questlog
    - add script for a majestic warwolf for reward
    
    
    Former-commit-id: 733a1730b925f0c34fe638d4575db40ec9798abc

--- data/actions/scripts/druid outfit quest/druidOutfitQuestWaterskin.lua
+++ data/actions/scripts/druid outfit quest/druidOutfitQuestWaterskin.lua
@@ -1,8 +1,8 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if(item.itemid == 5938) then
-		if(itemEx.actionid == 100 and itemEx.itemid == 5663) then
+		if(itemEx.itemid == 5663) then
 			doSendMagicEffect(toPosition, CONST_ME_LOSEENERGY)
-			doTransformItem(item.uid, 5937)
+			doTransformItem(item.uid, 5939)
 		end
 	end
 	return true
