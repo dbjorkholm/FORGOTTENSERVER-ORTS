726d5ff1c547fd50e0c7c21011e48ed45bd34e0f
Vinicius Oliveira <vinioliveirasilva@hotmail.com>
2014-03-29 14:42:00 -0300

    fix rashid startup
    Former-commit-id: 67f288f434dd3b0d95862977b2825d2d61c23218

--- data/globalevents/scripts/others/RashidStartUp.lua
+++ data/globalevents/scripts/others/RashidStartUp.lua
@@ -1,11 +1,11 @@
 local config = {
-	["Monday"] = {{x=32209, y=31158, z=7}},
-	["Tuesday"] = {{x=32290, y=32831, z=7}},
-	["Wednesday"] = {{x=32579, y=32754, z=7}},
-	["Thursday"] = {{x=33066, y=32880, z=6}},
-	["Friday"] = {{x=33239, y=32483, z=7}},
-	["Saturday"] = {{x=33171, y=31810, z=6}},
-	["Sunday"] = {{x=32326, y=31783, z=6}}
+	["Monday"] = {x=32209, y=31158, z=7},
+	["Tuesday"] = {x=32290, y=32831, z=7},
+	["Wednesday"] = {x=32579, y=32754, z=7},
+	["Thursday"] = {x=33066, y=32880, z=6},
+	["Friday"] = {x=33239, y=32483, z=7},
+	["Saturday"] = {x=33171, y=31810, z=6},
+	["Sunday"] = {x=32326, y=31783, z=6}
 }
 
 local day = true --Should it spawn as real tibia?
