e862e79f3792a794d199ee6dfc6715dc24e068d9
Royalot <zapf-benny@web.de>
2014-03-29 16:46:45 +0100

    fixed local p
    fixed #268

--- data/actions/scripts/hunter outfit quest/MusicSheet.lua
+++ data/actions/scripts/hunter outfit quest/MusicSheet.lua
@@ -1,36 +1,37 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-        if(item.itemid == 6087) then
-            if(Player(cid):getStorageValue(12050) < 1) then
-                    Player(cid):setStorageValue(12050, 1)
-					Player(cid):removeItem(6087, 1)
-                    Player(cid):sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the first part of a hymn.")
-            else
-                    Player(cid):sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the first verse of the hymn.", TALKTYPE_ORANGE_1)
-            end
-		elseif(item.itemid == 6088) then
-            if(Player(cid):getStorageValue(12051) < 1) then
-                    Player(cid):setStorageValue(12051, 1)
-					Player(cid):removeItem(6088, 1)
-                    Player(cid):sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the second part of a hymn.")
-            else
-                    Player(cid):sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the second verse of the hymn.")
-            end
-		elseif(item.itemid == 6089) then
-            if(Player(cid):getStorageValue(12052) < 1) then
-                    Player(cid):setStorageValue(12052, 1)
-					Player(cid):removeItem(6089, 1)
-                    Player(cid):sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the third part of a hymn.")
-            else
-                    Player(cid):sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the third verse of the hymn.")
-            end
-		elseif(item.itemid == 6090) then
-            if(Player(cid):getStorageValue(12053) < 1) then
-                    Player(cid):setStorageValue(12053, 1)
-					Player(cid):removeItem(6090, 1)
-                    Player(cid):sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the fourth part of a hymn.")
-            else
-                    Player(cid):sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the fourth verse of the hymn.")
-        end
-    end
-    return true
+	local p = Player(cid)
+	if(item.itemid == 6087) then
+		if(p:getStorageValue(12050) < 1) then
+				p:setStorageValue(12050, 1)
+				p:removeItem(6087, 1)
+				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the first part of a hymn.")
+		else
+				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the first verse of the hymn.", TALKTYPE_ORANGE_1)
+		end
+	elseif(item.itemid == 6088) then
+		if(p:getStorageValue(12051) < 1) then
+				p:setStorageValue(12051, 1)
+				p:removeItem(6088, 1)
+				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the second part of a hymn.")
+		else
+				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the second verse of the hymn.")
+		end
+	elseif(item.itemid == 6089) then
+		if(p:getStorageValue(12052) < 1) then
+				p:setStorageValue(12052, 1)
+				p:removeItem(6089, 1)
+				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the third part of a hymn.")
+		else
+				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the third verse of the hymn.")
+		end
+	elseif(item.itemid == 6090) then
+		if(p:getStorageValue(12053) < 1) then
+				p:setStorageValue(12053, 1)
+				p:removeItem(6090, 1)
+				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the fourth part of a hymn.")
+		else
+				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the fourth verse of the hymn.")
+		end
+	end
+return true
 end
