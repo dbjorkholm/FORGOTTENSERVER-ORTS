7aca8866dab25329c0afd9980a802663858ff71a
Royalot <zapf-benny@web.de>
2014-03-31 22:25:52 +0200

    Fixed Questlog for In service of Yalahar
    - set Storageids for Missions
    - old Missions will not be deleted
    - some bugfixes
    - put some dialogue in metatag with local p
    
    
    Former-commit-id: d3558e0f950461ed400d3c5f88141d8610cdb59b

--- data/npc/scripts/Oliver.lua
+++ data/npc/scripts/Oliver.lua
@@ -26,6 +26,7 @@ function creatureSayCallback(cid, type, msg)
 		if(getPlayerStorageValue(cid, 30) == 8 or getPlayerStorageValue(cid, 30) == 12) then
 			npcHandler:say("Nobody knows the trouble I've seen .. <tells a quite detailed report>. ", cid)
 			setPlayerStorageValue(cid, 30, getPlayerStorageValue(cid, 30) + 1)
+			Player(cid):setStorageValue(12013, Player(cid):getStorageValue(12013) + 1) -- StorageValue for Questlog "Mission 02: Watching the Watchmen"
 			talkState[talkUser] = 0
 		end
 	elseif(msgcontains(msg, "pass")) then
