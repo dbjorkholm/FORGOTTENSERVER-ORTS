f23ddbc1c07cd7ae17377ac335d5e92c21f34d5a
Royalot <zapf-benny@web.de>
2014-04-01 19:29:31 +0200

    update pick
    cause fragile ice shouldn't have an actionid.
    thats will be lot of work in mapping.

--- data/actions/scripts/tools/pick.lua
+++ data/actions/scripts/tools/pick.lua
@@ -1,15 +1,15 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
+	local iEx = Item(itemEx.uid)
 	if (itemEx.uid <= 65535 or itemEx.actionid > 0) then
 		if (itemEx.itemid == 354 or itemEx.itemid == 355) then
-			local iEx = Item(itemEx.uid)
 			iEx:transform(392)
 			iEx:decay()
 			toPosition:sendMagicEffect(CONST_ME_POFF)
-		elseif itemEx.itemid == 7200 then
+		end
+	elseif itemEx.itemid == 7200 then
 			iEx:transform(7236)
         		iEx:decay()
        		 	toPosition:sendMagicEffect(CONST_ME_HITAREA)
-		end
 	end
 	return true
 end
