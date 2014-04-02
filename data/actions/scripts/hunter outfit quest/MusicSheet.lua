af4580a52e67ac284de9218f5b815504d015e9cb
Royalot <zapf-benny@web.de>
2014-04-02 23:10:12 +0200

    some script optimization
    Former-commit-id: 294e5b27a031d60116f2cb223aebb64387c67933

--- data/actions/scripts/hunter outfit quest/MusicSheet.lua
+++ data/actions/scripts/hunter outfit quest/MusicSheet.lua
@@ -1,36 +1,40 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local p = Player(cid)
-	if(item.itemid == 6087) then
-		if(p:getStorageValue(12050) < 1) then
-				p:setStorageValue(12050, 1)
-				p:removeItem(6087, 1)
-				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the first part of a hymn.")
+	local player = Player(cid)
+	if item.itemid == 6087 then
+		if player:getStorageValue(12050) < 1  then
+				player:setStorageValue(12050, 1)
+				player:removeItem(6087, 1)
+				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the first part of a hymn.")
+				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
 		else
-				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the first verse of the hymn.", TALKTYPE_ORANGE_1)
+				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the first verse of the hymn.", TALKTYPE_ORANGE_1)
 		end
-	elseif(item.itemid == 6088) then
-		if(p:getStorageValue(12051) < 1) then
-				p:setStorageValue(12051, 1)
-				p:removeItem(6088, 1)
-				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the second part of a hymn.")
+	elseif item.itemid == 6088 then
+		if player:getStorageValue(12051) < 1 then
+				player:setStorageValue(12051, 1)
+				player:removeItem(6088, 1)
+				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the second part of a hymn.")
+				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
 		else
-				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the second verse of the hymn.")
+				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the second verse of the hymn.")
 		end
-	elseif(item.itemid == 6089) then
-		if(p:getStorageValue(12052) < 1) then
-				p:setStorageValue(12052, 1)
-				p:removeItem(6089, 1)
-				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the third part of a hymn.")
+	elseif item.itemid == 6089 then
+		if player:getStorageValue(12052) < 1 then
+				player:setStorageValue(12052, 1)
+				player:removeItem(6089, 1)
+				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the third part of a hymn.")
+				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
 		else
-				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the third verse of the hymn.")
+				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the third verse of the hymn.")
 		end
-	elseif(item.itemid == 6090) then
-		if(p:getStorageValue(12053) < 1) then
-				p:setStorageValue(12053, 1)
-				p:removeItem(6090, 1)
-				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the fourth part of a hymn.")
+	elseif item.itemid == 6090  then
+		if player:getStorageValue(12053) < 1 then
+				player:setStorageValue(12053, 1)
+				player:removeItem(6090, 1)
+				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the fourth part of a hymn.")
+				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
 		else
-				p:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the fourth verse of the hymn.")
+				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the fourth verse of the hymn.")
 		end
 	end
 return true
--- data/actions/scripts/hunter outfit quest/allHymnPianoTeleport.lua
+++ data/actions/scripts/hunter outfit quest/allHymnPianoTeleport.lua
@@ -1,13 +1,14 @@
 local portpos = {x = 32402, y = 32794, z = 9}
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
+local player = Player(cid)
 	if(itemEx.actionid == 33216) then
-		if(Player(cid):getStorageValue(12050) == 1 and Player(cid):getStorageValue(12051) == 1 and Player(cid):getStorageValue(12052) == 1 and Player(cid):getStorageValue(12053) == 1) then
-			Player(cid):teleportTo(portpos, false)
+		if player:getStorageValue(12050) == 1 and player:getStorageValue(12051) == 1 and player:getStorageValue(12052) == 1 and player:getStorageValue(12053) == 1 then
+			player:teleportTo(portpos, false)
 			Position(portpos):sendMagicEffect(CONST_ME_TELEPORT)
 			Position(toPosition):sendMagicEffect(CONST_ME_SOUND_YELLOW)
 		else
-			Player(cid):sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have not learned all the verses of the hymn")
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have not learned all the verses of the hymn")
 			Position(toPosition):sendMagicEffect(CONST_ME_POFF)
 		end
 	end
