37aa39a87dccb2f17e3f78a70ce3f77a38b8ba64
Daniel Björkholm <danielbjorkholm@live.se>
2014-04-03 09:32:50 +0200

    Script cleanup
    Former-commit-id: 85461f6a64b8e86b0652a4547fe35a5ac43fdcd1

--- data/actions/scripts/hunter outfit quest/MusicSheet.lua
+++ data/actions/scripts/hunter outfit quest/MusicSheet.lua
@@ -1,41 +1,24 @@
+local config = {
+	[6087] = {12050, 6087, "first"},
+	[6088] = {12051, 6088, "second"},
+	[6089] = {12052, 6089, "third"},
+	[6090] = {12053, 6090, "fourth"}
+}
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
-	if item.itemid == 6087 then
-		if player:getStorageValue(12050) < 1  then
-				player:setStorageValue(12050, 1)
-				player:removeItem(6087, 1)
-				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the first part of a hymn.")
-				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
-		else
-				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the first verse of the hymn.", TALKTYPE_ORANGE_1)
-		end
-	elseif item.itemid == 6088 then
-		if player:getStorageValue(12051) < 1 then
-				player:setStorageValue(12051, 1)
-				player:removeItem(6088, 1)
-				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the second part of a hymn.")
-				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
-		else
-				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the second verse of the hymn.")
-		end
-	elseif item.itemid == 6089 then
-		if player:getStorageValue(12052) < 1 then
-				player:setStorageValue(12052, 1)
-				player:removeItem(6089, 1)
-				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the third part of a hymn.")
-				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
-		else
-				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the third verse of the hymn.")
-		end
-	elseif item.itemid == 6090  then
-		if player:getStorageValue(12053) < 1 then
-				player:setStorageValue(12053, 1)
-				player:removeItem(6090, 1)
-				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You have learned the fourth part of a hymn.")
-				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
-		else
-				player:sendTextMessage(MESSAGE_STATUS_WARNING, "You already know the fourth verse of the hymn.")
-		end
+	local targetItem = config[item.itemid]
+	if not targetItem then
+		return true
 	end
-return true
+	
+	if player:getStorageValue(targetItem[1]) < 1 then
+		player:setStorageValue(targetItem[1], 1)
+		player:removeItem(targetItem[2], 1)
+		player:sendTextMessage(MESSAGE_STATUS_WARNING, string.format("You have learned the %s part of a hymn.", targetItem[3]))
+		player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+	else
+		player:sendTextMessage(MESSAGE_STATUS_WARNING, string.format("You already know the %s verse of the hymn.", targetItem[3]))
+	end
+	return true
 end
