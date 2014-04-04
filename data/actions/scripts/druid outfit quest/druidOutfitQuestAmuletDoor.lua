9b040b8bd3634ccd9203f002f2d766e8bd1c2723
Vinicius Oliveira <vinioliveirasilva@hotmail.com>
2014-04-04 04:57:25 -0300

    modified storage on druid outfit quest

--- data/actions/scripts/druid outfit quest/druidOutfitQuestAmuletDoor.lua
+++ data/actions/scripts/druid outfit quest/druidOutfitQuestAmuletDoor.lua
@@ -1,5 +1,5 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.uid == 3153) then
+	if(item.uid == 8001) then
 		if(getPlayerStorageValue(cid, 1004) >= 7) then
 			if(item.itemid == 6261) then
 				doTeleportThing(cid, toPosition, true)
