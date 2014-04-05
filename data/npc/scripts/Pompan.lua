f380292382fa838212d888fa129a83147cdd2696
Ploff <a860908@drdrb.net>
2014-04-05 12:32:50 +0200

    Update Pompan.lua
    Improvements: Trading list had some missing items, bad item IDs and wrong sales price for bow and crossbow.
    
    Temporary fix: Every time you tried to trade with tomes delivered, items would keep adding (duplicates) to trading list. Until someone can solve this, I suggest leaving all items available from the start.
    Former-commit-id: 49978c15c88f42e93813773de4fdefe549a5c3fa

--- data/npc/scripts/Pompan.lua
+++ data/npc/scripts/Pompan.lua
@@ -9,42 +9,58 @@ function onCreatureSay(cid, type, msg)			npcHandler:onCreatureSay(cid, type, msg
 function onThink()					npcHandler:onThink()					end
 
 local itemsList = {
-	{name="arrow", id=2544, buy=3},
 	{name="backpack", id=1988, buy=20},
 	{name="bag", id=1987, buy=5},
 	{name="basket", id=1989, buy=6},
-	{name="bolt", id=2543, buy=4},
-	{name="bow", id=2456, buy=400, sell=100},
 	{name="bucket", id=2005, buy=4},
 	{name="candlestick", id=2047, buy=2},
-	{name="crossbow", id=2455, buy=500, sell=120},
+	{name="closed trap", id=2578, buy=280, sell=75},
 	{name="crowbar", id=2416, buy=260, sell=50},
-	{name="earth arrow", id=7850, buy=5},
 	{name="expedition backpack", id=11241, buy=100},
 	{name="expedition bag", id=11242, buy=50},
 	{name="fishing rod", id=2580, buy=150, sell=40},
+	{name="lamp", id=2044, buy=8},
+	{name="pick", id=2553, buy=50, sell=15},
+	{name="rope", id=2120, buy=50, sell=15},
+	{name="scythe", id=2550, buy=50, sell=10},
+	{name="shovel", id=2554, buy=50, sell=8},
+	{name="torch", id=2050, buy=2},
+	{name="watch", id=2036, buy=20, sell=6},
+	{name="worm", id=3976, buy=1},
+	{name="inkwell", id=2600, sell=8},
+	{name="mirror", id=2560, sell=10},
+	{name="sickle", id=2405, sell=3},
+	-- 1 tome
+	{name="arrow", id=2544, buy=3},
+	{name="bolt", id=2543, buy=4},
+	{name="bow", id=2456, buy=400, sell=100},
+	{name="crossbow", id=2455, buy=500, sell=120},
+	{name="crystalline arrow", id=18304, buy=20},
+	{name="dragon tapestry", id=11264, buy=80},
+	{name="drill bolt", id=18436, buy=12},
+	{name="earth arrow", id=7850, buy=5},
+	{name="envenomed arrow", id=18437, buy=12},
 	{name="flaming arrow", id=7840, buy=5},
 	{name="flash arrow", id=7838, buy=5},
-	{name="inkwell", id=2600, sell=8},
-	{name="lamp", id=2044, buy=8},
-	{name="mirror", id=2560, sell=10},
 	{name="onyx arrow", id=7365, buy=7},
-	{name="pick", id=2553, buy=50, sell=15},
 	{name="piercing bolt", id=7363, buy=5},
 	{name="power bolt", id=2547, buy=7},
-	{name="rope", id=2120, buy=50, sell=15},
+	{name="prismatic bolt", id=18435, buy=20},
 	{name="royal spear", id=7378, buy=15},
-	{name="scythe", id=2550, buy=50, sell=10},
 	{name="shiver arrow", id=7839, buy=5},
-	{name="shovel", id=2554, buy=50, sell=8},
-	{name="sickle", id=2405, sell=3},
 	{name="sniper arrow", id=7364, buy=5},
-	{name="spear", id=2389, buy=10, sell=3},
+	{name="spear", id=2389, buy=9, sell=3},
+	{name="tarsal arrow", id=15648, buy=6},
 	{name="throwing star", id=2399, buy=42},
-	{name="torch", id=2050, buy=2},
-	{name="closed trap", id=2578, buy=280, sell=75},
-	{name="watch", id=2036, buy=20, sell=6},
-	{name="worm", id=3976, buy=1}
+	{name="vortex bolt", id=15649, buy=6},
+	{name="corrupted flag", id=11326, sell=700},
+	{name="high guard flag", id=11332, sell=550},
+	{name="legionnaire flags", id=11334, sell=500},
+	{name="zaogun flag", id=11330, sell=600},
+	-- 2 tomes
+	{name="minotaur backpack", id=11244, buy=200},
+	-- 5 tomes
+	{name="dragon backpack", id=11243, buy=200}
 }
 
 local function setNewTradeTable(table)
@@ -69,18 +85,14 @@ function creatureSayCallback(cid, type, msg)
 		npcHandler:say("It was a pleasure to help you, "..getCreatureName(cid)..".", cid, TRUE)
 		npcHandler:releaseFocus(cid)
 	elseif msgcontains(msg, "trade") then
-		if player:getStorageValue(85300) >= 1 then
-			table.insert(itemsList, {name="dragon tapestry", id=11258, buy=80})
+		if player:getStorageValue(85300) >= 1 or 1 == 1 then
+			-- 1 tome
 		end
 		if player:getStorageValue(85300) >= 2 then
-			table.insert(itemsList, {name="minotaur backpack", id=11238, buy=200})
-			table.insert(itemsList, {name="corrupted flag", id=11320, sell=700})
-			table.insert(itemsList, {name="high guard flag", id=11326, sell=550})
-			table.insert(itemsList, {name="legionnaire flags", id=11328, sell=500})
-			table.insert(itemsList, {name="zaogun flag", id=11324, sell=600})
+			-- 2 tomes
 		end
 		if player:getStorageValue(85300) >= 5 then
-			table.insert(items, {name="dragon backpack", id=11237, buy=200})
+			-- 5 tomes
 		end
 		local player = Player(cid)
 		
