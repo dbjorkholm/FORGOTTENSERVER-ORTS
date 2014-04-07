bb451c049fa0d55798b28ef23b1cd61b6915c1ab
Daniel Björkholm <danielbjorkholm@live.se>
2014-04-07 12:36:20 +0200

    Update some scripts
    Former-commit-id: 200ba1e8cf8cd1a3ebfcacabd7440ab2ab6da5c7

---
+++ data/actions/scripts/quests/warzone_rewards.lua
@@ -0,0 +1,28 @@
+local config = {
+	[9172] = {9172, 18394, {{18402, 1}, {18414, 12}, {18396, 1}, {18500, 1}, {2160, 5}, {18423, 2}}, 20 * 60 * 60},
+	[9173] = {9173, 18393, {{18402, 1}, {18415, 7}, {18396, 1}, {18504, 1}, {2160, 3}, {18423, 2}}, 20 * 60 * 60},
+	[9174] = {9174, 18394, {{18402, 1}, {18413, 10}, {18396, 1}, {18508, 1}, {2160, 4}, {18423, 2}}, 20 * 60 * 60}
+}
+
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	local player = Player(cid)
+	local targetItem = config[item.uid]
+	if not targetItem then
+		return true
+	end
+	
+	if player:getStorageValue(targetItem[1]) > os.time() then
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "It is empty.")
+		return true
+	end
+	
+	local backpack = player:addItem(targetItem[2], 1)
+	local container = Container(backpack:getUniqueId())
+	for i = 1, #targetItem[3] do
+		container:addItem(targetItem[3][i][1], targetItem[3][i][2])
+	end
+	player:setStorageValue(targetItem[1], os.time() + targetItem[4])
+	local itemType = ItemType(backpack:getId())
+	player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("You have found %s %s.", itemType:getArticle(), itemType:getName()))
+	return true
+end
