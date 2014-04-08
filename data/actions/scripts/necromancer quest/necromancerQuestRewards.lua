15d39036d15f7df827bf06c803a31fbb92c073c7
WibbenZ <wibbenz@gmail.com>
2014-04-08 18:17:44 +0200

    Update necromancerQuestRewards.lua
    Former-commit-id: ffbb6e6113911936e1f2ed44ba254acf2e1131d8

--- data/actions/scripts/necromancer quest/necromancerQuestRewards.lua
+++ data/actions/scripts/necromancer quest/necromancerQuestRewards.lua
@@ -1,15 +1,18 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.uid == 2226) then
-		if(getPlayerStorageValue(cid, 321) < 1) then
-			setPlayerStorageValue(cid, 321, 1)
-			bag = doPlayerAddItem(cid, 1987, 1)
-			doAddContainerItem(bag.uid, 2536, 1)
-			doAddContainerItem(bag.uid, 2436, 1)
-			doAddContainerItem(bag.uid, 2656, 1)
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You've found a bag.")
+	if(item.itemid == 1410 and item.actionid == 2000 and item.uid == 2226) then
+		local p = Player(cid)
+		if(p:getStorageValue(321) < 1) then
+			p:setStorageValue(321, 1)
+			local id = {2536, 2436, 2656}
+			local bag = p:addItem(1987, 1)
+			local c = Container(bag:getUniqueId())
+			for i = 1, #id do
+				c:addItem(id[i], 1)
+			end
+			p:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a bag.")
 		else
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "The chest is empty.")
+			p:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
 		end
 	end
-	return true
+return true
 end
