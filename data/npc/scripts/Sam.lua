ccbe758445682846d6f68f4a91347f8ef33dc1d3
WibbenZ <wibbenz@gmail.com>
2014-04-11 21:13:03 +0200

    Update Sam.lua
    a local value can not be used inside npcHandler:say..., needs to be cid, otherwise the npc dosen't speak.
    
    Former-commit-id: c8365cd2fd93f6579286e15322ced1be0316f5fe

--- data/npc/scripts/Sam.lua
+++ data/npc/scripts/Sam.lua
@@ -25,7 +25,7 @@ function creatureSayCallback(cid, type, msg)
 	local player = Player(cid)
 	if msgcontains(msg, "adorn") or msgcontains(msg, "helmet") or msgcontains(msg, "outfit") or msgcontains(msg, "addon") then
 		if player:getStorageValue( 12060) == 5 then
-			npcHandler:say("Oh, Gregor sent you? I see. It will be my pleasure to adorn your helmet. Your helmet is finished, I hope you like it.", player)
+			npcHandler:say("Oh, Gregor sent you? I see. It will be my pleasure to adorn your helmet. Your helmet is finished, I hope you like it.", cid)
 			player:setStorageValue(12060, 6)
 			player:addOutfitAddon(player:getSex() == 0 and 139 or 131, 2)
 			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
@@ -34,24 +34,24 @@ function creatureSayCallback(cid, type, msg)
  
 	if msgcontains(msg, "old backpack") then
 		if player:getStorageValue( 330) < 1 then
-			npcHandler:say("What? Are you telling me you found my old adventurer's backpack that I lost years ago??", player)
+			npcHandler:say("What? Are you telling me you found my old adventurer's backpack that I lost years ago??", cid)
 			talkState[talkUser] = 1
 		end
 	elseif msgcontains(msg, "yes") then
 		if talkState[talkUser] == 1 then
 			if getPlayerItemCount(cid, 3960) >= 1 then
 				npcHandler:say({"Thank you very much! This brings back good old memories! Please, as a reward, travel to Kazordoon and ask my old friend Kroox to provide you a special dwarven armor. ...",
-								"I will mail him about you immediately. Just tell him, his old buddy Sam is sending you."}, player, 0, 1, 3500)
+								"I will mail him about you immediately. Just tell him, his old buddy Sam is sending you."}, cid, 0, 1, 3500)
 				player:setStorageValue( 330, 1)
 				player:removeItem( 3960, 1)
 			else
-				npcHandler:say("You don't have it...", player)
+				npcHandler:say("You don't have it...", cid)
 			end
 			talkState[talkUser] = 0
 		end
 	elseif msgcontains(msg, "no") then
 		if talkState[talkUser] > 0 then
-			npcHandler:say("Then no.", player)
+			npcHandler:say("Then no.", cid)
 			talkState[talkUser] = 0
 		end
 	end
