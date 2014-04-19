a26d47e325d61f38872c40338569c65c5bd1cff9
Royalot <zapf-benny@web.de>
2014-04-19 10:41:44 +0200

    fixed the entrance to pharao thalas
    - clean a script
    - add the Cobra npc who port you to thalas
    - fixed the coalbasin teleport destination

--- data/actions/scripts/the ancient tombs quest/TombReward.lua
+++ data/actions/scripts/the ancient tombs quest/TombReward.lua
@@ -1,68 +1,27 @@
+local config = {
+	[12101] = {12101, 2341, "Helmet Adornment"}, -- Peninsula 	Omruc 	 Crystal Arrow 	 Helmet Adornment
+	[12102] = {12102, 2336, "Gem Holder"},-- Stone 	Thalas 	 Cobrafang Dagger 	 Gem Holder
+	[12103] = {12103, 2339, "Damaged Helmet"},-- Mountain 	Dipthrah 	 Ornamented Ankh 	 Damaged Helmet
+	[12104] = {12104, 2335, "Helmet Ornament"},-- Shadow 	Mahrdis 	 Burning Heart 	 Helmet Ornament
+	[12105] = {12105, 2338, "Left Horn"},-- Ancient Ruins 	Vashresamun 	 Blue Note 	 Left Horn
+	[12106] = {12106, 2337, "Right Horn"},-- Tarpit 	Morguthis 	 Sword Hilt 	 Right Horn
+	[12107] = {12107, 2340, "Helmet Piece"}-- Oasis 	Rahemos 	 Ancient Rune 	 Helmet Piece
+}
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
-		if itemEx.uid == 12101 then  -- Peninsula 	Omruc 	 Crystal Arrow 	 Helmet Adornment
-			if player:getStorageValue(12101) < 1 then
-				player:setStorageValue(12101, 1) 
-				player:setStorageValue(12100, 1) -- default start of The Ancient Tombs Quest
-				player:addItem(2341, 1)
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a Helmet Adornment.")
-			else
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
-			end
-		elseif itemEx.uid == 12102 then  -- Stone 	Thalas 	 Cobrafang Dagger 	 Gem Holder
-			if player:getStorageValue(12102) < 1 then
-				player:setStorageValue(12102, 1) 
-				player:setStorageValue(12100, 1) -- default start of The Ancient Tombs Quest
-				player:addItem(2336, 1)
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a Gem Holder.")
-			else
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
-			end
-		elseif itemEx.uid == 12103 then  -- Mountain 	Dipthrah 	 Ornamented Ankh 	 Damaged Helmet
-			if player:getStorageValue(12103) < 1 then
-				player:setStorageValue(12103, 1) 
-				player:setStorageValue(12100, 1) -- default start of The Ancient Tombs Quest
-				player:addItem(2339, 1)
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a Damaged Helmet.")
-			else
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
-			end
-		elseif itemEx.uid == 12104 then  -- Shadow 	Mahrdis 	 Burning Heart 	 Helmet Ornament
-			if player:getStorageValue(12104) < 1 then
-				player:setStorageValue(12104, 1) 
-				player:setStorageValue(12100, 1) -- default start of The Ancient Tombs Quest
-				player:addItem(2335, 1)
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a Helmet Ornament.")
-			else
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
-			end
-		elseif itemEx.uid == 12105 then  -- Ancient Ruins 	Vashresamun 	 Blue Note 	 Left Horn
-			if player:getStorageValue(12105) < 1 then
-				player:setStorageValue(12105, 1) 
-				player:setStorageValue(12100, 1) -- default start of The Ancient Tombs Quest
-				player:addItem(2338, 1)
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a Left Horn.")
-			else
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
-			end
-		elseif itemEx.uid == 12106 then  -- Tarpit 	Morguthis 	 Sword Hilt 	 Right Horn
-			if player:getStorageValue(12106) < 1 then
-				player:setStorageValue(12106, 1) 
-				player:setStorageValue(12100, 1) -- default start of The Ancient Tombs Quest
-				player:addItem(2337, 1)
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a Right Horn.")
-			else
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
-			end
-		elseif itemEx.uid == 12107 then  -- Oasis 	Rahemos 	 Ancient Rune 	 Helmet Piece
-			if player:getStorageValue(12107) < 1 then
-				player:setStorageValue(12107, 1) 
-				player:setStorageValue(12100, 1) -- default start of The Ancient Tombs Quest
-				player:addItem(2340, 1)
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a Helmet Piece.")
-			else
-				player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
-			end
+	local targetItem = config[itemEx.uid]
+	if not targetItem then
+		return true
+	end
+		
+	if player:getStorageValue(targetItem[1]) < 1 then
+		player:setStorageValue(targetItem[1], 1) 
+		player:setStorageValue(12100, 1) -- default start of The Ancient Tombs Quest
+		player:addItem(targetItem[2], 1)
+		player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("You've found a %s.", targetItem[3]))
+	else
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
 	end
 	return true
 end
