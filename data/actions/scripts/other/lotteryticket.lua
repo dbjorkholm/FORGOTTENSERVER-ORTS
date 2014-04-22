f7c47511ff122dcb4bbeba7d1413dfc309a7b8a5
Royalot <zapf-benny@web.de>
2014-04-22 21:48:36 +0200

    added LotteryTicket with Outfitquest
    Former-commit-id: cd0f630dd618cb554201e32643ce35122b20761b

---
+++ data/actions/scripts/other/lotteryticket.lua
@@ -0,0 +1,14 @@
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	local player = Player(cid)
+	local item = Item(item.uid)
+	if math.random(1, 50) == 1 then
+		player:getPosition():sendMagicEffect(CONST_ME_GIFT_WRAPS)
+		item:transform(5958)
+		player:say("You get the Prize!", TALKTYPE_ORANGE_1)
+	else
+		player:getPosition():sendMagicEffect(CONST_ME_POFF)
+		item:remove(1)
+		player:say("You get nothing!", TALKTYPE_ORANGE_1)
+	end
+	return true
+end
