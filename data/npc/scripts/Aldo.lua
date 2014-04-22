c1f39aef994aec2d9ac56dd818d74c1bf5a3377e
Jonas21 <reyes_27_9@hotmail.com>
2014-04-22 19:12:11 +0200

    Fix potential bug
    Npc Aldo give you free soft boots because in one "if" getPlayerItemCount(cid,6132, 1) is fail. You say hi, soft boots, yes, and Aldo give you one soft boots free. Now is fixed.

--- data/npc/scripts/Aldo.lua
+++ data/npc/scripts/Aldo.lua
@@ -19,7 +19,7 @@ function creatureSayCallback(cid, type, msg)
 		selfSay('Do you want to repair your worn soft boots for 10000 gold coins?', cid)
 		talkState[talkUser] = 1
 	elseif(msgcontains(msg, 'yes') and talkState[talkUser] == 1) then
-		if(getPlayerItemCount(cid, 10021, 1)) then
+		if(getPlayerItemCount(cid, 10021) == 1) then
 			if(doPlayerRemoveMoney(cid, 10000)) then
 				doPlayerRemoveItem(cid, 10021, 1)
 				doPlayerAddItem(cid, 6132, 1)
@@ -27,7 +27,7 @@ function creatureSayCallback(cid, type, msg)
 			else
 				selfSay('Sorry, you don\'t have enough gold.', cid)
 			end
-		elseif(getPlayerItemCount(cid, 10021, 1)) then
+		elseif(getPlayerItemCount(cid, 10021) == 1) then
 			if(doPlayerRemoveMoney(cid, 10000)) then
 				doPlayerRemoveItem(cid, 10021, 1)
 				doPlayerAddItem(cid, 6132, 1)
@@ -35,7 +35,7 @@ function creatureSayCallback(cid, type, msg)
 			else
 				selfSay('Sorry, you don\'t have enough gold.', cid)
 			end
-		elseif(getPlayerItemCount(cid,6132, 1)) then
+		elseif(getPlayerItemCount(cid,6132) == 1) then
 			if(doPlayerRemoveMoney(cid, 10000)) then
 				doPlayerRemoveItem(cid,6132, 1)
 				doPlayerAddItem(cid, 6132, 1)
