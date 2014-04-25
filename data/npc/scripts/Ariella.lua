59e0d41ded60f425f73e77fdcd33f1acf76d113c
WibbenZ <wibbenz@gmail.com>
2014-04-26 01:11:01 +0200

    pirate outfit second
    @346 not tested.

--- data/npc/scripts/Ariella.lua
+++ data/npc/scripts/Ariella.lua
@@ -1,19 +1,42 @@
-local YELL = {
-	"Have a drink in Meriana's only tavern!"
-}
- 
- 
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-yell_delay = 20
-frequency = 25
- 
-function onThink() 
-	if ((os.time() - yell_delay) >= frequency) then
-		yell_delay = os.time()
-		doCreatureSay(getNpcCid(), YELL[math.random(#YELL)], 1)
+function onCreatureAppear(cid)            npcHandler:onCreatureAppear(cid)            end
+function onCreatureDisappear(cid)        npcHandler:onCreatureDisappear(cid)            end
+function onCreatureSay(cid, type, msg)    npcHandler:onCreatureSay(cid, type, msg)    end
+function onThink()                        npcHandler:onThink()    end
+
+function PirateSecond(cid, message, keywords, parameters, node)
+    if(not npcHandler:isFocused(cid)) then
+        return false
+    end
+	
+	local p = Player(cid) 
+	if(p:getStorageValue(22034) ~= -1) then
+		if(p:getItemCount(6101) >= 1 and p:getItemCount(6102) >= 1 and p:getItemCount(6100) >= 1 and p:getItemCount(6099) >= 1) then
+			if(p:removeItem(6101, 1) and p:removeItem(6102, 1) and p:removeItem(6100, 1) and p:removeItem(6099, 1)) then
+				npcHandler:say("Ah, right! The pirate hat! Here you go.", cid)             
+				p:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+				p:setStorageValue(22034, 1)
+				local outfit = p:getSex() == 0 and 155 or 151
+				p:addOutfitAddon(addon, 2)
+				npcHandler:say("Ah, right! The pirate hat! Here you go.", cid)
+			end
+		else
+			npcHandler:say("You do not have all the required items.", cid)
+		end
+	else
+		npcHandler:say("It seems you already have this addon, don\'t you try to mock me son!", cid)
 	end
-	npcHandler:onThink() 
 end
+
+node1 = keywordHandler:addKeyword({'hat'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'To get pirate hat you need give me brutus bloodbeard\'s hat, lethal lissy\'s shirt, ron the ripper\'s sabre and deadeye devious\' eye patch. Do you have them with you?'})
+node1:addChildKeyword({'yes'}, PirateSecond, {})
+node1:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Alright then. Come back when you got all neccessary items.', reset = true})
+
+node2 = keywordHandler:addKeyword({'addon'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'To get pirate hat you need give me brutus bloodbeard\'s hat, lethal lissy\'s shirt, ron the ripper\'s sabre and deadeye devious\' eye patch. Do you have them with you?'})
+node2:addChildKeyword({'yes'}, PirateSecond, {})
+node2:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Alright then. Come back when you got all neccessary items.', reset = true})
+
+npcHandler:addModule(FocusModule:new())
