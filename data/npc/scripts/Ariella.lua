8a8909411b3bffdf8dbf62f4a22496af0b70a982
ninjalulz <danielbjorkholm@live.se>
2014-04-26 23:57:31 +0200

    Fix mistake

--- data/npc/scripts/Ariella.lua
+++ data/npc/scripts/Ariella.lua
@@ -2,11 +2,12 @@ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
+local config = {delay = 20, frequency = 25, message = "Have a drink in Meriana's only tavern!"} 
+
 function onCreatureAppear(cid)            npcHandler:onCreatureAppear(cid)            end
 function onCreatureDisappear(cid)        npcHandler:onCreatureDisappear(cid)            end
 function onCreatureSay(cid, type, msg)    npcHandler:onCreatureSay(cid, type, msg)    end
-function onThink() 
-    local config = {delay = 20, frequency = 25, message = "Have a drink in Meriana's only tavern!"} 
+function onThink()
     if (os.time() - config["delay"]) >= config["frequency"] then
         config["delay"] = os.time()
         Npc():say(config["message"], TALKTYPE_SAY)
