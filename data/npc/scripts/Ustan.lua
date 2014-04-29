d0379fce61501515ec84c045f8e7099064048194
Royalot <zapf-benny@web.de>
2014-04-29 19:44:37 +0200

    add the ape city quest till mission 2
    mapchanges:
    added hairycles
    {x = 32781, y = 32910, z = 8} AID = 12121
    {x = 32782, y = 32910, z = 8} AID = 12121
    
    Former-commit-id: 420ed7b027b6932ad116237d4a8067ce61a43123

--- data/npc/scripts/Ustan.lua
+++ data/npc/scripts/Ustan.lua
@@ -1,47 +1,44 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
-local talkState = {}
- 
-function onCreatureAppear(cid)
-	npcHandler:onCreatureAppear(cid)			
-end
-function onCreatureDisappear(cid)
-	npcHandler:onCreatureDisappear(cid)			
-end
-function onCreatureSay(cid, type, msg)
-	npcHandler:onCreatureSay(cid, type, msg)		
-end
-function onThink()
-	npcHandler:onThink()					
-end
+
+function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
+function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
+function onThink() npcHandler:onThink() end
 
 function creatureSayCallback(cid, type, msg)
-	if(not npcHandler:isFocused(cid)) then
+	local player = Player(cid)
+	if not npcHandler:isFocused(cid) then
 		return false
-	end
-	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
-
-	if(msgcontains(msg, "addon")) then
-		if(getPlayerStorageValue(cid, 1005) < 1) then
+	elseif msgcontains(msg, "cough syrup") then
+		if player:getStorageValue(12120) == 4 then
+			npcHandler:say("I had some cough syrup a while ago. It was stolen in an ape raid. I fear if you want more cough syrup you will have to buy it in the druids guild in carlin.", cid)
+			player:setStorageValue(12120, 5)
+			player:setStorageValue(12122, 3) -- The Ape City Questlog - Mission 2: The Cure
+			npcHandler.topic[cid] = 0
+		end
+	elseif msgcontains(msg, "addon") then
+		if player:getStorageValue(1005) < 1 then
 			npcHandler:say("Would you like to wear bear paws like I do? No problem, just bring me 50 bear paws and 50 wolf paws and I'll fit them on.", cid)
-			setPlayerStorageValue(cid, 1005, 1)
-			talkState[talkUser] = 0
+			player:setStorageValue(1005, 1)
+			npcHandler.topic[cid] = 0
 		end
-	elseif(msgcontains(msg, "paws")) then
-		if(getPlayerStorageValue(cid, 1005) == 1) then
+	elseif msgcontains(msg, "paws") then
+		if player:getStorageValue(1005) == 1 then
 			npcHandler:say("Have you brought 50 bear paws and 50 wolf paws?", cid)
-			talkState[talkUser] = 1
+			npcHandler.topic[cid] = 1
 		end
-	elseif(msgcontains(msg, "yes")) then
-		if(talkState[talkUser] == 1) then
-			if(getPlayerItemCount(cid, 5896) >= 50 and getPlayerItemCount(cid, 5897) >= 50) then
+	elseif msgcontains(msg, "yes") then
+		if npcHandler.topic[cid] == 1 then
+			if player:getItemCount(5896) >= 50 and player:getItemCount(5897) >= 50 then
 				npcHandler:say("Excellent! Like promised, here are your bear paws. ", cid)
-				doPlayerRemoveItem(cid, 5896, 50)
-				doPlayerRemoveItem(cid, 5897, 50)
-				setPlayerStorageValue(cid, 1005, 2)
-				doPlayerAddOutfit(cid, getPlayerSex(cid) == 0 and 148 or 144, 1)
-				talkState[talkUser] = 0	
+				player:removeItem(5896, 50)
+				player:removeItem(5897, 50)
+				player:setStorageValue(1005, 2)
+				player:addOutfitAddon(148, 1)
+				player:addOutfitAddon(144, 1)
+				npcHandler.topic[cid] = 0	
 			end
 		end
 	end
