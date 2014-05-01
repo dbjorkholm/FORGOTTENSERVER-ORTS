a2a0e86494b05d719649d8cc15b23a9c41b9dbba
PrinterLUA <mastero_90@hotmail.com>
2014-05-01 10:45:14 +0200

    Fixed notrash.lua
    https://github.com/PrinterLUA/FORGOTTENSERVER-ORTS/issues/389
    
    Former-commit-id: 561ded60d81eb05c830164f82b049530228c1930

--- data/movements/scripts/others/notrash.lua
+++ data/movements/scripts/others/notrash.lua
@@ -1,7 +1,4 @@
 function onAddItem(moveitem, tileitem, position)
-	if (tileitem.actionid > 0 or tileitem.uniqueid > 0) then
-		doRemoveItem(moveitem.uid)
-		doSendMagicEffect(position, CONST_ME_POFF)
-	end
-	return true
+	Item(moveitem.uid):remove()
+	position:sendMagicEffect(CONST_ME_POFF)
 end
