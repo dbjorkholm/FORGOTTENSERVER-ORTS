6c37d08bc829e6e6209248dc61f467eb2921fac3
Royalot <zapf-benny@web.de>
2014-05-02 23:12:46 +0200

    fixed walls inquistion
    fixed #400
    
    
    Former-commit-id: b65a11f3743c7d4d89ba120c9710a5b1296a0229

--- data/movements/scripts/the ape city/Mission4ParchmentDecyphering.lua
+++ data/movements/scripts/the ape city/Mission4ParchmentDecyphering.lua
@@ -1,6 +1,6 @@
 function onStepIn(cid, item, position, lastPosition)
-local player = Player(cid)
 	if item.actionid == 12124 then
+	local player = Player(cid)
 		if player:getStorageValue(12124) == 1 then
 			player:setStorageValue(12124, 2) -- The Ape City Questlog - Mission 4: Parchment Decyphering
 			player:say("!-! -O- I_I (/( --I Morgathla", TALKTYPE_ORANGE_1)
