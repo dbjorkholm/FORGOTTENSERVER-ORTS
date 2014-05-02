b65a11f3743c7d4d89ba120c9710a5b1296a0229
Royalot <zapf-benny@web.de>
2014-05-02 23:12:46 +0200

    fixed walls inquistion
    fixed #400

--- data/actions/scripts/inquisition quest/inquisitionQuestBrotherLever2.lua
+++ data/actions/scripts/inquisition quest/inquisitionQuestBrotherLever2.lua
@@ -1,76 +1,44 @@
-function onUse(cid, item, frompos, item2, topos)
-	wall1 = {x=33223, y=31724, z=11, stackpos=1}
-	wall2 = {x=33223, y=31725, z=11, stackpos=1}
-	wall3 = {x=33223, y=31726, z=11, stackpos=1}
-	wall4 = {x=33223, y=31727, z=11, stackpos=1}
-	wall5 = {x=33223, y=31728, z=11, stackpos=1}
-	wall6 = {x=33223, y=31729, z=11, stackpos=1}
-	wall7 = {x=33223, y=31730, z=11, stackpos=1}
-	wall8 = {x=33223, y=31731, z=11, stackpos=1}
-	wall9 = {x=33223, y=31732, z=11, stackpos=1}
+local config = {
+	WallPositions = {
+		[1] = Position({x=33223, y=31724, z=11}),
+		[2] = Position({x=33223, y=31725, z=11}),
+		[3] = Position({x=33223, y=31726, z=11}),
+		[4] = Position({x=33223, y=31727, z=11}),
+		[5] = Position({x=33223, y=31728, z=11}),
+		[6] = Position({x=33223, y=31729, z=11}),
+		[7] = Position({x=33223, y=31730, z=11}),
+		[8] = Position({x=33223, y=31731, z=11}),
+		[9] = Position({x=33223, y=31732, z=11})
+	},
+	walldown = 1526,
+	wallup = 1049
+}
 
-	getwall1 = getThingfromPos(wall1)
-	getwall2 = getThingfromPos(wall2)
-	getwall3 = getThingfromPos(wall3)
-	getwall4 = getThingfromPos(wall4)
-	getwall5 = getThingfromPos(wall5)
-	getwall6 = getThingfromPos(wall6)
-	getwall7 = getThingfromPos(wall7)
-	getwall8 = getThingfromPos(wall8)
-	getwall9 = getThingfromPos(wall9)
-	
-
-	if item.itemid == 1945 and getwall1.itemid == 1526 then
-		doRemoveItem(getwall1.uid,1)
-		doRemoveItem(getwall2.uid,1)
-		doRemoveItem(getwall3.uid,1)
-		doRemoveItem(getwall4.uid,1)
-		doRemoveItem(getwall5.uid,1)
-		doRemoveItem(getwall6.uid,1)
-		doRemoveItem(getwall7.uid,1)
-		doRemoveItem(getwall8.uid,1)
-		doRemoveItem(getwall9.uid,1)
-		
-		doCreateItem(1049,1,wall1)
-		doCreateItem(1049,1,wall2)
-		doCreateItem(1049,1,wall3)
-		doCreateItem(1049,1,wall4)
-		doCreateItem(1049,1,wall5)
-		doCreateItem(1049,1,wall6)
-		doCreateItem(1049,1,wall7)
-		doCreateItem(1049,1,wall8)
-		doCreateItem(1049,1,wall9)
-
-		doTransformItem(item.uid,item.itemid+1)
-	elseif item.itemid == 1946 then
-	
-		doRemoveItem(getwall1.uid,1)
-		doRemoveItem(getwall2.uid,1)
-		doRemoveItem(getwall3.uid,1)
-		doRemoveItem(getwall4.uid,1)
-		doRemoveItem(getwall5.uid,1)
-		doRemoveItem(getwall6.uid,1)
-		doRemoveItem(getwall7.uid,1)
-		doRemoveItem(getwall8.uid,1)
-		doRemoveItem(getwall9.uid,1)
-
-		doTransformItem(item.uid,item.itemid-1)	
-		
-		doCreateItem(1526,1,wall1)
-		doCreateItem(1526,1,wall2)
-		doCreateItem(1526,1,wall3)
-		doCreateItem(1526,1,wall4)
-		doCreateItem(1526,1,wall5)
-		doCreateItem(1526,1,wall6)
-		doCreateItem(1526,1,wall7)
-		doCreateItem(1526,1,wall8)
-		doCreateItem(1526,1,wall9)
-
-		
-		
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	if itemEx.itemid == 1945 then
+		for i = 1, #config["WallPositions"] do
+			local tile = Tile(config["WallPositions"][i])
+			if tile then
+				local thing = tile:getItemById(config["walldown"])
+				if thing and thing:isItem() then
+					Item(item.uid):transform(1946)
+					thing:transform(config["wallup"])
+				end
+			end
+		end	
+	elseif itemEx.itemid == 1946 then
+		for i = 1, #config["WallPositions"] do
+			local tile = Tile(config["WallPositions"][i])
+			if tile then
+				local thing = tile:getItemById(config["wallup"])
+				if thing and thing:isItem() then
+					Item(item.uid):transform(1945)
+					thing:transform(config["walldown"])
+				end
+			end
+		end
 	else
 		doPlayerSendCancel(cid,"Sorry, not possible.")
 	end
-
-	return 1
+	return true
 end
--- data/actions/scripts/inquisition quest/inquisitionQuestBrotherLever3.lua
+++ data/actions/scripts/inquisition quest/inquisitionQuestBrotherLever3.lua
@@ -1,96 +1,48 @@
-function onUse(cid, item, frompos, item2, topos)
-	wall1 = {x=33226, y=31735, z=11, stackpos=1}
-	wall2 = {x=33227, y=31735, z=11, stackpos=1}
-	wall3 = {x=33228, y=31735, z=11, stackpos=1}
-	wall4 = {x=33229, y=31735, z=11, stackpos=1}
-	wall5 = {x=33230, y=31735, z=11, stackpos=1}
-	wall6 = {x=33231, y=31735, z=11, stackpos=1}
-	wall7 = {x=33232, y=31735, z=11, stackpos=1}
-	wall8 = {x=33233, y=31735, z=11, stackpos=1}
-	wall9 = {x=33234, y=31735, z=11, stackpos=1}
-	wall10 = {x=33235, y=31735, z=11, stackpos=1}
-	wall11 = {x=33236, y=31735, z=11, stackpos=1}
-	wall12 = {x=33237, y=31735, z=11, stackpos=1}
-	wall13 = {x=33238, y=31735, z=11, stackpos=1}
-	getwall1 = getThingfromPos(wall1)
-	getwall2 = getThingfromPos(wall2)
-	getwall3 = getThingfromPos(wall3)
-	getwall4 = getThingfromPos(wall4)
-	getwall5 = getThingfromPos(wall5)
-	getwall6 = getThingfromPos(wall6)
-	getwall7 = getThingfromPos(wall7)
-	getwall8 = getThingfromPos(wall8)
-	getwall9 = getThingfromPos(wall9)
-	getwall10 = getThingfromPos(wall10)
-	getwall11 = getThingfromPos(wall11)
-	getwall12 = getThingfromPos(wall12)
-	getwall13 = getThingfromPos(wall13)
-	
+local config = {
+	WallPositions = {
+		[1] = Position({x=33226, y=31735, z=11}),
+		[2] = Position({x=33227, y=31735, z=11}),
+		[3] = Position({x=33228, y=31735, z=11}),
+		[4] = Position({x=33229, y=31735, z=11}),
+		[5] = Position({x=33230, y=31735, z=11}),
+		[6] = Position({x=33231, y=31735, z=11}),
+		[7] = Position({x=33232, y=31735, z=11}),
+		[8] = Position({x=33233, y=31735, z=11}),
+		[9] = Position({x=33234, y=31735, z=11}),
+		[10] = Position({x=33235, y=31735, z=11}),
+		[11] = Position({x=33236, y=31735, z=11}),
+		[12] = Position({x=33237, y=31735, z=11}),
+		[13] = Position({x=33238, y=31735, z=11})
+	},
+	walldown = 1524,
+	wallup = 1050
+}
 
-	if item.itemid == 1945 and getwall1.itemid == 1524 then
-		doRemoveItem(getwall1.uid,1)
-		doRemoveItem(getwall2.uid,1)
-		doRemoveItem(getwall3.uid,1)
-		doRemoveItem(getwall4.uid,1)
-		doRemoveItem(getwall5.uid,1)
-		doRemoveItem(getwall6.uid,1)
-		doRemoveItem(getwall7.uid,1)
-		doRemoveItem(getwall8.uid,1)
-		doRemoveItem(getwall9.uid,1)
-		doRemoveItem(getwall10.uid,1)
-		doRemoveItem(getwall11.uid,1)
-		doRemoveItem(getwall12.uid,1)
-		doRemoveItem(getwall13.uid,1)
-		
-		doCreateItem(1050,1,wall1)
-		doCreateItem(1050,1,wall2)
-		doCreateItem(1050,1,wall3)
-		doCreateItem(1050,1,wall4)
-		doCreateItem(1050,1,wall5)
-		doCreateItem(1050,1,wall6)
-		doCreateItem(1050,1,wall7)
-		doCreateItem(1050,1,wall8)
-		doCreateItem(1050,1,wall9)
-		doCreateItem(1050,1,wall10)
-		doCreateItem(1050,1,wall11)
-		doCreateItem(1050,1,wall12)
-		doCreateItem(1050,1,wall13)
-		doTransformItem(item.uid,item.itemid+1)
-	elseif item.itemid == 1946 then
-	
-		doRemoveItem(getwall1.uid,1)
-		doRemoveItem(getwall2.uid,1)
-		doRemoveItem(getwall3.uid,1)
-		doRemoveItem(getwall4.uid,1)
-		doRemoveItem(getwall5.uid,1)
-		doRemoveItem(getwall6.uid,1)
-		doRemoveItem(getwall7.uid,1)
-		doRemoveItem(getwall8.uid,1)
-		doRemoveItem(getwall9.uid,1)
-		doRemoveItem(getwall10.uid,1)
-		doRemoveItem(getwall11.uid,1)
-		doRemoveItem(getwall12.uid,1)
-		doRemoveItem(getwall13.uid,1)
-		doTransformItem(item.uid,item.itemid-1)	
-		
-		doCreateItem(1524,1,wall1)
-		doCreateItem(1524,1,wall2)
-		doCreateItem(1524,1,wall3)
-		doCreateItem(1524,1,wall4)
-		doCreateItem(1524,1,wall5)
-		doCreateItem(1524,1,wall6)
-		doCreateItem(1524,1,wall7)
-		doCreateItem(1524,1,wall8)
-		doCreateItem(1524,1,wall9)
-		doCreateItem(1524,1,wall10)
-		doCreateItem(1524,1,wall11)
-		doCreateItem(1524,1,wall12)
-		doCreateItem(1524,1,wall13)
-		
-		
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	if itemEx.itemid == 1945 then
+		for i = 1, #config["WallPositions"] do
+			local tile = Tile(config["WallPositions"][i])
+			if tile then
+				local thing = tile:getItemById(config["walldown"])
+				if thing and thing:isItem() then
+					Item(item.uid):transform(1946)
+					thing:transform(config["wallup"])
+				end
+			end
+		end	
+	elseif itemEx.itemid == 1946 then
+		for i = 1, #config["WallPositions"] do
+			local tile = Tile(config["WallPositions"][i])
+			if tile then
+				local thing = tile:getItemById(config["wallup"])
+				if thing and thing:isItem() then
+					Item(item.uid):transform(1945)
+					thing:transform(config["walldown"])
+				end
+			end
+		end
 	else
 		doPlayerSendCancel(cid,"Sorry, not possible.")
 	end
-
-	return 1
+	return true
 end
