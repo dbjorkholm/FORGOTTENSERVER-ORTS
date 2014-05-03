f77f330b131ef073d5a3608d5e2166cc45307d3f
Royalot <zapf-benny@web.de>
2014-05-03 11:43:48 +0200

    some fixes
    fix #408

--- data/actions/scripts/in service of yalahar quest/inServiceOfYalaharQuestBogDoorReward.lua
+++ data/actions/scripts/in service of yalahar quest/inServiceOfYalaharQuestBogDoorReward.lua
@@ -1,10 +1,13 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if(item.uid == 3079) then
-		if(getPlayerStorageValue(cid, 25) == 1 and getPlayerStorageValue(cid, 24) == 1 and getPlayerStorageValue(cid, 23) == 1) then
+		local player = Player(cid)
+		if(player:getStorageValue(25) == 1 and player:getStorageValue(24) == 1 and player:getStorageValue(23) == 1) then
 			if(item.itemid == 1257) then
-				doTeleportThing(cid, toPosition, true)
-				doTransformItem(item.uid, 1258)
+				player:teleportTo(toPosition, true)
+				Item(item.uid):transform(item.itemid + 1)
 			end
+		else
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
 		end
 	end
 	return true
