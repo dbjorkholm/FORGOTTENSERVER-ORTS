6cbf2f03cb86519caf6ee96087af8481fa98d35c
Royalot <zapf-benny@web.de>
2014-05-03 15:57:55 +0200

    add a quest to questlog

--- data/npc/scripts/Duncan.lua
+++ data/npc/scripts/Duncan.lua
@@ -43,6 +43,7 @@ function creatureSayCallback(cid, type, msg)
 			npcHandler.topic[cid] = 2
 		elseif npcHandler.topic[cid] == 2 then
 			player:setStorageValue(storage, 1)
+			player:setStorageValue(12010, 1) --this for default start of Outfit and Addon Quests
 			npcHandler:say("Good! Come back to me once you have gathered 100 eye patches.", cid)
 			npcHandler.topic[cid] = 0
 		elseif npcHandler.topic[cid] == 3 then
