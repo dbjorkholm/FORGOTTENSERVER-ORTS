2c9d6acc8f4902e6e66416680becc189376250d0
Royalot <zapf-benny@web.de>
2014-05-04 23:22:11 +0200

    fixed demon oak bugs
    - fixed #378
    - fixed oldrak buying hallowed axe + meta
    - fixed teleporter
    mapchanges:
    teleport at
    Position: [X: 32714] [Y: 32345] [Z: 7].
    set destination {x=32716, y=32340, z=7}
    
    Former-commit-id: 5d0ef185ab9c964b92792c4313e446a800186eba

--- data/npc/scripts/Oldrak.lua
+++ data/npc/scripts/Oldrak.lua
@@ -4,7 +4,6 @@ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-local talkState = {}
 local start_storage = 1013
  
 function onCreatureAppear(cid)				npcHandler:onCreatureAppear(cid) end
@@ -41,60 +40,60 @@ keywordHandler:addKeyword({'have'}, StdModule.say, {npcHandler = npcHandler, onl
 keywordHandler:addKeyword({'time'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "Now, it is "..getTibianTime().."."})
  
 function creatureSayCallback(cid, type, msg)
+	local player = Player(cid)
    	if (not npcHandler:isFocused(cid)) then 
 		return false 
 	end
-	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_PRIVATE and 0 or cid
 	if  (msgcontains(msg, 'mission') or msgcontains(msg, 'demon oak')) then
-		if(getPlayerStorageValue(cid, storages.done) < 1 and getPlayerStorageValue(cid, start_storage) < 2) then
+		if(player:getStorageValue(storages.done) < 1 and player:getStorageValue(start_storage) < 2) then
 			npcHandler:say("How do you know? Did you go into the infested area?", cid)
-			talkState[talkUser] = 1
-		elseif (getPlayerStorageValue(cid, start_storage) == 2 and getPlayerStorageValue(cid, storages.done) < 1) then
+			npcHandler.topic[cid] = 1
+		elseif (player:getStorageValue(start_storage) == 2 and player:getStorageValue(storages.done) < 1) then
 			npcHandler:say("You better don't return here until you've defeated the Demon Oak.", cid)	
-		elseif (getPlayerStorageValue(cid, storages.done) == 1) then 
+		elseif (player:getStorageValue(storages.done) == 1) then 
 			npcHandler:say({"You chopped down the demon oak?!? Unbelievable!! Let's hope it doesn't come back. As long as evil is still existent in the soil of the plains, it won't be over. Still, the demons suffered a setback, that's for sure. ...", "For your brave action, I tell you a secret which has been kept for many many years. There is an old house south of the location where you found the demon oak. There should be a grave with the name 'Yesim Adeit' somewhere close by. ...", "It belongs to a Daramian nobleman named 'Teme Saiyid'. I knew him well and he told me -almost augured- that someone will come who is worthy to obtain his treasure. I'm sure this 'someone' is you. Good luck in finding it!"}, cid, 0, 1, 7000)
-			setPlayerStorageValue(cid, storages.done, 2)
+			player:setStorageValue(storages.done, 2)
 		end
-	elseif (msgcontains(msg, 'yes') and talkState[talkUser] == 1) then
-		if (getPlayerStorageValue(cid, start_storage) == 1) then
+	elseif (msgcontains(msg, 'yes') and npcHandler.topic[cid] == 1) then
+		if (player:getStorageValue(start_storage) == 1) then
 			npcHandler:say("A demon oak?!? <mumbles some blessings> May the gods be on our side. You'll need a {hallowed axe} to harm that tree. Bring me a simple {axe} and I'll prepare it for you.",cid)
-		   	setPlayerStorageValue(cid, start_storage, 2)
-			talkState[talkUser] = 0
+		   	player:setStorageValue(start_storage, 2)
+			npcHandler.topic[cid] = 0
 		else
 			npcHandler:say("I don't believe a word of it! How rude to lie to a monk!",cid)
-			talkState[talkUser] = 0
+			npcHandler.topic[cid] = 0
 		end
 	elseif (msgcontains(msg, 'axe') or msgcontains(msg, 'hallowed axe')) then
-		if (getPlayerStorageValue(cid, start_storage) < 2) then 
+		if (player:getStorageValue(start_storage) == 2) then 
 			npcHandler:say("Ahh, you've got an axe. Very good. I can make a hallowed axe out of it. It will cost you... er... a donation of 1,000 gold. Alright?",cid)	
-			talkState[talkUser] = 2
+			npcHandler.topic[cid] = 2
 		else
 			npcHandler:say("You have to first talk about {demon oak} or the {mission} before we continue.",cid)
-			talkState[talkUser] = 0
+			npcHandler.topic[cid] = 0
 		end
-	elseif (msgcontains(msg, 'yes') and talkState[talkUser] == 2) then
-		if (getPlayerStorageValue(cid, start_storage) == 2) then
-			if (doPlayerRemoveMoney(cid, 1000)) then
-        			if (doPlayerRemoveItem(cid,2386, 1))  then  
-           				npcHandler:say("Let's see....<mumbles a prayer>....here we go. The blessing on this axe will be absorbed by all the demonic energy around here. I presume it will not last very long, so better hurry. Actually, I can refresh the blessing as often as you like.",cid)
-					doPlayerAddItem(cid, 8293, 1)
+	elseif (msgcontains(msg, 'yes') and npcHandler.topic[cid] == 2) then
+		if (player:getStorageValue(start_storage) == 2) then
+			if (player:removeMoney(1000)) then
+        		if (player:removeItem(2386, 1))  then  
+           			npcHandler:say("Let's see....<mumbles a prayer>....here we go. The blessing on this axe will be absorbed by all the demonic energy around here. I presume it will not last very long, so better hurry. Actually, I can refresh the blessing as often as you like.",cid)
+					player:addItem(8293, 1)
 					doSendMagicEffect(getThingPos(getNpcCid()), 49)
-           				talkState[talkUser] = 0
-        			else
-           				npcHandler:say("There is no axe with you.",cid)
-					talkState[talkUser] = 0
+           			npcHandler.topic[cid] = 0
+        		else
+           			npcHandler:say("There is no axe with you.",cid)
+					npcHandler.topic[cid] = 0
 				end
 			else
 				npcHandler:say("There is not enough of money with you.",cid)	
-				talkState[talkUser] = 0
+				npcHandler.topic[cid] = 0
        			end 
     		end
-	elseif (msgcontains(msg, 'no') and talkState[talkUser] == 1) then
+	elseif (msgcontains(msg, 'no') and npcHandler.topic[cid] == 1) then
 		npcHandler:say("What a pity! Let me know when you managed to get in there. Maybe I can help you when we know what we are dealing with.",cid)
-		talkState[talkUser] = 0
-	elseif (msgcontains(msg, 'no') and talkState[talkUser] == 2) then
+		npcHandler.topic[cid] = 0
+	elseif (msgcontains(msg, 'no') and npcHandler.topic[cid] == 2) then
 		npcHandler:say("No then.",cid)
-		talkState[talkUser] = 0
+		npcHandler.topic[cid] = 0
     	end
 	return true
 end
