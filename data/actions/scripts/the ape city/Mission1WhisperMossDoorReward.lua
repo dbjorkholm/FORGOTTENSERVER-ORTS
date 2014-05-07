75d854dffa30d52e24fa6c717ac5a9d22319d3a4
Royalot <zapf-benny@web.de>
2014-05-07 22:38:58 +0200

    fixes push reward chest
    changed aid to uid
    - fix #432
    - fix #433

--- data/actions/scripts/the ape city/Mission1WhisperMossDoorReward.lua
+++ data/actions/scripts/the ape city/Mission1WhisperMossDoorReward.lua
@@ -1,14 +1,13 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if item.actionid == 12121 then
 	local player = Player(cid)
-		if item.itemid == 3551 then --door
+		if item.itemid == 3551 and item.actionid == 12121 then --door
 			if player:getStorageValue (12120) > 0 then
 				player:teleportTo(toPosition, true)
 				Item(item.uid):transform(item.itemid + 1)
 			else
 				player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
 			end
-		elseif item.itemid == 1738 then --chest reward
+		elseif item.itemid == 1738 and item.uid == 12121 then --chest reward
 			if player:getStorageValue(12121) == 1 then
 				player:setStorageValue(12121, 2)  -- The Ape City Questlog - Mission 1: Whisper Moss
 				player:addItem(4838, 1)
@@ -17,6 +16,5 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 				player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
 			end
 		end
-	end
 	return true
 end
