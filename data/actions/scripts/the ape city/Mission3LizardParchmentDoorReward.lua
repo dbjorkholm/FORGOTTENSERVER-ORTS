cdb54669de5e723212dcdf59700587a21631f744
Royalot <zapf-benny@web.de>
2014-05-07 22:38:58 +0200

    fixes push reward chest
    changed aid to uid
    - fix #432
    - fix #433
    
    
    Former-commit-id: 75d854dffa30d52e24fa6c717ac5a9d22319d3a4

--- data/actions/scripts/the ape city/Mission3LizardParchmentDoorReward.lua
+++ data/actions/scripts/the ape city/Mission3LizardParchmentDoorReward.lua
@@ -1,14 +1,13 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if item.actionid == 12123 then
 	local player = Player(cid)
-		if item.itemid == 3551 then --door
+		if item.itemid == 3551 and item.actionid == 12123 then --door
 			if player:getStorageValue (12120) > 7 then
 				player:teleportTo(toPosition, true)
 				Item(item.uid):transform(item.itemid + 1)
 			else
 				player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
 			end
-		elseif item.itemid == 1741 then --chest reward
+		elseif item.itemid == 1741 and item.uid == 12123 then --chest reward
 			if player:getStorageValue(12123) == 1 then
 				player:setStorageValue(12123, 2) -- The Ape City Questlog - Mission 3: Lizard Parchment
 				player:addItem(5956, 1)
@@ -17,6 +16,5 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 				player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
 			end
 		end
-	end
 	return true
 end
