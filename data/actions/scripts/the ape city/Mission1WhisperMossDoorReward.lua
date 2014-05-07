cc5b62c506cccb57be8540125e164a3a66479cf9
pitis91 <patriiciio.88@gmail.com>
2014-05-07 20:39:18 +0200

    Update Mission1WhisperMossDoorReward.lua
    I've separated the door/reward funcions.
    the motive is simple, if you use an actionid in a chest you can move it to anyway....
    if you use uniqueid it will be unamoveable item.

--- data/actions/scripts/the ape city/Mission1WhisperMossDoorReward.lua
+++ data/actions/scripts/the ape city/Mission1WhisperMossDoorReward.lua
@@ -1,3 +1,5 @@
+this is the Mission1WhisperMossDoorReward.lua
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if item.actionid == 12121 then
 	local player = Player(cid)
@@ -8,7 +10,17 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 			else
 				player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
 			end
-		elseif item.itemid == 1738 then --chest reward
+		end
+	end
+	return true
+end
+
+and this is the Mission1WhisperMossReward.lua
+
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	if item.uniqueid == 12121 then
+	local player = Player(cid)
+		if item.itemid == 1738 then --chest reward
 			if player:getStorageValue(12121) == 1 then
 				player:setStorageValue(12121, 2)  -- The Ape City Questlog - Mission 1: Whisper Moss
 				player:addItem(4838, 1)
@@ -20,3 +32,9 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	end
 	return true
 end
+
+there are the action.xml new lines
+
+	<!--- The Ape City Quest -->
+	<action actionid="12121" script="the ape city/Mission1WhisperMossDoorReward.lua" />
+	<action uniqueid="12121" script="the ape city/Mission1WhisperMossReward.lua" />
