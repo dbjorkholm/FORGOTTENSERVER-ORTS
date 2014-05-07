9e7154224974297e06eac925d3d6322f74878243
Royalot <zapf-benny@web.de>
2014-05-07 22:09:51 +0200

    ape city quest to mission 7 + some fixes
    in map changes:
    AID 12127:
    {fromx = 32863, tox = 32864, fromy = 32497, toy = 32509, z = 9}
    
    
    Former-commit-id: b6037b0e199f3e5b35345a9e4c70227427995a98

--- data/actions/scripts/tools/crowbar.lua
+++ data/actions/scripts/tools/crowbar.lua
@@ -1,40 +1,57 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
+	local targetItem = Item(itemEx.uid)
 	-- In Service Of Yalahar Quest
 	if(itemEx.uid == 3071) then
-		if(player:getStorageValue(26) < 1) then
-			doSetMonsterOutfit(cid, "skeleton", 3 * 1000)
-			doSendMagicEffect(getPlayerPosition(cid), CONST_ME_ENERGYHIT)
-			for x = -1, 1 do
-				for y = -1, 1 do
-					doSendMagicEffect({x = getPlayerPosition(cid).x + x, y = getPlayerPosition(cid).y + y, z = getPlayerPosition(cid).z}, CONST_ME_YELLOWENERGY)
-				end
-			end
-			player:setStorageValue(26, 1)
-		end
-	elseif(itemEx.uid == 3072) then
-		if(player:getStorageValue(27) < 1) then
-			for x = -1, 1 do
-				for y = -1, 1 do
-					if(math.random(1, 2) == 2) then
-						doSummonCreature("rat", {x = getPlayerPosition(cid).x + x, y = getPlayerPosition(cid).y + y, z = getPlayerPosition(cid).z})
-						doSendMagicEffect({x = getPlayerPosition(cid).x + x, y = getPlayerPosition(cid).y + y, z = getPlayerPosition(cid).z}, CONST_ME_TELEPORT)
+			if(player:getStorageValue(26) < 1) then
+				doSetMonsterOutfit(cid, "skeleton", 3 * 1000)
+				fromPosition:sendMagicEffect(CONST_ME_ENERGYHIT)
+				player:setStorageValue(26, 1)
+				player:setStorageValue(12012, player:getStorageValue(12012) + 1) -- StorageValue for Questlog "Mission 01: Something Rotten"
+				for x = -1, 1 do
+					for y = -1, 1 do
+						doSendMagicEffect({x = player:getPosition().x + x, y = player:getPosition().y + y, z = player:getPosition().z}, CONST_ME_YELLOWENERGY)
 					end
 				end
 			end
-			player:setStorageValue(27, 1)
+		elseif(itemEx.uid == 3072) then
+			if(player:getStorageValue(27) < 1) then
+				player:setStorageValue(27, 1)
+				player:setStorageValue(12012, player:getStorageValue(12012) + 1) -- StorageValue for Questlog "Mission 01: Something Rotten"
+				for x = -1, 1 do
+					for y = -1, 1 do
+						if(math.random(1, 2) == 2) then
+							Game.createMonster("rat", {x = player:getPosition().x + x, y = player:getPosition().y + y, z = player:getPosition().z})
+							doSendMagicEffect({x = player:getPosition().x + x, y = player:getPosition().y + y, z = player:getPosition().z}, CONST_ME_TELEPORT)
+						end
+					end
+				end
+			end
+		elseif(itemEx.uid == 3073) then
+			if(player:getStorageValue(28) < 1) then
+				player:say("You have used the crowbar on a grate.", TALKTYPE_ORANGE_1)
+				player:setStorageValue(28, 1)
+				player:setStorageValue(12012, player:getStorageValue(12012) + 1) -- StorageValue for Questlog "Mission 01: Something Rotten"
+			end
+		elseif(itemEx.uid == 3074) then
+			if(player:getStorageValue(29) < 1) then
+				doSetMonsterOutfit(cid, "bog raider", 5 * 1000)
+				player:say("You have used the crowbar on a knot.", TALKTYPE_ORANGE_1)
+				player:setStorageValue(29, 1)
+				player:setStorageValue(12012, player:getStorageValue(12012) + 1) -- StorageValue for Questlog "Mission 01: Something Rotten"
+			end
 		end
-	elseif(itemEx.uid == 3073) then
-		if(player:getStorageValue(28) < 1) then
-			doCreatureSay(cid, "You have used the crowbar on a grate.", TALKTYPE_ORANGE_1)
-			player:setStorageValue(28, 1)
-		end
-	elseif(itemEx.uid == 3074) then
-		if(player:getStorageValue(29) < 1) then
-			doSetMonsterOutfit(cid, "bog raider", 5 * 1000)
-			doCreatureSay(cid, "You have used the crowbar on a knot.", TALKTYPE_ORANGE_1)
-			player:setStorageValue(29, 1)
-		end
+	-- The Ape City - Mission 7: Destroying Casks With Crowbar
+	if itemEx.itemid == 5539 and itemEx.actionid == 12127 and player:getStorageValue(12127) <= 3 then --hit cask with crowbar
+			toPosition:sendMagicEffect(CONST_ME_POFF)
+			player:setStorageValue(12127, player:getStorageValue(12127) + 1) -- The Ape City Questlog - Mission 7: Destroying Casks With Crowbar
+			if player:getStorageValue(12127) == 4 then
+				player:setStorageValue(12120, 17)
+			end
+			player:say("You destroyed a cask.", TALKTYPE_ORANGE_1)
+			targetItem:transform(2249)
+			addEvent(function(toPosition) toPosition:sendMagicEffect(CONST_ME_MAGIC_GREEN) end, 30 * 1000, toPosition)
+			addEvent(function () local tile = toPosition:getTile() if tile then local thing = tile:getItemById(2249) if thing and thing:isItem() then thing:transform(5539) end end end, 30 * 1000)			
 	end
 	-- Postman Quest
 	if(itemEx.actionid == 100 and itemEx.itemid == 2593) then
--- data/actions/scripts/tools/pick.lua
+++ data/actions/scripts/tools/pick.lua
@@ -1,4 +1,5 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
+	local player = Player(cid)
 	local iEx = Item(itemEx.uid)
 	if (itemEx.uid <= 65535 or itemEx.actionid > 0) then
 		if (itemEx.itemid == 354 or itemEx.itemid == 355) then
@@ -8,8 +9,26 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		end
 	elseif itemEx.itemid == 7200 then
 			iEx:transform(7236)
-        		iEx:decay()
-       		 	toPosition:sendMagicEffect(CONST_ME_HITAREA)
+        	iEx:decay()
+       		toPosition:sendMagicEffect(CONST_ME_HITAREA)
+	end
+	--The Ice Islands Quest, Nibelor 1: Breaking the Ice
+	if itemEx.itemid == 3621 and itemEx.actionid == 12026 then
+		local tile1 = toPosition:getTile()
+		local thing1 = tile1:getItemById(7185) 
+		local chakoyas = {"chakoya toolshaper","chakoya tribewarden","chakoya windcaller"}
+		if player:getStorageValue(12026) > 0 and player:getStorageValue(12002) < 3 and player:getStorageValue(12001) == 3  then
+			player:setStorageValue(12002, player:getStorageValue(12002) + 1)
+			player:setStorageValue(12026, player:getStorageValue(12026) + 1) -- Questlog The Ice Islands Quest, Nibelor 1: Breaking the Ice
+			doSummonCreature(chakoyas[math.random(1, 3)], toPosition)
+			toPosition:sendMagicEffect(CONST_ME_TELEPORT)
+			thing1:transform(7186) 
+			addEvent(function () local tile = toPosition:getTile() if tile then local thing = tile:getItemById(7186) if thing and thing:isItem() then thing:transform(7185) end end end, 60 * 1000)			
+			if player:getStorageValue(12002) >= 2 then
+				player:setStorageValue(12001, 4)
+				player:setStorageValue(12026, 4) -- Questlog The Ice Islands Quest, Nibelor 1: Breaking the Ice
+			end
+		end
 	end
 	return true
 end
