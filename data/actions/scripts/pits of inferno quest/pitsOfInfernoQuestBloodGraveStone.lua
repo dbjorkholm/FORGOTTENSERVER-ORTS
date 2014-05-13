23bf1a4af79ae1d43924830ddf5b8d1fc064dc09
ninjalulz <danielbjorkholm@live.se>
2014-05-13 21:59:52 +0200

    Fix #474

--- data/actions/scripts/pits of inferno quest/pitsOfInfernoQuestBloodGraveStone.lua
+++ data/actions/scripts/pits of inferno quest/pitsOfInfernoQuestBloodGraveStone.lua
@@ -1,14 +1,20 @@
-local newPos = {x = 32791, y = 32332, z = 10}
-local bloodPos = {x = 32791, y = 32334, z = 9}
+local config = {
+	bloodPos = Position({x = 32791, y = 32333, z = 9}),
+	newPos = Position({x = 32791, y = 32332, z = 10})
+}
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local player = Player(cid)
-	local blood = pushThing(Tile(bloodPos):getItemById(2016))
-	if blood.uid > 0 and blood.type == 2 then
-		Item(blood.uid):remove()
-		player:getPosition():sendMagicEffect(CONST_ME_DRAWBLOOD)
-		player:teleportTo(newPos)
-		player:say('Muahaha..',TALKTYPE_ORANGE_1)
-		player:getPosition():sendMagicEffect(CONST_ME_MORTAREA)
+	local tile = config["bloodPos"]:getTile()
+	if tile then
+		local thing = tile:getItemById(2016, 2)
+		if thing and thing:isItem() then
+			thing:remove()
+			local player = Player(cid)
+			player:getPosition():sendMagicEffect(CONST_ME_DRAWBLOOD)
+			player:teleportTo(config["newPos"])
+			player:say('Muahaha..', TALKTYPE_ORANGE_1)
+			player:getPosition():sendMagicEffect(CONST_ME_MORTAREA)
+		end
 	end
 	return true
 end
