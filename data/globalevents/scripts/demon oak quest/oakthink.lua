bd502797ebd349655a873d8c3056c82dc0216c71
PrinterLUA <mastero_90@hotmail.com>
2014-05-14 20:11:47 +0200

    DemonOak OnThink Optimized

--- data/globalevents/scripts/demon oak quest/oakthink.lua
+++ data/globalevents/scripts/demon oak quest/oakthink.lua
@@ -1,4 +1,4 @@
-local demonoak_pos = {x = 32716, y = 32352, z = 7}
+local demonOak_pos = {x = 32716, y = 32352, z = 7}
 local questAreaPosition =
 {
 	{x = 32706, y = 32345, z = 7, stackpos = 255},
@@ -6,13 +6,12 @@ local questAreaPosition =
 }
 
 function onThink(interval, lastExecution)
-	local list = getSpectators(demonoak_pos, 15, 15, false, true)
-	if list and #list > 0 then
-		for _, uid in ipairs(list) do
-			if isPlayer(uid) and not isInRange(getCreaturePosition(uid), questAreaPosition[1], questAreaPosition[2]) and getCreaturesInQuestArea(TYPE_PLAYER, questAreaPosition[1], questAreaPosition[2], GET_COUNT) == 0 then
-				doCreatureSay(uid, sounds[1][math.random(#sounds[1])], TALKTYPE_ORANGE_2, false, uid, demonoak_pos)
-			end
+	local specs = Game.getSpectators(demonOak_pos, false, true, 0, 15, 0, 15)
+	for i = 1, #specs do
+		if isInRange(specs[i]:getPosition(), questAreaPosition[1], questAreaPosition[2]) then
+			return true
 		end
+		specs[i]:say(sounds[1][math.random(#sounds[1])], TALKTYPE_ORANGE_2, false, 0, demonOak_pos)
 	end
  	return true
 end
