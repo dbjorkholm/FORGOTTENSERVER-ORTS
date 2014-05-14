a0663cee536477f55d210e5474958a972cd72c2b
PrinterLUA <mastero_90@hotmail.com>
2014-05-14 19:43:52 +0200

    Firebug #482

--- data/actions/scripts/dreamer challenge quest/dreamerChallengeQuestFireBug.lua
+++
@@ -1,8 +0,0 @@
-function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(itemEx.uid == 2243 and item.itemid == 5468) then
-		doTransformItem(itemEx.uid, 1387)
-		doSendMagicEffect(toPosition, CONST_ME_FIREAREA)
-		doRemoveItem(item.uid, 1)
-	end
-	return true
-end
