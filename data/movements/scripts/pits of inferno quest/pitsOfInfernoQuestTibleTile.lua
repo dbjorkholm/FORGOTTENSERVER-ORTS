7d6522a4ff35aa68d23423030f69f31d69211cee
PrinterLUA <mastero_90@hotmail.com>
2014-05-17 19:11:02 +0200

    Fixed https://github.com/PrinterLUA/FORGOTTENSERVER-ORTS/issues/507

--- data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestTibleTile.lua
+++ data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestTibleTile.lua
@@ -1,14 +1,19 @@
 function onStepIn(cid, item, position, lastPosition)
-	if(getPlayerItemCount(cid, 1970) >= 1) then
-		if(item.uid == 2001) then
-			doTeleportThing(cid, {x = 32791, y = 32327, z = 10})
-			doSendMagicEffect({x = 32791, y = 32327, z = 10}, CONST_ME_TELEPORT)
-		elseif(item.uid == 2000) then
-			doTeleportThing(cid, {x = 32791, y = 32331, z = 10})
-			doSendMagicEffect({x = 32791, y = 32331, z = 10}, CONST_ME_TELEPORT)
+	local player = Player(cid)
+	if not player then
+		return false
+	end
+
+	if player:getItemCount(1970) >= 1 then
+		if item.uid == 2000 then
+			player:teleportTo({x = 32791, y = 32331, z = 10}, false)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		elseif item.uid == 2001 then
+			player:teleportTo({x = 32791, y = 32327, z = 10}, false)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		end
 	else
-		doTeleportThing(cid, lastPosition)
+		player:teleportTo(lastPosition)
 	end
 	return true
 end
