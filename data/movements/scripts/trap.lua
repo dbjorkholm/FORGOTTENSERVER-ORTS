0f4c98e79de7310f759b17ca9a97b75d80e790cb
Royalot <zapf-benny@web.de>
2014-05-17 22:24:31 +0200

    added meta movementscript from tfs
    - I added only that scripts without error
    but if one makes error we can revert that
    - added in traps the jungle maw #498

--- data/movements/scripts/trap.lua
+++ data/movements/scripts/trap.lua
@@ -1,27 +1,30 @@
-function onStepIn(cid, item, pos)
-	if item.itemid == 2579 then
-		if isPlayer(cid) ~= TRUE then
-			doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -15, -30, CONST_ME_NONE)
-			doTransformItem(item.uid, item.itemid - 1)
-		end
-	else
-		if isPlayer(cid) == TRUE then
-			doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -50, -100, CONST_ME_NONE)
-			doTransformItem(item.uid, item.itemid + 1)
-		end
+function onStepIn(cid, item, position, fromPosition)
+	if item.itemid == 2579 then -- trap item
+		doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -15, -30, CONST_ME_NONE)
+		Item(item.uid):transform(item.itemid - 1)
+	elseif item.itemid == 1510 then -- blades
+		doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -50, -100, CONST_ME_NONE)
+		Item(item.uid):transform(item.itemid + 1)
+	elseif item.itemid == 1513 then -- spikes
+		doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -50, -100, CONST_ME_NONE)
+	elseif item.itemid == 4208 then -- jungle maw
+		doTargetCombatHealth(0, cid, COMBAT_EARTHDAMAGE, -15, -30, CONST_ME_NONE)
+		Item(item.uid):transform(item.itemid + 1)
 	end
-	return TRUE
+	return true
 end
 
-function onStepOut(cid, item, pos)
-	doTransformItem(item.uid, item.itemid - 1)
-	return TRUE
+function onStepOut(cid, item, position, fromPosition)
+	Item(item.uid):transform(item.itemid - 1)
+	return true
 end
 
-function onRemoveItem(item, tile, pos)
-	if getDistanceBetween(getThingPos(item.uid), pos) > 0 then
-		doTransformItem(item.uid, item.itemid - 1)
-		doSendMagicEffect(getThingPos(item.uid), CONST_ME_POFF)
+function onRemoveItem(item, tile, position)
+	local trap = Item(item.uid)
+	local thingPos = trap:getPosition()
+	if getDistanceBetween(thingPos, position) > 0 then
+		trap:transform(item.itemid - 1)
+		thingPos:sendMagicEffect(CONST_ME_POFF)
 	end
-	return TRUE
+	return true
 end
