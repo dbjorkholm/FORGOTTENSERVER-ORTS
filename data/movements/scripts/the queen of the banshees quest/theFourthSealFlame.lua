4ccfd96d02ff58c4a6da31ee0902b68596ce91cf
PrinterLUA <mastero_90@hotmail.com>
2014-05-19 20:21:52 +0200

    The Queen of the Banshees Quest, Seal 4
    Former-commit-id: 4293ab61ef2a9839ab70878349f8e47d2c0d8a25

---
+++ data/movements/scripts/the queen of the banshees quest/theFourthSealFlame.lua
@@ -0,0 +1,25 @@
+function onStepIn(cid, item, position, lastPosition)
+	local player = Player(cid)
+	if not player then
+		return false
+	end
+	if player:getStorageValue(50016) < 1 then	
+		local Tile = Position({x = 32243, y = 31892, z = 14}):getTile()
+		if Tile then
+			local thing = Tile:getItemById(2016, 2)
+			if thing and thing:isItem() then
+				thing:remove()
+				player:setStorageValue(50016, 1)
+				player:teleportTo({x = 32261, y = 31849, z = 15}, false)
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			else
+				player:teleportTo(lastPosition, true)
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			end
+		end
+	else
+		player:teleportTo(lastPosition, true)
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	end
+	return true
+end
