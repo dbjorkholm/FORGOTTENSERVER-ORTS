da04e8ec6df4e92bbb8b312987c44a487f71bdb2
PrinterLUA <mastero_90@hotmail.com>
2014-05-19 17:15:41 +0200

    The Queen of the Banshees Quest, Seal 6 (Addition Questlog for seal 1 and 6)
    Others:
    Teleport room pos: x = 32267, y = 31852, z = 15, added all cords there
    back to the seals.
    Added also questlog for seal 1 and seal 6
    
    Seal 6:
    Added uid to all switches in this pos: 32311, 31974, 13
    Added uid to the blue flame also: 32311, 31978, 13
    
    
    Former-commit-id: bfcf8924b84340d99be0b19e6db2828cd2d8c4d1

--- data/movements/scripts/the queen of the banshees quest/theFirstSealFlame.lua
+++ data/movements/scripts/the queen of the banshees quest/theFirstSealFlame.lua
@@ -3,8 +3,16 @@ function onStepIn(cid, item, position, lastPosition)
 	if not player then
 		return false
 	end
-	Game.createMonster("Ghost", {x = 32276, y = 31902, z = 13}, false, true)
-	Game.createMonster("Ghost", {x = 32274, y = 31902, z = 13}, false, true)
-	Game.createMonster("Demon Skeleton", {x = 32276, y = 31904, z = 13}, false, true)
+	if player:getStorageValue(50013) < 1 then
+		player:setStorageValue(50013, 1)
+		player:teleportTo({x = 32266, y = 31849, z = 15}, false)
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		Game.createMonster("Ghost", {x = 32276, y = 31902, z = 13}, false, true)
+		Game.createMonster("Ghost", {x = 32274, y = 31902, z = 13}, false, true)
+		Game.createMonster("Demon Skeleton", {x = 32276, y = 31904, z = 13}, false, true)
+	else
+		player:teleportTo(lastPosition, true)
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	end
 	return true
 end
---
+++ data/movements/scripts/the queen of the banshees quest/theSixthSealFlame.lua
@@ -0,0 +1,47 @@
+local function doCheckCampfires(cpPos, itemid)
+	local Tile = Position(cpPos):getTile()
+	if Tile then
+		local thing = Tile:getItemById(itemid)
+		if thing and thing:isItem() then
+			return true
+		else
+			return false
+		end
+	end
+end
+
+local config = {
+	[1] = {pos = {x = 32309, y = 31975, z = 13}, cpId = 1421},
+	[2] = {pos = {x = 32309, y = 31976, z = 13}, cpId = 1421},
+	[3] = {pos = {x = 32311, y = 31975, z = 13}, cpId = 1421},
+	[4] = {pos = {x = 32311, y = 31976, z = 13}, cpId = 1421},
+	[5] = {pos = {x = 32313, y = 31975, z = 13}, cpId = 1423},
+	[6] = {pos = {x = 32313, y = 31976, z = 13}, cpId = 1423}
+}
+
+function onStepIn(cid, item, position, lastPosition)
+	local campfiresDone = 0
+	local player = Player(cid)
+	if not player then
+		return false
+	end
+	if player:getStorageValue(50014) < 1 then
+		for i = 1, 6 do
+			if doCheckCampfires(config[i].pos, config[i].cpId) then
+				campfiresDone = campfiresDone + 1
+			end
+		end
+		if campfiresDone == 6 then
+			player:setStorageValue(50014, 1)
+			player:teleportTo({x = 32261, y = 31856, z = 15}, false)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		else
+			player:teleportTo(lastPosition, true)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		end			
+	else
+		player:teleportTo(lastPosition, true)
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	end
+	return true
+end
