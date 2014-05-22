6aacdb0b0b1bc80c0c9619df09d494a5ea6cc3b5
Royalot <zapf-benny@web.de>
2014-05-22 20:34:36 +0200

    fix #524

--- data/actions/scripts/other/fluids.lua
+++ data/actions/scripts/other/fluids.lua
@@ -16,59 +16,58 @@ local fluidType = {3, 4, 5, 7, 10, 11, 13, 15, 19, 43}
 local fluidMessage = {"Aah...", "Urgh!", "Mmmh.", "Aaaah...", "Aaaah...", "Urgh!", "Urgh!", "Aah...", "Urgh!", "Aaaah..."}
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local iex = Item(itemEx.uid)
-	local i = Item(item.uid)
-	local p = Player(cid)
-	local ie = ItemType(itemEx.itemid)
-    if ie:isFluidContainer() and itemEx.type == 0 then
-        iex:transform(itemEx.itemid, item.type)
-        i:transform(item.itemid, 0)
+	local targetItem = Item(itemEx.uid)
+	local player = Player(cid)
+	local targetItemId = ItemType(itemEx.itemid)
+    if targetItemId:isFluidContainer() and itemEx.type == 0 then
+        targetItem:transform(itemEx.itemid, item.type)
+        Item(item.uid):transform(item.itemid, 0)
         return true
     end
-    if ie:isFluidContainer() and item.type == 0 then
-        iex:transform(itemEx.itemid, 0)
-        i:transform(item.itemid, itemEx.type)
+    if targetItemId:isFluidContainer() and item.type == 0 then
+        targetItem:transform(itemEx.itemid, 0)
+        Item(item.uid):transform(item.itemid, itemEx.type)
         return true
     end
 
     if itemEx.itemid == 1 then
         if item.type == 0 then
-            p:sendTextMessage(MESSAGE_STATUS_SMALL, "It is empty.")
+            player:sendTextMessage(MESSAGE_STATUS_SMALL, "It is empty.")
         elseif itemEx.uid == cid then
-            i:transform(item.itemid, 0)
+            Item(item.uid):transform(item.itemid, 0)
             if item.type == 3 or item.type == 15 or item.type == 43 then
-				p:addCondition(drunk)
+				player:addCondition(drunk)
             elseif item.type == 4 then
-                p:addCondition(poison)
+                player:addCondition(poison)
             elseif item.type == 7 then
-                p:addMana(math.random(50, 150))
+                player:addMana(math.random(50, 150))
                 fromPosition:sendMagicEffect(CONST_ME_MAGIC_BLUE)
             elseif item.type == 10 then
-                p:addHealth(60)
+                player:addHealth(60)
                 fromPosition:sendMagicEffect(CONST_ME_MAGIC_BLUE)
             end
             for i = 0, #fluidType do
                 if item.type == fluidType[i] then
-                    p:say(fluidMessage[i], TALKTYPE_ORANGE_1)
+                    player:say(fluidMessage[i], TALKTYPE_ORANGE_1)
                     return true
                 end
             end
-            p:say("Gulp.", TALKTYPE_ORANGE_1)
+            player:say("Gulp.", TALKTYPE_ORANGE_1)
         else
-            i:transform(item.itemid, 0)
+            Item(item.uid):transform(item.itemid, 0)
             Game.createItem(2016, item.type, toPosition):decay()
         end
     else
-        local fluidSource = ie:getFluidSource()
+        local fluidSource = targetItemId:getFluidSource()
         if fluidSource ~= 0 then
-            i:transform(item.itemid, fluidSource)
+            Item(item.uid):transform(item.itemid, fluidSource)
         elseif item.type == 0 then
-            p:sendTextMessage(MESSAGE_STATUS_SMALL, "It is empty.")
+            player:sendTextMessage(MESSAGE_STATUS_SMALL, "It is empty.")
         else
             if toPosition.x == CONTAINER_POSITION then
-                toPosition = p:getPosition()
+                toPosition = player:getPosition()
             end
-            i:transform(item.itemid, 0)
+            Item(item.uid):transform(item.itemid, 0)
             Game.createItem(2016, item.type, toPosition):decay()
         end
     end
