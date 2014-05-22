8115c3b00ab0dd7f19f94afa0dc1c79cb12e2bfd
EPuncker <gm.perflex@gmail.com>
2014-05-22 15:58:16 -0300

    - Fixed Black Knight quest rewards #541 - Added RME link to readme #532 - Removed unused variable #528 - Added Medusa boss Gorgo #511
    Former-commit-id: aba7ba862e8b60b8faba214b451b270b8355704d

--- data/actions/scripts/black knight quest/blackKnightQuestReward.lua
+++ data/actions/scripts/black knight quest/blackKnightQuestReward.lua
@@ -1,14 +1,10 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 local player = Player(cid)
-        if(player:getStorageValue(10011) < 1) then
-		player:addExperience(1000, true, true)
-                player:setStorageValue(10011, 1)
-        end
         if(item.uid == 9055) then
                 if(player:getStorageValue(490) < 1) then
                         player:setStorageValue(490, 1)
                         player:addItem(2519, 1)
-                        player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a crown shield.")
+                        player:sendTextMessage(MESSAGE_INFO_DESCR, "You have found a crown shield.")
                 else
                         player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
                 end
@@ -16,18 +12,9 @@ local player = Player(cid)
                 if(player:getStorageValue(491) < 1) then
                         player:setStorageValue(491, 1)
                         player:addItem(2488, 1)
-                        player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found crown legs.")
+                        player:sendTextMessage(MESSAGE_INFO_DESCR, "You have found a crown legs.")
                 else
                         player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
                 end
-        elseif(item.uid == 3030) then
-                if(player:getStorageValue(492) < 1) then
-                        player:setStorageValue(492, 1)
-                        player:addItem(2392, 1)
-                        player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a fire sword.")
-                else
-                        player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
-                end
-        end
         return true
 end
