9dfe8036a5a9b70c265764dd7ec768cf6c741720
Royalot <zapf-benny@web.de>
2014-05-22 22:46:29 +0200

    new frontier quest mission04
    - mission04
    - fix #543
    
    
    Former-commit-id: 8d515f9e2fe107d3280fa3949e2f4d07b231a867

--- data/actions/scripts/the new frontier quest/theNewFrontierQuestMineDoor.lua
+++ data/actions/scripts/the new frontier quest/theNewFrontierQuestMineDoor.lua
@@ -1,10 +1,11 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.actionid == 8005) then
-		if(getPlayerStorageValue(cid, 1015) >= 12) then
-			if(item.itemid == 8553) then
-				doTeleportThing(cid, toPosition, true)
-				doTransformItem(item.uid, item.itemid + 1)
-			end
+	if item.itemid == 8553 and item.actionid == 12134 then --door
+		local player = Player(cid)
+		if player:getStorageValue (12134) > 0 then
+			player:teleportTo(toPosition, true)
+			Item(item.uid):transform(item.itemid + 1)
+		else
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
 		end
 	end
 	return true
