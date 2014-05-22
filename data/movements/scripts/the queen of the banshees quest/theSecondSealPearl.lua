e2e6bdc3ac08bc7238680b662a50e7ac59eac591
PrinterLUA <mastero_90@hotmail.com>
2014-05-22 20:30:19 +0200

    Banshee Quest almost done!
    I had to go, so i will finish it later :)

---
+++ data/movements/scripts/the queen of the banshees quest/theSecondSealPearl.lua
@@ -0,0 +1,44 @@
+local function doCheckPearls(pearlPos, itemid)
+	local Tile = Position(pearlPos):getTile()
+	if Tile then
+		local thing = Tile:getItemById(itemid)
+		if thing and thing:isItem() then
+			thing:remove()
+			Position(pearlPos):sendMagicEffect(CONST_ME_MAGIC_RED)
+			return true
+		else
+			return false
+		end
+	end
+end
+
+local config = {
+	[1] = {pos = {x = 32173, y = 31871, z = 15}, pId = 2143},
+	[2] = {pos = {x = 32180, y = 31871, z = 15}, pId = 2144}
+}
+
+function onStepIn(cid, item, position, lastPosition)
+	local player = Player(cid)
+	local pearls = 0
+	if not player then
+		return false
+	end
+	if player:getStorageValue(50019) < 1 then	
+		for i = 1, 2 do
+			if doCheckPearls(config[i].pos, config[i].pId) then
+				pearls = pearls + 1
+			end
+		end
+		if pearls == 2 then
+			player:teleportTo({x = player:getPosition().x, y = player:getPosition().y-6, z = 15}, false)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		else
+			player:teleportTo(lastPosition, true)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		end		
+	else
+		player:teleportTo(lastPosition, true)
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	end
+	return true
+end
