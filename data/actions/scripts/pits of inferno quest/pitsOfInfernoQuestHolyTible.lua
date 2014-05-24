c73a5578ba15c035a5c7f6da13f74a7bfaafe807
pitis91 <pitis91@users.noreply.github.com>
2014-05-24 16:57:30 +0200

    Update pitsOfInfernoQuestHolyTible.lua
    now the book will have the history written on it

--- data/actions/scripts/pits of inferno quest/pitsOfInfernoQuestHolyTible.lua
+++ data/actions/scripts/pits of inferno quest/pitsOfInfernoQuestHolyTible.lua
@@ -1,12 +1,13 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if(item.uid == 2021) then
 		if(getPlayerStorageValue(cid, 231) < 1) then
-			setPlayerStorageValue(cid, 231, 1)
-			doPlayerAddItem(cid, 1970, 1)
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You've found a holy tible.")
-		else
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "The chest is empty.")
-		end
-	end
+			doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR,"You have found the holy Tible.")
+				local book = doPlayerAddItem(cid,1970,1)
+					doSetItemText(book,"Banor I praise your name.\nBe with me in the battle.\nBe my shield, let me be your sword.\nI will honour the godly spark in my soul. May it flourish and grow.")
+					setPlayerStorageValue(cid, 231, 1)
+						else
+							doPlayerSendTextMessage(cid,MESSAGE_INFO_DESCR,"The chest is empty.")
+								end
+									end
 	return true
-end
+	end
