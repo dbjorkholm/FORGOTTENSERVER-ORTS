0d159069e39532598626c28db36bebf6dd281e4c
Daniel Björkholm <danielbjorkholm@live.se>
2014-05-24 17:38:24 +0200

    Fix crash issue
    It's not possible to throw items on teleports anymore.

--- data/movements/scripts/others/notrash.lua
+++
@@ -1,6 +0,0 @@
-function onAddItem(moveitem, tileitem, position)
-	if tileitem.actionid > 0 or tileitem.uid > 0 then
-		Item(moveitem.uid):remove()
-		Position(moveitem.uid):sendMagicEffect(CONST_ME_POFF)
-	end
-end
