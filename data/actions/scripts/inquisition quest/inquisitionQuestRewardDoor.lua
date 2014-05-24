2838a34a372d6f789a94da58fa730c8330bf9952
pitis91 <pitis91@users.noreply.github.com>
2014-05-24 14:06:46 +0200

    Update inquisitionQuestRewardDoor.lua
    in real tibia you can enter when you want having the reward too

--- data/actions/scripts/inquisition quest/inquisitionQuestRewardDoor.lua
+++ data/actions/scripts/inquisition quest/inquisitionQuestRewardDoor.lua
@@ -3,7 +3,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		if(getPlayerStorageValue(cid, 200) == 23) then
 			return (doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You forgot to ask henricus for outfit."))
 		end
-		if(getPlayerStorageValue(cid, 200) == 24 and getPlayerStorageValue(cid, 300) < 1) then
+		if(getPlayerStorageValue(cid, 200) >= 24) then
 			if(item.itemid == 5105) then
 				doTeleportThing(cid, toPosition, true)
 				doTransformItem(item.uid, 5106)
