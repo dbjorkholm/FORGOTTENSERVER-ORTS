9f3a19ee491b2c7605d7dfc278277436e8b4c6ea
pitis91 <pitis91@users.noreply.github.com>
2014-05-24 18:24:06 +0200

    Update pitsOfInfernoQuestLevers.lua
    here i've improved the wallback created by me :D
    
    what it does? make the script more real tibia, now when you get all levers and pull the last the fire walls from vocations return in flames and when pass 900seconds will return all the levers to his position and stones will return :D

--- data/actions/scripts/pits of inferno quest/pitsOfInfernoQuestLevers.lua
+++ data/actions/scripts/pits of inferno quest/pitsOfInfernoQuestLevers.lua
@@ -21,15 +21,93 @@ local stones = {
 	{x = 32852, y = 32333, z = 12}
 }
 
+function wallback1(cid, item, frompos, item2, topos)
+local gatepos = {x=32833, y=32333, z=11, stackpos=1}
+local itemuid = 2025
+local itemitemid = 1946
+doCreateItem(6289,1,gatepos)
+doTransformItem(itemuid,itemitemid-1)
+end
+
+function wallback2(cid, item, frompos, item2, topos)
+local gatepos = {x=32831, y=32333, z=11, stackpos=1}
+local itemuid = 2026
+local itemitemid = 1946
+doCreateItem(6289,1,gatepos)
+doTransformItem(itemuid,itemitemid-1)
+end
+
+function wallback3(cid, item, frompos, item2, topos)
+local gatepos = {x=32837, y=32333, z=11, stackpos=1}
+local itemuid = 2027
+local itemitemid = 1946
+doCreateItem(6289,1,gatepos)
+doTransformItem(itemuid,itemitemid-1)
+end
+
+function wallback4(cid, item, frompos, item2, topos)
+local gatepos = {x=32835, y=32333, z=11, stackpos=1}
+local itemuid = 2028
+local itemitemid = 1946
+doCreateItem(6289,1,gatepos)
+doTransformItem(itemuid,itemitemid-1)
+end
+
+function stonesback(cid, item, frompos, item2, topos)
+local gatepos = {x=32851, y=32333, z=12, stackpos=1}
+local gatepos1 = {x=32852, y=32333, z=12, stackpos=1}
+local itemuid16 = 2065
+local itemuid15 = 2064
+local itemuid14 = 2063
+local itemuid13 = 2062
+local itemuid12 = 2061
+local itemuid11 = 2060
+local itemuid10 = 2059
+local itemuid9 = 2058
+local itemuid8 = 2057
+local itemuid7 = 2056
+local itemuid6 = 2055
+local itemuid5 = 2054
+local itemuid4 = 2053
+local itemuid3 = 2052
+local itemuid2 = 2051
+local itemuid1 = 2050
+local itemitemid = 1946
+doCreateItem(1304,1,gatepos)
+doCreateItem(1304,1,gatepos1)
+doTransformItem(itemuid16,itemitemid-1)
+doTransformItem(itemuid1,itemitemid-1)
+doTransformItem(itemuid2,itemitemid-1)
+doTransformItem(itemuid3,itemitemid-1)
+doTransformItem(itemuid4,itemitemid-1)
+doTransformItem(itemuid5,itemitemid-1)
+doTransformItem(itemuid6,itemitemid-1)
+doTransformItem(itemuid7,itemitemid-1)
+doTransformItem(itemuid8,itemitemid-1)
+doTransformItem(itemuid9,itemitemid-1)
+doTransformItem(itemuid10,itemitemid-1)
+doTransformItem(itemuid11,itemitemid-1)
+doTransformItem(itemuid12,itemitemid-1)
+doTransformItem(itemuid13,itemitemid-1)
+doTransformItem(itemuid14,itemitemid-1)
+doTransformItem(itemuid15,itemitemid-1)
+end
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if(item.uid == 2065) then
-		if(item.itemid == 1945) or (item.itemid == 1946) then
-			if(getGlobalStorageValue(1000) == 14) then
+		if(item.itemid == 1945) then
+			if(getGlobalStorageValue(1000) == 15) then
 				for i = 1, 2 do
 					doRemoveItem(getTileItemById(stones[i], 1304).uid, 1)
-					doSendMagicEffect(stones[i], CONST_ME_EXPLOSION)
+					doSendMagicEffect(stones[i], CONST_ME_EXPLOSIONHIT)
 				end
 				doTransformItem(item.uid, 1946)
+				addEvent(wallback1, 1 * 1000)
+				addEvent(wallback2, 1 * 1000)
+				addEvent(wallback3, 1 * 1000)
+				addEvent(wallback4, 1 * 1000)
+				addEvent(stonesback, 900 * 1000)
+				setGlobalStorageValue(1000, 0)
 			else
 				doCreatureSay(cid, "The final lever won't budge... yet.", TALKTYPE_ORANGE_1)
 			end
@@ -39,7 +117,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if(getGlobalStorageValue(1000) < 0) then
 		setGlobalStorageValue(1000, 0)
 	end
-	if(item.itemid == 1945) or (item.itemid == 1946) then
+	if(item.itemid == 1945) then
 		if((getGlobalStorageValue(1000) + 1) == pos[item.uid].number) then
 			setGlobalStorageValue(1000, pos[item.uid].number)
 			doCreatureSay(cid, pos[item.uid].text, TALKTYPE_ORANGE_1)
