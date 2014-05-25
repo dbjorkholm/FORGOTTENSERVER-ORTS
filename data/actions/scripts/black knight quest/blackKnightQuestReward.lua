d691856110958b385f030818f6e37ce7071ed974
Jonas21 <reyes_27_9@hotmail.com>
2014-05-26 00:18:15 +0200

    Fix nº 2 reward bk quest

--- data/actions/scripts/black knight quest/blackKnightQuestReward.lua
+++ data/actions/scripts/black knight quest/blackKnightQuestReward.lua
@@ -6,16 +6,16 @@ local player = Player(cid)
 			player:addItem(2519, 1)
 			player:sendTextMessage(MESSAGE_INFO_DESCR, "You have found a crown shield.")
 		else
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "The tree is empty.")
 		end
 		
 		elseif(item.uid == 3027) then
 		if(player:getStorageValue(491) < 1) then
 			player:setStorageValue(491, 1)
 			player:addItem(2488, 1)
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "You have found a crown legs.")
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "You have found a crown armor.")
 		else
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "The tree is empty.")
 		end
 	end	
 	
