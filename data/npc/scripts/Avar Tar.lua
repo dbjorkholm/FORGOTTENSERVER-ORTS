747376e6566090a0594a6ec3d1834831258ebe73
pitis91 <pitis91@users.noreply.github.com>
2014-05-25 18:27:00 +0200

    Update Avar Tar.lua
    fixed get outfits from avar tar, same as real tibia

--- data/npc/scripts/Avar Tar.lua
+++ data/npc/scripts/Avar Tar.lua
@@ -21,35 +21,46 @@ function creatureSayCallback(cid, type, msg)
 		return false
 	end
 	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
-
+	local player = Player(cid)
 	if(msgcontains(msg, "outfit")) then
-		npcHandler:say("I'm tired of all these young unskilled wannabe heroes. Every Tibian can show his skills or actions by wearing a special outfit. To prove oneself worthy of the demon outfit, this is how it goes: ... ", cid)
-		npcHandler:say("The base outfit will be granted for completing the annihilator quest, which isn't much of a challenge nowadays, in my opinion. Anyway ... ", cid)
-		npcHandler:say("The shield however will only be granted to those adventurers who have finished the demon helmet quest. ... ", cid)
-		npcHandler:say("Well, the helmet is for those who really are tenacious and have hunted down all 6666 demons and finished the demon oak as well. ... ", cid)
-		npcHandler:say("Are you interested? ", cid)
+		npcHandler:say(
+		{
+		"I'm tired of all these young unskilled wannabe heroes. Every Tibian can show his skills or actions by wearing a special outfit. To prove oneself worthy of the demon outfit, this is how it goes: ... ", 
+		"The base outfit will be granted for completing the annihilator quest, which isn't much of a challenge nowadays, in my opinion. Anyway ... ", 
+		"The shield however will only be granted to those adventurers who have finished the demon helmet quest. ... ", 
+		"Well, the helmet is for those who really are tenacious and have hunted down all 6666 demons and finished the demon oak as well. ... ", 
+		"Are you interested?"
+		}, player)
 		talkState[talkUser] = 1
 	elseif(msgcontains(msg, "base")) then
-		if(talkState[talkUser] == 2 and getPlayerStorageValue(cid, 310) == 1 and canPlayerWearOutfit(cid, getPlayerSex(cid) == 0 and 541 or 542, 0) == false) then
-			doPlayerAddOutfit(cid, getPlayerSex(cid) == 0 and 541 or 542, 0)
-			npcHandler:say("Receive the base outfit, " .. getPlayerName(cid) .. ".", cid)
+		if(talkState[talkUser] == 2 and getPlayerStorageValue(cid, 2215) == 1 and canPlayerWearOutfit(cid, getPlayerSex(cid) == 0 and 541 or 542, 0) == false) then
+			player:addOutfit(542, 0)
+			player:addOutfit(541, 0)
+			player:setStorageValue(2215, 2)
+			npcHandler:say("Receive the base outfit, " .. getPlayerName(cid) .. ".", player)
 			talkState[talkUser] = 0
 		end
 	elseif(msgcontains(msg, "shield")) then
-		if(talkState[talkUser] == 2 and getPlayerStorageValue(cid, 305) == 1 and canPlayerWearOutfit(cid, getPlayerSex(cid) == 0 and 541 or 542, 1) == false) then
-			doPlayerAddOutfit(cid, getPlayerSex(cid) == 0 and 541 or 542, 1)
-			npcHandler:say("Receive the base outfit, " .. getPlayerName(cid) .. ".", cid)
+		if talkState[talkUser] == 2 then
+		if player:getStorageValue(2215) == 2 and player:getStorageValue(2217) == 1 then
+			player:addOutfitAddon(player:getSex() == 0 and 542 or 541, 1)
+			player:setStorageValue(2217, 2)
+			npcHandler:say("Receive the shield, " .. getPlayerName(cid) .. ".", player)
 			talkState[talkUser] = 0
 		end
-	-- elseif(msgcontains(msg, "helmet")) then
-		-- if(talkState[talkUser] == 2 and getPlayerStorageValue(cid, ) == 1 and canPlayerWearOutfit(cid, getPlayerSex(cid) == 0 and 541 or 542, 2) == false) then
-			-- doPlayerAddOutfit(cid, getPlayerSex(cid) == 0 and 541 or 542, 2)
-			-- npcHandler:say("Receive the base outfit, " .. getPlayerName(cid) .. ".", cid)
-			-- talkState[talkUser] = 0
-		-- end
+		end
+	elseif(msgcontains(msg, "helmet")) then
+		if talkState[talkUser] == 2 then
+		if player:getStorageValue(2215) == 2 and player:getStorageValue(1010) == 3 then
+			player:addOutfitAddon(player:getSex() == 0 and 542 or 541, 2)
+			player:setStorageValue(1010, 4)
+			npcHandler:say("Receive the helmet, " .. getPlayerName(cid) .. ".", player)
+			talkState[talkUser] = 0
+		end
+		end
 	elseif(msgcontains(msg, "yes")) then
 		if(talkState[talkUser] == 1) then
-			npcHandler:say("So you want to have the demon outfit, hah! Let's have a look first if you really deserve it. Tell me: {base}, {shield} or {helmet}?", cid)
+			npcHandler:say("So you want to have the demon outfit, hah! Let's have a look first if you really deserve it. Tell me: {base}, {shield} or {helmet}?", player)
 			talkState[talkUser] = 2
 		end
 	end
