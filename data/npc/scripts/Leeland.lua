8b6fb523cf89da4fa4c6277015654dbe42351983
Royalot <zapf-benny@web.de>
2014-05-26 21:03:27 +0200

    new frontier mission 05
    the behaviour to bribe the npc can be only add if Black Bert works. so
    far only talk the keywords.
    
    
    Former-commit-id: 6ccbdbf360815f4fdd980fe0860c1e56de229262

--- data/npc/scripts/Leeland.lua
+++ data/npc/scripts/Leeland.lua
@@ -1,35 +1,28 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
-local talkState = {}
-function onCreatureAppear(cid)
-	npcHandler:onCreatureAppear(cid)
-end
-function onCreatureDisappear(cid)
-	npcHandler:onCreatureDisappear(cid)
-end
-function onCreatureSay(cid, type, msg)
-	npcHandler:onCreatureSay(cid, type, msg)
-end
-function onThink()
-	npcHandler:onThink()
-end
-function creatureSayCallback(cid, type, msg)
-	if(not(npcHandler:isFocused(cid))) then
+
+function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
+function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
+function onThink() npcHandler:onThink() end
+
+local function creatureSayCallback(cid, type, msg)
+	local player = Player(cid)
+	if not npcHandler:isFocused(cid) then
 		return false
-	end
-	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
-	
-	if(msgcontains(msg, "farmine")) then
-		if(getPlayerStorageValue(cid, 1015) == 15) then
+	--The New Frontier
+	elseif msgcontains(msg, "farmine") then
+		if player:getStorageValue(1015) == 15 then
 			npcHandler:say("Oh yes, that project the whole dwarven community is so excited about. I guess I already know why you are here, but speak up.", cid)
-			talkState[talkUser] = 1
+			npcHandler.topic[cid] = 1
 		end
-	elseif(msgcontains(msg, "impress")) then
-		if(talkState[talkUser] == 1) then
-			if(getPlayerStorageValue(cid, 1024) < 1) then
-				npcHandler:say("Your pathetic whimpering amuses me. For this I grant you my assistance. But listen, one day I'll ask you to return this favour. From now on, you owe me one.", cid)
-				setPlayerStorageValue(cid, 1024, 1)
+	elseif msgcontains(msg, "impress") or msgcontains(msg, "plea") then
+		if npcHandler.topic[cid] == 1 then
+			if player:getStorageValue(1024) < 1 then
+				npcHandler:say("Oh yes, that project the whole dwarven community is so excited about. I guess I already know why you are here, but speak up.", cid)
+				player:setStorageValue(1024, 1)
+				player:setStorageValue(12135, player:getStorageValue(12135) + 1) --Questlog, The New Frontier Quest "Mission 05: Getting Things Busy"
 			end
 		end
 	end
