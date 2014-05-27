c9953b1beb9a6c19f4a29f6a41cf07fe57100da5
PrinterLUA <mastero_90@hotmail.com>
2014-05-27 21:27:38 +0200

    Revert "Hot Cuisine Quest! Dish 2"
    This reverts commit 875a364b46320ae014aeda4aa972335490e4ca49 [formerly 3b8b07561b1112be55db7d8d47507510de326628].
    
    
    Former-commit-id: 4dac184d42f637ca8f5506acee529907c75f64d7

--- data/npc/scripts/Jean Pierre.lua
+++ data/npc/scripts/Jean Pierre.lua
@@ -8,28 +8,8 @@ function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) 
 function onThink() npcHandler:onThink() end
 
 local cfg_ingredients = {
-	[1] = {{2666, 2}, {8838, 20}, {8843, 1}, {9114, 1}, {2692, 5}, {2006, 2, 3}},
-	[2] = {{7250, 2}, {2685, 2}, {8842, 1}, {2328, 2}, {2805, 5}, {2006, 1, 15}}
+	[1] = {{2666, 2}, {8838, 20}, {8843, 1}, {9114, 1}, {2692, 5}, {2006, 2, 3}}
 }
-
-local function playerHasIngredients(cid)
-	local player = Player(cid)
-	local table = cfg_ingredients[player:getStorageValue(50023)]
-	if table ~= nil then
-		for i = 1, #table do
-			local itemCount = player:getItemCount(table[i][1], table[i][3] or -1)
-        		if itemCount < table[i][2] then
-                		itemCount = table[i][2] - itemCount
-				return false
-			end
-       		end
-	end
-	for i = 1, #table do
-        	player:removeItem(unpack(table[i]))
-	end
-	return true
-end
-
 
 local function greetCallback(cid)
 	local player = Player(cid)
@@ -62,12 +42,6 @@ local function creatureSayCallback(cid, type, msg)
 						"Two pieces of meat, two vials of beer, twenty potatoes, one onion, one bulb of garlic and five ounces of flour. Make sure that the ingredients are fresh and smell good."
 						}, cid, 0, 1, 4000)
 				npcHandler.topic[cid] = 4
-			elseif player:getStorageValue(50023) == 2 then
-				npcHandler:say({"The next dish we are going to prepare together is called {Hydra Tongue Salad}. The common hydra tongue is a pest plant with an surprisingly aromatic taste. ...",
-						"We'll add some other vegetables and spices for the delicate and distinctive taste. Bring me the following ingredients and I'll show you how it's done. ...",
-						"Two hydra tongue plants, two tomatoes, one cucumber, two eggs, one troll green and one vial of wine."
-						}, cid, 0, 1, 4000)
-				npcHandler.topic[cid] = 6
 			end
 		end
 	elseif msgcontains(msg, "apprentice") then
@@ -81,31 +55,26 @@ local function creatureSayCallback(cid, type, msg)
 			player:setStorageValue(50022, 1)
 			player:setStorageValue(50023, 1)
 		elseif npcHandler.topic[cid] == 5 then
-			if playerHasIngredients(cid) then
-				npcHandler:say({"Aah, so let's see! <prepares the food quickly while he explains it to you> We cook the meat in a large pot together with the chopped onion until it's separated from the bones. Now we also have a fine meat broth! ...",
-						"Cut the potatoes into small pieces and add them to the pot. Add some flour to thicken the stew. Finally, spice it up with some garlic and add beer for the typical dwarvish taste! ...",
-						"And voilà, we're done. I developed this recipe while talking to Maryza in the Jolly Axeman. She said to eat it when one's health is low. Enjoy!"
-						}, cid, 0, 1, 4000)
-				player:setStorageValue(50023, 2)	
-				player:addItem(9992, 1)
-				npcHandler.topic[cid] = 0
-			else
-				npcHandler:say("Make sure that you have all the ingredients, with you.", cid)
-				npcHandler.topic[cid] = 0
+			local table = cfg_ingredients[player:getStorageValue(50023)]
+			for i = 1, #table do
+				local itemCount = player:getItemCount(table[i][1], table[i][3] or -1)
+        			if itemCount < table[i][2] then
+                			itemCount = table[i][2] - itemCount
+                			npcHandler:say("Make sure that you have all the ingredients, with you.", cid)
+					npcHandler.topic[cid] = 0
+					return false
+       				end
 			end
-		elseif npcHandler.topic[cid] == 7 then
-			if playerHasIngredients(cid) then
-				npcHandler:say({"Okay, here we go! <cuts the ingredients so fast that his hands seem almost blurry> This one is easy, just chop the hydra tongues, tomatoes and cucumber into tiny pieces. ...",
-						"Now for the sauce - our base is wine, in which we mix the raw eggs until it got a nice smooth consistency. Add grinded troll green, whose flavour is quite similar to basil and shake the sauce in a mug. ...",
-						"Pour it over the salad, and voilà, we're done! This is a Venorean recipe and very tasty. I recommend eating it when you're suffering from some kind of dangerous condition. Enjoy!"
-						}, cid, 0, 1, 4000)
-				player:setStorageValue(50023, 3)
-				player:addItem(9993, 1)
-				npcHandler.topic[cid] = 0
-			else
-				npcHandler:say("Make sure that you have all the ingredients, with you.", cid)
-				npcHandler.topic[cid] = 0
-			end				
+			for i = 1, #table do
+        			player:removeItem(unpack(table[i]))
+			end
+			npcHandler:say({"Aah, so let's see! <prepares the food quickly while he explains it to you> We cook the meat in a large pot together with the chopped onion until it's separated from the bones. Now we also have a fine meat broth! ...",
+					"Cut the potatoes into small pieces and add them to the pot. Add some flour to thicken the stew. Finally, spice it up with some garlic and add beer for the typical dwarvish taste! ...",
+					"And voilà, we're done. I developed this recipe while talking to Maryza in the Jolly Axeman. She said to eat it when one's health is low. Enjoy!"
+					}, cid, 0, 1, 4000)
+			player:setStorageValue(50023, 2)	
+			player:addItem(9992, 1)
+			npcHandler.topic[cid] = 0
 		end
 	--Dishes
 	elseif msgcontains(msg, "rotworm stew") then
@@ -113,11 +82,6 @@ local function creatureSayCallback(cid, type, msg)
 			npcHandler:say("Did you gather all necessary ingredients to cook Rotworm Stew with me?", cid)
 			npcHandler.topic[cid] = 5
 		end
-	elseif msgcontains(msg, "hydra tongue salad") then
-		if npcHandler.topic[cid] == 6 then
-			npcHandler:say("Did you gather all necessary ingredients to prepare a Hydra Tongue Salad with me?", cid)
-			npcHandler.topic[cid] = 7
-		end	
 	end
 	return true
 end
