3f48b70078a7760450ed1e1b020c86294f594fd3
exsonmedina <medinaexson@outlook.com>
2014-05-27 21:56:50 -0430

    Add addons 2 mage full
    zoltan npc had no dialogue for second addon mage outfits

--- data/npc/scripts/Zoltan.lua
+++ data/npc/scripts/Zoltan.lua
@@ -2,25 +2,67 @@ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
-function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
-function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
-function onThink()				npcHandler:onThink()					end
+function onCreatureAppear(cid)            npcHandler:onCreatureAppear(cid)            end
+function onCreatureDisappear(cid)        npcHandler:onCreatureDisappear(cid)            end
+function onCreatureSay(cid, type, msg)    npcHandler:onCreatureSay(cid, type, msg)    end
+function onThink()                        npcHandler:onThink()    end
 
-function creatureSayCallback(cid, type, msg)
-	if not npcHandler:isFocused(cid) then
-		return false
-	elseif msgcontains(msg, "myra") then
-		local player, storage = Player(cid), 50011
-		if player:getStorageValue(storage) == 10 then
-			player:addOutfitAddon(138, 2)
-			player:addOutfitAddon(133, 2)
-			player:setStorageValue(storage, 11)
-			npcHandler:say({"Bah, I know. I received some sort of 'nomination' from our outpost in Port Hope. ...", "Usually it takes a little more than that for an award though. However, I honour Myra's word. ..."}, cid)
-		end
-	end
-	return true
+-- Storage IDs --     
+mage        = 22005 
+summoner    = 22011   
+
+newaddon    = 'I bow to you, player, and hereby grant you the right to wear Ferumbras hat as accessory. Congratulations!'
+noitems        = 'You do not have all the required items.'
+noitems2    = 'You do not have all the required items or you do not have the first addon, which by the way, is a requirement for this addon.'
+already        = 'It seems you already have this addon, don\'t you try to mock me son!'
+ 
+-- MAGE START --   
+function MageSecond(cid, message, keywords, parameters, node)
+
+    if(not npcHandler:isFocused(cid)) then
+        return false
+    end
+
+    if isPremium(cid) then
+    addon = getPlayerStorageValue(cid,mage+1)
+    if addon == -1 then
+        if getPlayerSex(cid) == 0 then
+        if getPlayerItemCount(cid,5903) >= 1 then
+        if doPlayerRemoveItem(cid,5903,1) then
+            selfSay(newaddon, cid)
+             
+            doSendMagicEffect(getCreaturePosition(cid), 13) 
+            doPlayerAddOutfit(cid, 141, 2)
+            setPlayerStorageValue(cid,summoner+1,1)
+        end
+        else
+            selfSay(noitems, cid)
+        end
+        elseif getPlayerSex(cid) == 1 then
+        if getPlayerItemCount(cid,5903) >= 1 then
+        if doPlayerRemoveItem(cid,5903,1) then
+            selfSay(newaddon, cid)
+             
+            doSendMagicEffect(getCreaturePosition(cid), 13)
+            doPlayerAddOutfit(cid, 130, 2)
+            setPlayerStorageValue(cid,mage+1,1)
+        end
+        else
+            selfSay(noitems, cid)
+        end
+        end
+    else
+        selfSay(already, cid)
+    end
+    end
+
 end
+-- MAGE END --
 
-npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
-npcHandler:addModule(FocusModule:new())
+node5 = keywordHandler:addKeyword({'proof'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = '... I cannot believe my eyes. You retrieved this hat from Ferumbras remains? That is incredible. If you give it to me, I will grant you the right to wear this hat as addon. What do you say?'})
+    node5:addChildKeyword({'yes'}, MageSecond, {})
+    node5:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Alright then. Come back when you got all neccessary items.', reset = true}) 
+
+
+
+npcHandler:addModule(FocusModule:new()))
