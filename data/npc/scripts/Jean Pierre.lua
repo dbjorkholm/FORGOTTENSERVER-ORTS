f2a5d58b1fc871a9a7e880d1a2c8d138972b55d4
PrinterLUA <mastero_90@hotmail.com>
2014-05-29 11:59:23 +0200

    Hot Cuisine Quest! Dish 3-4 & Added Juice Squeezer!
    Former-commit-id: 922c4e8807ab4ce46d2e45f5d28b968d021a0c1e

--- data/npc/scripts/Jean Pierre.lua
+++ data/npc/scripts/Jean Pierre.lua
@@ -9,7 +9,9 @@ function onThink() npcHandler:onThink() end
 
 local cfg_ingredients = {
 	[1] = {{2666, 2}, {8838, 20}, {8843, 1}, {9114, 1}, {2692, 5}, {2006, 2, 3}},
-	[2] = {{7250, 2}, {2685, 2}, {8842, 1}, {2328, 2}, {2805, 5}, {2006, 1, 15}}
+	[2] = {{7250, 2}, {2685, 2}, {8842, 1}, {2328, 2}, {2805, 5}, {2006, 1, 15}},
+	[3] = {{4298, 1}, {8844, 3}, {2691, 5}, {2328, 2}, {2803, 3}, {2788, 5}},
+	[4] = {{4265, 1}, {8841, 2}, {2675, 2}, {5907, 2}, {2006, 2, 14}, {2799, 1}}
 }
 
 local function playerHasIngredients(cid)
@@ -68,7 +70,18 @@ local function creatureSayCallback(cid, type, msg)
 						"Two hydra tongue plants, two tomatoes, one cucumber, two eggs, one troll green and one vial of wine."
 						}, cid, 0, 1, 4000)
 				npcHandler.topic[cid] = 6
-			end
+			elseif player:getStorageValue(50023) == 3 then
+				npcHandler:say({"The next dish we are going to prepare together is called {Roasted Dragon Wings}. Oh, don't give me that look! Of course you don't have to bring a whole dragon up here. ...",
+						"The 'dragon' part derives from the fiery afterburn of this meal, but the wings we use are much smaller, though similar in shape. Bring me the following ingredients and I'll show you how it's done. ...",
+						"One fresh dead bat, three jalapeño peppers, five brown breads, two eggs, one powder herb and five red mushrooms."
+						}, cid, 0, 1, 4000)
+				npcHandler.topic[cid] = 8	
+			elseif player:getStorageValue(50023) == 4 then
+				npcHandler:say({"The next dish we are going to prepare together is called {Tropical Fried Terrorbird}. You might have guessed it, we're not going to use a terrorbird. But! ...",
+						"The dish is quite fried and tropical. Bring me the following ingredients and we're going to prepare it: One fresh dead chicken, two lemons, two oranges, two mangos, one stone herb and two vials of coconut milk."
+						}, cid, 0, 1, 4000)
+				npcHandler.topic[cid] = 10
+			end	
 		end
 	elseif msgcontains(msg, "apprentice") then
 		if npcHandler.topic[cid] == 2 then
@@ -105,7 +118,35 @@ local function creatureSayCallback(cid, type, msg)
 			else
 				npcHandler:say("Make sure that you have all the ingredients, with you.", cid)
 				npcHandler.topic[cid] = 0
-			end				
+			end
+		elseif npcHandler.topic[cid] == 9 then
+			if playerHasIngredients(cid) then
+				npcHandler:say({"Let's get started! <starts grinding and chopping at amazing speed> The trick with this one is the crunchy crust around the wings. ...",
+						"First, we grate the dry brown bread into very small crumbs and mix that with the two eggs. Add grinded peppers for the spicy taste and the powder herb for a hint of curry flavour. ...",
+						"Carefully separate the bat wings, clean them of any possible hairs and coat them in our mixture. Roast them in a pan together with sliced mushrooms and serve. ...",
+						"Voilà, we're done! This recipe is from the area around Thais and should help you protect yourself in your battles. Enjoy!"
+						}, cid, 0, 1, 4000)
+				player:setStorageValue(50023, 4)
+				player:addItem(9994, 1)
+				npcHandler.topic[cid] = 0
+			else
+				npcHandler:say("Make sure that you have all the ingredients, with you.", cid)
+				npcHandler.topic[cid] = 0
+			end
+		elseif npcHandler.topic[cid] == 11 then
+			if playerHasIngredients(cid) then
+				npcHandler:say({"This is a recipe right from the jungles of Tiquanda! <grinds and chops during his talk> Grind the stone herb and mix it with the coconut milk, then bathe the chicken in it for a while. ...",
+						"In the meantime, peel the oranges and mangos, chop them into pieces and add them to the mix. Take the chicken out of its bath and fry it, preferably over open fire. ...",
+						"Take the fruits out of the spicy coconut milk and heat them on an oven. Once the chicken is fried, add the fruits and spray some squeezed lemon over it. ...",
+						"Voilà, we're done! They say that this dish has magical abilities and can awaken secret powers in you during your battles. Enjoy!"
+						}, cid, 0, 1, 4000)
+				player:setStorageValue(50023, 5)
+				player:addItem(9994, 5)
+				npcHandler.topic[cid] = 0
+			else
+				npcHandler:say("Make sure that you have all the ingredients, with you.", cid)
+				npcHandler.topic[cid] = 0
+			end			
 		end
 	--Dishes
 	elseif msgcontains(msg, "rotworm stew") then
@@ -117,7 +158,17 @@ local function creatureSayCallback(cid, type, msg)
 		if npcHandler.topic[cid] == 6 then
 			npcHandler:say("Did you gather all necessary ingredients to prepare a Hydra Tongue Salad with me?", cid)
 			npcHandler.topic[cid] = 7
-		end	
+		end
+	elseif msgcontains(msg, "roasted dragon wings") then
+		if npcHandler.topic[cid] == 8 then
+			npcHandler:say("Did you gather all necessary ingredients to prepare Roasted Dragon Wings with me?", cid)
+			npcHandler.topic[cid] = 9
+		end
+	elseif msgcontains(msg, "tropical fried terrorbird") then
+		if npcHandler.topic[cid] == 10 then
+			npcHandler:say("Did you gather all necessary ingredients to prepare a Tropical Fried Terrorbird with me?", cid)
+			npcHandler.topic[cid] = 11
+		end			
 	end
 	return true
 end
