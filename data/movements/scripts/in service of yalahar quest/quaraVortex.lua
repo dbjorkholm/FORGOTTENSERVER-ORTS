054959d12ac75a6026dc539c33cc7ab326586ead
Royalot <zapf-benny@web.de>
2014-05-30 16:44:33 +0200

    optimization
    Former-commit-id: 1eaedd35196726c01ee95098f4d3562d9f9604f2

--- data/movements/scripts/in service of yalahar quest/quaraVortex.lua
+++ data/movements/scripts/in service of yalahar quest/quaraVortex.lua
@@ -1,12 +1,19 @@
 function onStepIn(cid, item, position, lastPosition)
-	if(item.actionid == 7812) then
-		if(getPlayerSlotItem(cid, CONST_SLOT_HEAD).itemid == 5461) then
-			doTeleportThing(cid, {x = 32950, y = 31181, z = 9})
-			doCreatureSay(cid, "The vortex throws you out in this vicious place.", TALKTYPE_ORANGE_1)
-			doSendMagicEffect({x = 32950, y = 31181, z = 9}, CONST_ME_WATERSPLASH)
+	local player = Player(cid)
+	if not player then
+		return true
+	end
+	
+	local Helmetitem = player:getSlotItem(CONST_SLOT_HEAD)	
+	if item.actionid == 7812 then
+		if Helmetitem ~= nil and Helmetitem:getId() == 5461 then 
+				local destination = Position({x = 32950, y = 31181, z = 9})
+				player:teleportTo(destination)
+				destination:sendMagicEffect(CONST_ME_WATERSPLASH)
+				player:say("The vortex throws you out in this vicious place.", TALKTYPE_ORANGE_1)
 		else
-			doTeleportThing(cid, lastPosition)
-			doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "You must wear helmet of the deep in order to dive.")
+			player:teleportTo(lastPosition)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear helmet of the deep in order to dive.")
 		end
 	end
 	return true
