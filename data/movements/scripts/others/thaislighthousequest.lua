b2ccf7919f341757c05482149295a6ae871e2557
Royalot <zapf-benny@web.de>
2014-05-31 21:45:44 +0200

    thais light house quest
    add mechanism to reach the quest
    in map changes:
    {x = 32227, y = 32278, z = 8} lever aid=50023
    {x = 32225, y = 32285, z = 10} lever aid=50024
    {x = 32225, y = 32268, z = 9} aid=50025

---
+++ data/movements/scripts/others/thaislighthousequest.lua
@@ -0,0 +1,28 @@
+function onStepIn(cid, item, pos, fromPosition)
+	--stairs appear if stepon stone tile
+	if item.actionid == 50025 then
+		local stairstile = Tile(Position({x = 32225, y = 32282, z = 9}))
+		stairstile:getItemById(424):transform(8280)
+		Item(item.uid):transform(425)
+	--the created portal
+	elseif item.actionid == 50026 then
+		local player = Player(cid)
+		if not player then
+			return true
+		end
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:teleportTo({x = 32225, y = 32274, z = 10})
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	end
+	return true
+end
+
+	--stairs disappear if stepout stone tile
+function onStepOut(cid, item, position, fromPosition)
+	if item.actionid == 50025 then
+		local stairstile = Tile(Position({x = 32225, y = 32282, z = 9}))
+		stairstile:getItemById(8280):transform(424)
+		Item(item.uid):transform(426)
+	end
+	return true
+end
