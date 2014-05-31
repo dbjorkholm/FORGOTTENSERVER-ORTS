f0e629b901725d31ef0519a58b509a2ec1e721a5
ninjalulz <danielbjorkholm@live.se>
2014-05-31 12:29:48 +0200

    Fixes #627

--- data/npc/scripts/Miles, The Guard.lua
+++ data/npc/scripts/Miles, The Guard.lua
@@ -1,49 +1,34 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
-local talkState = {}
  
-function onCreatureAppear(cid)
-	npcHandler:onCreatureAppear(cid)			
-end
-function onCreatureDisappear(cid)
-	npcHandler:onCreatureDisappear(cid)			
-end
-function onCreatureSay(cid, type, msg)
-	npcHandler:onCreatureSay(cid, type, msg)		
-end
-function onThink()
-	npcHandler:onThink()					
-end
+function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid)	end
+function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
+function onThink() npcHandler:onThink()	end
 
 function creatureSayCallback(cid, type, msg)
-	if(not npcHandler:isFocused(cid)) then
+	local storage = 204
+	
+	if not npcHandler:isFocused(cid) then
 		return false
-	end
-	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
- 
-	if(msgcontains(msg, "trouble") and talkState[talkUser] == nil and getPlayerStorageValue(cid, 204) < 1) then
+	elseif msgcontains(msg, "trouble") and npcHandler.topic[cid] ~= 3 and Player(cid):getStorageValue(storage) < 1 then
 		npcHandler:say("I'm fine. There's no trouble at all.", cid)
-		talkState[talkUser] = 1
-	elseif(msgcontains(msg, "foresight of the authorities")) then
-		if(talkState[talkUser] == 1) then
-			npcHandler:say("Well, of course. We live in safety and peace.", cid)
-			talkState[talkUser] = 2
-		end
-	elseif(msgcontains(msg, "also for the gods")) then
-		if(talkState[talkUser] == 2) then
-			npcHandler:say("I think the gods are looking after us and their hands shield us from evil.", cid)
-			talkState[talkUser] = 3
-		end
-	elseif(msgcontains(msg, "trouble will arise in the near future")) then
-		if(talkState[talkUser] == 3) then
-			npcHandler:say("I think the gods and the government do their best to keep away harm from the citizens.", cid)
-			talkState[talkUser] = 0
-			if(getPlayerStorageValue(cid, 204) < 1) then
-				setPlayerStorageValue(cid, 204, 1)
-				Player(cid):setStorageValue(12111, Player(cid):getStorageValue(12111) + 1) -- The Inquisition Questlog- "Mission 1: Interrogation"
-				doSendMagicEffect(getCreaturePosition(cid), CONST_ME_HOLYAREA)
-			end
+		npcHandler.topic[cid] = 1
+	elseif msgcontains(msg, "foresight of the authorities") and npcHandler.topic[cid] == 1 then
+		npcHandler:say("Well, of course. We live in safety and peace.", cid)
+		npcHandler.topic[cid] = 2
+	elseif msgcontains(msg, "also for the gods") and npcHandler.topic[cid] == 2 then
+		npcHandler:say("I think the gods are looking after us and their hands shield us from evil.", cid)
+		npcHandler.topic[cid] = 3
+	elseif msgcontains(msg, "trouble will arise in the near future") and npcHandler.topic[cid] == 3 then
+		npcHandler:say("I think the gods and the government do their best to keep away harm from the citizens.", cid)
+		npcHandler.topic[cid] = 0
+		local player = Player(cid)
+		if player:getStorageValue(storage) < 1 then
+			player:setStorageValue(storage, 1)
+			player:setStorageValue(12111, player:getStorageValue(12111) + 1) -- The Inquisition Questlog- "Mission 1: Interrogation"
+			player:getPosition():sendMagicEffect(CONST_ME_HOLYAREA)
 		end
 	end
 	return true
