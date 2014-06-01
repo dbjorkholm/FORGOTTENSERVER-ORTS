599d22d04ece508ec43e50d9c54b83a7dfe1d8e9
Skillert <peter.mollberg@gmail.com>
2014-06-01 18:30:11 +0200

    Koshei The Deathless quest fix.

--- data/actions/scripts/koshei amulet quest/kosheiAmuletQuestSwitch.lua
+++ data/actions/scripts/koshei amulet quest/kosheiAmuletQuestSwitch.lua
@@ -1,6 +1,6 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if(item.uid == 3070) then
-		if(getTileItemById({x = 33281, y = 32442, z = 8}, 3689).uid > 0 and getTileItemById({x = 33286, y = 32444, z = 8}, 3689).uid > 0) then
+		if(getTileItemById({x = 33281, y = 32442, z = 8}, 3698).uid > 0 and getTileItemById({x = 33286, y = 32444, z = 8}, 3698).uid > 0) then
 			if(getTileItemById({x = 33273, y = 32458, z = 8}, 7520).uid > 0) then
 				doTransformItem(getTileItemById({x = 33273, y = 32458, z = 8}, 7520).uid, 7525)
 			else
