c5388ac444a2da3a24996ea589c992a0f6c98e4b
Skillert <peter.mollberg@gmail.com>
2014-06-01 22:37:13 +0200

    Sorry about that, @ninjalulz
    Former-commit-id: e6ecfd82b7c3d4f4c08ca492c1c08a2943402a7b

--- data/actions/scripts/koshei amulet quest/kosheiAmuletQuestBoss.lua
+++ data/actions/scripts/koshei amulet quest/kosheiAmuletQuestBoss.lua
@@ -4,7 +4,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		if(player:getStorageValue(49) == 2 and player:getStorageValue(47) < 1) then
 			player:removeItem(8266, 1)
 			player:setStorageValue(47, 1)
-			toPosition:sendMagicEffect(toPosition, CONST_ME_MAGIC_RED)
+			toPosition:sendMagicEffect(CONST_ME_MAGIC_RED)
 			player:say("At last.. I have it back, my precious amulet. I am glad you didn't use it! I allow you to ...ahh... enter door ...ahh", TALKTYPE_ORANGE_1, false, cid, toPosition)
 		end
 	end
