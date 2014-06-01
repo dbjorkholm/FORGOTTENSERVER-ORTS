d2c381b5f0650a541dbbb9e747119718198a1fea
Royalot <zapf-benny@web.de>
2014-06-01 20:03:32 +0200

    update bloodherb

--- data/actions/scripts/quests/bloodherbquestRewards.lua
+++ data/actions/scripts/quests/bloodherbquestRewards.lua
@@ -1,13 +1,11 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
-	if item.uid == 50032  then
-		if player:getStorageValue(50032) < 1  then
-			player:setStorageValue(50032, 1)
-			player:addItem(2798, 1)
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "You have found a Blood Herb egg.")
-		else
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "The tree is empty.")
-		end
+	if player:getStorageValue(50032) ~= 1  then
+		player:setStorageValue(50032, 1)
+		player:addItem(2798, 1)
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You have found a Blood Herb.")
+	else
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "The tree is empty.")
 	end
 	return true
 end
