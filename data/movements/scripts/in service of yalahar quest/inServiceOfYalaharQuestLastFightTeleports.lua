7464a37190e509898922079baaf92c7cb458eb19
Royalot <zapf-benny@web.de>
2014-06-01 16:14:55 +0200

    all in service of yalahar movements in meta

--- data/movements/scripts/in service of yalahar quest/inServiceOfYalaharQuestLastFightTeleports.lua
+++ data/movements/scripts/in service of yalahar quest/inServiceOfYalaharQuestLastFightTeleports.lua
@@ -1,19 +1,26 @@
 function onStepIn(cid, item, position, lastPosition)
+	local player = Player(cid)
+	if not player then
+		return true
+	end
+	
 	if(item.uid == 7809 and isPlayer(cid)) then
-		if(getPlayerStorageValue(cid, 30) == 51) then
-			doTeleportThing(cid, {x = 32783, y = 31174, z = 10})
-			doSendMagicEffect({x = 32783, y = 31174, z = 10}, CONST_ME_TELEPORT)
-			doCreatureSay(cid,"The apparatus in the centre looks odd! You should inspect it.", TALKTYPE_ORANGE_1)
+		if(player:getStorageValue(30) == 51) then
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			player:teleportTo({x = 32783, y = 31174, z = 10})
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			player:say("The apparatus in the centre looks odd! You should inspect it.", TALKTYPE_ORANGE_1)
 
 		else
-			doTeleportThing(cid, lastPosition)
+			player:teleportTo(lastPosition)
 		end
 	elseif(item.uid == 7810 and isPlayer(cid)) then
 		if(getGlobalStorageValue(982) < 1) then
-			doTeleportThing(cid, {x = 32784, y = 31178, z = 9})
-			doSendMagicEffect({x = 32784, y = 31178, z = 9}, CONST_ME_TELEPORT)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			player:teleportTo({x = 32784, y = 31178, z = 9})
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		else
-			doTeleportThing(cid, lastPosition)
+			player:teleportTo(lastPosition)
 		end
 	end
 	return true
--- data/movements/scripts/in service of yalahar quest/inServiceOfYalaharQuestMorik.lua
+++ data/movements/scripts/in service of yalahar quest/inServiceOfYalaharQuestMorik.lua
@@ -1,10 +1,15 @@
 function onStepIn(cid, item, position, lastPosition)
+	local player = Player(cid)
+	if not player then
+		return true
+	end
+	
 	if(item.uid == 3087) then
-		if(getPlayerStorageValue(cid, 30) == 51) then
-			setPlayerStorageValue(cid, 30, 52)
-			Player(cid):setStorageValue(12021, 3) -- StorageValue for Questlog "Mission 10: The Final Battle"
-			doCreatureSay(cid, "It seems by defeating Azerus you have stopped this army from entering your world! Better leave this ghastly place forever.", TALKTYPE_ORANGE_1)
-			doSendMagicEffect(getPlayerPosition(cid), CONST_ME_MAGIC_BLUE)
+		if(player:getStorageValue(30) == 51) then
+			player:setStorageValue(30, 52)
+			player:setStorageValue(12021, 3) -- StorageValue for Questlog "Mission 10: The Final Battle"
+			player:say("It seems by defeating Azerus you have stopped this army from entering your world! Better leave this ghastly place forever.", TALKTYPE_ORANGE_1)
+			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
 		end
 	end
 	return true
