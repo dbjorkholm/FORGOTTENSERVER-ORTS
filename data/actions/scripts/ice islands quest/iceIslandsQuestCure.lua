aae05524eb41fe56caffb4f5fcbcda764e29f82b
PrinterLUA <mastero_90@hotmail.com>
2014-06-03 15:29:29 +0200

    Create Cake https://github.com/PrinterLUA/FORGOTTENSERVER-ORTS/issues/542

--- data/actions/scripts/ice islands quest/iceIslandsQuestCure.lua
+++ data/actions/scripts/ice islands quest/iceIslandsQuestCure.lua
@@ -1,33 +1,19 @@
+local fruits = {2673, 2674, 2675, 2676, 2677, 2678, 2679, 2680, 2681, 2682, 2684, 2685, 5097, 8839, 8840, 8841}
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-local player = Player(cid)
-local targetItem = Item(itemEx.uid)
+	local player = Player(cid)
+	local targetItem = Item(itemEx.uid)
 	if player:getStorageValue(12001) >= 21 then
 		if itemEx.itemid == 7106 and item.itemid == 7286 then
 			toPosition:sendMagicEffect(CONST_ME_MAGIC_BLUE)
 			player:say("You take some hot water from the geyser.", TALKTYPE_ORANGE_1)
-			Item(item.uid):transform(7246)
-		elseif itemEx.itemid == 7261 and item.itemid == 2566 then
-			if player:getStorageValue(12004) < 1 then
-				player:addItem(7248, 1)
-				player:setStorageValue(12004, 1)
-				toPosition:sendMagicEffect(CONST_ME_MAGIC_BLUE)
-				player:say("You cut a leaf from a frostbite herb.", TALKTYPE_ORANGE_1)
-			end
+			Item(item.uid):transform(7246)	
 		elseif itemEx.itemid == 388 and item.itemid == 2565 then
 			if player:getStorageValue(12005) < 1 then
 				player:addItem(8301, 1)
 				player:setStorageValue(12005, 1)
 				toPosition:sendMagicEffect(CONST_ME_MAGIC_RED)
 				player:say("You retrive a fine sulphur from a lava hole.", TALKTYPE_ORANGE_1)
-			end
-		elseif itemEx.itemid == 2733 and item.itemid == 2566 then
-			if player:getStorageValue(12006) < 1 then
-				player:addItem(7245, 1)
-				player:setStorageValue(12006, 1)
-				targetItem:transform(2723)
-				addEvent(function () local tile = toPosition:getTile() if tile then local thing = tile:getTopVisibleThing() if thing and thing:isItem() then thing:transform(2733) end end end, 60 * 1000)			
-				toPosition:sendMagicEffect(CONST_ME_MAGIC_GREEN)
-				player:say("You cut a flower from a cactus.", TALKTYPE_ORANGE_1)
 			end
 		elseif itemEx.itemid == 4184 and item.itemid == 2565 then
 			if player:getStorageValue(12007) < 1 then
@@ -36,15 +22,41 @@ local targetItem = Item(itemEx.uid)
 				toPosition:sendMagicEffect(CONST_ME_MAGIC_RED)
 				player:say("You retrive spores from a mushroom.", TALKTYPE_ORANGE_1)
 			end
-		elseif itemEx.itemid == 4017 and item.itemid == 2566 then
-			if player:getStorageValue(12008) < 1 then
-				player:addItem(7249, 1)
-				player:setStorageValue(12008, 1)
-				targetItem:transform(4014)
-				addEvent(function () local tile = toPosition:getTile() if tile then local thing = tile:getTopVisibleThing() if thing and thing:isItem() then thing:transform(4017) end end end, 60 * 1000)			
-				toPosition:sendMagicEffect(CONST_ME_MAGIC_GREEN)
-				player:say("You cut a flower from a bush.", TALKTYPE_ORANGE_1)
+		end
+	end
+	if item.itemid == 2566 then --kitchen knife
+		if player:getStorageValue(12001) >= 21 then
+			if itemEx.itemid == 7261 then
+				if player:getStorageValue(12004) < 1 then
+					player:addItem(7248, 1)
+					player:setStorageValue(12004, 1)
+					toPosition:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+					player:say("You cut a leaf from a frostbite herb.", TALKTYPE_ORANGE_1)
+				end
+			elseif itemEx.itemid == 2733 then
+				if player:getStorageValue(12006) < 1 then
+					player:addItem(7245, 1)
+					player:setStorageValue(12006, 1)
+					targetItem:transform(2723)
+					addEvent(function () local tile = toPosition:getTile() if tile then local thing = tile:getTopVisibleThing() if thing and thing:isItem() then thing:transform(2733) end end end, 60 * 1000)			
+					toPosition:sendMagicEffect(CONST_ME_MAGIC_GREEN)
+					player:say("You cut a flower from a cactus.", TALKTYPE_ORANGE_1)
+				end
+			elseif itemEx.itemid == 4017 then
+				if player:getStorageValue(12008) < 1 then
+					player:addItem(7249, 1)
+					player:setStorageValue(12008, 1)
+					targetItem:transform(4014)
+					addEvent(function () local tile = toPosition:getTile() if tile then local thing = tile:getTopVisibleThing() if thing and thing:isItem() then thing:transform(4017) end end end, 60 * 1000)			
+					toPosition:sendMagicEffect(CONST_ME_MAGIC_GREEN)
+					player:say("You cut a flower from a bush.", TALKTYPE_ORANGE_1)
+				end
 			end
+		end
+		if isInArray(fruits, itemEx.itemid) and player:removeItem(6278, 1) then
+			targetItem:remove(1)
+			player:addItem(6279, 1)
+			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_GREEN)
 		end
 	end
 	return true
--- data/movements/scripts/dough.lua
+++ data/movements/scripts/dough.lua
@@ -1,7 +1,14 @@
+local config = {
+	[2693] = 2689,
+	[6277] = 6278
+}
+
 function onAddItem(moveitem, tileitem, position)
-	if moveitem.itemid == 2693 then
-		doTransformItem(moveitem.uid, 2689)
-		doSendMagicEffect(position, CONST_ME_HITBYFIRE)
+	local targetItem = Item(moveitem.uid)
+	local table = config[moveitem.itemid]
+	if table then
+		targetItem:transform(table)
+		position:sendMagicEffect(CONST_ME_HITBYFIRE)
 	end
 	return TRUE
 end
