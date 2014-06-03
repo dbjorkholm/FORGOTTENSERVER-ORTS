67ae26d2d2a7e7673d19992590bb9663137ab2e3
Royalot <zapf-benny@web.de>
2014-06-03 21:36:59 +0200

    new frontier update
    - to mission 07
    - add missing npc
    - some fixes and so meta convert

--- data/actions/scripts/the new frontier quest/theNewFrontierQuestTrapDoor.lua
+++ data/actions/scripts/the new frontier quest/theNewFrontierQuestTrapDoor.lua
@@ -1,10 +1,11 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.actionid == 8008) then
-		if(getPlayerStorageValue(cid, 1015) == 22) then
-			if(item.itemid == 10782) then
-				doTeleportThing(cid, toPosition, true)
-				doTransformItem(item.uid, item.itemid + 1)
-			end
+	if item.actionid == 8008 then
+	local player = Player(cid)
+		if player:getStorageValue (1015) == 22 then
+			player:teleportTo(toPosition, true)
+			Item(item.uid):transform(item.itemid + 1)
+		else
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
 		end
 	end
 	return true
--- data/npc/scripts/Zitss.lua
+++ data/npc/scripts/Zitss.lua
@@ -1,42 +1,36 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
-local talkState = {}
-function onCreatureAppear(cid)
-	npcHandler:onCreatureAppear(cid)
-end
-function onCreatureDisappear(cid)
-	npcHandler:onCreatureDisappear(cid)
-end
-function onCreatureSay(cid, type, msg)
-	npcHandler:onCreatureSay(cid, type, msg)
-end
-function onThink()
-	npcHandler:onThink()
-end
-function creatureSayCallback(cid, type, msg)
-	if(not(npcHandler:isFocused(cid))) then
+
+function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
+function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
+function onThink() npcHandler:onThink() end
+
+local function creatureSayCallback(cid, type, msg)
+	local Questlinestorage = 1015 --Questlinestorage
+	local player = Player(cid)
+	if not npcHandler:isFocused(cid) then
 		return false
-	end
-	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
-	
-	if(msgcontains(msg, "work")) then
-		if(getPlayerStorageValue(cid, 1015) == 23) then
-			npcHandler:say("Zere iz a great tournament of ztrengz each decade. It determinez ze granted privilegez for zertain individualz of power for ze comming decade. ...", cid)
-			npcHandler:say("My mazter wantz to zurprize hiz opponentz by an unexpected move. He will uze warriorz from ze outzide, zomeone zat no one can azzezz. ... ", cid)
-			npcHandler:say("One of ziz warriorz could be you. Or you could ztay here and rot in ze dungeon. Are you interezted in ziz deal? ", cid)
-			talkState[talkUser] = 1
+	elseif msgcontains(msg, "work") then
+		if player:getStorageValue(Questlinestorage) == 23 then
+			npcHandler:say({"Zere iz a great tournament of ztrengz each decade. It determinez ze granted privilegez for zertain individualz of power for ze comming decade. ...",
+							"My mazter wantz to zurprize hiz opponentz by an unexpected move. He will uze warriorz from ze outzide, zomeone zat no one can azzezz. ... ",
+							"One of ziz warriorz could be you. Or you could ztay here and rot in ze dungeon. Are you interezted in ziz deal? "}, cid)
+			npcHandler.topic[cid] = 1
 		end
-	elseif(msgcontains(msg, "yes")) then
-		if(talkState[talkUser] == 1) then
-			npcHandler:say("You are zmart for a zoftzkin, but before you begin to feel too zmart, you should know zat we will zeal our deal wiz you drinking a ztrong poizon zat will inevitably kill you if you want to trick me and not attend ze tournament. ... ", cid)
-			npcHandler:say("Zo are you ready to drink ziz poizon here? ", cid)
-			talkState[talkUser] = 2
-		elseif(talkState[talkUser] == 2) then
-			npcHandler:say("Excellent! Now you may leave ziz area zrough ze teleporter to ze norz. It will bring you to a hidden boat. Ziz boat will take you to ze tournament izle. ... ", cid)
-			npcHandler:say("Zere you'll learn anyzing you need to know about ze great tournament. ", cid)
-			setPlayerStorageValue(cid, 1015, 24)
-			talkState[talkUser] = 0
+	elseif msgcontains(msg, "yes") then
+		if npcHandler.topic[cid] == 1 then
+			npcHandler:say({"You are zmart for a zoftzkin, but before you begin to feel too zmart, you should know zat we will zeal our deal wiz you drinking a ztrong poizon zat will inevitably kill you if you want to trick me and not attend ze tournament. ... ",
+							"Zo are you ready to drink ziz poizon here? "}, cid)
+			npcHandler.topic[cid] = 2
+		elseif npcHandler.topic[cid] == 2 then
+			npcHandler:say({"Excellent! Now you may leave ziz area zrough ze teleporter to ze norz. It will bring you to a hidden boat. Ziz boat will take you to ze tournament izle. ... ",
+							"Zere you'll learn anyzing you need to know about ze great tournament. "}, cid)
+			player:setStorageValue(Questlinestorage, 24)
+			player:setStorageValue(12137, 3) --Questlog, The New Frontier Quest "Mission 07: Messengers Of Peace"
+			player:setStorageValue(12138, 1) --Questlog, The New Frontier Quest "Mission 08: An Offer You Can't Refuse"
+			npcHandler.topic[cid] = 0
 		end
 	end
 	return true
