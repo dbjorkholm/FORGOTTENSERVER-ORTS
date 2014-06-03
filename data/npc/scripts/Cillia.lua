0dec7c6059c3fdf985730b47e4c24b7c6b1218b7
Sam <sum9900@web.de>
2014-06-03 17:39:43 +0200

    Add NPC Cillia, cleanup globalevents / fix raids

---
+++ data/npc/scripts/Cillia.lua
@@ -0,0 +1,35 @@
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
+function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
+function onThink() npcHandler:onThink() end
+
+local ticketPrice = 50
+local exhibitionPosition = Position(32390, 32195, 8)
+
+function creatureSayCallback(cid, type, msg)
+	if not npcHandler:isFocused(cid) then
+		return false
+	end
+
+	if msgcontains(msg, 'yes') then
+		local player = Player(cid)
+		if player and player:removeMoney(ticketPrice) then
+			selfSay('And here we go!', cid)
+			player:teleportTo(exhibitionPosition)
+			exhibitionPosition:sendMagicEffect(CONST_ME_TELEPORT)
+		else
+			selfSay('The exhibition is not for free. You have to pay 50 Gold to get in. Next please!', cid)
+		end
+	else
+		selfSay('Then not.', cid)
+	end
+	npcHandler:releaseFocus(cid)
+	return true
+end
+
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:addModule(FocusModule:new())
