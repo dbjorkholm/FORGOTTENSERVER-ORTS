bef4f3bad3f79ae6d60ca666f15f983e8628d65b
Royalot <zapf-benny@web.de>
2014-06-05 22:38:45 +0200

    some additions and fixes
    fix npc name
    some dialoguefix
    in map changes:
    fixed some broken tiles in caves
    added missing caves like {x = 32538, y = 31488, z = 11}
    or {x = 32549, y = 31255, z = 9}
    cutted rooms {x = 32539, y = 31439, z = 10}
    or here {x = 32635, y = 31404, z = 10}
    and a lot of cave ceiling
    
    {x = 33163, y = 31227, z = 11} aid=12138
    
    
    Former-commit-id: af4ca38704c9a7e49eabd8c13e7f60a1b3ceca47

---
+++ data/movements/scripts/the new frontier quest/theNewFrontierQuestJailExit.lua
@@ -0,0 +1,19 @@
+function onStepIn(cid, item, position, lastPosition)
+	local player = Player(cid)
+	if not player then
+		return true
+	end
+	if player:getStorageValue(12138) >= 1 then
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:teleportTo({x = 33145, y = 31247, z = 6})
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	else
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:teleportTo(lastPosition)
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:sendTextMessage(MESSAGE_STATUS_SMALL, "You don't have access to this area.")
+	end
+	return true
+end
+
+
