7624cfe2cd6e79d74b0a6c1c0e766866ca5b2ba4
Royalot <zapf-benny@web.de>
2014-06-07 23:31:39 +0200

    new frontier update
    - moved blessedsteak to other folder
    - add missed npc and convert to metatable
    - fixed some teleports
    
    
    Former-commit-id: ca8fa72999ff0f59ce2a89fbb30c0833566d038e

--- data/npc/scripts/Zurak.lua
+++ data/npc/scripts/Zurak.lua
@@ -1,37 +1,38 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
-local talkState = {}
-function onCreatureAppear(cid)
-	npcHandler:onCreatureAppear(cid)
-end
-function onCreatureDisappear(cid)
-	npcHandler:onCreatureDisappear(cid)
-end
-function onCreatureSay(cid, type, msg)
-	npcHandler:onCreatureSay(cid, type, msg)
-end
-function onThink()
-	npcHandler:onThink()
-end
-function creatureSayCallback(cid, type, msg)
-	if(not(npcHandler:isFocused(cid))) then
+
+function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
+function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
+function onThink() npcHandler:onThink() end
+
+local function creatureSayCallback(cid, type, msg)
+	local Questlinestorage = 1015 --Questlinestorage
+	local player = Player(cid)
+	if not npcHandler:isFocused(cid) then
 		return false
-	end
-	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
-	
-	if(msgcontains(msg, "passage")) then
-		if(getPlayerStorageValue(cid, 1015) >= 24) then
+	elseif msgcontains(msg, "trip") then
+		if player:getStorageValue(Questlinestorage) >= 24 then
 			npcHandler:say("You want trip to Izzle of Zztrife?", cid)
-			talkState[talkUser] = 1
+			npcHandler.topic[cid] = 1
 		end
-	elseif(msgcontains(msg, "yes")) then
-		if(talkState[talkUser] == 1) then
+	elseif msgcontains(msg, "yes") then
+		if npcHandler.topic[cid] == 1 then
 			npcHandler:say("It'zz your doom you travel to.", cid)
-			doTeleportThing(cid, {x = 33103, y = 31055, z = 7})
-			doSendMagicEffect({x = 33103, y = 31055, z = 7}, CONST_ME_TELEPORT)
-			talkState[talkUser] = 0
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			player:teleportTo({x = 33114, y = 31060, z = 7})
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			npcHandler.topic[cid] = 0
 		end
+	elseif msgcontains(msg, "no") then
+		if npcHandler.topic[cid] == 1 then
+			npcHandler:say("Zzoftzzkinzz zzo full of fear.", cid)
+			npcHandler.topic[cid] = 0
+		end
+	elseif msgcontains(msg, 'hurry') or msgcontains(msg, 'job')  then
+		npcHandler:say('Me zzimple ferryman. I arrange {trip} to Izzle of Zztrife.', cid)
+		npcHandler.topic[cid] = 0
 	end
 	return true
 end
