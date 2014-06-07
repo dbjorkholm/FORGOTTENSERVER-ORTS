c280faeed7c5e4e342c6cf614fe179a68fecc3fc
pitis91 <pitis91@users.noreply.github.com>
2014-06-08 00:32:16 +0200

    Update concentrated_demonic_blood.lua
    now the magic effect is working same as real tibia, inside of dead monster, in your backpack in the floor, depot...
    second attempt ¬¬

--- data/actions/scripts/other/concentrated_demonic_blood.lua
+++ data/actions/scripts/other/concentrated_demonic_blood.lua
@@ -1,6 +1,7 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local item = Item(item.uid)
-	fromPosition:sendMagicEffect(CONST_ME_DRAWBLOOD)
+	local itemPosition = item:getPosition()
+	itemPosition:sendMagicEffect(CONST_ME_DRAWBLOOD)
 	item:transform(math.random(7588, 7589))
 	return true
 end
