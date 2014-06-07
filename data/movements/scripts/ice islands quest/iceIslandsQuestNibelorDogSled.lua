cf8710b80c25452c49cc01a8cac990877add0900
Royalot <zapf-benny@web.de>
2014-06-07 20:33:55 +0200

    nibelor dogs sled revised
    {x = 32303, y = 31081, z = 7} uid="12027"
    {x = 32327, y = 31045, z = 7} uid="12028"
    
    
    Former-commit-id: e93f7a044faebaf71fbb80717d46308611b4a69d

--- data/movements/scripts/ice islands quest/iceIslandsQuestNibelorDogSled.lua
+++ data/movements/scripts/ice islands quest/iceIslandsQuestNibelorDogSled.lua
@@ -1,6 +1,8 @@
 local config = {
 	[12025] = {x = 32407, y = 31067, z = 7},
-	[12026] = {x = 32365, y = 31059, z = 7}
+	[12026] = {x = 32365, y = 31059, z = 7},
+	[12027] = {x = 32329, y = 31045, z = 7},
+	[12028] = {x = 32301, y = 31080, z = 7}
 }
 
 function onStepIn(cid, item, position, lastPosition)
@@ -12,14 +14,16 @@ function onStepIn(cid, item, position, lastPosition)
 	if not sled then
 		return true
 	end
-	if player:getItemCount(2671) >= 1 then --if ham then
-		player:removeItem(2671, 1)
-		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-		player:teleportTo(sled)
-		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-	else
-		player:sendTextMessage(MESSAGE_STATUS_SMALL, "You don't have Ham for the dogs.")
-		player:getPosition():sendMagicEffect(CONST_ME_POFF)
+	-- Nibelor 5: Cure the Dogs needed or Barbarian Test Quest needed
+	if ((item.uid == 12025 or item.uid == 12026) and player:getStorageValue(12030) == 8) or ((item.uid == 12027 or item.uid == 12028) and player:getStorageValue(12024) == 3) then 
+		if player:getItemCount(2671) >= 1 then --if ham then
+			player:removeItem(2671, 1)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			player:teleportTo(sled)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		else
+			player:getPosition():sendMagicEffect(CONST_ME_POFF)
+		end
 	end
 	return true
 end
