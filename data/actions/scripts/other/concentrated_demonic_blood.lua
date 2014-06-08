85162f4519adc2435fa4285270b816daf92de230
pitis91 <pitis91@users.noreply.github.com>
2014-06-08 17:57:15 +0200

    Update concentrated_demonic_blood.lua
    Former-commit-id: 110b3debda361a7810423a1dd22884deddf3c454

--- data/actions/scripts/other/concentrated_demonic_blood.lua
+++ data/actions/scripts/other/concentrated_demonic_blood.lua
@@ -1,7 +1,6 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local item = Item(item.uid)
-	local itemPosition = item:getPosition()
-	itemPosition:sendMagicEffect(CONST_ME_DRAWBLOOD)
+	item:getPosition():sendMagicEffect(CONST_ME_DRAWBLOOD)
 	item:transform(math.random(7588, 7589))
 	return true
 end
