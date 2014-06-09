705a5234f69696659225e4736bb82745c4db574a
Daniel Björkholm <danielbjorkholm@live.se>
2014-06-09 23:58:18 +0200

    Pits of Inferno Maze, #671
    Former-commit-id: 92554cbe10b0a40b4cc7d56d0b7efda61858ae6d

--- data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestTileTeleports.lua
+++ data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestTileTeleports.lua
@@ -1,89 +1,42 @@
-function onStepIn(cid, item, pos)
+local config = {
+	[28810] = Position(32838, 32304, 9),
+	[28811] = Position(32839, 32320, 9),
+	[28812] = Position(32844, 32310, 9),
+	[28813] = Position(32847, 32307, 9),
+	[28814] = Position(32856, 32306, 9),
+	[28815] = Position(32827, 32308, 9),
+	[28816] = Position(32840, 32317, 9),
+	[28817] = Position(32855, 32296, 9),
+	[28818] = Position(32857, 32307, 9),
+	[28819] = Position(32856, 32289, 9),
+	[28820] = Position(32843, 32313, 9),
+	[28821] = Position(32861, 32320, 9),
+	[28822] = Position(32841, 32323, 9),
+	[28823] = Position(32847, 32287, 9),
+	[28824] = Position(32854, 32323, 9),
+	[28825] = Position(32855, 32304, 9),
+	[28826] = Position(32841, 32323, 9),
+	[28827] = Position(32861, 32317, 9),
+	[28828] = Position(32827, 32314, 9),
+	[28829] = Position(32858, 32296, 9),
+	[28830] = Position(32861, 32301, 9),
+	[28831] = Position(32855, 32321, 9),
+	[28832] = Position(32855, 32320, 9),
+	[28833] = Position(32855, 32318, 9),
+	[28834] = Position(32855, 32319, 9)
+}
 
-local pole1 = {x=32827, y=32314, z=9}
-local pole2 = {x=32841, y=32323, z=9}
-local pole3 = {x=32855, y=32296, z=9}
-local pole4 = {x=32840, y=32317, z=9}
-local pole5 = {x=32854, y=32323, z=9}
-local pole6 = {x=32858, y=32296, z=9}
-local pole7 = {x=32856, y=32289, z=9}
-local pole8 = {x=32847, y=32287, z=9}
-local pole9 = {x=32861, y=32301, z=9}
-local pole10 = {x=32839, y=32321, z=9}
-local pole11 = {x=32827, y=32308, z=9}
-local pole12 = {x=32858, y=32304, z=9}
-local pole13 = {x=32851, y=32323, z=9}
-local pole14 = {x=32847, y=32287, z=9}
-local pole15 = {x=32855, y=32318, z=9}
-local pole16 = {x=32839, y=32321, z=9}
-local pole17 = {x=32861, y=32301, z=9}
-
-    if item.actionid == 28810 then
-		doTeleportThing(cid,pole1)
-        else
-		 if item.actionid == 28811 then
-		doTeleportThing(cid,pole2)
-		else
-		 if item.actionid == 28812 then
-		doTeleportThing(cid,pole3)
-		else
-		 if item.actionid == 28813 then
-		doTeleportThing(cid,pole4)
-		else
-		 if item.actionid == 28814 then
-		doTeleportThing(cid,pole5)
-		else
-		 if item.actionid == 28815 then
-		doTeleportThing(cid,pole6)
-		else
-		 if item.actionid == 28816 then
-		doTeleportThing(cid,pole7)
-		else
-		 if item.actionid == 28817 then
-		doTeleportThing(cid,pole8)
-		else
-		 if item.actionid == 28818 then
-		doTeleportThing(cid,pole9)
-		else
-		 if item.actionid == 28819 then
-		doTeleportThing(cid,pole10)
-		else
-		 if item.actionid == 28820 then
-		doTeleportThing(cid,pole11)
-		else
-		 if item.actionid == 28821 then
-		doTeleportThing(cid,pole12)
-		else
-		 if item.actionid == 28822 then
-		doTeleportThing(cid,pole13)
-		else
-		 if item.actionid == 28823 then
-		doTeleportThing(cid,pole14)
-		else
-		 if item.actionid == 28824 then
-		doTeleportThing(cid,pole15)
-		else
-		 if item.actionid == 28825 then
-		doTeleportThing(cid,pole16)
-		else
-		 if item.actionid == 28826 then
-		doTeleportThing(cid,pole17)
-		end
-end  
+function onStepIn(cid, item, pos, fromPosition)
+	local player = Player(cid)
+	if not player then
+		return true
+	end
+	
+	local targetTile = config[item.actionid]
+	if not targetTile then
+		return true
+	end
+	
+	player:teleportTo(targetTile)
+	return true
 end
-end
-end
-end
-end
-end
-end
-end
-end
-end
-end
-end
-end
-end
-end
-end
-
