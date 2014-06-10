cb274768b172bc7141718fdba3ba634ab149e2f1
Royalot <zapf-benny@web.de>
2014-06-10 19:03:24 +0200

    fixed mortalcombat tirecz
    thanks to ninja for help :)

--- data/creaturescripts/scripts/the new frontier quest/theNewFrontierQuestTirecz.lua
+++ data/creaturescripts/scripts/the new frontier quest/theNewFrontierQuestTirecz.lua
@@ -3,27 +3,20 @@ function onKill(cid, target)
 	local monster = Monster(target)
 	if monster then
 		if string.lower(monster:getName()) == "tirecz" then
-			local deathpos = monster:getPosition()
-			deathpos:sendMagicEffect(CONST_ME_TELEPORT)
-			local portal = Game.createItem(1387, 1, deathpos)
-			portal:setActionId(12133)
-			addEvent(function(target) local portaltile = Tile(deathpos) if portaltile:getItemById(1387) then portaltile:getItemById(1387):remove() deathpos:sendMagicEffect(CONST_ME_POFF) Game.setStorageValue(3157, -1) end end, 1 * 60 * 1000, cid) -- 60 sec
-			player:say("You have won! As new champion take the ancient armor as reward before you leave.", TALKTYPE_ORANGE_1)
+			Game.setStorageValue(12139, -1)
 			clearArena({x = 33064, y = 31030, z = 3}, {x = 33085, y = 31050, z = 3})
-			--getplayers in the Area does not work :(
-			--local WestX = 33065 -- WestX of Arena
-			--local EastX = 33086 -- EastX of Arena
-			--local NorthY = 31029 -- NorthY of Arena
-			--local SouthY = 31050 -- SouthY of Arena
-			--for allx = WestX, EastX do
-            --    for ally = NorthY, SouthY do
-			--		local getPlayers = Tile(Position({x = allx, y = ally, 3})):getTopCreature()
-			--		if not getPlayers == nil then
-			--			getPlayers:teleportTo({x = 33064, y = 31028, z = 7})
-			--			getPlayers:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-			--		end
-			--	end
-			--end
+			local spectators = Game.getSpectators(Position({x = 33074, y = 31039, z = 3}), false, true, 10, 10, 10, 10)
+			if spectators ~= nil then
+				for _, spectator in ipairs(spectators) do
+					spectator:teleportTo({x = 33064, y = 31028, z = 7})
+					spectator:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+					spectator:say("You have won! As new champion take the ancient armor as reward before you leave.", TALKTYPE_ORANGE_1)
+					if spectator:getStorageValue(1015) == 25 then
+						spectator:setStorageValue(12139, 2) --Questlog, The New Frontier Quest "Mission 09: Mortal Combat"
+						spectator:setStorageValue(1015, 26)
+					end
+				end
+			end
 		end
 	end
 	return true
