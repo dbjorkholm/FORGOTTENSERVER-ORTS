1aba3f5de03899ba4439a68b2de74a69ccf21f6d
Skillert <peter.mollberg@gmail.com>
2014-06-11 11:53:27 +0200

    Added action to tool saw for wooden ties.
    Former-commit-id: d8a25a13384939a30e80cd5b4aaf2fb8440746bb

---
+++ data/actions/scripts/tools/saw.lua
@@ -0,0 +1,8 @@
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	local targetItem = Item(itemEx.uid)
+	if itemEx.itemid == 5901 then
+		targetItem:transform(10033)
+		toPosition:sendMagicEffect(CONST_ME_POFF) --Not sure if there's any magic effect when you saw?
+		return true
+	end
+end
