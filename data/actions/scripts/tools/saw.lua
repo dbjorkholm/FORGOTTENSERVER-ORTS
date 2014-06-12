434c8a46ad83f42e86f07e2ef606da125280732e
Skillert <peter.mollberg@gmail.com>
2014-06-12 12:11:30 +0200

    Minor correction to saw.lua
    Former-commit-id: 210a4e11bbd26a0a134cd1f01c0753b64211b2ac

--- data/actions/scripts/tools/saw.lua
+++ data/actions/scripts/tools/saw.lua
@@ -2,7 +2,9 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local targetItem = Item(itemEx.uid)
 	if itemEx.itemid == 5901 then
 		targetItem:transform(10033)
-		toPosition:sendMagicEffect(CONST_ME_POFF) --Not sure if there's any magic effect when you saw?
-		return true
+		toPosition:sendMagicEffect(CONST_ME_POFF) --Not sure if there's any magic effect when you use saw?
+	else
+		return false
 	end
+	return true
 end
