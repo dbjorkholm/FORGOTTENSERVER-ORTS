68c3941fabfe09292f3bb564f5952e0e5785be0d
Royalot <zapf-benny@web.de>
2014-06-13 22:05:50 +0200

    fix typo
    Former-commit-id: cadb40412427a663376435f094f4b015edef587d

--- data/actions/scripts/the ape city/Mission7DestroyingCasksWithCrowbar.lua
+++ data/actions/scripts/the ape city/Mission7DestroyingCasksWithCrowbar.lua
@@ -2,7 +2,7 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
 	if item.itemid == 3551 and item.actionid == 12127 then --door
-		if player:getStorageValue (Storage.Storage.TheApeCity.Questline) >= 16 then
+		if player:getStorageValue (Storage.TheApeCity.Questline) >= 16 then
 			player:teleportTo(toPosition, true)
 			Item(item.uid):transform(item.itemid + 1)
 		else
