ab01ce28e87b24ad4619d137ad440ac2acf71d17
Royalot <zapf-benny@web.de>
2014-06-16 22:09:19 +0200

    some bugfixes

--- data/actions/scripts/in service of yalahar quest/inServiceOfYalaharQuestMatrixReward.lua
+++ data/actions/scripts/in service of yalahar quest/inServiceOfYalaharQuestMatrixReward.lua
@@ -1,16 +1,16 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
 	if(item.uid == 3084) then
-		if(player:getStorageValue(678) < 1) then
-			player:setStorageValue(678, 1)
+		if(player:getStorageValue(Storage.InServiceofYalahar.MatrixReward) < 1) then
+			player:setStorageValue(Storage.InServiceofYalahar.MatrixReward, 1)
 			player:addItem(9744, 1)
 			player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a matrix.")
 		else
 			player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
 		end
 	elseif(item.uid == 3085) then
-		if(player:getStorageValue(678) < 1) then
-			player:setStorageValue(678, 1)
+		if(player:getStorageValue(Storage.InServiceofYalahar.MatrixReward) < 1) then
+			player:setStorageValue(Storage.InServiceofYalahar.MatrixReward, 1)
 			player:addItem(9743, 1)
 			player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a matrix.")
 		else
