74791a859609c0eb781a18c0f675ddd28dcc6fa6
Skillert <peter.mollberg@gmail.com>
2014-06-18 09:54:37 +0200

    The Outlaw Camp quest - checked and converted oven.lua to meta

--- data/actions/scripts/the outlaw camp quest/oven.lua
+++ data/actions/scripts/the outlaw camp quest/oven.lua
@@ -1,40 +1,22 @@
--- Script by Viperthepala
 function onUse(cid, item, frompos, item2, topos)
 
-local oven1pos = {x=32623, y=32188, z=9, stackpos=1}
-local oven2pos = {x=32623, y=32189, z=9, stackpos=1}
+	local oven1 = Tile(Position({x = 32623, y = 32188, z = 9}))
+	local oven2 = Tile(Position({x=32623, y=32189, z=9}))
 
-local getoven1 = getThingfromPos(oven1pos)
-local getoven2 = getThingfromPos(oven2pos)
-
-
-    if item.uid == 3400 and 
-	item.itemid == 1945 and 
-	getoven1.itemid == 1787 then
-        doRemoveItem(getoven1.uid,1)
-		 doCreateItem(1787,1,oven2pos)
-        doTransformItem(item.uid,item.itemid+1)
-    elseif item.uid == 3400 and
-        item.itemid == 1945 and
-        getoven1.itemid == 1786 then
-        doRemoveItem(getoven1.uid,1)
-                 doCreateItem(1786,1,oven2pos)
-        doTransformItem(item.uid,item.itemid+1)
-    elseif item.uid == 3400 and 
-	item.itemid == 1946 and 
-	getoven2.itemid == 1787 then
-		doRemoveItem(getoven2.uid,1)
-        doCreateItem(1787,1,oven1pos)
-        doTransformItem(item.uid,item.itemid-1)
-    elseif item.uid == 3400 and
-        item.itemid == 1946 and
-        getoven2.itemid == 1786 then
-                doRemoveItem(getoven2.uid,1)
-        doCreateItem(1786,1,oven1pos)
-        doTransformItem(item.uid,item.itemid-1)
-    else
-        doPlayerSendCancel(cid,"Sorry, not possible.")
-    end
-    
-return TRUE
+	if item.itemid == 1945 and oven1:getItemById(1787) then
+		oven1:getItemById(1787):remove()
+		Game.createItem(1787,1,oven2)
+	elseif item.itemid == 1945 and oven1:getItemById(1786) then
+		oven1:getItemById(1786):remove()
+		Game.createItem(1786,1,oven2)
+	elseif item.itemid == 1946 and oven2:getItemById(1787) then
+		oven2:getItemById(1787):remove()
+		Game.createItem(1787,1,oven1)
+	elseif item.itemid == 1946 and oven2:getItemById(1786) then
+		oven2:getItemById(1786):remove()
+		Game.createItem(1786,1,oven1)
+	else
+		Player(cid):sendTextMessage(MESSAGE_STATUS_SMALL, "Sorry, not possible.")
+	end
+	return true
 end
