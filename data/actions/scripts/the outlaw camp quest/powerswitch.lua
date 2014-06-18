9faa74b7ad5eccdb7d34a14143ecf90b6b2b8846
Skillert <peter.mollberg@gmail.com>
2014-06-18 10:30:46 +0200

    checked and converted powerswitch.lua to meta

--- data/actions/scripts/the outlaw camp quest/powerswitch.lua
+++ data/actions/scripts/the outlaw camp quest/powerswitch.lua
@@ -1,40 +1,20 @@
 -- Script by Viperthepala
 function onUse(cid, item, frompos, item2, topos)
 
-local event = 0
+	local power1 = Tile(Position({x = 32594, y = 32214, z = 9}))
+	local power2 = Tile(Position({x = 32613, y = 32220, z = 10}))
+	local wall = Tile(Position({x=32604, y=32216, z=9}))
 
-local powerpos = {x=32594, y=32214, z=9, stackpos=255}
-local power2pos = {x=32613, y=32220, z=10, stackpos=255}
-local wallpos = {x=32604, y=32216, z=9, stackpos=1}
-
-local getpower = getThingfromPos(powerpos)
-local getpower2 = getThingfromPos(power2pos)
-local getwall = getThingfromPos(wallpos)
-
-
-    if item.uid == 3401 and 
-	item.itemid == 1945 and 
-	getpower.itemid == 2166 and
-	getwall.itemid == 1026 then
-    doRemoveItem(getpower.uid,1)
-    doRemoveItem(getwall.uid,1)
-	doCreateItem(2166,1,power2pos)
-    doTransformItem(item.uid,item.itemid+1)
-	event = addEvent(doCreateItem, 600000, 1026, 1, wallpos)
-	doSendMagicEffect(power, 8)
-    elseif item.uid == 3401 and 
-	item.itemid == 1946 and 
-	getpower.itemid == 2166 and
-	getwall.itemid == 1026 then
-    doRemoveItem(getpower.uid,1)
-    doRemoveItem(getwall.uid,1)
-	doCreateItem(2166,1,power2pos)
-    doTransformItem(item.uid,item.itemid-1)
-	event = addEvent(doCreateItem, 600000, 1026, 1, wallpos)
-	doSendMagicEffect(power, 8)
-    else
-        doPlayerSendCancel(cid,"Sorry, not possible.")
-    end
-    
-return TRUE
+	if power1:getItemById(2166) and wall:getItemById(1026) then
+		power1:getItemById(2166):remove()
+		wall:getItemById(1026):remove()
+		Game.createItem(2166,1,power2)
+		--FIXME:
+		--event = addEvent(doCreateItem, 600000, 1026, 1, wallpos)
+		--doSendMagicEffect(power, 8)
+		power1:sendMagicEffect(CONST_ME_TELEPORT)
+	else
+		Player(cid):sendTextMessage(MESSAGE_STATUS_SMALL, "Sorry, not possible.")
+	end
+	return true
 end
