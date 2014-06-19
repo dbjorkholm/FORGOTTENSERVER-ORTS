41d208137334a46d8bf861edd79528a66b12a562
Skillert <peter.mollberg@gmail.com>
2014-06-19 08:49:41 +0200

    Added Position to be able to create a new item, cheers @ninjalulz

--- data/actions/scripts/the outlaw camp quest/oven.lua
+++ data/actions/scripts/the outlaw camp quest/oven.lua
@@ -2,19 +2,21 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 
 	local oven1 = Tile(Position({x = 32623, y = 32188, z = 9}))
 	local oven2 = Tile(Position({x=32623, y=32189, z=9}))
+	local oven1pos = oven1:getPosition()
+	local oven2pos = oven2:getPosition()
 
 	if item.itemid == 1945 and oven1:getItemById(1787) then
 		oven1:getItemById(1787):remove()
-		Game.createItem(1787,1,oven2)
+		Game.createItem(1787,1,oven2pos)
 	elseif item.itemid == 1945 and oven1:getItemById(1786) then
 		oven1:getItemById(1786):remove()
-		Game.createItem(1786,1,oven2)
+		Game.createItem(1786,1,oven2pos)
 	elseif item.itemid == 1946 and oven2:getItemById(1787) then
 		oven2:getItemById(1787):remove()
-		Game.createItem(1787,1,oven1)
+		Game.createItem(1787,1,oven1pos)
 	elseif item.itemid == 1946 and oven2:getItemById(1786) then
 		oven2:getItemById(1786):remove()
-		Game.createItem(1786,1,oven1)
+		Game.createItem(1786,1,oven1pos)
 	else
 		Player(cid):sendTextMessage(MESSAGE_STATUS_SMALL, "Sorry, not possible.")
 	end
--- data/actions/scripts/the outlaw camp quest/powerswitch.lua
+++ data/actions/scripts/the outlaw camp quest/powerswitch.lua
@@ -3,11 +3,12 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local power1 = Tile(Position({x = 32594, y = 32214, z = 9}))
 	local power2 = Tile(Position({x = 32613, y = 32220, z = 10}))
 	local wall = Tile(Position({x=32604, y=32216, z=9}))
+	local power2pos = power2:getPosition()
 
 	if item.itemid == 1945 and power1:getItemById(2166) and wall:getItemById(1026) then
 		power1:getItemById(2166):remove()
 		wall:getItemById(1026):remove()
-		Game.createItem(2166,1,power2)
+		Game.createItem(2166,1,power2pos)
 		--FIXME:
 		--event = addEvent(doCreateItem, 600000, 1026, 1, wallpos)
 		--doSendMagicEffect(power, 8)
