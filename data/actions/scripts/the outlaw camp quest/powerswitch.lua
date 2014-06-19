3d245d5e3baf5e026ad5bba11192b501fb81577d
Skillert <peter.mollberg@gmail.com>
2014-06-19 19:27:13 +0200

    minor Cheers @ninjalulz
    Former-commit-id: 7053e6f50d85d8e678e48f211013a2052d6aa2fc

--- data/actions/scripts/the outlaw camp quest/powerswitch.lua
+++ data/actions/scripts/the outlaw camp quest/powerswitch.lua
@@ -1,9 +1,9 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 
-	local power1 = Tile(Position({x = 32594, y = 32214, z = 9}))
 	local wall = Tile(Position({x=32604, y=32216, z=9}))
 	local power2pos = Position({x = 32613, y = 32220, z = 10})
-	local power2 = power2pos:getTile()
+	local power1pos = Position({x = 32594, y = 32214, z = 9})
+	local power1 = power1pos:getTile()
 
 	if item.itemid == 1945 and power1:getItemById(2166) and wall:getItemById(1026) then
 		power1:getItemById(2166):remove()
@@ -12,12 +12,8 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		--FIXME:
 		--event = addEvent(doCreateItem, 600000, 1026, 1, wallpos)
 		--doSendMagicEffect(power, 8)
-		power1:sendMagicEffect(CONST_ME_TELEPORT)
-	else
-		Player(cid):sendTextMessage(MESSAGE_STATUS_SMALL, "Sorry, not possible.")
+		power1pos:sendMagicEffect(CONST_ME_TELEPORT)
 	end
-	if item.itemid == 1946 then
-		Item(item.uid):transform(1945)
-	end
+		Item(item.uid):transform(item.itemid == 1945 and 1946 or 1945)
 	return true
 end
