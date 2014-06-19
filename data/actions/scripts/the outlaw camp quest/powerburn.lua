22948176c37e1734f94bdf53e2f3b90802ca4c98
Skillert <peter.mollberg@gmail.com>
2014-06-19 18:38:33 +0200

    minor - changed so ring transforms into fire field - removed magic effect and changed the other one to Game.createItem to   create a fire field. - changed transform for lever cheers @ninjalulz
    Former-commit-id: e994cb29c04792dd66e7b35afbcb7ec8ddb637c7 [formerly 90c51ec98e9ca53d21e2d63c8e029bebd546adbf]
    Former-commit-id: 14e7f229e288e32b277d16506bb507889b19c5f6

--- data/actions/scripts/the outlaw camp quest/powerburn.lua
+++ data/actions/scripts/the outlaw camp quest/powerburn.lua
@@ -4,20 +4,16 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local barrel = Tile(Position({x = 32614, y = 32209, z = 10}))
 	local wall = Tile(Position({x = 32614, y = 32205, z = 10}))
 	local stone = Tile(Position({x = 32614, y = 32206, z = 10}))
-	local burn1 = Tile(Position({x = 32613, y = 32220, z = 10}))
-	local burn2 = Tile(Position({x = 32615, y = 32221, z = 10}))
+	local burn = Position({x = 32615, y = 32221, z = 10})
 
 	if item.itemid == 1945 and power1:getItemById(2166) and wall:getItemById(1025) and stone:getItemById(1304) and barrel:getItemById(1774) then
-		power1:getItemById(2166):remove()
+		power1:getItemById(2166):transform(1487)
 		wall:getItemById(1025):remove()
 		stone:getItemById(1304):transform(1025)
-		burn1:sendMagicEffect(CONST_ME_FIREAREA)
-		burn2:sendMagicEffect(CONST_ME_FIREAREA)
+		Game.createItem(1487, 1, burn)
 	else
 		Player(cid):sendTextMessage(MESSAGE_STATUS_SMALL, "Sorry, not possible.")
 	end
-	if item.itemid == 1946 then
-		Item(item.uid):transform(1945)
-	end
+	Item(item.uid):transform(item.itemid == 1945 and 1946 or 1945)
 	return true
 end
