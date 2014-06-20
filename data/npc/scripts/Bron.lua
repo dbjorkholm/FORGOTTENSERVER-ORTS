13c9e57d1136a388f84322c3d900ccef193e8183
EPuncker <gm.perflex@gmail.com>
2014-06-20 16:36:51 -0300

    Tried to fix Zoltan as the other pull request got closed, feel free to suggest changes Changes to Bron by @ninjalulz Added scripts to Caramellia
    Former-commit-id: 1784f9c71a8aa748b8148fea049e7a15631f1721 [formerly bcc5f782179c744378e28b64e3c6d5135f48408f]
    Former-commit-id: 9bb788b69d1018d4acd4655d9759a6b9d3f47e2c

--- data/npc/scripts/Bron.lua
+++ data/npc/scripts/Bron.lua
@@ -1,8 +1,7 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
-local talkState = {}
- 
+
 function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
 function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
@@ -22,7 +21,6 @@ function creatureSayCallback(cid, type, msg)
 	if(not npcHandler:isFocused(cid)) then
 		return false
 	end
-	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
 
 	local player = Player(cid)
 	if(msgcontains(msg, "recruitment")) then
@@ -32,21 +30,21 @@ function creatureSayCallback(cid, type, msg)
 								"Convincing Ajax that it is not always necessary to use brute force... this would be such an achievement. Definitely a hard task though. ...",
 								"Listen, I simply have to ask, maybe a stranger can influence him better than I can. Would you help me with my brother?"
 							}, cid)
-			talkState[talkUser] = 1
+			npcHandler.topic[cid] = 1
 		end
 	elseif(msgcontains(msg, "fist")) then
 		if player:getStorageValue(1000) == 3 then
 			npcHandler:say("Oh! He really said that? I am so proud of you, " .. player:getName() .. ". These are really good news. Everything would be great... if only there wasn't this person near my house.", cid)
-			talkState[talkUser] = 3
+			npcHandler.topic[cid] = 3
 		end
 	elseif(msgcontains(msg, "person")) then
-		if(talkState[talkUser] == 3) then
+		if(npcHandler.topic[cid] == 3) then
 			npcHandler:say(
 							{
 								"This... person... makes me want to... say something bad... must... control myself. <sweats> I really don't know what to do anymore....",
 								"I wonder if Ajax has an idea. Could you ask him about Gelagos?"
 							}, cid)
-			talkState[talkUser] = 4
+			npcHandler.topic[cid] = 4
 		end
 	elseif(msgcontains(msg, "fighting spirit")) then
 		if player:getStorageValue(1000) == 5 then
@@ -54,26 +52,26 @@ function creatureSayCallback(cid, type, msg)
 				player:removeItem(5884, 1)
 				player:setStorageValue(1000, 6)
 				npcHandler:say("Fighting spirit? What am I supposed to do with this fi... - oh! I feel strange... ME MIGHTY! ME WILL CHASE OFF ANNOYING KIDS!GROOOAARR!! RRRRRRRRRRRRAAAAAAAGE!!", cid)
-				talkState[talkUser] = 0
+				npcHandler.topic[cid] = 0
 			end	
 		end
 	elseif(msgcontains(msg, "cloth")) then
 		if player:getStorageValue(1000) == 7 then
 			npcHandler:say("Have you really managed to fulfil the task and brought me 50 pieces of red cloth and 50 pieces of green cloth?", cid)
-			talkState[talkUser] = 8
+			npcHandler.topic[cid] = 8
 		end	
 	elseif(msgcontains(msg, "silk")) then
 		if player:getStorageValue(1000) == 8 then
 			npcHandler:say("Oh, did you bring 10 rolls of spider silk yarn for me?", cid)
-			talkState[talkUser] = 9
+			npcHandler.topic[cid] = 9
 		end
 	elseif(msgcontains(msg, "sweat")) then
 		if player:getStorageValue(1000) == 9 then
 			npcHandler:say("Were you able to get hold of a flask with pure warrior's sweat?", cid)
-			talkState[talkUser] = 10
+			npcHandler.topic[cid] = 10
 		end	
 	elseif(msgcontains(msg, "yes")) then
-		if(talkState[talkUser] == 1) then
+		if(npcHandler.topic[cid] == 1) then
 			npcHandler:say(
 							{
 								"Really! That is such an incredibly nice offer! I already have a plan. You have to teach him that sometimes words are stronger than fists. ...",
@@ -83,23 +81,23 @@ function creatureSayCallback(cid, type, msg)
 								"This way he might learn that violence doesn't always help, but that a friendly word might just do the trick. ...",
 								"Have you understood everything I told you and are really willing to take this risk?"
 							}, cid)
-			talkState[talkUser] = 2
-		elseif(talkState[talkUser] == 2) then
+			npcHandler.topic[cid] = 2
+		elseif(npcHandler.topic[cid] == 2) then
 			npcHandler:say("You are indeed not only well educated, but also very courageous. I wish you good luck, you are my last hope.", cid)
 			player:setStorageValue(1000, 1)
-			talkState[talkUser] = 0
-		elseif(talkState[talkUser] == 4) then
+			npcHandler.topic[cid] = 0
+		elseif(npcHandler.topic[cid] == 4) then
 			npcHandler:say("Again, I have to thank you for your selfless offer to help me. I hope that Ajax can come up with something, now that he has experienced the power of words.", cid)
 			player:setStorageValue(1000, 4)
-			talkState[talkUser] = 0
+			npcHandler.topic[cid] = 0
 		elseif(getPlayerStorageValue(cid, 1000) == 6) then
 			npcHandler:say(
 							{
 								"I'm impressed... I am sure this was Ajax' idea. I would love to give him a present, but if I leave my hut to gather ingredients, hewill surely notice. ...",
 								"Would you maybe help me again, one last time, my friend? I assure you that your efforts will not be in vain."
 							}, cid)
-			talkState[talkUser] = 6
-		elseif(talkState[talkUser] == 6) then
+			npcHandler.topic[cid] = 6
+		elseif(npcHandler.topic[cid] == 6) then
 			npcHandler:say(
 							{
 								"Great! You see, I really would love to sew a nice shirt for him. I just need a few things for that, so please listen closely: ...",
@@ -108,39 +106,39 @@ function creatureSayCallback(cid, type, msg)
 								"The only remaining thing needed would be a bottle of warrior's sweat to spray it over the shirt... he just loves this smell. ...",
 								"Have you understood everything I told you and are willing to handle this task?"
 							}, cid)
-			talkState[talkUser] = 7
-		elseif(talkState[talkUser] == 7) then
+			npcHandler.topic[cid] = 7
+		elseif(npcHandler.topic[cid] == 7) then
 			npcHandler:say("Thank you, my friend! Come back to me once you have collected 50 pieces of red cloth and 50 pieces of green cloth.", cid)
 			player:setStorageValue(1000, 7)
-			talkState[talkUser] = 0
-		elseif(talkState[talkUser] == 8) then
+			npcHandler.topic[cid] = 0
+		elseif(npcHandler.topic[cid] == 8) then
 			if player:getItemCount(5910) >= 50 and player:getItemCount(5911) >= 50 then
 				npcHandler:say("Terrific! I will start to trim it while you gather 10 rolls of spider silk. I'm sure that Ajax will love it.", cid)
 				player:removeItem(5910, 50)
 				player:removeItem(5911, 50)
 				player:setStorageValue(1000, 8)
-				talkState[talkUser] = 0	
+				npcHandler.topic[cid] = 0	
 			end
-		elseif(talkState[talkUser] == 9) then
+		elseif(npcHandler.topic[cid] == 9) then
 			if player:getItemCount(5886) >= 10 then
 				npcHandler:say("I'm impressed! You really managed to get spider silk yarn for me! I will immediately start to work on this shirt. Please don't forget to bring me warrior's sweat!", cid)
 				player:removeItem(5886, 10)
 				player:setStorageValue(1000, 9)
-				talkState[talkUser] = 0	
+				npcHandler.topic[cid] = 0	
 			end
-		elseif(talkState[talkUser] == 9) then
+		elseif(npcHandler.topic[cid] == 9) then
 			if player:getItemCount(5885) >= 1 then
 				npcHandler:say("Good work, " .. player:getName() .. "! Now I can finally finish this present for Ajax. Because you were such a great help, I have also a present for you. Will you accept it?", cid)
 				player:removeItem(5885, 1)
 				player:setStorageValue(1000, 10)
-				talkState[talkUser] = 0
+				npcHandler.topic[cid] = 0
 			end
 		elseif player:getStorageValue(1000) == 10 then
 			npcHandler:say("I have kept this traditional barbarian wig safe for many years now. It is now yours! I hope you will wear it proudly, friend.", cid)
 			player:setStorageValue(1000, 11)
 			player:addOutfitAddon(147, 1)
 			player:addOutfitAddon(143, 1)
-			talkState[talkUser] = 0	
+			npcHandler.topic[cid] = 0	
 		end
 	end
 	return true
