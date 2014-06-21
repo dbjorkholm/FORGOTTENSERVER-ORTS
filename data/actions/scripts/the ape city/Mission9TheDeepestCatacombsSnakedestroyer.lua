14963efdcbf732089cec6f124a8717280aaee7eb
Royalot <zapf-benny@web.de>
2014-06-21 02:46:34 +0200

    fix #746
    Former-commit-id: 1723b3cc7d0dae0e210a8ba255ae1044cb3d20cc

--- data/actions/scripts/the ape city/Mission9TheDeepestCatacombsSnakedestroyer.lua
+++ data/actions/scripts/the ape city/Mission9TheDeepestCatacombsSnakedestroyer.lua
@@ -1,12 +1,12 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 local targetItem = Item(itemEx.uid)
 local player = Player(cid)
-	if itemEx.itemid == 4861 and itemEx.actionid == 12130 and player:getStorageValue(TheApeCity.Mission09) == 1 then -- destroy cobra statue
+	if itemEx.itemid == 4861 and itemEx.actionid == 12130 and player:getStorageValue(Storage.TheApeCity.Mission09) == 1 then -- destroy cobra statue
 		targetItem:transform(4862)
         targetItem:decay()
        	toPosition:sendMagicEffect(CONST_ME_POFF)
-		player:setStorageValue(TheApeCity.Mission09, 2) -- The Ape City Questlog - Mission 9: The Deepest Catacombs
-		player:setStorageValue(TheApeCity.Questline, 22)
+		player:setStorageValue(Storage.TheApeCity.Mission09, 2) -- The Ape City Questlog - Mission 9: The Deepest Catacombs
+		player:setStorageValue(Storage.TheApeCity.Questline, 22)
 	elseif itemEx.itemid == 4996 then -- destroy large amphora
 		targetItem:transform(4997)
         targetItem:decay()
