077cd0db74e9643975920d38cbfd85aa4642d4f1
Royalot <zapf-benny@web.de>
2014-06-22 21:34:52 +0200

    sort storagetables in values
    Former-commit-id: 77056f50744cd6d7cb0dfc28af2ceff721798f88

--- data/lib/StorageTables.lua
+++ data/lib/StorageTables.lua
@@ -1,6 +1,6 @@
 -- STORAGES --
 -- (HIGHLY RECOMMENDED -> ONE mission per STORAGE) --
--- Sort it alphabetical -
+-- Sort it in Values -
 Storage = {
 	OutfitQuest = {
 		DefaultStart = 12010, -- Is needed for each Quest to start the log
@@ -37,15 +37,81 @@ Storage = {
 		-- Norseman-outfit Quest
 		NorsemanAddon = 12065,
 		
-		-- Pirate-outfit Quest
-		PirateSabreAddon = 50002,
 
 		-- Warrior-outfit Quest
 		WarriorShoulderAddon = 12067,
 		WarriorSwordAddon = 12068,
 		
 		-- Wizard-outfit Quest
-		WizardAddon = 12066
+		WizardAddon = 12066,
+
+		-- Pirate-outfit Quest
+		PirateSabreAddon = 50002
+	},
+	TheAncientTombs = {
+		DefaultStart = 12100,
+		Omruc = 12101,
+		Thalas = 12102,
+		Diphtrah = 12103,
+		Mahrdis = 12104,
+		Vashresamun = 12105,
+		Morguthis = 12106,
+		Rahemos = 12107,
+		Ashmunrah = 12108,
+		VashresamunInstruments = 12109
+	},
+	TheApeCity = {
+		Questline = 12120, -- Storage through the Quest
+		Mission01 = 12121,
+		Mission02 = 12122,
+		Mission03 = 12123,
+		Mission04 = 12124,
+		Mission05 = 12125,
+		Mission06 = 12126,
+		Mission07 = 12127,
+		Mission08 = 12128,
+		Mission09 = 12129
+	},
+	TheNewFrontier = {
+		Questline = 12130, -- Storage through the Quest
+		Mission01 = 12131,
+		Mission02 = 12132,
+		Mission03 = 12133,
+		Mission04 = 12134,
+		Mission05 = 12135,
+		Mission06 = 12136,
+		Mission07 = 12137,
+		Mission08 = 12138,
+		Mission09 = 12139,
+		Mission10 = 12140,
+		TomeofKnowledge = 12141,
+		Beaver1 = 12142,
+		Beaver2 = 12143,
+		Beaver3 = 12144,
+		BribeKing = 12145,
+		BribeLeeland = 12146,
+		BribeAngus = 12147,
+		BribeWydrin = 12148,
+		BribeTelas = 12149,
+		BribeHumgolf = 12150
+	},
+	TheInquisition = {
+		Questline = 12160, -- Storage through the Quest
+		Mission01 = 12161,
+		Mission02 = 12162,
+		Mission03 = 12163,
+		Mission04 = 12164,
+		Mission05 = 12165,
+		Mission06 = 12166,
+		Mission07 = 12167,
+		GrofGuard = 12170,
+		KulagGuard = 12171,
+		MilesGuard = 12172,
+		TimGuard = 12173,
+		WalterGuard = 12174,
+		StorkusVampiredust = 12175,
+		EnterTeleport = 12176,
+		Reward = 12177
 	},
 	BarbarianTest = {
 		Questline = 12190, -- Storage through the Quest
@@ -54,6 +120,33 @@ Storage = {
 		Mission03 = 12193,
 		MeadTotalSips = 12194,
 		MeadSuccessSips = 12195
+	},
+	TheIceIslands = {
+		Questline = 12200, -- Storage through the Quest
+		Mission01 = 12201, -- Befriending the Musher
+		Mission02 = 12202, -- Nibelor 1: Breaking the Ice
+		Mission03 = 12203, -- Nibelor 2: Ecological Terrorism
+		Mission04 = 12204, -- Nibelor 3: Artful Sabotage
+		Mission05 = 12205, -- Nibelor 4: Berserk Brewery
+		Mission06 = 12206, -- Nibelor 5: Cure the Dogs
+		Mission07 = 12207, -- The Secret of Helheim
+		Mission08 = 12208, -- The Contact
+		Mission09 = 12209, -- Formorgar Mines 1: The Mission
+		Mission10 = 12210, -- Formorgar Mines 2: Ghostwhisperer
+		Mission11 = 12211, -- Formorgar Mines 3: The Secret
+		Mission12 = 12212, -- Formorgar Mines 4: Retaliation
+		PickAmount = 12213,
+		PaintSeal = 12214,
+		SulphurLava = 12215,
+		SporesMushroom = 12216,
+		FrostbiteHerb = 12217,
+		FlowerCactus = 12218,
+		FlowerBush = 12219,
+		MemoryCrystal = 12220,
+		Obelisk01 = 12221,
+		Obelisk02 = 12222,
+		Obelisk03 = 12223,
+		Obelisk04 = 12224
 	},
 	InServiceofYalahar = {
 		Questline = 12240, -- Storage through the Quest
@@ -88,97 +181,5 @@ Storage = {
 		MatrixState = 12269,
 		SideDecision = 12270,
 		MatrixReward = 12271
-	},
-	TheAncientTombs = {
-		DefaultStart = 12100,
-		Omruc = 12101,
-		Thalas = 12102,
-		Diphtrah = 12103,
-		Mahrdis = 12104,
-		Vashresamun = 12105,
-		Morguthis = 12106,
-		Rahemos = 12107,
-		Ashmunrah = 12108,
-		VashresamunInstruments = 12109
-	},
-	TheApeCity = {
-		Questline = 12120, -- Storage through the Quest
-		Mission01 = 12121,
-		Mission02 = 12122,
-		Mission03 = 12123,
-		Mission04 = 12124,
-		Mission05 = 12125,
-		Mission06 = 12126,
-		Mission07 = 12127,
-		Mission08 = 12128,
-		Mission09 = 12129
-	},
-	TheIceIslands = {
-		Questline = 12200, -- Storage through the Quest
-		Mission01 = 12201, -- Befriending the Musher
-		Mission02 = 12202, -- Nibelor 1: Breaking the Ice
-		Mission03 = 12203, -- Nibelor 2: Ecological Terrorism
-		Mission04 = 12204, -- Nibelor 3: Artful Sabotage
-		Mission05 = 12205, -- Nibelor 4: Berserk Brewery
-		Mission06 = 12206, -- Nibelor 5: Cure the Dogs
-		Mission07 = 12207, -- The Secret of Helheim
-		Mission08 = 12208, -- The Contact
-		Mission09 = 12209, -- Formorgar Mines 1: The Mission
-		Mission10 = 12210, -- Formorgar Mines 2: Ghostwhisperer
-		Mission11 = 12211, -- Formorgar Mines 3: The Secret
-		Mission12 = 12212, -- Formorgar Mines 4: Retaliation
-		PickAmount = 12213,
-		PaintSeal = 12214,
-		SulphurLava = 12215,
-		SporesMushroom = 12216,
-		FrostbiteHerb = 12217,
-		FlowerCactus = 12218,
-		FlowerBush = 12219,
-		MemoryCrystal = 12220,
-		Obelisk01 = 12221,
-		Obelisk02 = 12222,
-		Obelisk03 = 12223,
-		Obelisk04 = 12224
-	},
-	TheInquisition = {
-		Questline = 12160, -- Storage through the Quest
-		Mission01 = 12161,
-		Mission02 = 12162,
-		Mission03 = 12163,
-		Mission04 = 12164,
-		Mission05 = 12165,
-		Mission06 = 12166,
-		Mission07 = 12167,
-		GrofGuard = 12170,
-		KulagGuard = 12171,
-		MilesGuard = 12172,
-		TimGuard = 12173,
-		WalterGuard = 12174,
-		StorkusVampiredust = 12175,
-		EnterTeleport = 12176,
-		Reward = 12177
-	},
-	TheNewFrontier = {
-		Questline = 12130, -- Storage through the Quest
-		Mission01 = 12131,
-		Mission02 = 12132,
-		Mission03 = 12133,
-		Mission04 = 12134,
-		Mission05 = 12135,
-		Mission06 = 12136,
-		Mission07 = 12137,
-		Mission08 = 12138,
-		Mission09 = 12139,
-		Mission10 = 12140,
-		TomeofKnowledge = 12141,
-		Beaver1 = 12142,
-		Beaver2 = 12143,
-		Beaver3 = 12144,
-		BribeKing = 12145,
-		BribeLeeland = 12146,
-		BribeAngus = 12147,
-		BribeWydrin = 12148,
-		BribeTelas = 12149,
-		BribeHumgolf = 12150
 	}
 }
