10a6f85c212482dee3366ca573f8c03f1965a6e7
Daniel Björkholm <danielbjorkholm@live.se>
2014-06-22 23:07:22 +0200

    Small fixes
    #601
    
    
    Former-commit-id: 11d0ba601fbca74856765beaa20e0be17743e46e

--- data/actions/scripts/elemental spheres quest/ElementalSpheresQuestMachine.lua
+++ data/actions/scripts/elemental spheres quest/ElementalSpheresQuestMachine.lua
@@ -1,35 +1,42 @@
-local t = {
-	[1] = {x=33183, y=32197, z=13},
-	[2] = {x=33331, y=32076, z=13},
-	[3] = {x=33265, y=32202, z=13},
-	[4] = {x=33087, y=32096, z=13}
+local config = {
+	[1] = Position(33183, 32197, 13),
+	[2] = Position(33331, 32076, 13),
+	[3] = Position(33265, 32202, 13),
+	[4] = Position(33087, 32096, 13)
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(itemEx.actionid == 100) then
-		return true
-	end
-	if(isInArray({7911, 7912}, item.itemid)) then
-		if(isInArray({33268, 33269}, toPosition.x) and toPosition.y == 31830 and toPosition.z == 10 and Player(cid):getStorageValue(key)(10002) >= 20) then
-			local v = Player(cid):getVocation():getId()
-			if(v > 4) then
-				v = v - 4
+	if isInArray({7911, 7912}, item.itemid) then
+		local player = Player(cid)
+		if isInArray({33268, 33269}, toPosition.x) and toPosition.y == 31830 and toPosition.z == 10 and player:getStorageValue(10002) >= 20 then
+			local vocation = player:getVocation():getId()
+			if vocation > 4 then
+				vocation = vocation - 4
 			end
-			Player(cid):setStorageValue(10002, Player(cid):getStorageValue(key)(10002) - 20)
-			Position(getThingPos(cid)):sendMagicEffect(CONST_ME_TELEPORT)
-			Position(t[v]):sendMagicEffect(CONST_ME_TELEPORT)
+			player:setStorageValue(10002, player:getStorageValue(10002) - 20)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			player:teleportTo(config[vocation], false)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		end
 		toPosition.x = toPosition.x + (item.itemid == 7911 and 1 or -1)
-		local get = getTileItemById(toPosition, item.itemid == 7911 and 7912 or 7911)
-		doTransformItem(get.uid, get.itemid + 4) -- need to pass to the metatable functions
-		doTransformItem(item.uid, item.itemid + 4) -- need to pass to the metatable functions
-		Creature(cid):say("ON", TALKTYPE_ORANGE_1, false, cid, (toPosition or getCreaturePosition(cid)))
+		local tile = toPosition:getTile()
+		if tile then
+			local thing = tile:getItemById(item.itemid == 7911 and 7912 or 7911)
+			if thing then
+				thing:transform(thing:getId() + 4)
+			end
+		end
+		Item(item.uid):transform(item.itemid + 4)
 	else
 		toPosition.x = toPosition.x + (item.itemid == 7915 and 1 or -1)
-		local get = getTileItemById(toPosition, item.itemid == 7915 and 7916 or 7915)
-		doTransformItem(get.uid, get.itemid - 4) -- need to pass to the metatable functions
-		doTransformItem(item.uid, item.itemid - 4) -- need to pass to the metatable functions
-		Creature(cid):say("OFF", TALKTYPE_ORANGE_1, false, cid, (toPosition or getCreaturePosition(cid)))
+		local tile = toPosition:getTile()
+		if tile then
+			local thing = tile:getItemById(item.itemid == 7915 and 7916 or 7915)
+			if thing then
+				thing:transform(thing:getId() - 4)
+			end
+		end
+		Item(item.uid):transform(item.itemid - 4)
 	end	
-return true
+	return true
 end
