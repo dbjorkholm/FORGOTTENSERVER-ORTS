55bb4547e2ee0a6b7ec01798c208b28ca07d28da
EPuncker <gm.perflex@gmail.com>
2014-06-23 17:00:24 -0300

    More updates Changed Enraged White Deers into normal white deers

---
+++ data/npc/scripts/Florentine.lua
@@ -0,0 +1,23 @@
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+
+local rnd_sounds = 0
+function onThink()
+	if(rnd_sounds < os.time()) then
+		rnd_sounds = (os.time() + 5)
+		if(math.random(1, 100) < 25) then
+			Npc():say("Embellish your home with flowers!", TALKTYPE_SAY)
+		end
+	end
+	npcHandler:onThink()
+end
+
+npcHandler:setMessage(MESSAGE_GREET, "Welcome, |PLAYERNAME|. What can I do for you?")
+npcHandler:setMessage(MESSAGE_SENDTRADE, "Of course, just browse through my wares.")
+
+npcHandler:addModule(FocusModule:new())
