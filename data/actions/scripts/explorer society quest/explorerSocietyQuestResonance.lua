7ca9ea753f6e31e724c9555d24a28bd734a0b604
Skillert <peter.mollberg@gmail.com>
2014-06-24 21:16:21 +0200

    Minor - Converted last koshei script to meta and made it work like "RL" - Converted explorer society quests to meta
    Former-commit-id: 7bf502118f8b42ffa954a63989fe978e1087e6f0

--- data/actions/scripts/explorer society quest/explorerSocietyQuestResonance.lua
+++ data/actions/scripts/explorer society quest/explorerSocietyQuestResonance.lua
@@ -1,8 +1,9 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(itemEx.uid == 3018 and item.itemid == 7281) then
-		if(getPlayerStorageValue(cid, 90) == 60) then
-			setPlayerStorageValue(cid, 90, 61)
-			doSendMagicEffect(toPosition, CONST_ME_MAGIC_BLUE)
+	local player = Player(cid)
+	if itemEx.uid == 3018 and item.itemid == 7281 then
+		if player:getStorageValue(90) == 60 then
+			player:setStorageValue(90, 61)
+			toPosition:sendMagicEffect(CONST_ME_MAGIC_BLUE)
 		end
 	end
 	return true
