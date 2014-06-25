7dc0076c1cb927e4fbccfe128698178e52e91c0c
Royalot <zapf-benny@web.de>
2014-06-25 21:20:31 +0200

    CotR Quest to mission02

--- data/actions/scripts/children of the revolution quest/childrenOfTheRevolutionQuestZalamonDoor.lua
+++ data/actions/scripts/children of the revolution quest/childrenOfTheRevolutionQuestZalamonDoor.lua
@@ -1,11 +1,12 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.uid == 3170) then
-		if(getPlayerStorageValue(cid, 1015) >= 24) then
-			if(item.itemid == 10791) then
-				doTeleportThing(cid, toPosition, true)
-				doTransformItem(item.uid, 10792)
-			end
+	local player = Player(cid)
+	if player:getStorageValue(Storage.TheNewFrontier.Mission08) >= 2 then
+		if item.itemid == 10791 then
+			player:teleportTo(toPosition, true)
+			Item(item.uid):transform(item.itemid + 1)
 		end
+	else
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "The door seems to be sealed against unwanted intruders.")
 	end
 	return true
 end
--- data/movements/scripts/children of the revolution quest/childrenOfTheRevolutionQuestTemple.lua
+++ data/movements/scripts/children of the revolution quest/childrenOfTheRevolutionQuestTemple.lua
@@ -1,8 +1,13 @@
 function onStepIn(cid, item, position, lastPosition)
-	if(item.uid == 3163) then
-		if(getPlayerStorageValue(cid, 1050) == 4) then
-			setPlayerStorageValue(cid, 1050, 5)
-			doCreatureSay(cid, "The temple has been corrupted and is lost. Zalamon should be informed about this as soon as possible.", TALKTYPE_ORANGE_1)
+	local player = Player(cid)
+	if not player then
+		return true
+	end
+	if item.uid == 3163 then
+		if player:getStorageValue(Storage.ChildrenoftheRevolution.Questline) == 4 then
+			player:setStorageValue(Storage.ChildrenoftheRevolution.Questline, 5)
+			player:setStorageValue(Storage.ChildrenoftheRevolution.Mission01, 2) --Questlog, Children of the Revolution "Mission 1: Corruption"
+			player:say("The temple has been corrupted and is lost. Zalamon should be informed about this as soon as possible.", TALKTYPE_ORANGE_1)
 		end
 	end
 	return true
