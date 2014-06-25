a9fbdf35684da7ff04494f364bcb2b7c5ae4740f
EPuncker <gm.perflex@gmail.com>
2014-06-25 10:02:25 -0300

    More NPC updates
    Former-commit-id: 850099fed78d899328f1328c143b90b092376334

---
+++ data/npc/scripts/Sarina.lua
@@ -0,0 +1,26 @@
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+
+local rnd_sounds = 0
+function onThink()
+	if(rnd_sounds < os.time()) then
+		rnd_sounds = (os.time() + 5)
+		if(math.random(1, 100) < 25) then
+			Npc():say("General equipment and all sorts of goods. Visit my store!", TALKTYPE_SAY)
+		end
+	end
+	npcHandler:onThink()
+end
+
+keywordHandler:addKeyword({'equipment'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = "I sell equipment for your adventure! Just ask me for a {trade} to see my wares."})
+
+npcHandler:setMessage(MESSAGE_GREET, "Oh, please come in, |PLAYERNAME|. What can I do for you? If you need adventure equipment, ask me for a {trade}.")
+npcHandler:setMessage(MESSAGE_FAREWELL, "Good bye, |PLAYERNAME|.")
+npcHandler:setMessage(MESSAGE_WALKAWAY, "Good bye, |PLAYERNAME|.")
+npcHandler:setMessage(MESSAGE_SENDTRADE, "Of course, just browse through my wares. {Footballs} have to be purchased separately.")
+npcHandler:addModule(FocusModule:new())
