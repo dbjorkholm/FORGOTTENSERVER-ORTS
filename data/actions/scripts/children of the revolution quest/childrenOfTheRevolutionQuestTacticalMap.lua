3e243d28dce8ec6bcad2692878dff721b3ce6bc2
Royalot <zapf-benny@web.de>
2014-06-25 21:20:31 +0200

    CotR Quest to mission02
    Former-commit-id: 7dc0076c1cb927e4fbccfe128698178e52e91c0c

---
+++ data/actions/scripts/children of the revolution quest/childrenOfTheRevolutionQuestTacticalMap.lua
@@ -0,0 +1,12 @@
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	if item.uid == 3162 then
+		local player = Player(cid)
+		if player:getStorageValue(Storage.ChildrenoftheRevolution.Questline) == 1 then
+			player:setStorageValue(Storage.ChildrenoftheRevolution.Questline, 2)
+			player:addItem(11101, 1)
+			player:say("A batch of documents has been stashed in the shelf. These might be of interest to Zalamon.", TALKTYPE_ORANGE_1)
+			toPosition:sendMagicEffect(CONST_ME_POFF)
+		end
+	end
+	return true
+end
