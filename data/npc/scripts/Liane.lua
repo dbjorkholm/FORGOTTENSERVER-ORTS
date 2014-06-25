f0f43594cf8f7201bdb885c26196ae3e85685db2
EPuncker <gm.perflex@gmail.com>
2014-06-25 02:11:23 -0300

    Fixed furniture npcs that had most of ids wrong according to items.xml Added few NPCs Updated some ref #702

--- data/npc/scripts/Liane.lua
+++ data/npc/scripts/Liane.lua
@@ -1,34 +1,54 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
-local talkState = {}
- 
-function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
-function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
-function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
-function onThink() npcHandler:onThink() end
+
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+
+local random_texts = {
+	'Welcome to the post office!',
+	'Hey, send a letter to your friend now and then. Keep in touch, you know.',
+	'If you need help with letters or parcels, just ask me. I can explain everything.',
+	'No, no, no, there IS no parcel bug, I\'m telling you!'
+}
+
+local rnd_sounds = 0
+function onThink()
+	if(rnd_sounds < os.time()) then
+		rnd_sounds = (os.time() + 10)
+		if(math.random(1, 100) < 20) then
+			Npc():say(random_texts[math.random(1, #random_texts)], TALKTYPE_SAY)
+		end
+	end
+	npcHandler:onThink()
+end
 
 local function creatureSayCallback(cid, type, msg)
 	if(not npcHandler:isFocused(cid)) then
 		return false
 	end
-	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
+	local player = Player(cid)
 
 	if(msgcontains(msg, "measurements")) then
-		if(getPlayerStorageValue(cid, 250) == 28) then
+		if player:getStorageValue(250) == 28 then
 			npcHandler:say("I have more urgent problem to attend then that. Those hawks are hunting my carrier pigeons. Bring me 12 arrows and I'll see if I have the time for this nonsense. Do you have 12 arrows with you? ", cid)
-			talkState[talkUser] = 1
+			npcHandler.topic[cid] = 1
 		end
 	elseif(msgcontains(msg, "yes")) then
-		if(getPlayerItemCount(cid, 2544) >= 12) then
-			doPlayerRemoveItem(cid, 2544, 12)
+		if player:getItemCount(2544) >= 12 then
+			player:removeItem(2544, 12)
 			npcHandler:say("Great! Now I'll teach them a lesson ... For those measurements ... <tells you her measurements> ", cid)
-			setPlayerStorageValue(cid, 250, 29)
-			talkState[talkUser] = 0
+			player:setStorageValue(250, 29)
+			npcHandler.topic[cid] = 0
 		end
 	end
 	return true
 end
- 
+
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:setMessage(MESSAGE_GREET, "Hello. How may I help you |PLAYERNAME|? Ask me for a trade if you want to buy something. I can also explain the {mail} system.")
+npcHandler:setMessage(MESSAGE_FAREWELL, "It was a pleasure to help you, |PLAYERNAME|.")
+npcHandler:setMessage(MESSAGE_WALKAWAY, "It was a pleasure to help you, |PLAYERNAME|.")
+npcHandler:setMessage(MESSAGE_SENDTRADE, "Here. Don't forget that you need to buy a label too if you want to send a parcel. Always write the name of the {receiver} in the first line.")
 npcHandler:addModule(FocusModule:new())
