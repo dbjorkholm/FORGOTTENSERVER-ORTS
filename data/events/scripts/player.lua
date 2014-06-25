863f345778fd11b473e93c8037c9c44667a4ca90
ninjalulz <danielbjorkholm@live.se>
2014-06-26 01:03:32 +0200

    Fix #760
    Former-commit-id: 881b54613a26771c5e3ffeaf7fdeffc4fcaa8929 [formerly 726ba8b0d77747d74ecc3b377e0ea50e3de9b3c8]
    Former-commit-id: c51713e5f40c81c3161956dcd7ecac518bd280e6

--- data/events/scripts/player.lua
+++ data/events/scripts/player.lua
@@ -100,6 +100,9 @@ function Player:onMoveItem(item, count, fromPosition, toPosition)
 	if isInArray({1738, 1740, 1747, 1748, 1749}, item:getId()) and item:getActionId() > 0 and item:getActionId() < 65535 then
 		self:sendCancelMessage("You cannot move this object.")
 		return false
+	elseif item:getId() == 7466 then
+		self:sendCancelMessage("You cannot move this object.")
+		return false
 	end
 	return true
 end
