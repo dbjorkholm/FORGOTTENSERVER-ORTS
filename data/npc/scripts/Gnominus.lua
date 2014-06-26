1fa3704cf678821d8d1950a700ae4e481dccb822
EPuncker <gm.perflex@gmail.com>
2014-06-26 01:59:43 -0300

    fix comflict
    Former-commit-id: 4e7586fcd8cfd30ee3984ad3211112054a9fa142

--- data/npc/scripts/Gnominus.lua
+++ data/npc/scripts/Gnominus.lua
@@ -1,28 +1,30 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
-
-function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
-function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
-function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
-function onThink()				npcHandler:onThink()					end
+local talkState = {}
+ 
+function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
+function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
+function onThink() npcHandler:onThink() end
 
 local function creatureSayCallback(cid, type, msg)
 	if(not npcHandler:isFocused(cid)) then
 		return false
 	end
+	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
 
 	if(msgcontains(msg, "recruitment")) then
 		if(getPlayerStorageValue(cid, 900) == 3) then
 			npcHandler:say("Your examination is quite easy. Just step through the green crystal apparatus in the south! We will examine you with what we call g-rays. Where g stands for gnome of course ... ", cid)
 			npcHandler:say("Afterwards walk up to Gnomedix for your ear examination. ", cid)
-			npcHandler.topic[cid] = 1
+			talkState[talkUser] = 1
 		end
 	elseif(msgcontains(msg, "apparatus")) then
-		if(npcHandler.topic[cid] == 1) then
+		if(talkState[talkUser] == 1) then
 			npcHandler:say("Don't be afraid. It won't hurt! Just step in! ", cid)
 			setPlayerStorageValue(cid, 900, 4)
-			npcHandler.topic[cid] = 0
+			talkState[talkUser] = 0
 		end
 	end
 	return true
