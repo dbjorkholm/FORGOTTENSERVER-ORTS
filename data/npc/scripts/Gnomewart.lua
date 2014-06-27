e218bc1022c20352e4d517ca1b0ff2f2e982aaa4
EPuncker <gm.perflex@gmail.com>
2014-06-26 23:31:33 -0300

    more updates
    Former-commit-id: 80d16ed58b4a24cb2879a46b706b6e10e0807aca

--- data/npc/scripts/Gnomewart.lua
+++ data/npc/scripts/Gnomewart.lua
@@ -11,22 +11,22 @@ local function creatureSayCallback(cid, type, msg)
 	if(not npcHandler:isFocused(cid)) then
 		return false
 	end
-
+	local player = Player(cid)
 	if(msgcontains(msg, "endurance")) then
-		if(getPlayerStorageValue(cid, 900) == 9) then
-			npcHandler:say("Ah, the test is a piece of mushroomcake! Just take the teleporter over there in the south and follow the hallway. ... ", cid)
-			npcHandler:say("You'll need to run quite a bit. It is important that you don't give up! Just keep running and running and running and ... I guess you got the idea. ... ", cid)
-			npcHandler:say("At the end of the hallway you'll find a teleporter. Step on it and you are done! I'm sure you'll do a true gnomerun! Afterwards talk to me. ", cid)
-			setPlayerStorageValue(cid, 900, 10)
+		if player:getStorageValue(900) == 9 then
+			npcHandler:say("Ah, the test is a piece of mushroomcake! Just take the teleporter over there in the south and follow the hallway. ...", cid)
+			npcHandler:say("You'll need to run quite a bit. It is important that you don't give up! Just keep running and running and running and ... I guess you got the idea. ...", cid)
+			npcHandler:say("At the end of the hallway you'll find a teleporter. Step on it and you are done! I'm sure you'll do a true gnomerun! Afterwards talk to me.", cid)
+			player:setStorageValue(900, 10)
 			npcHandler.topic[cid] = 0
-		elseif(getPlayerStorageValue(cid, 900) == 11) then
-			npcHandler:say("You have passed the test and are ready to create your soul melody. Talk to Gnomelvis in the east about it. ", cid)
-			setPlayerStorageValue(cid, 900, 12)
+		elseif player:getStorageValue(900) == 11 then
+			npcHandler:say("You have passed the test and are ready to create your soul melody. Talk to Gnomelvis in the east about it.", cid)
+			player:setStorageValue(900, 12)
 			npcHandler.topic[cid] = 0
 		end
 	end
 	return true
 end
- 
+
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
