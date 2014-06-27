75956b6c3953c70fe5ce90d1c060583f6f4041fe
Skillert <peter.mollberg@gmail.com>
2014-06-27 13:00:53 +0200

    Fixes, cheers @ninjalulz
    Former-commit-id: 5bf2c605c3bdd61228544754aa0aa49eaa9035d8 [formerly 91f84a76491106f936ad406967d95d35ec7a0dd9]
    Former-commit-id: 7e215206398333c17a948b4df252f9b8b9d7e1d8

--- data/actions/scripts/tools/noxiousclaw.lua
+++ data/actions/scripts/tools/noxiousclaw.lua
@@ -2,20 +2,19 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local depleteChance = 01
 	local player = Player(cid)
 	local targetItem = Item(item.uid)
-	if player:getCondition(CONDITION_POISON) then
-		if math.random(100) <= depleteChance then
-			player:addCondition(CONDITION_CURSED)
-			targetItem:transform(10312)
-			targetItem:decay()
-			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
-		else
+	if math.random(100) <= depleteChance then
+		player:addCondition(CONDITION_CURSED)
+		targetItem:transform(10312)
+		targetItem:decay()
+		player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+	else
+		if player:getCondition(CONDITION_POISON) then
 			player:removeCondition(CONDITION_POISON)
-			targetItem:transform(10311)
-			targetItem:decay()
-			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
-			depleteChance = depleteChance + 1
 		end
-		return true
+		targetItem:transform(10311)
+		targetItem:decay()
+		player:getPosition():sendMagicEffect(CONST_ME_MAGIC_GREEN)
+		depleteChance = depleteChance + 1
 	end
-	return false
+	return true
 end
