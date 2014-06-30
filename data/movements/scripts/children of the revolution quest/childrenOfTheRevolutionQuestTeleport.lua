4aee09e25f86c3e86b8c93aacc44596e550e2695
Royalot <zapf-benny@web.de>
2014-06-30 18:14:39 +0200

    some update for CotR (wip)
    revamp lizard statue room {x = 33261, y = 31086, z = 8}
    
    
    Former-commit-id: d53852e9857891fe124e1b37062a58e79b4729ad [formerly c0f522cf5a5621dd61b77f5124fe054fd518b0b1]
    Former-commit-id: ae5ccdcce8ecdbd1a0b212916d8af41db4328df3

--- data/movements/scripts/children of the revolution quest/childrenOfTheRevolutionQuestTeleport.lua
+++ data/movements/scripts/children of the revolution quest/childrenOfTheRevolutionQuestTeleport.lua
@@ -1,6 +1,6 @@
 local config = {
-	[3167] = {1050, Position({x = 33257, y = 31116, z = 8})},
-	[3168] = {1050, Position({x = 33356, y = 31125, z = 7})}
+	[3167] = {Storage.ChildrenoftheRevolution.Questline, Position({x = 33257, y = 31116, z = 8}), Position({x = 33356, y = 31126, z = 7})},
+	[3168] = {Storage.ChildrenoftheRevolution.Questline, Position({x = 33356, y = 31125, z = 7}), Position({x = 33261, y = 31079, z = 8})}
 }
 
 function onStepIn(cid, item, position, lastPosition)
@@ -15,8 +15,14 @@ function onStepIn(cid, item, position, lastPosition)
 	end
 	
 	if player:getStorageValue(targetTile[1]) >= 19 then
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		player:teleportTo(targetTile[2])
 		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	else
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:teleportTo(targetTile[3])
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:say("This portal should not be entered unprepared", TALKTYPE_ORANGE_1)
 	end
 	return true
 end
