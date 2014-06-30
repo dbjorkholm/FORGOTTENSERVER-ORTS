cb97cbbee35b96ecee62d013b2210b2c19a361e6
Royalot <zapf-benny@web.de>
2014-06-30 21:12:45 +0200

    finish CotR Quest

--- data/movements/scripts/children of the revolution quest/childrenOfTheRevolutionQuestStairs.lua
+++ data/movements/scripts/children of the revolution quest/childrenOfTheRevolutionQuestStairs.lua
@@ -1,8 +1,18 @@
 function onStepIn(cid, item, position, lastPosition)
-	if(item.uid == 3169) then
-		if(getTileItemById({x = 33265, y = 31116, z = 8}, 3687).uid > 0) then
-			doTeleportThing(cid, {x = 33265, y = 31116, z = 7})
-			doCreatureSay(cid, "The area around the gate is suspiciously quiet, you have a bad feeling about this.", TALKTYPE_ORANGE_1)
+	local player = Player(cid)
+	if not player then 
+		return true
+	end
+	if item.uid == 3169 then
+		if Tile(Position({x = 33265, y = 31116, z = 8})):getItemById(3687) then
+			if Tile(Position({x = 33265, y = 31116, z = 7})):getItemById(3219) then
+			Tile(Position({x = 33265, y = 31116, z = 7})):getItemById(3219):transform(9197)
+			addEvent(function() Tile(Position({x = 33265, y = 31116, z = 7})):getItemById(9197):transform(3219) end, 5 * 30 * 1000)
+			end
+			player:say("The area around the gate is suspiciously quiet, you have a bad feeling about this.", TALKTYPE_ORANGE_1)
+			if player:getStorageValue(Storage.ChildrenoftheRevolution.Mission05) == 1 then
+				player:setStorageValue(Storage.ChildrenoftheRevolution.Mission05, 2) --Questlog, Children of the Revolution "Mission 5: Phantom Army"
+			end
 		end
 	end
 	return true
