4fdd580b9bd98050ddb8fbb1a640d502150e94c3
Royalot <zapf-benny@web.de>
2014-06-30 21:12:45 +0200

    finish CotR Quest
    Former-commit-id: c6b6173d6c35d8f0603c8613c2c110e4b0f61eb9 [formerly cb97cbbee35b96ecee62d013b2210b2c19a361e6]
    Former-commit-id: 0bae8378103cab5b8d452fd01c845f44a86bd5d0

--- data/movements/scripts/children of the revolution quest/childrenOfTheRevolutionQuestClick.lua
+++ data/movements/scripts/children of the revolution quest/childrenOfTheRevolutionQuestClick.lua
@@ -1,63 +1,65 @@
-local pos = {
-	{x = 33253, y = 31081, z = 8},
-	{x = 33259, y = 31090, z = 8},
-	{x = 33266, y = 31085, z = 8},
-	{x = 33263, y = 31094, z = 8}
+local config = {
+	Position({x = 33258, y = 31080, z = 8}),
+	Position({x = 33266, y = 31084, z = 8}),
+	Position({x = 33259, y = 31089, z = 8}),
+	Position({x = 33263, y = 31093, z = 8})
 }
-
-function doClearMissionArea(fromPos, toPos)
-	for x = fromPos.x, toPos.x do
-		for y = fromPos.y, toPos.y do
-			for z = fromPos.z, toPos.z do
-				if(getTopCreature({x = x, y = y, z = z}).uid > 0) then
-					if(isPlayer(getTopCreature({x = x, y = y, z = z}).uid)) then
-						doTeleportThing(getTopCreature({x = x, y = y, z = z}).uid, {x = 33353, y = 31410, z = 8})
-						doSendMagicEffect({x = 33353, y = 31410, z = 8}, CONST_ME_TELEPORT)
-						setPlayerStorageValue(getTopCreature({x = x, y = y, z = z}).uid, 1050, 20)
-					else
-						doRemoveCreature(getTopCreature({x = x, y = y, z = z}).uid)
-					end
+function doClearMissionArea()
+	Game.setStorageValue(Storage.ChildrenoftheRevolution.Mission05, -1)
+	local spectators = Game.getSpectators(Position({x = 33268, y = 31119, z = 7}), false, true, 26, 26, 20, 20)
+		if spectators ~= nil then
+			for _, spectator in ipairs(spectators) do
+				spectator:teleportTo({x = 33353, y = 31410, z = 8})
+				spectator:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+				if spectator:getStorageValue(Storage.ChildrenoftheRevolution.Questline) == 19 then
+					spectator:setStorageValue(Storage.ChildrenoftheRevolution.Questline, 20)
 				end
 			end
 		end
-	end
+	local monsters = Game.getSpectators(Position({x = 33268, y = 31119, z = 7}), false, false, 26, 26, 20, 20)
+		if monsters ~= nil then
+			for _, monster in ipairs(monsters) do
+				monster:getPosition():sendMagicEffect(CONST_ME_POFF)
+				monster:remove()
+			end
+		end
 	return true
 end
 
 function onStepIn(cid, item, position, lastPosition)
-	start = false
-	if(getPlayerStorageValue(cid, 1050) == 19) then
-		if(item.actionid == 8014) then
+	local player = Player(cid)
+	if not player then 
+		return true
+	end
+	startwavesCotR = false
+	if player:getStorageValue(Storage.ChildrenoftheRevolution.Questline) == 19 then
+		if Game.getStorageValue(Storage.ChildrenoftheRevolution.Mission05) ~= 1 then
 			for i = 1, 4 do
-				if(getTopCreature(pos[i]).uid > 0) then
-					if(isPlayer(getTopCreature(pos[i]).uid)) then
-						if(getGlobalStorageValue(8014) < 1) then
-							doCreatureSay(getTopCreature(pos[i]).uid, "A clicking sound tatters the silence.", TALKTYPE_ORANGE_1)
-							start = true
-						end
-					end
-				end
-			end				
-			if(start == true) then				
-				doTransformItem(getTileItemById({x = 33265, y = 31116, z = 8}, 3653).uid), 3687)
-				addEvent(doTransformItem, 30 * 1000, getTileItemById({x = 33265, y = 31116, z = 8}, 3687).uid), 3653)
-				setGlobalStorageValue(8014, 1)
-				addEvent(setGlobalStorageValue, 8014, 0)
-				creature = ""
-				for k = 1, 4 do
-					for j = 1, 20 do
-						if(k < 3) then
-							creature = "eternal guardian"
-						else
-							creature = "lizard chosen"
-						end
-						summonPos = {x = math.random(33239, 33295), y = math.random(31100, 31132), z = 7}
-						addEvent(doSummonCreature, k * 30 * 1000, creature, summonPos)
-						addEvent(doSendMagicEffect, k * 30 * 1000, summonPos, CONST_ME_TELEPORT)
-					end
-				end
-				addEvent(doClearMissionArea, 5 * 30 * 1000, {x = 33239, y = 31100, z = 7}, {x = 33295, y = 31132, z = 7})
+				local otherplayer = Tile(Position(config[i])):getTopCreature() if otherplayer == nil or not otherplayer:isPlayer() then return true end
+				otherplayer:say("A clicking sound tatters the silence.", TALKTYPE_ORANGE_1)
+				startwavesCotR = true
 			end
+		end				
+		if(startwavesCotR == true) then				
+			Tile(Position({x = 33265, y = 31116, z = 8})):getItemById(3653):transform(3687)
+			addEvent(function() Tile(Position({x = 33265, y = 31116, z = 8})):getItemById(3687):transform(3653) end, 30 * 1000) -- 30 sec until stair disappear
+			Game.setStorageValue(Storage.ChildrenoftheRevolution.Mission05, 1)
+			creatureCotR = ""
+			for k = 1, 4 do
+				for j = 1, 20 do
+					addEvent(function() 
+					if k < 3 then
+						creatureCotR = "eternal guardian"
+					else
+						creatureCotR = "lizard chosen"
+					end					
+					CotRsummonPos = {x = math.random(33252, 33288), y = math.random(31105, 31134), z = 7} 
+					Game.createMonster(creatureCotR, CotRsummonPos) 
+					Position(CotRsummonPos):sendMagicEffect(CONST_ME_TELEPORT)
+					end, k * 30 * 1000) -- 30 sec
+				end
+			end
+			addEvent(doClearMissionArea, 5 * 30 * 1000)
 		end
 	end
 	return true
