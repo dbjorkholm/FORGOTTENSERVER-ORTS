aaa6e45cdba55f5e096925f71ee900b4180ba1cb
Skillert <peter.mollberg@gmail.com>
2014-07-01 09:46:30 +0200

    Claw of 'The Noxious Spawn' - Removed depleteChance + 1 since I don't think it was fully working as   I wanted it to. - Changed chance that the claw will be fully depleted from 1% to 5%. - Added params to the cursed cond. in hope to actually curse the player   once it is fully depleted.
    Former-commit-id: b5f8df62a2897822735cc2575145176fe0dc356a

--- data/actions/scripts/tools/noxiousclaw.lua
+++ data/actions/scripts/tools/noxiousclaw.lua
@@ -1,9 +1,18 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local depleteChance = 01
+
+	local cursed = Condition(CONDITION_CURSED)
+	cursed:setParameter(CONDITION_PARAM_DELAYED, true) -- Condition will delay the first damage from when it's added
+	cursed:setParameter(CONDITION_PARAM_MINVALUE, -800) -- Minimum damage the condition can do at total
+	cursed:setParameter(CONDITION_PARAM_MAXVALUE, -1200) -- Maximum damage
+	cursed:setParameter(CONDITION_PARAM_STARTVALUE, -1) -- The damage the condition will do on the first hit
+	cursed:setParameter(CONDITION_PARAM_TICKINTERVAL, 4000) -- Delay between damages
+	cursed:setParameter(CONDITION_PARAM_FORCEUPDATE, true) -- Re-update condition when adding it(ie. min/max value)
+
+	local depleteChance = 05
 	local player = Player(cid)
 	local targetItem = Item(item.uid)
 	if math.random(100) <= depleteChance then
-		player:addCondition(CONDITION_CURSED)
+		player:addCondition(cursed)
 		targetItem:transform(10312)
 		targetItem:decay()
 		player:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
@@ -15,7 +24,6 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		targetItem:transform(10311)
 		targetItem:decay()
 		player:getPosition():sendMagicEffect(CONST_ME_MAGIC_GREEN)
-		depleteChance = depleteChance + 1
 	end
 	return true
 end
