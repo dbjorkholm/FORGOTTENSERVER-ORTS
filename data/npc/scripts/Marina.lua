3bce2128865c7f40ad1780dec7f7f9f1fad35470
Royalot <zapf-benny@web.de>
2014-07-01 19:26:50 +0200

    minor change

--- data/npc/scripts/Marina.lua
+++ data/npc/scripts/Marina.lua
@@ -13,15 +13,15 @@ local function creatureSayCallback(cid, type, msg)
 	end
 	local player = Player(cid)
 	if(msgcontains(msg, "silk yarn")) then
-		if player:getStorageValue(78) < 1 then
+		if player:getStorageValue(Storage.FriendsandTraders.TheMermaidMarina) < 1 then
 			npcHandler:say("Um. You mean, you really want me to touch that gooey spider silk just because you need yarn? Well... do you think that I'm pretty?", cid)
 			npcHandler.topic[cid] = 1
-		elseif player:getStorageValue(78) == 1 then
+		elseif player:getStorageValue(Storage.FriendsandTraders.TheMermaidMarina) == 2 then
 			npcHandler:say("Okay... a deal is a deal, would you like me to create a {spool of yarn} from {10 pieces of spider silk}?", cid)
 			npcHandler.topic[cid] = 5
 		end
-	elseif(msgcontains(msg, "50 honeycombs")) then
-		if(npcHandler.topic[cid] == 3) then
+	elseif(msgcontains(msg, "honey")) or (msgcontains(msg, "comb")) then
+		if player:getStorageValue(Storage.FriendsandTraders.TheMermaidMarina) == 1 then
 			npcHandler:say("Did you bring me the 50 honeycombs I requested and do you absolutely admire my beauty?", cid)
 			npcHandler.topic[cid] = 4
 		end
@@ -33,13 +33,16 @@ local function creatureSayCallback(cid, type, msg)
 			npcHandler:say("<giggles> It's funny how easy it is to get humans to say what you want. Now, proving it will be even more fun! ...", cid)
 			npcHandler:say("You want me to touch something gooey, so you have to touch something gooey for me too. <giggles> ...", cid)
 			npcHandler:say("I love honey and I haven't eaten it in a while, so bring me 50 honeycombs and worship my beauty a little more, then we will see.", cid)
-			npcHandler.topic[cid] = 3
+			player:setStorageValue(Storage.FriendsandTraders.TheMermaidMarina, 1)
 		elseif(npcHandler.topic[cid] == 4) then
 			if player:getItemCount(5902) >= 50 then
 				player:removeItem(5902, 50)
 				npcHandler:say("Oh goodie! Thank you! Okay... I guess since my fingers are sticky now anyway, I will help you. From now on, if you bring me {10 pieces of spider silk}, I will create one {spool of yarn}.", cid)
 				npcHandler.topic[cid] = 0
-				player:setStorageValue(78, 1)
+				player:setStorageValue(Storage.FriendsandTraders.TheMermaidMarina, 2)
+			else
+				npcHandler:say("You don't have enough honey.", cid)
+				npcHandler.topic[cid] = 0
 			end
 		elseif(npcHandler.topic[cid] == 5) then
 			if player:getItemCount(5879) >= 10 then
@@ -49,6 +52,7 @@ local function creatureSayCallback(cid, type, msg)
 				npcHandler.topic[cid] = 0
 			else
 				npcHandler:say("You don't have the required items.", cid)
+				npcHandler.topic[cid] = 0
 			end
 		end
 	end
