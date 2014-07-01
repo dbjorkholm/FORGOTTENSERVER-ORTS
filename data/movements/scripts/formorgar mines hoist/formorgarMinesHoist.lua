8d56bde1faba7e8ff3a54aebc7a53f96cfab996d
Daniel Björkholm <danielbjorkholm@live.se>
2014-07-01 17:50:44 +0200

    minor fix

--- data/movements/scripts/formorgar mines hoist/formorgarMinesHoist.lua
+++ data/movements/scripts/formorgar mines hoist/formorgarMinesHoist.lua
@@ -11,21 +11,22 @@ function onStepIn(cid, item, position, lastPosition)
 	if player:getStorageValue(470) == 1 and player:getStorageValue(471) == 1 then
 		if item.uid == 3059 then
 			player:teleportTo(loc2)
-			toPosition(loc2):sendMagicEffect(CONST_ME_TELEPORT)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		elseif item.uid == 3060 then
 			if Tile(Position(32156, 31125, 10)):getItemById(1945) > 0 then
 				player:teleportTo(loc3)
-				toPosition(loc3):sendMagicEffect(CONST_ME_TELEPORT)
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 			else
 				player:teleportTo(loc1)
-				toPosition(loc1):sendMagicEffect(CONST_ME_TELEPORT)
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 			end
 		elseif item.uid == 3061 then
 			player:teleportTo(loc2)
-			toPosition(loc2):sendMagicEffect(CONST_ME_TELEPORT)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		end
 	else
 		player:teleportTo(lastPosition)
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must first find the hoist instruction before using it.")
 	end
 	return true
