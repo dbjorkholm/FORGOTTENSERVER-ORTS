8a254873c71c86e65944d9e72699af58e55161ce
Royalot <zapf-benny@web.de>
2014-07-02 22:57:14 +0200

    isle of striffe new positions
    Former-commit-id: 4a4f9d87ef66fff1a3fc78a37668eca150ba382e

--- data/creaturescripts/scripts/the new frontier quest/theNewFrontierQuestTirecz.lua
+++ data/creaturescripts/scripts/the new frontier quest/theNewFrontierQuestTirecz.lua
@@ -4,11 +4,11 @@ function onKill(cid, target)
 	if monster then
 		if string.lower(monster:getName()) == "tirecz" then
 			Game.setStorageValue(Storage.TheNewFrontier.Mission09, -1)
-			clearArena({x = 33064, y = 31030, z = 3}, {x = 33085, y = 31050, z = 3})
-			local spectators = Game.getSpectators(Position({x = 33074, y = 31039, z = 3}), false, true, 10, 10, 10, 10)
+			clearArena({x = 33053, y = 31024, z = 3}, {x = 33074, y = 31044, z = 3})
+			local spectators = Game.getSpectators(Position({x = 33063, y = 31034, z = 3}), false, true, 10, 10, 10, 10)
 			if spectators ~= nil then
 				for _, spectator in ipairs(spectators) do
-					spectator:teleportTo({x = 33064, y = 31028, z = 7})
+					spectator:teleportTo({x = 33053, y = 31022, z = 7})
 					spectator:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 					spectator:say("You have won! As new champion take the ancient armor as reward before you leave.", TALKTYPE_ORANGE_1)
 					if spectator:getStorageValue(Storage.TheNewFrontier.Questline) == 25 then
