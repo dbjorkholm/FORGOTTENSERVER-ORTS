cc18fcae6425394b34998cd98131bb95ef43c08f
Skillert <peter.mollberg@gmail.com>
2014-07-07 20:57:11 +0200

    Minor
    Former-commit-id: 554e7ab38f81170efb8f5bd9f295604d3dfa4d4d

--- data/movements/scripts/bigfoot burden quest/bigfootBurdenQuestEndurance.lua
+++ data/movements/scripts/bigfoot burden quest/bigfootBurdenQuestEndurance.lua
@@ -1,28 +1,33 @@
 local condition = createConditionObject(CONDITION_PARALYZE)
 setConditionParam(condition, CONDITION_PARAM_TICKS, 2000)
 setConditionFormula(condition, -0.9, 0, -0.9, 0)
-			
+
 function onStepIn(cid, item, position, lastPosition)
-	if(item.actionid == 7816) then
+	local player = Player(cid)
+	if not player then
+		return true
+	end
+
+	if item.actionid == 7816 then
 		random = math.random(1, 5)
-		if(random == 1) then
-			doTeleportThing(cid, lastPosition)
-		elseif(random == 2) then
-			doMoveCreature(cid, SOUTH)
-		elseif(random == 3) then
-			doAddCondition(cid, condition)
+		if random == 1 then
+			player:teleportTo(lastPosition)
+		elseif random == 2 then
+			player:moveTo(SOUTH)
+		elseif random == 3 then
+			player:addCondition(condition)
 		end
-	elseif(item.actionid == 7817) then
-		setPlayerStorageValue(cid, 900, 11)
-		doTeleportThing(cid, {x = 32760, y = 31811, z = 10})
-		doSendMagicEffect({x = 32760, y = 31811, z = 10}, CONST_ME_TELEPORT)
-		doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "You passed endurance test.")
-	elseif(item.actionid == 7818) then
-		if(getPlayerStorageValue(cid, 900) == 10) then
-			doTeleportThing(cid, {x = 32759, y = 31811, z = 11})
-			doSendMagicEffect({x = 32759, y = 31811, z = 11}, CONST_ME_TELEPORT)
+	elseif item.actionid == 7817 then
+		player:setStorageValue(900, 11)
+		player:teleportTo(Position(32760, 31811, 10))
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You passed endurance test.")
+	elseif item.actionid == 7818 then
+		if player:getStorageValue(900) == 10 then
+			player:teleportTo(Position(32759, 31811, 11))
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		else
-			doTeleportThing(cid, lastPosition)
+			player:teleportTo(lastPosition)
 		end
 	end
 	return true
--- data/movements/scripts/bigfoot burden quest/bigfootBurdenQuestTruffles.lua
+++ data/movements/scripts/bigfoot burden quest/bigfootBurdenQuestTruffles.lua
@@ -1,14 +1,16 @@
 function onStepIn(cid, item, position, lastPosition)
-	if(string.lower(getCreatureName(cid)) == "mushroom sniffer") then
-		chance = math.random(1, 3)
-		if(getTileItemById(position, 18340).uid > 0 and getTileItemById(position, 18340).actionid ~= 100) then
-			if(chance < 3) then
-				doSendMagicEffect(position, CONST_ME_POFF)
-				doTransformItem(getTileItemById(position, 18340).uid, 18218)
-				addEvent(doTransformItem, 40 * 1000, getTileItemById(position, 18218).uid, 18340)
-			elseif(chance == 3) then
-				doTransformItem(getTileItemById(position, 18340).uid, 18341)
-				doSendMagicEffect(position, CONST_ME_GROUNDSHAKER)
+	local tile = Tile(position)
+
+	if string.lower(Creature():getName()) == "mushroom sniffer" then
+		local chance = math.random(1, 3)
+		if(tile:getItemById(18340) and tile:getItemById(18340):getActionId() ~= 100) then
+			if chance < 3 then
+				position:sendMagicEffect(CONST_ME_POFF)
+				tile:getItemById(18340):transform(18218)
+				addEvent(function(position) local tile = Tile(position) if not tile then return end local thing = tile:getItemById(18218) if thing then thing:transform(18340) end end, 40000, position)
+			elseif chance == 3 then
+				tile:getItemById(18340):transform(18341)
+				position:sendMagicEffect(CONST_ME_GROUNDSHAKER)
 			end
 		end
 	end
