554e7ab38f81170efb8f5bd9f295604d3dfa4d4d
Skillert <peter.mollberg@gmail.com>
2014-07-07 20:57:11 +0200

    Minor

--- data/movements/scripts/bigfoot burden quest/bigfootBurdenQuestShooting.lua
+++ data/movements/scripts/bigfoot burden quest/bigfootBurdenQuestShooting.lua
@@ -1,34 +1,43 @@
 local function doCreateDummy(cid, pos)
-	if(getPlayerStorageValue(cid, 902) < 5) then
-		if(getTileItemById(pos, 18226).uid > 0) then
-			doRemoveItem(getTileItemById(pos, 18226).uid, 1)
-		elseif(getTileItemById(pos, 18227).uid > 0) then
-			doRemoveItem(getTileItemById(pos, 18227).uid, 1)
+	local player = Player(cid)
+	local tile = Tile(pos)
+
+	if player:getStorageValue(902) < 5 then
+		if tile:getItemById(18226) then
+			tile:getItemById(18226):remove()
+		elseif tile:getItemById(18227) then
+			tile:getItemById(18227):remove()
 		end
-		doSendMagicEffect(pos, CONST_ME_POFF)
-		doCreateItem(math.random(18226, 18227), 1, pos)
-	elseif(getPlayerStorageValue(cid, 902) == 5) then
-		if(getTileItemById(pos, 18226).uid > 0) then
-			doRemoveItem(getTileItemById(pos, 18226).uid, 1)
-		elseif(getTileItemById(pos, 18227).uid > 0) then
-			doRemoveItem(getTileItemById(pos, 18227).uid, 1)
+		pos:sendMagicEffect(CONST_ME_POFF)
+		local dumdum = math.random(18226, 18227)
+		Game.createItem(dumdum, 1, pos)
+	elseif getPlayerStorageValue(cid, 902) == 5 then
+		if tile:getItemById(18226) then
+			tile:getItemById(18226):remove()
+		elseif tile:getItemById(18227) then
+			tile:getItemById(18227):remove()
 		end
-		mayNotMove(cid, false)
-		setPlayerStorageValue(cid, 900, 9)
+		-- Commenting away mayNotMove since this function no longer exists.
+		-- mayNotMove(cid, false)
+		player:setStorageValue(900, 9)
 	end
 	return addEvent(doCreateDummy, 2 * 1000, cid, pos)
 end
 
 function onStepIn(cid, item, position, lastPosition)
-	if(item.actionid == 8030) then
-		if(getPlayerStorageValue(cid, 900) == 8) then
-			setPlayerStorageValue(cid, 902, 0)
-			mayNotMove(cid, true)
-			doCreateDummy(cid, {x = getPlayerPosition(cid).x, y = getPlayerPosition(cid).y - 5, z = 10})
-			doSendMagicEffect(position, CONST_ME_MAGIC_BLUE)
-		else
-			doTeleportThing(cid, lastPosition)
-		end
+	local player = Player(cid)
+	if not player then
+		return true
+	end
+
+	if player:getStorageValue(900) == 8 then
+		player:setStorageValue(902, 0)
+		-- Commenting away mayNotMove since this function no longer exists.
+		-- mayNotMove(cid, true)
+		doCreateDummy(cid, {x = player:getPosition().x, y = player:getPosition().y - 5, z = 10})
+		position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+	else
+		player:teleportTo(lastPosition)
 	end
 	return true
 end
