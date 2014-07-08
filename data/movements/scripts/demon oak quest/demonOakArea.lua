8ffc31a56ab6c6cf3550843c70c739d05483c552
Skillert <peter.mollberg@gmail.com>
2014-07-08 13:01:10 +0200

    Fixes, cheers @TheSumm and @ninjalulz

--- data/movements/scripts/demon oak quest/demonOakArea.lua
+++ data/movements/scripts/demon oak quest/demonOakArea.lua
@@ -1,6 +1,5 @@
 function onStepIn(cid, item, position, fromPosition)
-	local chance = math.random(1, 24)
-	if Player(cid) and chance == 1 then
+	if Player(cid) and math.random(1, 24) == 1 then
 		doTargetCombatHealth(0, cid, COMBAT_EARTHDAMAGE, -270, -310, CONST_ME_BIGPLANTS)
 	end
 	return true
--- data/movements/scripts/demon oak quest/demonOakTreeEnter.lua
+++ data/movements/scripts/demon oak quest/demonOakTreeEnter.lua
@@ -1,7 +1,7 @@
 local enterPosition = {x=32716, y=32347, z=7}
 local kickPosition = {x=32716, y=32340, z=7}
-local enter_storage = 1013
-local squarestorage = 1014
+local enterStorage = 1013
+local squareStorage = 1014
 
 function onStepIn(cid, item, position, lastPosition, fromPosition, toPosition, actor)
 	local player = Player(cid)
@@ -11,11 +11,11 @@ function onStepIn(cid, item, position, lastPosition, fromPosition, toPosition, a
 
 	if player:getStorageValue(1010) < 1 then
 		if player:getLevel() >= 120 then
-			if player:getStorageValue(squarestorage) == 5 and (not isPlayerInArea({x = 32706, y = 32345, z = 7, stackpos = 255}, {x = 32725, y = 32357, z = 7, stackpos = 255})) then
+			if player:getStorageValue(squareStorage) == 5 and (not isPlayerInArea({x = 32706, y = 32345, z = 7, stackpos = 255}, {x = 32725, y = 32357, z = 7, stackpos = 255})) then
 				player:teleportTo(enterPosition)
 				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 				player:setStorageValue(1011, 1)
-				player:setStorageValue(enter_storage, 1)
+				player:setStorageValue(enterStorage, 1)
 				player:say("I AWAITED YOU! COME HERE AND GET YOUR REWARD!", TALKTYPE_ORANGE_2, false, cid, (positions.demonOak or player:getPosition()))
 			else
 				player:teleportTo(kickPosition)
