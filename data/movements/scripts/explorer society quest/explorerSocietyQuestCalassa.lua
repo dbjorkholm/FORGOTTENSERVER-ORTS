520ab9eddee6f4e51e3563f812d8dfce66127e04
EPuncker <gm.perflex@gmail.com>
2014-07-09 01:13:42 -0300

    rest explorerSocietyQuestCalassa.lua to meta and added player check

--- data/movements/scripts/explorer society quest/explorerSocietyQuestCalassa.lua
+++ data/movements/scripts/explorer society quest/explorerSocietyQuestCalassa.lua
@@ -1,15 +1,18 @@
 function onStepIn(cid, item, position, lastPosition)
-	if(item.actionid == 2070) then
 	local player = Player(cid)
-		if(getPlayerSlotItem(cid, CONST_SLOT_HEAD).itemid == 5461) then
-			player:teleportTo({x = 31914, y = 32713, z = 12})
-			player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
-			player:getPosition():sendMagicEffect(CONST_ME_LOSEENERGY)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You enter the realm of Calassa.")
-		else
-			player:teleportTo(lastPosition)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear helmet of the deep in order to dive.")
-		end
+	if not player then
+		return true
+	end
+
+	local Helmetitem = player:getSlotItem(CONST_SLOT_HEAD)
+	if Helmetitem ~= nil and Helmetitem:getId() == 5461 then
+		player:teleportTo({x = 31914, y = 32713, z = 12})
+		player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
+		player:getPosition():sendMagicEffect(CONST_ME_LOSEENERGY)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You enter the realm of Calassa.")
+	else
+		player:teleportTo(lastPosition)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear helmet of the deep in order to dive.")
 	end
 	return true
 end
