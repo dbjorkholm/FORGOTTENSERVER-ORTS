46e0a4755a26ea26e8ccb40497a3182e0dbec5ff
Skillert <peter.mollberg@gmail.com>
2014-07-09 13:21:54 +0200

    Fix, cheers @PrinterLUA
    Former-commit-id: 36289f6f29483b32f153c9412f27f4392f6cd22f [formerly c226d22a96cadf91bd747edb557847fbf1ee4d3a]
    Former-commit-id: 175b6557039c7db2bea44c2990f10788e1e09c8d

--- data/movements/scripts/bigfoot burden quest/bigfootBurdenQuestGTCS.lua
+++ data/movements/scripts/bigfoot burden quest/bigfootBurdenQuestGTCS.lua
@@ -11,7 +11,7 @@ local destination = {
 	[3135] = {pos = Position(33001, 31915, 9), storage = 16}, -- Gnomebase Alpha
 	[3136] = {pos = Position(32904, 31894, 13), storage = 16},
 	[3137] = {pos = Position(32979, 31907, 9), storage = 16},
-	[35669] = {pos = Position(32986, 31864, 9), storage = 1}, -- leave warzone 3 
+	[35669] = {pos = Position(32986, 31864, 9), storage = 1}, -- leave warzone 3
 	[3215] = {pos = Position(32369, 32241, 7), storage = 1, crystal = true},
 	[3216] = {pos = Position(32212, 31133, 7), storage = 1, crystal = true},
 	[3217] = {pos = Position(32317, 32825, 7), storage = 1, crystal = true},
@@ -20,36 +20,39 @@ local destination = {
 }
 
 function onStepIn(cid, item, position, lastPosition)
-	local aid = destination[item.actionid]
 	local player = Player(cid)
-	if player ~= nil then
-		if aid then
-			if player:getStorageValue(900) >= aid.storage then
-				if aid.crystal == true then
-					if player:getItemCount(18457) >= 1 then
-						player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-						player:teleportTo(aid.pos)
-						player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-						player:removeItem(18457, 1)
-					else
-						player:teleportTo(lastPosition)
-						player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-						player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You need a teleport crystal to use this device.")
-					end
-				else
-					player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-					player:teleportTo(aid.pos)
-					player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-				end
+	if not player then
+		return
+	end
+
+	local aid = destination[item.actionid]
+	if not aid then
+		return
+	end
+
+	if player:getStorageValue(900) >= aid.storage then
+		if aid.crystal == true then
+			if player:removeItem(18457, 1) then
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+				player:teleportTo(aid.pos)
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 			else
-				if aid.storage == 2 then
-					player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have no idea on how to use this device. Xelvar in Kazordoon might tell you more about it.")	
-				else
-					player:teleportTo(lastPosition)
-					player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-					player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Sorry, you haven't got access to use this teleport!")
-				end
+				player:teleportTo(lastPosition)
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You need a teleport crystal to use this device.")
 			end
+		else
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			player:teleportTo(aid.pos)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		end
+	else
+		if aid.storage == 2 then
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have no idea on how to use this device. Xelvar in Kazordoon might tell you more about it.") 
+		else
+			player:teleportTo(lastPosition)
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Sorry, you haven't got access to use this teleport!")
 		end
 	end
 	return true
