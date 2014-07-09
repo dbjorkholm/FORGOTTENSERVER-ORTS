2c317ec4c2114c629b131e8b738fb372bc67a167
Skillert <peter.mollberg@gmail.com>
2014-07-09 09:44:48 +0200

    converted some more to meta

--- data/movements/scripts/dreamer challenge quest/dreamerChallengeQuestStoneTeleport.lua
+++ data/movements/scripts/dreamer challenge quest/dreamerChallengeQuestStoneTeleport.lua
@@ -1,11 +1,16 @@
 function onStepIn(cid, item, position, fromPosition)
-	doTeleportThing(cid, {x = 32920, y = 32296, z = 13})
-	doSendMagicEffect({x = 32920, y = 32296, z = 13}, CONST_ME_TELEPORT)
- 	doTransformItem(getTileItemById({x = 32873, y = 32263, z = 14}, 1946).uid, 1945)
-	doTransformItem(getTileItemById(position, 1387).uid, 1355)
-	doTransformItem(getTileItemById({x = 32874, y = 32263, z = 14}, 3733).uid, 3729)
-	doTransformItem(getTileItemById({x = 32875, y = 32263, z = 14}, 3734).uid, 3730)
-	doTransformItem(getTileItemById({x = 32874, y = 32264, z = 14}, 3735).uid, 3731)
-	doTransformItem(getTileItemById({x = 32875, y = 32264, z = 14}, 3736).uid, 3732)
+	local player = Player(cid)
+	if not player then
+		return false
+	end
+
+	player:teleportTo(Position(32920, 32296, 13))
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+ 	Tile(Position(32873, 32263, 14)):getItemById(1946):transform(1945)
+	Tile(position):getItemById(1387):transform(1355)
+	Tile(Position(32874, 32263, 14)):getItemById(3733):transform(3729)
+	Tile(Position(32875, 32263, 14)):getItemById(3734):transform(3730)
+	Tile(Position(32874, 32264, 14)):getItemById(3735):transform(3731)
+	Tile(Position(32875, 32264, 14)):getItemById(3736):transform(3732)
 	return true
 end
