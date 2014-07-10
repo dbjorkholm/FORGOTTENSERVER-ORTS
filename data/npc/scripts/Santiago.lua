a47824128c03c106213b01f8b915d3da66569f70
PrinterLUA <mastero_90@hotmail.com>
2014-07-10 17:15:09 +0200

    Minor fix and minor content added
    Former-commit-id: b7c40ba5fe2de21b8bbde944fa4968acf5cb7d0d

--- data/npc/scripts/Santiago.lua
+++ data/npc/scripts/Santiago.lua
@@ -5,7 +5,23 @@ NpcSystem.parseParameters(npcHandler)
 function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
 function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
-function onThink()				npcHandler:onThink()					end
+local random_texts = {
+	"Evil little beasts... I hope someone helps me fight them.",
+	"Nasty creepy crawlies!",
+	"Don't be shy, can't hurt to greet me with 'hello' or 'hi'!", 
+	"Hey! You over there, could you help me with a little quest? Just say 'hi' or 'hello' to talk to me!"
+}
+
+local rnd_sounds = 0
+function onThink()
+	if rnd_sounds < os.time() then
+		rnd_sounds = (os.time() + 10)
+		if math.random(100) < 20 then
+			Npc():say(random_texts[math.random(#random_texts)], TALKTYPE_SAY)
+		end
+	end
+	npcHandler:onThink()
+end
 
 local storeTalkCid = {}
 
