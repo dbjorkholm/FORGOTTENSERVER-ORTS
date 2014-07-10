315e366798e63653fe0d8b57635c35b1ebcb23f0
Skillert <peter.mollberg@gmail.com>
2014-07-10 13:09:50 +0200

    Fixes, cheers @TheSumm
    Former-commit-id: 0d66446cff0c30ebb599f674ec8bd9c6da0936c4 [formerly b884438ef64c542f9115311aa17e45087a5ddb99]
    Former-commit-id: bf713de2fce818cbb93d25449e50a4a6e372880e

--- data/movements/scripts/gravedigger quest/tps.lua
+++ data/movements/scripts/gravedigger quest/tps.lua
@@ -1,18 +1,22 @@
-function onStepIn(cid, item, position)
-
 local amfora = Position(32988, 32397, 9)
 local amfora2 = Position(32987, 32401, 9)
-local player = Player(cid)
-local creature = Creature(cid)
 
-	if item.actionid == 4530 and (player:getStorageValue(9995) == 1) and (player:getStorageValue(9996) < 1) then
+function onStepIn(cid, item, position)
+	local player = Player(cid)
+	local creature = Creature(cid)
+
+	if not player then
+		return true
+	end
+
+	if item.actionid == 4530 and player:getStorageValue(9995) == 1 and player:getStorageValue(9996) < 1 then
 		creature:teleportTo(amfora2)
 		amfora2:sendMagicEffect(CONST_ME_POFF)
 	else
 		creature:teleportTo(amfora)
 		amfora:sendMagicEffect(CONST_ME_POFF)
 	end
-	
+
 	if item.actionid == 4531 then
 		creature:teleportTo(amfora)
 		amfora:sendMagicEffect(CONST_ME_POFF)
