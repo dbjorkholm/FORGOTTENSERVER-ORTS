84415e7c32375c788919135cc3cf01da8ffe7ee5
Skillert <peter.mollberg@gmail.com>
2014-07-10 13:27:49 +0200

    Fixes, cheers @TheSumm and @ninjalulz
    Former-commit-id: c74a35b455c34cbeb5712928e50c78c8e89e76b2 [formerly f8b5bff25ff2edf38d67e36804aa1240b6fbc4a8]
    Former-commit-id: 4b5fd86baa9d0f0518d28edb1124cfc2507dea2a

--- data/movements/scripts/gravedigger quest/tps3.lua
+++ data/movements/scripts/gravedigger quest/tps3.lua
@@ -3,7 +3,6 @@ local dormitori2 = Position(33015, 32440, 10)
 
 function onStepIn(cid, item, position)
 	local player = Player(cid)
-	local creature = Creature(cid)
 
 	if not player then
 		return true
@@ -11,11 +10,11 @@ function onStepIn(cid, item, position)
 
 	if item.actionid == 4534 and player:getStorageValue(9931) == 1 and player:getStorageValue(9932) < 1 then
 		player:setStorageValue(9932,1)
-		creature:teleportTo(dormitori2)
+		player:teleportTo(dormitori2)
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You should hurry, try not to dwell here any longer than a few minutes.')
 		dormitori2:sendMagicEffect(CONST_ME_POFF)
 	else
-		creature:teleportTo(dormitori)
+		player:teleportTo(dormitori)
 		dormitori:sendMagicEffect(CONST_ME_POFF)
 	end
 	return true
--- data/movements/scripts/gravedigger quest/tps4.lua
+++ data/movements/scripts/gravedigger quest/tps4.lua
@@ -3,17 +3,16 @@ local sacrifice2 = Position(33015, 32422, 11)
 
 function onStepIn(cid, item, position)
 	local player = Player(cid)
-	local creature = Creature(cid)
 
 	if not player then
 		return true
 	end
 
 	if item.actionid == 4541 and player:getStorageValue(9948) == 1 then
-		creature:teleportTo(sacrifice2)
+		player:teleportTo(sacrifice2)
 		sacrifice2:sendMagicEffect(CONST_ME_POFF)
 	else
-		creature:teleportTo(sacrifice)
+		player:teleportTo(sacrifice)
 		sacrifice:sendMagicEffect(CONST_ME_POFF)
 	end
 	return true
