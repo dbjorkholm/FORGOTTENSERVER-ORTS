5e1f19511a4914ffd053e3b4dedc78e5ed1286f2
PrinterLUA <mastero_90@hotmail.com>
2014-07-10 16:13:18 +0200

    Continue adding content on Rook Tutorial Island
    *Santiago & Zirella are done, just missing keywords and real outfits!
    *Added more missing content into Santiago and also Santiago Questlog
    *Blocking players from going to fast on their mission or trying to go
    out from their mission! (Ex, if you are up on the Santiago mountain and
    talk to him and trying go back or forward to Zirella, you will not be
    able to go pass! As Rl Tibia)
    *Added Zirella & Zirella Questlog
    *Added "branch" mission!

--- data/movements/scripts/rookgaard/rookgaard tutorial island/rookgaardTutorialIslandTutorialTiles.lua
+++ data/movements/scripts/rookgaard/rookgaard tutorial island/rookgaardTutorialIslandTutorialTiles.lua
@@ -7,10 +7,11 @@ local config = {
 	[50063] = {tutorialId = 22, storageValue = 6},
 	[50064] = {tutorialId = 4, storageValue = 7},
 	[50065] = {effPos = Position(32033, 32278, 8), text = "You can't see any cockroaches here. 'Open' this chest and see if you can find something to light the room better.", storageValue = 8},
-	[50066] = {effPos = Position(32033, 32278, 8), text = "You can't see so well here. 'Open' this chest and see if you can find something to light the room.", storageValue = 9},
+	[50066] = {text = "Maybe you shouldn't stay in this forest longer than necessary. Zirella is waiting for her firewood!", storageValue = 13},
 	[50067] = {effPos = Position(32035, 32285, 8), text = "Look at the metallic object on the floor - this is a sewer grate. Right-click on it and select 'Use' to climb down.", storageValue = 10},
 	[50068] = {tutorialId = 7, text = "You smell stinky cockroaches around here. When you see one, walk to it and attack it by left-clicking it in your battle list!", storageValue = 11},
 	[50069] = {tutorialId = 23, effPos = Position(32035, 32285, 9), text = "Right-click on the lower right end of the ladder - anywhere in the red frame - and select 'Use' to climb up.", storageValue = 12},
+	[50075] = {"Do you have trouble finding those dead trees? Here are some - just 'Use' them to break a branch.", storageValue = 14, effPos = Position(32067, 32281, 7), effPos2 = Position(32073, 32276, 7)}
 }
 
 function onStepIn(cid, item, position, fromPosition)
@@ -21,13 +22,30 @@ function onStepIn(cid, item, position, fromPosition)
 
 	local targetTableAid = config[item.actionid]
 	if not targetTableAid then
+		if item.actionid == 50069 then
+			if player:getStorageValue(Storage.RookgaardTutorialIsland.cockroachLegsMsgStorage) < 1 then
+				return
+			end
+		elseif item.actionid == 50076 then
+			print(1)
+			if player:getStorageValue(Storage.RookgaardTutorialIsland.tutorialHintsStorage) == 15 then
+				player:setStorageValue(Storage.RookgaardTutorialIsland.tutorialHintsStorage, 16)
+				Position(32062, 32271, 7):sendMagicEffect(CONST_ME_TUTORIALARROW)
+				Position(32062, 32271, 7):sendMagicEffect(CONST_ME_TUTORIALARROW)
+				player:sendTutorial(24)
+			end
+		elseif item.actionid == 50077 then
+			if player:getStorageValue(Storage.RookgaardTutorialIsland.ZirellaNpcGreetStorage) == 8 and player:getStorageValue(Storage.RookgaardTutorialIsland.tutorialHintsStorage) < 17  then
+				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "This is Zirella's door. Right-click on the lower part of the door and select 'Use' to open it.")
+				player:setStorageValue(Storage.RookgaardTutorialIsland.tutorialHintsStorage, 17)
+			end
+			if player:getStorageValue(50084) == 1 then
+				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Good, now continue to the east to find a place to try out your shovel.")
+				player:setStorageValue(50084, 2)
+			end
+		end
 		return
-	end
-
-	if item.actionid == 50069 and player:getStorageValue(Storage.RookgaardTutorialIsland.cockroachLegsMsgStorage) < 1 then
-		return
-	end
-		
+	end	
 
 	if player:getStorageValue(Storage.RookgaardTutorialIsland.tutorialHintsStorage) < targetTableAid.storageValue then
 		player:setStorageValue(Storage.RookgaardTutorialIsland.tutorialHintsStorage, targetTableAid.storageValue)
@@ -35,6 +53,13 @@ function onStepIn(cid, item, position, fromPosition)
 		if targetTableAid.tutorialId then
 			player:sendTutorial(targetTableAid.tutorialId)
 		end
+
+		if item.actionid == 50075 then
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, targetTableAid.text)
+			targetTableAid.effPos:sendMagicEffect(CONST_ME_TUTORIALARROW)
+			targetTableAid.effPos2:sendMagicEffect(CONST_ME_TUTORIALARROW)
+		end
+				
 
 		if targetTableAid.text then
 			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, targetTableAid.text)
--- data/npc/scripts/Santiago.lua
+++ data/npc/scripts/Santiago.lua
@@ -15,8 +15,10 @@ local function greetCallback(cid)
 		player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 1)
 		player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 1)
 		npcHandler:setMessage(MESSAGE_GREET, "Hello ".. player:getName() ..", nice to see you on Rookgaard! I saw you walking by and wondered if you could help me. Could you? Please, say {yes}!")
+		storeTalkCid[cid] = 0
 	elseif player:getStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage) == 1 then
 		npcHandler:setMessage(MESSAGE_GREET, "Oh, ".. player:getName() ..", it's you again! It's probably impolite to disturb a busy adventurer like you, but I really need help. Please, say {yes}!")
+		storeTalkCid[cid] = 0
 	elseif player:getStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage) == 2 then
 		npcHandler:say("Oh, what's wrong? As I said, simply go to my house south of here and go upstairs. Then come back and we'll continue our chat.", cid)
 		Position(32033, 32277, 6):sendMagicEffect(CONST_ME_TUTORIALARROW)
@@ -43,21 +45,24 @@ local function greetCallback(cid)
 			return false
 		end
 	elseif player:getStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage) == 7 then
-		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I was talking about health?")
+		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I asked you if you saw your 'level up'! You did, {right}?")
 		storeTalkCid[cid] = 4
 	elseif player:getStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage) == 8 then
-		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I was about to show you something?")
+		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I asked you if those nasty cockroaches {hurt} you! Did they?")
 		storeTalkCid[cid] = 5
 	elseif player:getStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage) == 9 then
-		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I was about to show you how you regain health, right?")
+		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I asked you if I should demonstrate some damage on you. Let's do it, {okay}?")
 		storeTalkCid[cid] = 6
 	elseif player:getStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage) == 10 then
-		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I gave you a fish to eat?")
+		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I was about to show you how you regain health, right?")
 		storeTalkCid[cid] = 7
 	elseif player:getStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage) == 11 then
-		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I asked you if you saw Zirella! Did you?")
+		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I gave you a fish to eat?")
 		storeTalkCid[cid] = 8
 	elseif player:getStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage) == 12 then
+		npcHandler:setMessage(MESSAGE_GREET, "Welcome back! Where were we... ? Ah, right, I asked you if you saw Zirella! Did you?")
+		storeTalkCid[cid] = 9
+	elseif player:getStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage) == 13 then
 		npcHandler:setMessage(MESSAGE_GREET, "Hello again, ".. player:getName() .."! It's great to see you. If you like, we can chat a little. Just use the highlighted {keywords} again to choose a {topic}.")
 	end
 	return true
@@ -69,21 +74,34 @@ local function creatureSayCallback(cid, type, msg)
 	end
 	
 	local player = Player(cid)
-	if msgcontains(msg, "yes") or msgcontains(msg, "right") then
+	if msgcontains(msg, "yes") or msgcontains(msg, "right") or msgcontains(msg, "ok") then
 		if storeTalkCid[cid] == 0 then
 			npcHandler:say("Great, please go to my house, just a few steps south of here. Upstairs in my room, you'll find a chest. You can keep what you find inside of it! Come back after you got it and greet me to talk to me again. {Yes}?", cid)
+			Position(32033, 32277, 6):sendMagicEffect(CONST_ME_TUTORIALARROW)
 			storeTalkCid[cid] = 1
 		elseif storeTalkCid[cid] == 1 then
 			npcHandler:say("Alright! Do you see the button called 'Quest Log'? There you can check the status of quests, like this one. {Bye} for now!", cid)
 			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 2)
 			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 2)
-			Position(32033, 32277, 6):sendMagicEffect(CONST_ME_TUTORIALARROW)
 			player:sendTutorial(3)
 			npcHandler:releaseFocus(cid)
 		elseif storeTalkCid[cid] == 2 then
-			npcHandler:say("Ah, no need to say anything, I can see it suits you perfectly. Now we're getting to the fun part, let's get you armed! Are you ready for some {action}?", cid)
-			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 4)
-			storeTalkCid[cid] = 3
+			if player:getItemCount(2651) >= 1 then
+				local coatSlot = player:getSlotItem(CONST_SLOT_ARMOR)
+				if coatSlot then
+					npcHandler:say("Ah, no need to say anything, I can see it suits you perfectly. Now we're getting to the fun part, let's get you armed! Are you ready for some {action}?", cid)
+					player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 4)
+					storeTalkCid[cid] = 3
+				else
+					npcHandler:say("Oh, you don't wear it properly yet. You need to drag and drop it from your bag to your armor slot. Here, let me show you again. Is it a little {clearer} now?", cid)
+					player:sendTutorial(5)
+					storeTalkCid[cid] = 2
+				end
+			else
+				player:addItem(2651, 1)
+				npcHandler:say("Oh no, did you lose my coat? Well, lucky you, I have a spare one here. Don't lose it again! Now we're getting to the fun part, let's get you armed! Are you ready for some {action}?", cid)
+				storeTalkCid[cid] = 3
+			end
 		elseif storeTalkCid[cid] == 3 then
 			npcHandler:say("I knew I could count on you. Here, take this good and sturdy weapon in your hand. Then go back to my house and down the ladder. Good luck, and {bye} for now!", cid)
 			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 4)
@@ -92,45 +110,54 @@ local function creatureSayCallback(cid, type, msg)
 			player:addItem(2382, 1)
 			npcHandler:releaseFocus(cid)
 		elseif storeTalkCid[cid] == 4 then
-			npcHandler:say("Oh, but I saw it! Now you have more health points, can carry more stuff and walk faster. Talking about health, did you get {hurt} by those cockroaches?", cid)
+			npcHandler:say("That's just great! Now you have more health points, can carry more stuff and walk faster. Talking about health, did you get {hurt} by those cockroaches?", cid)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 8)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 6)
 			storeTalkCid[cid] = 5
 		elseif storeTalkCid[cid] == 5 then
+			npcHandler:say("Really? You look fine to me, must have been just a scratch. Well, there are much more dangerous monsters than cockroaches out there. Take a look at your status bar. You have 155 Health right now. I'll show you something, {yes}?", cid)
+			player:sendTutorial(19)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 9)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 7)
+			storeTalkCid[cid] = 6
+		elseif storeTalkCid[cid] == 6 then
 			npcHandler:say("This is an important lesson from me - an experienced veteran fighter. Take this! Look at your status bar again. As you can see, you've lost health. Now I'll tell you how to heal that, {yes}?", cid)
 			player:getPosition():sendMagicEffect(CONST_ME_MORTAREA)
 			Npc():getPosition():sendMagicEffect(CONST_ME_MORTAREA)
 			player:addHealth(-20)
-			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 9)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 10)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 8)
 			player:sendTutorial(19)
-			storeTalkCid[cid] = 6
-		elseif storeTalkCid[cid] == 6 then
+			storeTalkCid[cid] = 7
+		elseif storeTalkCid[cid] == 7 then
 			npcHandler:say({"Here, take this fish which I've caught myself. Find it in your inventory, then 'Use' it to eat it. This will slowly refill your health. ...",
 					"By the way: If your hitpoints are below 150, you will regenerate back to 150 hitpoints after few seconds as long as you are not hungry, outside a protection zone and do not have a battle sign. {Easy}, yes?"
 					}, cid, 0, 1, 3000)
-			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 6)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 9)
 			player:addItem(2667, 1)
-			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 10)
-			storeTalkCid[cid] = 7
-		elseif storeTalkCid[cid] == 7 then
-			npcHandler:say("I knew you'd get it right away. You can loot food from many creatures, such as deer and rabbits. You can find them in the forest nearby. By the way... have you seen {Zirella}?", cid)
 			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 11)
-			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 7)
 			storeTalkCid[cid] = 8
 		elseif storeTalkCid[cid] == 8 then
-			npcHandler:say("Really?? She was looking for someone to help her. Maybe you could go and see her. She lives just to the east and down the mountain. So, thank you again and {bye} for now!", cid)
+			npcHandler:say("I knew you'd get it right away. You can loot food from many creatures, such as deer and rabbits. You can find them in the forest nearby. By the way... have you seen {Zirella}?", cid)
 			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 12)
-			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 8)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 10)
+			storeTalkCid[cid] = 9
+		elseif storeTalkCid[cid] == 9 then
+			npcHandler:say("Really?? She was looking for someone to help her. Maybe you could go and see her. She lives just to the east and down the mountain. So, thank you again and {bye} for now!", cid)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 13)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 11)
 			player:addMapMark(Position(32045, 32270, 6), MAPMARK_GREENSOUTH, "To Zirella")
 			npcHandler:releaseFocus(cid)
 		end
 	elseif msgcontains(msg, "hurt") then
-		if storeTalkCid[cid] == 5 then
+		if storeTalkCid[cid] == 6 then
 			npcHandler:say("This is an important lesson from me - an experienced veteran fighter. Take this! Look at your status bar again. As you can see, you've lost health. Now I'll tell you how to heal that, {yes}?", cid)
 			player:getPosition():sendMagicEffect(CONST_ME_MORTAREA)
 			Npc():getPosition():sendMagicEffect(CONST_ME_MORTAREA)
 			player:addHealth(-20)
-			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 9)
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 8)
 			player:sendTutorial(19)
-			storeTalkCid[cid] = 6
+			storeTalkCid[cid] = 7
 		end
 	elseif msgcontains(msg, "action") then
 		if storeTalkCid[cid] == 3 then
@@ -142,11 +169,11 @@ local function creatureSayCallback(cid, type, msg)
 			npcHandler:releaseFocus(cid)
 		end
 	elseif msgcontains(msg, "easy") then
-		if storeTalkCid[cid] == 7 then
+		if storeTalkCid[cid] == 8 then
 			npcHandler:say("I knew you'd get it right away. You can loot food from many creatures, such as deer and rabbits. You can find them in the forest nearby. By the way... have you seen {Zirella}?", cid)
 			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoNpcGreetStorage, 11)
-			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 7)
-			storeTalkCid[cid] = 8
+			player:setStorageValue(Storage.RookgaardTutorialIsland.SantiagoQuestLog, 10)
+			storeTalkCid[cid] = 9
 		end
 	end
 	return true
