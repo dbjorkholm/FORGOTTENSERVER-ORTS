3d9df1b6787966ae9fd7948c6b260ac333a092b8
Skillert <peter.mollberg@gmail.com>
2014-07-10 20:05:10 +0200

    Changed message and added effect, cheers @ninjalulz and @EPuncker

--- data/movements/scripts/explorer society quest/explorerSocietyQuestCalassa.lua
+++ data/movements/scripts/explorer society quest/explorerSocietyQuestCalassa.lua
@@ -12,7 +12,8 @@ function onStepIn(cid, item, position, fromPosition)
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You enter the realm of Calassa.")
 	else
 		player:teleportTo(fromPosition)
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear helmet of the deep in order to dive.")
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear an underwater exploration helmet in order to dive.")
 	end
 	return true
 end
