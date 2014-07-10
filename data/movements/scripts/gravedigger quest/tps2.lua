4b5fd86baa9d0f0518d28edb1124cfc2507dea2a
Skillert <peter.mollberg@gmail.com>
2014-07-10 13:27:49 +0200

    Fixes, cheers @TheSumm and @ninjalulz
    Former-commit-id: f8b5bff25ff2edf38d67e36804aa1240b6fbc4a8

--- data/movements/scripts/gravedigger quest/tps2.lua
+++ data/movements/scripts/gravedigger quest/tps2.lua
@@ -3,17 +3,16 @@ local brain2 = Position(33022, 32334, 10)
 
 function onStepIn(cid, item, position)
 	local player = Player(cid)
-	local creature = Creature(cid)
 
 	if not player then
 		return true
 	end
 
 	if item.actionid == 4532 and player:getStorageValue(9998) == 1 and player:getStorageValue(9999) < 1 then
-		creature:teleportTo(brain2)
+		player:teleportTo(brain2)
 		brain2:sendMagicEffect(CONST_ME_POFF)
 	else
-		creature:teleportTo(brain)
+		player:teleportTo(brain)
 		brain:sendMagicEffect(CONST_ME_POFF)
 	end
 	return true
