b7c40ba5fe2de21b8bbde944fa4968acf5cb7d0d
PrinterLUA <mastero_90@hotmail.com>
2014-07-10 17:15:09 +0200

    Minor fix and minor content added

--- data/actions/scripts/rookgaard/rookgaard tutorial island/rookgaardTutorialIslandTutorialZirellaDoor.lua
+++ data/actions/scripts/rookgaard/rookgaard tutorial island/rookgaardTutorialIslandTutorialZirellaDoor.lua
@@ -1,10 +1,12 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player= Player(cid)
-	if player:getStorageValue(Storage.RookgaardTutorialIsland.ZirellaNpcGreetStorage) > 7 then
-		Item(item.uid):transform(item.itemid + 1)
-		player:teleportTo(toPosition, true)
-	else
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The door seems to be sealed against unwanted intruders.")
+	if item.itemid == 6898 then
+		if player:getStorageValue(Storage.RookgaardTutorialIsland.ZirellaNpcGreetStorage) > 7 then
+			Item(item.uid):transform(item.itemid + 1)
+			player:teleportTo(toPosition, true)
+		else
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The door seems to be sealed against unwanted intruders.")
+		end
 	end
 	return true
 end
