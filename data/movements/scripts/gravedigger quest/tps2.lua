b884438ef64c542f9115311aa17e45087a5ddb99
Skillert <peter.mollberg@gmail.com>
2014-07-10 13:09:50 +0200

    Fixes, cheers @TheSumm

--- data/movements/scripts/gravedigger quest/tps2.lua
+++ data/movements/scripts/gravedigger quest/tps2.lua
@@ -1,18 +1,22 @@
-function onStepIn(cid, item, position)
-
 local brain = Position(33022, 32338, 10)
 local brain2 = Position(33022, 32334, 10)
-local player = Player(cid)
-local creature = Creature(cid)
 
-	if item.actionid == 4532 and (player:getStorageValue(9998) == 1) and (player:getStorageValue(9999) < 1) then
+function onStepIn(cid, item, position)
+	local player = Player(cid)
+	local creature = Creature(cid)
+
+	if not player then
+		return true
+	end
+
+	if item.actionid == 4532 and player:getStorageValue(9998) == 1 and player:getStorageValue(9999) < 1 then
 		creature:teleportTo(brain2)
 		brain2:sendMagicEffect(CONST_ME_POFF)
 	else
 		creature:teleportTo(brain)
 		brain:sendMagicEffect(CONST_ME_POFF)
 	end
-	
+
 	if item.actionid == 4533 then
 		creature:teleportTo(brain)
 		brain:sendMagicEffect(CONST_ME_POFF)
--- data/movements/scripts/gravedigger quest/tps3.lua
+++ data/movements/scripts/gravedigger quest/tps3.lua
@@ -1,11 +1,15 @@
-function onStepIn(cid, item, position)
-
 local dormitori = Position(33018, 32437, 10)
 local dormitori2 = Position(33015, 32440, 10)
-local player = Player(cid)
-local creature = Creature(cid)
 
-	if item.actionid == 4534 and (player:getStorageValue(9931) == 1) and (player:getStorageValue(9932) < 1) then
+function onStepIn(cid, item, position)
+	local player = Player(cid)
+	local creature = Creature(cid)
+
+	if not player then
+		return true
+	end
+
+	if item.actionid == 4534 and player:getStorageValue(9931) == 1 and player:getStorageValue(9932) < 1 then
 		player:setStorageValue(9932,1)
 		creature:teleportTo(dormitori2)
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You should hurry, try not to dwell here any longer than a few minutes.')
@@ -14,7 +18,7 @@ local creature = Creature(cid)
 		creature:teleportTo(dormitori)
 		dormitori:sendMagicEffect(CONST_ME_POFF)
 	end
-	
+
 	if item.actionid == 4535 then
 		creature:teleportTo(dormitori)
 		dormitori:sendMagicEffect(CONST_ME_POFF)
