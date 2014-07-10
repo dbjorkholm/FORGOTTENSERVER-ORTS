1c849edc5411d23c79e0e7bfc3dd5fe57c84c63b
Skillert <peter.mollberg@gmail.com>
2014-07-10 13:01:04 +0200

    Fixes, cheers @TheSumm

--- data/movements/scripts/explorer society quest/explorerSocietyQuestCalassa.lua
+++ data/movements/scripts/explorer society quest/explorerSocietyQuestCalassa.lua
@@ -1,13 +1,17 @@
 function onStepIn(cid, item, position, fromPosition)
 	local player = Player(cid)
-		if player:getSlotItem(CONST_SLOT_HEAD) == 5461 then
-			player:teleportTo(Position(31914, 32713, 12))
-			player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
-			player:getPosition():sendMagicEffect(CONST_ME_LOSEENERGY)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You enter the realm of Calassa.")
-		else
-			player:teleportTo(fromPosition)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear helmet of the deep in order to dive.")
-		end
+	if not player then
+		return true
+	end
+
+	if player:getSlotItem(CONST_SLOT_HEAD) == 5461 then
+		player:teleportTo(Position(31914, 32713, 12))
+		player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
+		player:getPosition():sendMagicEffect(CONST_ME_LOSEENERGY)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You enter the realm of Calassa.")
+	else
+		player:teleportTo(fromPosition)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear helmet of the deep in order to dive.")
+	end
 	return true
 end
