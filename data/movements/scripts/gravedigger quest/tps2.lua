05a0cb870ef437334ffbcaaa3e4f224387f07b08
Skillert <peter.mollberg@gmail.com>
2014-07-10 12:19:51 +0200

    Minor cleanup and optimization of gravediggerQ movement, removed unused script

--- data/movements/scripts/gravedigger quest/tps2.lua
+++ data/movements/scripts/gravedigger quest/tps2.lua
@@ -1,22 +1,21 @@
-function onStepIn(cid, item, pos)
+function onStepIn(cid, item, position)
 
-local brain = {x = 33022, y = 32338, z = 10}
-local brain2 = {x = 33022, y = 32334, z = 10}
+local brain = Position(33022, 32338, 10)
+local brain2 = Position(33022, 32334, 10)
 local player = Player(cid)
 local creature = Creature(cid)
 
 	if item.actionid == 4532 and (player:getStorageValue(9998) == 1) and (player:getStorageValue(9999) < 1) then
 		creature:teleportTo(brain2)
-		Position(brain2):sendMagicEffect(CONST_ME_POFF)
+		brain2:sendMagicEffect(CONST_ME_POFF)
 	else
 		creature:teleportTo(brain)
-		Position(brain):sendMagicEffect(CONST_ME_POFF)
+		brain:sendMagicEffect(CONST_ME_POFF)
 	end
 	
 	if item.actionid == 4533 then
 		creature:teleportTo(brain)
-		Position(brain):sendMagicEffect(CONST_ME_POFF)
+		brain:sendMagicEffect(CONST_ME_POFF)
 	end
-	
-
+	return true
 end
