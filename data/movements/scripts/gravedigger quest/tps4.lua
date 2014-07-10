bf713de2fce818cbb93d25449e50a4a6e372880e
Skillert <peter.mollberg@gmail.com>
2014-07-10 13:09:50 +0200

    Fixes, cheers @TheSumm
    Former-commit-id: b884438ef64c542f9115311aa17e45087a5ddb99

--- data/movements/scripts/gravedigger quest/tps4.lua
+++ data/movements/scripts/gravedigger quest/tps4.lua
@@ -1,18 +1,22 @@
-function onStepIn(cid, item, position)
-
 local sacrifice = Position(33021, 32419, 11)
 local sacrifice2 = Position(33015, 32422, 11)
-local player = Player(cid)
-local creature = Creature(cid)
 
-	if item.actionid == 4541 and (player:getStorageValue(9948) == 1) then
+function onStepIn(cid, item, position)
+	local player = Player(cid)
+	local creature = Creature(cid)
+
+	if not player then
+		return true
+	end
+
+	if item.actionid == 4541 and player:getStorageValue(9948) == 1 then
 		creature:teleportTo(sacrifice2)
 		sacrifice2:sendMagicEffect(CONST_ME_POFF)
 	else
 		creature:teleportTo(sacrifice)
 		sacrifice:sendMagicEffect(CONST_ME_POFF)
 	end
-	
+
 	if item.actionid == 4542 then
 		creature:teleportTo(sacrifice)
 		sacrifice:sendMagicEffect(CONST_ME_POFF)
