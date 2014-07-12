995485786c387fa67d37f1917f283e52af70d149
edumntg <edumntg@gmail.com>
2014-07-12 01:12:33 -0430

    Added Achievements Lib and Achievements Commands for GOD in talkactions. (Use them to add/remove achievements from players)

--- data/libs.lua
+++ data/libs.lua
@@ -5,4 +5,5 @@ dofile('data/lib/StorageTables.lua')
 dofile('data/lib/SvargrondArenaQuest.lua')
 dofile('data/lib/customfunctions.lua')
 dofile('data/lib/012-tables.lua')
+dofile('data/lib/achievements.lua')
 
