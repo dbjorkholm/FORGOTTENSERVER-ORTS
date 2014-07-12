183aff585b8f53d52d305f3f3a2460fee8fdf8ea
edumntg <edumntg@gmail.com>
2014-07-12 13:42:59 -0430

    Removed Rashid worldchange from startup.lua. It was already added and I didn't noticed it.

--- data/globalevents/scripts/startup.lua
+++ data/globalevents/scripts/startup.lua
@@ -36,8 +36,6 @@ local config = {
 local yasirEnabled = false
 local yasirChance = 20
 
-local rashidEnabled = true
-
 function onStartup()
 	db.query("TRUNCATE TABLE `players_online`")
 	db.query("DELETE FROM `guild_wars` WHERE `status` = 0")
@@ -60,34 +58,6 @@ function onStartup()
 			end
 			Game.loadMap('data/world/yasir/' .. randTown['mapName'] .. '.otbm')
 			addEvent(function() local npc = Game.createNpc('Yasir', randTown['yasirPosition']) if npc then npc:setMasterPos(randTown['yasirPosition'], 3) end end, 3000)
-		end
-	end
-	
-	-- Rashid (Daily)
-	if rashidEnabled then
-		local days =
-		{
-			--please check positions
-			[1] = {x = 32328, y = 31782, z = 6}, --Sunday // Domingo
-			[2] = {x = 32207, y = 31155, z = 7}, --Monday // Lunes
-			[3] = {x = 32300, y = 32837, z = 7}, --Tuesday // Martes
-			[4] = {x = 32577, y = 32753, z = 7}, --Wednesday // Miercoles
-			[5] = {x = 33066, y = 32879, z = 6}, --Thursday // Jueves
-			[6] = {x = 33235, y = 32483, z = 7}, --Friday // Viernes
-			[7] = {x = 33166, y = 31810, z = 6} -- Saturday // Sabado
-		}
-		
-		local day = os.date("*t").wday
-		if days[day] then
-			local pos = Position(days[day].x, days[day].y, days[day].z)
-			local npc = Game.createNpc('Rashid', pos)
-			if npc then
-				npc:setMasterPos(pos, 3)
-			else
-				print("[!] -> Cannot set masterpos for Rashid.")
-			end
-		else
-			print("[!] -> Cannot create Rashid. Day: " .. day .. ".")
 		end
 	end
 
