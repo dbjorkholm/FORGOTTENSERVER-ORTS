88fae7728cbb30807e477f25225eedaef72d6310
Skillert <peter.mollberg@gmail.com>
2014-07-13 16:35:00 +0200

    Added aid's to script

--- data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestBazirTreasureTiles.lua
+++ data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestBazirTreasureTiles.lua
@@ -1,6 +1,6 @@
 local config = {
-	[] = Position(32745, 32394, 14),
-	[] = Position(32745, 32394, 14)
+	[50082] = Position(32745, 32394, 14),
+	[50083] = Position(32745, 32394, 14)
 }
 
 function onStepIn(cid, item, position, fromPosition)
