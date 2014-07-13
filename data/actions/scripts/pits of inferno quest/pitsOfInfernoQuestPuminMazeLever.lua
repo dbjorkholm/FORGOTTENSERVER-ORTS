73af78bdf24c4ccc6bc43a85b181182d092da23c
Skillert <peter.mollberg@gmail.com>
2014-07-13 13:35:28 +0200

    Fixes, cheers @ninjalulz

--- data/actions/scripts/pits of inferno quest/pitsOfInfernoQuestPuminMazeLever.lua
+++ data/actions/scripts/pits of inferno quest/pitsOfInfernoQuestPuminMazeLever.lua
@@ -1,8 +1,9 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local portal = Tile(Position(32816, 32345, 13)):getItemById(1387)
-	if portal ~= 1387 then
-		local portal = Game.createItem(1387, 1, Position(32816, 32345, 13))
-		portal:setActionId() -- Is it possible to set destination here instead of using a different script?
+	if not portal then
+		doCreateTeleport(1387, Position(32767, 32366, 15), Position(32816, 32345, 13))
+	else
+		portal:remove()
 	end
 	Item(item.uid):transform(item.itemid == 1945 and 1946 or 1945)
 	return true
--- data/movements/scripts/pits of inferno quest/pitsOfInfernoQuestPuminMazeTeleport.lua
+++
@@ -1,7 +0,0 @@
-function onStepIn(cid, item, position, fromPosition)
-	local player = Player(cid)
-
-	player:teleportTo(Position(32767, 32366, 15))
-	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-	return true
-end
