4c4d3eb55f397af5b98beb104e58c0e8e937b728
Skillert <peter.mollberg@gmail.com>
2014-07-13 17:55:39 +0200

    Fixes, cheers @TheSumm and @ninjalulz
    Former-commit-id: 8d05a4f5ac07f2028314c87be0605210d9d4eb71

--- data/actions/scripts/other/deeperBanutaShortcut.lua
+++ data/actions/scripts/other/deeperBanutaShortcut.lua
@@ -1,11 +1,7 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
-	local targetItem = Item(itemEx.actionid)
-	if not targetItem then
-		return true
-	end
 	
-	if player:getStorageValue(Storage.DeeperBanuta.Shortcut) == 0 and targetItem == 62378 then
+	if player:getStorageValue(Storage.DeeperBanuta.Shortcut) ~= 1 and itemEx.actionid == 62378 then
 		player:removeItem(10523, 1)
 		player:setStorageValue(Storage.DeeperBanuta.Shortcut, 1)
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You discovered a secret tunnel.")
