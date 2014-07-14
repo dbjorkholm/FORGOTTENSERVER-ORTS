2c7e639a00f51c8ea9ee0a18b2d3a9a1526a885d
Skillert <peter.mollberg@gmail.com>
2014-07-14 19:56:37 +0200

    Fixes, cheers @TheSumm
    Former-commit-id: 07cb2519df6c7f07c5c3ad751797fec1212db72b [formerly 14d9679d7afc786e57d99f4f1054db04a80d6095]
    Former-commit-id: b202345478f08af333fe4581b70f2168a55edee0

--- data/movements/scripts/in service of yalahar quest/quaraVortex.lua
+++ data/movements/scripts/in service of yalahar quest/quaraVortex.lua
@@ -4,9 +4,9 @@ function onStepIn(cid, item, position, fromPosition)
 		return true
 	end
 	
-	local HeadItem = player:getSlotItem(CONST_SLOT_HEAD)	
+	local headItem = player:getSlotItem(CONST_SLOT_HEAD)	
 	if item.actionid == 7812 then
-		if HeadItem ~= nil and isInArray({5461, 12541, 15408}, headItem:getId()) then 
+		if headItem and isInArray({5461, 12541, 15408}, headItem:getId()) then 
 				player:teleportTo(Position(32950, 31181, 9))
 				player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
 				player:say("The vortex throws you out in this vicious place.", TALKTYPE_MONSTER_SAY)
