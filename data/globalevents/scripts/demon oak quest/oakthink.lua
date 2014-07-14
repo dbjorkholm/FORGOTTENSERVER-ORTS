a32e2e435f9b31146da748ed1733a2e4eddb8279
Sam <sum9900@web.de>
2014-07-14 04:16:44 +0200

    Demon oak think: use position constant
    Former-commit-id: 39c3189b227cc60c597566584b3b262274ef0bb3

--- data/globalevents/scripts/demon oak quest/oakthink.lua
+++ data/globalevents/scripts/demon oak quest/oakthink.lua
@@ -1,9 +1,8 @@
-local demonOakPosition = Position(32716, 32352, 7)
-local questAreaPosition =
-{
+local questAreaPosition = {
 	{x = 32706, y = 32345, z = 7, stackpos = 255},
 	{x = 32725, y = 32357, z = 7, stackpos = 255}
 }
+
 local sounds = {
 	'Release me and you will be rewarded greatefully!',
 	'What is this? Demon Legs lying here? Someone might have lost them!',
@@ -15,12 +14,12 @@ local sounds = {
 }
 
 function onThink(interval, lastExecution)
-	local specs = Game.getSpectators(demonOakPosition, false, true, 0, 15, 0, 15)
+	local specs = Game.getSpectators(DEMON_OAK_POSITION, false, true, 0, 15, 0, 15)
 	for i = 1, #specs do
 		if isInRange(specs[i]:getPosition(), questAreaPosition[1], questAreaPosition[2]) then
 			return true
 		end
-		specs[i]:say(sounds[math.random(#sounds)], TALKTYPE_MONSTER_YELL, false, 0, demonOakPosition)
+		specs[i]:say(sounds[math.random(#sounds)], TALKTYPE_MONSTER_YELL, false, 0, DEMON_OAK_POSITION)
 	end
 	return true
 end
