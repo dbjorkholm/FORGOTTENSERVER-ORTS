b3266793429d7563f3e25e46dd9f02b6becb1467
Skillert <peter.mollberg@gmail.com>
2014-07-14 21:34:43 +0200

    Fix Pumin NPC's
    Former-commit-id: b70bbca5e821dcd2ff39cbe3432835b20b60e3c5 [formerly 4cc8dd72b36fcf6ad7d1218f255fc48a59853e48]
    Former-commit-id: eecaf082da0793d13020a2cb7d092a5a38f2f566

---
+++ data/npc/scripts/A Dead Bureaucrat2.lua
@@ -0,0 +1,38 @@
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+function onThink()				npcHandler:onThink()					end
+
+local function greetCallback(cid)
+	npcHandler:setMessage(MESSAGE_GREET, "Hello " .. (Player(cid):getSex() == 0 and "beautiful lady" or "handsome gentleman") .. ", welcome to the atrium of Pumin's Domain. We require some information from you before we can let you pass. Where do you want to go?")
+	return true
+end
+
+local function creatureSayCallback(cid, type, msg)
+	if not npcHandler:isFocused(cid) then
+		return false
+	end
+
+	local player = Player(cid)
+
+	if msgcontains(msg, "hi") and player:getStorageValue(Storage.pitsOfInferno.Pumin) == 15 then
+		player:setStorageValue(Storage.pitsOfInferno.Pumin, 16)
+		npcHandler:say("Hey! You are back! How can I help you this time?", cid)
+	elseif msgcontains(msg, "287") then
+		if player:getStorageValue(Storage.pitsOfInferno.Pumin) == 16 then
+			player:setStorageValue(Storage.pitsOfInferno.Pumin, 17)
+			npcHandler:say("Sure, you can get it from me. Here you are. Bye", cid)
+		end
+	end
+	return true
+end
+
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:setCallback(CALLBACK_GREET, greetCallback)
+npcHandler:setMessage(MESSAGE_WALKAWAY, "Good bye and don't forget me!")
+npcHandler:setMessage(MESSAGE_FAREWELL, "Good bye and don't forget me!")
+npcHandler:addModule(FocusModule:new())
