76fb5b638285091a2ed82c1f9328beb03dd2b429
Skillert <peter.mollberg@gmail.com>
2014-07-14 19:34:33 +0200

    Minor - Added missing helmets for underwater exploration - Changed text - Minor "cleanup"
    Former-commit-id: 89793ff1405da23edcbfd078dabb64c1a5da7094

--- data/movements/scripts/in service of yalahar quest/quaraVortex.lua
+++ data/movements/scripts/in service of yalahar quest/quaraVortex.lua
@@ -4,16 +4,16 @@ function onStepIn(cid, item, position, fromPosition)
 		return true
 	end
 	
-	local Helmetitem = player:getSlotItem(CONST_SLOT_HEAD)	
+	local HeadItem = player:getSlotItem(CONST_SLOT_HEAD)	
 	if item.actionid == 7812 then
-		if Helmetitem ~= nil and Helmetitem:getId() == 5461 then 
-				local destination = Position({x = 32950, y = 31181, z = 9})
-				player:teleportTo(destination)
-				destination:sendMagicEffect(CONST_ME_WATERSPLASH)
+		if HeadItem ~= nil and isInArray({5461, 12541, 15408}, headItem:getId()) then 
+				player:teleportTo(Position(32950, 31181, 9))
+				player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
 				player:say("The vortex throws you out in this vicious place.", TALKTYPE_MONSTER_SAY)
 		else
 			player:teleportTo(fromPosition)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear helmet of the deep in order to dive.")
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear an underwater exploration helmet in order to dive.")
 		end
 	end
 	return true
