15a33d25fdb981adbe53a28348bf499e960421f8
Skillert <peter.mollberg@gmail.com>
2014-07-14 08:30:15 +0200

    Fix, cheers @ninjalulz
    Former-commit-id: ef5e6234366d2669d56e69a15919158c3b2dffa5

--- data/actions/scripts/pits of inferno quest/pitsOfInfernoQuestBazirMirror.lua
+++ data/actions/scripts/pits of inferno quest/pitsOfInfernoQuestBazirMirror.lua
@@ -1,25 +1,30 @@
 local config = {
-	[39511] = {Position(32739, 32392, 14), Position(32739, 32391, 14)},
-	[39512] = {Position(32739, 32391, 14), Position(32739, 32392, 14)}
+    [39511] = {
+        fromPosition = Position(32739, 32392, 14), 
+        toPosition = Position(32739, 32391, 14)
+    },
+    [39512] = {
+        teleportPlayer = true, 
+        fromPosition = Position(32739, 32391, 14), 
+        toPosition = Position(32739, 32392, 14)
+    }
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local player = Player(cid)
-	local targetItem = config[item.actionid]
-	if not targetItem then
-		return true
-	end
+    local targetActionId = config[item.actionid]
+    if not targetActionId then
+        return true
+    end
 
-	local tile = targetItem[1]:getTile()
-	if tile then
-		local thing = tile:getTopTopItem()
-		if thing and thing:getId(6434) then
-			if targetItem[1] = 39512 then
-				player:teleportTo(Position(32717, 32388, 13))
-				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-			end
-			thing:moveTo(targetItem[2])
-		end
-	end
-	return true
+    if targetActionId.teleportPlayer then
+        local player = Player(cid)
+        player:teleportTo(Position(32717, 32388, 13))
+        player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+    end
+
+    local tapestry = Tile(targetActionId.fromPosition):getItemById(6434)
+    if tapestry then
+        tapestry:moveTo(targetActionId.toPosition)
+    end
+    return true
 end
