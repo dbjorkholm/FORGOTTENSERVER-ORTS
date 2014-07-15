12374da56f5f79c5eb53a36366a5762a68eb0751
Skillert <peter.mollberg@gmail.com>
2014-07-15 12:13:42 +0200

    Fix indentation, cheers @ninjalulz
    Former-commit-id: dc349c093d3500e34c73b0232aa4d7da66a35528

--- data/movements/scripts/in service of yalahar quest/quaraVortex.lua
+++ data/movements/scripts/in service of yalahar quest/quaraVortex.lua
@@ -6,9 +6,9 @@ function onStepIn(cid, item, position, fromPosition)
 	
 	local headItem = player:getSlotItem(CONST_SLOT_HEAD)	
 	if headItem and isInArray({5461, 12541, 15408}, headItem:getId()) then 
-			player:teleportTo(Position(32950, 31181, 9))
-			player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
-			player:say("The vortex throws you out in this vicious place.", TALKTYPE_MONSTER_SAY)
+		player:teleportTo(Position(32950, 31181, 9))
+		player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
+		player:say("The vortex throws you out in this vicious place.", TALKTYPE_MONSTER_SAY)
 	else
 		player:teleportTo(fromPosition)
 		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
