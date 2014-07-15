8f07303e3450e1b8250d7ff048133a8b8c806aa6
Skillert <peter.mollberg@gmail.com>
2014-07-15 08:41:00 +0200

    Removed unnecessary check, cheers @ninjalulz

--- data/movements/scripts/in service of yalahar quest/quaraVortex.lua
+++ data/movements/scripts/in service of yalahar quest/quaraVortex.lua
@@ -5,16 +5,14 @@ function onStepIn(cid, item, position, fromPosition)
 	end
 	
 	local headItem = player:getSlotItem(CONST_SLOT_HEAD)	
-	if item.actionid == 7812 then
-		if headItem and isInArray({5461, 12541, 15408}, headItem:getId()) then 
-				player:teleportTo(Position(32950, 31181, 9))
-				player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
-				player:say("The vortex throws you out in this vicious place.", TALKTYPE_MONSTER_SAY)
-		else
-			player:teleportTo(fromPosition)
-			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear an underwater exploration helmet in order to dive.")
-		end
+	if headItem and isInArray({5461, 12541, 15408}, headItem:getId()) then 
+			player:teleportTo(Position(32950, 31181, 9))
+			player:getPosition():sendMagicEffect(CONST_ME_WATERSPLASH)
+			player:say("The vortex throws you out in this vicious place.", TALKTYPE_MONSTER_SAY)
+	else
+		player:teleportTo(fromPosition)
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You must wear an underwater exploration helmet in order to dive.")
 	end
 	return true
 end
