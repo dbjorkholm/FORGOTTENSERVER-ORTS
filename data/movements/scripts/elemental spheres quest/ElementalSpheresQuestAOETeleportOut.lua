2be76f3136322a8a3c5af48334a782f2793e0e79
Skillert <peter.mollberg@gmail.com>
2014-07-16 22:28:20 +0200

    Fix, cheers @ninjalulz
    Former-commit-id: 5c980e311059bd0f79057633e96f87c44b808c9e [formerly 42f3ec82203c3410ed23d9c7ad59ce4e66a50153]
    Former-commit-id: 74739e8ca716ee531241117b851be019d4030267

--- data/movements/scripts/elemental spheres quest/ElementalSpheresQuestAOETeleportOut.lua
+++ data/movements/scripts/elemental spheres quest/ElementalSpheresQuestAOETeleportOut.lua
@@ -1,19 +1,17 @@
-local Area_fromPos, Area_toPos = Position(33235, 31801, 12), Position(33299, 31867, 12)
-
 function onStepIn(cid, item, position, fromPosition)
 	local player = Player(cid)
 	if not player then
 		return true
 	end
 	
-	if not isPlayerInArea(Area_fromPos, Area_toPos) then
+	if not isPlayerInArea(Position(33235, 31801, 12), Position(33299, 31867, 12)) then
 		Game.setStorageValue(10004)
 	end
 
 	player:teleportTo(Position(33265, 31838, 10))
 	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 	for i = 10005, 10008 do
-		player:setStorageValue(i, 0)
+		player:setStorageValue(i)
 	end
 	return true
 end
