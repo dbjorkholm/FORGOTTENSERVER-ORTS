fabc4f88751e490c578b80fa1b819094e5533483
Skillert <peter.mollberg@gmail.com>
2014-07-19 15:58:43 +0200

    Removed tables from Position

--- data/movements/scripts/others/furygates.lua
+++ data/movements/scripts/others/furygates.lua
@@ -1,10 +1,10 @@
 local config = {
-	[9710] = {Position(32834, 32082, 7)},
-	[9711] = {Position(32680, 31720, 7)},
-	[9712] = {Position(32268, 32162, 7)},
-	[9713] = {Position(32263, 31847, 7)},
-	[9714] = {Position(33221, 31921, 7)},
-	[9716] = {Position(32574, 31981, 7)}
+	[9710] = Position(32834, 32082, 7),
+	[9711] = Position(32680, 31720, 7),
+	[9712] = Position(32268, 32162, 7),
+	[9713] = Position(32263, 31847, 7),
+	[9714] = Position(33221, 31921, 7),
+	[9716] = Position(32574, 31981, 7)
 }
 
 function onStepIn(cid, item, position)
@@ -12,8 +12,7 @@ function onStepIn(cid, item, position)
 	if not player then
 		return true
 	end
-
-	local gate = config[item.actionid]
+	
 	if item.actionid == gate and Game.getStorageValue(gate) == 1 then
 		if player:getLevel() >= 60 then
 			player:teleportTo(Position(33290, 31786, 13))
@@ -21,12 +20,11 @@ function onStepIn(cid, item, position)
 		else
 			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have to be at least level 60 to enter this gate.")
 			player:getPosition():sendMagicEffect(CONST_ME_POFF)
-			player:teleportTo(gate[1])
+			player:teleportTo(gate)
 		end
 	elseif item.actionid == 9715 and Game.getStorageValue(gate) == 1 then
-		player:teleportTo(gate[1])
+		player:teleportTo(gate)
 		player:getPosition():sendMagicEffect(CONST_ME_FIREAREA)
 	end
 	return true
 end
-
