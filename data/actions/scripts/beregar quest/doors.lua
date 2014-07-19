c8762d34d6c3ad65198c7a2ee7e828ad42da13c4
Skillert <peter.mollberg@gmail.com>
2014-07-19 22:32:40 +0200

    Fixes, cheers @TheSumm

--- data/actions/scripts/beregar quest/doors.lua
+++ data/actions/scripts/beregar quest/doors.lua
@@ -1,17 +1,18 @@
+local doors = {
+	{storage = Storage.hiddenCityOfBeregar.JusticeForAll, value = 6}
+	{storage = Storage.hiddenCityOfBeregar.SweetAsChocolateCake, value = 3}
+}
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
+	if item.itemid ~= 1225 then
+		return true
+	end
+
 	local player = Player(cid)
-	
-	local config = {
-	{door = 50098, value = player:getStorageValue(Storage.hiddenCityOfBeregar.JusticeForAll) == 6}
-	{door = 50099, value = player:getStorageValue(Storage.hiddenCityOfBeregar.SweetAsChocolateCake) == 3}
-	}
-	
-	for i, #config do
-		if config.door and config.value then
-			if item.itemid == 1225 then
+	for i = 1, #config do
+		if player:getStorageValue(config[i].storage) = config[i].value then
 				player:teleportTo(toPosition, true)
 				Item(item.uid):transform(1226)
-			end
 		end
 	end
 	return true
