6a7065244a666d773562874ef46bbaa6098d018d
Skillert <peter.mollberg@gmail.com>
2014-07-19 14:32:57 +0200

    Hidden City of Beregar Quest update - Added message to wagon system if you haven't discovered the "shortcut" yet - Added script for elevator - Added elevator script to movements.xml - Updated Storages-Aid-Uid Used.txt

--- data/actions/scripts/beregar quest/oreWagon.lua
+++ data/actions/scripts/beregar quest/oreWagon.lua
@@ -14,6 +14,8 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if player:getStorageValue(Storage.hiddenCityOfBeregar.OreWagon) == 1 then
 		player:teleportTo(targetItem)
 		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	else
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You don't know how to use this yet.")
 	end
 	return true
 end
