d5c9e872d7d7ca7757475907e14ef781d5953479
Skillert <peter.mollberg@gmail.com>
2014-07-20 23:52:58 +0200

    Correction

--- data/movements/scripts/beregar quest/bellow.lua
+++ data/movements/scripts/beregar quest/bellow.lua
@@ -12,8 +12,8 @@ function onStepIn(cid, item, position, fromPosition)
 		crucible:setActionId(50121)
 		Position(32695, 31494, 10):sendMagicEffect(CONST_ME_POFF) -- smoke?
 	elseif crucible and crucible:getActionId(50121) then
-		Player(cid):say("TSSSSHHHHH", TALKTYPE_MONSTER_SAY, false, 0, Position(32695, 31494, 11))
-		Player(cid):say("CHOOOOOOOHHHHH", TALKTYPE_MONSTER_SAY, false, 0, Position(32698, 31492, 11))
+		player:say("TSSSSHHHHH", TALKTYPE_MONSTER_SAY, false, 0, Position(32695, 31494, 11))
+		player:say("CHOOOOOOOHHHHH", TALKTYPE_MONSTER_SAY, false, 0, Position(32698, 31492, 11))
 	end
 	return true
 end
