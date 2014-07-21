f99ba557229384de9d7ca9792fa0ed1547a79a81
Daniel Björkholm <danielbjorkholm@live.se>
2014-07-21 18:23:31 +0200

    Replace chatchannel scripts

--- data/chatchannels/scripts/advertising-rook.lua
+++ data/chatchannels/scripts/advertising-rook.lua
@@ -1,40 +1,40 @@
-function canJoin(cid)
-        return Player(cid):getVocation():getId() == VOCATION_NONE or Player(cid):getAccountType() >= ACCOUNT_TYPE_SENIORTUTOR
+function canJoin(player)
+	return player:getVocation():getId() == VOCATION_NONE or player:getAccountType() >= ACCOUNT_TYPE_SENIORTUTOR
 end
 
 local CHANNEL_ADVERTISING_ROOK = 6
 
-local muted = createConditionObject(CONDITION_CHANNELMUTEDTICKS)
-setConditionParam(muted, CONDITION_PARAM_SUBID, CHANNEL_ADVERTISING_ROOK)
-setConditionParam(muted, CONDITION_PARAM_TICKS, 120000)
+local muted = Condition(CONDITION_CHANNELMUTEDTICKS, CONDITIONID_DEFAULT)
+muted:setParameter(CONDITION_PARAM_SUBID, CHANNEL_ADVERTISING_ROOK)
+muted:setParameter(CONDITION_PARAM_TICKS, 120000)
 
-function onSpeak(cid, type, message)
-        if(Player(cid):getAccountType() >= ACCOUNT_TYPE_GAMEMASTER) then
-                if(type == TALKTYPE_CHANNEL_Y) then
-                        return TALKTYPE_CHANNEL_O
-                end
-                return true
-        end
+function onSpeak(player, type, message)
+	if player:getAccountType() >= ACCOUNT_TYPE_GAMEMASTER then
+		if type == TALKTYPE_CHANNEL_Y then
+			return TALKTYPE_CHANNEL_O
+		end
+		return true
+	end
 
-        if(Player(cid):getLevel() == 1) then
-                Player(cid):sendCancelMessage("You may not speak into channels as long as you are on level 1.")
-                return false
-        end
+	if player:getLevel() == 1 then
+		player:sendCancelMessage("You may not speak into channels as long as you are on level 1.")
+		return false
+	end
 
-        if(getCreatureCondition(cid, CONDITION_CHANNELMUTEDTICKS, CHANNEL_ADVERTISING_ROOK)) then
-                Player(cid):sendCancelMessage("You may only place one offer in two minutes.")
-                return false
-        end
-        doAddCondition(cid, muted)
+	if player:getCondition(CONDITION_CHANNELMUTEDTICKS, CONDITIONID_DEFAULT, CHANNEL_ADVERTISING_ROOK) then
+		player:sendCancelMessage("You may only place one offer in two minutes.")
+		return false
+	end
+	player:addCondition(muted)
 
-        if(type == TALKTYPE_CHANNEL_O) then
-                if(Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER) then
-                        type = TALKTYPE_CHANNEL_Y
-                end
-        elseif(type == TALKTYPE_CHANNEL_R1) then
-                if(not getPlayerFlagValue(cid, PlayerFlag_CanTalkRedChannel)) then
-                        type = TALKTYPE_CHANNEL_Y
-                end
-        end
-return type
+	if type == TALKTYPE_CHANNEL_O then
+		if player:getAccountType() < ACCOUNT_TYPE_GAMEMASTER then
+			type = TALKTYPE_CHANNEL_Y
+		end
+	elseif type == TALKTYPE_CHANNEL_R1 then
+		if not getPlayerFlagValue(player, PlayerFlag_CanTalkRedChannel) then
+			type = TALKTYPE_CHANNEL_Y
+		end
+	end
+	return type
 end
--- data/chatchannels/scripts/help.lua
+++ data/chatchannels/scripts/help.lua
@@ -1,77 +1,77 @@
 local CHANNEL_HELP = 7
 
-local muted = createConditionObject(CONDITION_CHANNELMUTEDTICKS)
-setConditionParam(muted, CONDITION_PARAM_SUBID, CHANNEL_HELP)
-setConditionParam(muted, CONDITION_PARAM_TICKS, 3600000)
+local muted = Condition(CONDITION_CHANNELMUTEDTICKS, CONDITIONID_DEFAULT)
+muted:setParameter(CONDITION_PARAM_SUBID, CHANNEL_HELP)
+muted:setParameter(CONDITION_PARAM_TICKS, 3600000)
 
-function onSpeak(cid, type, message)
-        local playerAccountType = Player(cid):getAccountType()
-        if(Player(cid):getLevel() == 1 and playerAccountType == ACCOUNT_TYPE_NORMAL) then
-                Player(cid):sendCancelMessage("You may not speak into channels as long as you are on level 1.")
-                return false
-        end
+function onSpeak(player, type, message)
+	local playerAccountType = player:getAccountType()
+	if player:getLevel() == 1 and playerAccountType == ACCOUNT_TYPE_NORMAL then
+		player:sendCancelMessage("You may not speak into channels as long as you are on level 1.")
+		return false
+	end
 
-        if(getCreatureCondition(cid, CONDITION_CHANNELMUTEDTICKS, CHANNEL_HELP)) then
-                Player(cid):sendCancelMessage("You are muted from the Help channel for using it inappropriately.")
-                return false
-        end
+	if player:getCondition(CONDITION_CHANNELMUTEDTICKS, CONDITIONID_DEFAULT, CHANNEL_HELP) then
+		player:sendCancelMessage("You are muted from the Help channel for using it inappropriately.")
+		return false
+	end
 
-        if(playerAccountType >= ACCOUNT_TYPE_TUTOR) then
-                if(string.sub(message, 1, 6) == "!mute ") then
-                        local targetName = string.sub(message, 7)
-                        local target = Player(name):getId(targetName) 
-                        if(target ~= false) then
-                                if(playerAccountType > target:getAccountType()) then 
-                                        if(not getCreatureCondition(target, CONDITION_CHANNELMUTEDTICKS, CHANNEL_HELP)) then
-                                                doAddCondition(target, muted)
-                                                sendChannelMessage(CHANNEL_HELP, TALKTYPE_CHANNEL_R1, getPlayerName(target) .. " has been muted by " .. getPlayerName(cid) .. " for using Help Channel inappropriately.")
-                                        else
-                                                Player(cid):sendCancelMessage("That player is already muted.")
-                                        end
-                                else
-                                        Player(cid):sendCancelMessage("You are not authorized to mute that player.")
-                                end
-                        else
-                                Player(cid):sendCancelMessage(RETURNVALUE_PLAYERWITHTHISNAMEISNOTONLINE)
-                        end
-                        return false
-                elseif(string.sub(message, 1, 8) == "!unmute ") then
-                        local targetName = string.sub(message, 9)
-                        local target = Player(name):getId(targetName)
-                        if(target ~= false) then
-                                if(playerAccountType > getPlayerAccountType(target)) then
-                                        if(getCreatureCondition(target, CONDITION_CHANNELMUTEDTICKS, CHANNEL_HELP)) then
-                                                doRemoveCondition(target, CONDITION_CHANNELMUTEDTICKS, CHANNEL_HELP)
-                                                sendChannelMessage(CHANNEL_HELP, TALKTYPE_CHANNEL_R1, getPlayerName(target) .. " has been unmuted by " .. getPlayerName(cid) .. ".")
-                                        else
-                                                Player(cid):sendCancelMessage("That player is not muted.")
-                                        end
-                                else
-                                        Player(cid):sendCancelMessage("You are not authorized to unmute that player.")
-                                end
-                        else
-                                Player(cid):sendCancelMessage(RETURNVALUE_PLAYERWITHTHISNAMEISNOTONLINE)
-                        end
-                        return false
-                end
-        end
+	if playerAccountType >= ACCOUNT_TYPE_TUTOR then
+		if string.sub(message, 1, 6) == "!mute " then
+			local targetName = string.sub(message, 7)
+			local target = Player(targetName)
+			if target ~= nil then
+				if playerAccountType > target:getAccountType() then
+					if not target:getCondition(CONDITION_CHANNELMUTEDTICKS, CONDITIONID_DEFAULT, CHANNEL_HELP) then
+						target:addCondition(muted)
+						sendChannelMessage(CHANNEL_HELP, TALKTYPE_CHANNEL_R1, target:getName() .. " has been muted by " .. player:getName() .. " for using Help Channel inappropriately.")
+					else
+						player:sendCancelMessage("That player is already muted.")
+					end
+				else
+					player:sendCancelMessage("You are not authorized to mute that player.")
+				end
+			else
+				player:sendCancelMessage(RETURNVALUE_PLAYERWITHTHISNAMEISNOTONLINE)
+			end
+			return false
+		elseif string.sub(message, 1, 8) == "!unmute " then
+			local targetName = string.sub(message, 9)
+			local target = Player(targetName)
+			if target ~= nil then
+				if playerAccountType > target:getAccountType() then
+					if target:getCondition(CONDITION_CHANNELMUTEDTICKS, CONDITIONID_DEFAULT, CHANNEL_HELP) then
+						target:removeCondition(CONDITION_CHANNELMUTEDTICKS, CONDITIONID_DEFAULT, CHANNEL_HELP)
+						sendChannelMessage(CHANNEL_HELP, TALKTYPE_CHANNEL_R1, target:getName() .. " has been unmuted by " .. player:getName() .. ".")
+					else
+						player:sendCancelMessage("That player is not muted.")
+					end
+				else
+					player:sendCancelMessage("You are not authorized to unmute that player.")
+				end
+			else
+				player:sendCancelMessage(RETURNVALUE_PLAYERWITHTHISNAMEISNOTONLINE)
+			end
+			return false
+		end
+	end
 
-        if(type == TALKTYPE_CHANNEL_Y) then
-                if(playerAccountType >= ACCOUNT_TYPE_TUTOR or getPlayerFlagValue(cid, PlayerFlag_TalkOrangeHelpChannel)) then
-                        type = TALKTYPE_CHANNEL_O
-                end
-        elseif(type == TALKTYPE_CHANNEL_O) then
-                if(playerAccountType < ACCOUNT_TYPE_TUTOR and not getPlayerFlagValue(cid, PlayerFlag_TalkOrangeHelpChannel)) then
-                        type = TALKTYPE_CHANNEL_Y
-                end
-        elseif(type == TALKTYPE_CHANNEL_R1) then
-                if(playerAccountType < ACCOUNT_TYPE_GAMEMASTER and not getPlayerFlagValue(cid, PlayerFlag_CanTalkRedChannel)) then
-                        if(playerAccountType >= ACCOUNT_TYPE_TUTOR or getPlayerFlagValue(cid, PlayerFlag_TalkOrangeHelpChannel)) then
-                                type = TALKTYPE_CHANNEL_O
-                        else
-                                type = TALKTYPE_CHANNEL_Y
-                        end
-                end
-        end
-        return type
+	if type == TALKTYPE_CHANNEL_Y then
+		if playerAccountType >= ACCOUNT_TYPE_TUTOR or getPlayerFlagValue(player, PlayerFlag_TalkOrangeHelpChannel) then
+			type = TALKTYPE_CHANNEL_O
+		end
+	elseif type == TALKTYPE_CHANNEL_O then
+		if playerAccountType < ACCOUNT_TYPE_TUTOR and not getPlayerFlagValue(player, PlayerFlag_TalkOrangeHelpChannel) then
+			type = TALKTYPE_CHANNEL_Y
+		end
+	elseif type == TALKTYPE_CHANNEL_R1 then
+		if playerAccountType < ACCOUNT_TYPE_GAMEMASTER and not getPlayerFlagValue(player, PlayerFlag_CanTalkRedChannel) then
+			if playerAccountType >= ACCOUNT_TYPE_TUTOR or getPlayerFlagValue(player, PlayerFlag_TalkOrangeHelpChannel) then
+				type = TALKTYPE_CHANNEL_O
+			else
+				type = TALKTYPE_CHANNEL_Y
+			end
+		end
+	end
+	return type
 end
