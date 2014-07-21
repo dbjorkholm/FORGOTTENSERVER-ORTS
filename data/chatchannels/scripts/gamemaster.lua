70f89313c07abaf3398e7fb9fa90232876fa5f05
Daniel Björkholm <danielbjorkholm@live.se>
2014-07-21 18:23:31 +0200

    Replace chatchannel scripts
    Former-commit-id: f99ba557229384de9d7ca9792fa0ed1547a79a81

--- data/chatchannels/scripts/gamemaster.lua
+++ data/chatchannels/scripts/gamemaster.lua
@@ -1,20 +1,21 @@
-function canJoin(cid)
-        return Player(cid):getAccountType() >= ACCOUNT_TYPE_GAMEMASTER
+function canJoin(player)
+	return player:getAccountType() >= ACCOUNT_TYPE_GAMEMASTER
 end
 
-function onSpeak(cid, type, message)
-        if(type == TALKTYPE_CHANNEL_Y) then
-                if(Player(cid):getAccountType() == ACCOUNT_TYPE_GOD) then
-                        type = TALKTYPE_CHANNEL_O
-                end
-        elseif(type == TALKTYPE_CHANNEL_O) then
-                if(Player(cid):getAccountType() ~= ACCOUNT_TYPE_GOD) then
-                        type = TALKTYPE_CHANNEL_Y
-                end
-        elseif(type == TALKTYPE_CHANNEL_R1) then
-                if(Player(cid):getAccountType() ~= ACCOUNT_TYPE_GOD and not getPlayerFlagValue(cid, PlayerFlag_CanTalkRedChannel)) then
-                        type = TALKTYPE_CHANNEL_Y
-                end
-        end
-return type
+function onSpeak(player, type, message)
+	local playerAccountType = player:getAccountType()
+	if type == TALKTYPE_CHANNEL_Y then
+		if playerAccountType == ACCOUNT_TYPE_GOD then
+			type = TALKTYPE_CHANNEL_O
+		end
+	elseif type == TALKTYPE_CHANNEL_O then
+		if playerAccountType ~= ACCOUNT_TYPE_GOD then
+			type = TALKTYPE_CHANNEL_Y
+		end
+	elseif type == TALKTYPE_CHANNEL_R1 then
+		if playerAccountType ~= ACCOUNT_TYPE_GOD and not getPlayerFlagValue(player, PlayerFlag_CanTalkRedChannel) then
+			type = TALKTYPE_CHANNEL_Y
+		end
+	end
+	return type
 end
