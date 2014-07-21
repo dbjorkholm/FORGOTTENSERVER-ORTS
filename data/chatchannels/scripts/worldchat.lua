7dbfacdd3a3cf2beb292d148d186bed019e5ce14
Daniel Björkholm <danielbjorkholm@live.se>
2014-07-21 18:23:31 +0200

    Replace chatchannel scripts
    Former-commit-id: d3f01a2eb40812412a4ae7ba93aac994862bd5f7 [formerly f99ba557229384de9d7ca9792fa0ed1547a79a81]
    Former-commit-id: 70f89313c07abaf3398e7fb9fa90232876fa5f05

--- data/chatchannels/scripts/worldchat.lua
+++ data/chatchannels/scripts/worldchat.lua
@@ -1,21 +1,22 @@
-function onSpeak(cid, type, message)
-        if(Player(cid):getLevel() == 1 and getPlayerAccountType(cid) < ACCOUNT_TYPE_GAMEMASTER) then
-                Player(cid):sendCancelMessage("You may not speak into channels as long as you are on level 1.")
-                return false
-        end
+function onSpeak(player, type, message)
+	local playerAccountType = player:getAccountType()
+	if player:getLevel() == 1 and playerAccountType < ACCOUNT_TYPE_GAMEMASTER then
+		player:sendCancelMessage("You may not speak into channels as long as you are on level 1.")
+		return false
+	end
 
-        if(type == TALKTYPE_CHANNEL_Y) then
-                if(Player(cid):getAccountType() >= ACCOUNT_TYPE_GAMEMASTER) then
-                        type = TALKTYPE_CHANNEL_O
-                end
-        elseif(type == TALKTYPE_CHANNEL_O) then
-                if(Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER) then
-                        type = TALKTYPE_CHANNEL_Y
-                end
-        elseif(type == TALKTYPE_CHANNEL_R1) then
-                if(Player(cid):getAccountType() < ACCOUNT_TYPE_GAMEMASTER and not getPlayerFlagValue(cid, PlayerFlag_CanTalkRedChannel)) then
-                        type = TALKTYPE_CHANNEL_Y
-                end
-        end
-        return type
+	if type == TALKTYPE_CHANNEL_Y then
+		if playerAccountType >= ACCOUNT_TYPE_GAMEMASTER then
+			type = TALKTYPE_CHANNEL_O
+		end
+	elseif type == TALKTYPE_CHANNEL_O then
+		if playerAccountType < ACCOUNT_TYPE_GAMEMASTER then
+			type = TALKTYPE_CHANNEL_Y
+		end
+	elseif type == TALKTYPE_CHANNEL_R1 then
+		if playerAccountType < ACCOUNT_TYPE_GAMEMASTER and not getPlayerFlagValue(player, PlayerFlag_CanTalkRedChannel) then
+			type = TALKTYPE_CHANNEL_Y
+		end
+	end
+	return type
 end
