c2f6b2367efa349e2ca07c7ba5b37a2cbf5e81bc
Skillert <peter.mollberg@gmail.com>
2014-07-23 10:42:32 +0200

    Added HCoB movement scripts

---
+++ data/movements/scripts/beregar quest/gap.lua
@@ -0,0 +1,10 @@
+function onStepIn(cid, item, position, fromPosition)
+	local player = Player(cid)
+	if not player then
+		return true
+	end
+		
+	player:teleportTo(Position(32569, 31507, 9))
+	player:say("Use the wagon to pass the gap.", TALKTYPE_MONSTER_SAY, false, 0, pos)
+	return true
+end
