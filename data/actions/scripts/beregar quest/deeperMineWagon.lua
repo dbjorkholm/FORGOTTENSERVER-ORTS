3f93b64b9c3de6f90e6b9884f6277f29f1ff54e8
Skillert <peter.mollberg@gmail.com>
2014-07-23 22:40:23 +0200

    Fixes, cheers @ninjalulz - Small note, looking at what I did, me, myself is wondering what the hell I've written ;p

--- data/actions/scripts/beregar quest/deeperMineWagon.lua
+++ data/actions/scripts/beregar quest/deeperMineWagon.lua
@@ -1,17 +1,17 @@
-local wagons = {
-	{storage = Storage.hiddenCityOfBeregar.RoyalRescue, value = 1, teleportPos = Position(32566, 31505, 9)}
-	{storage = Storage.hiddenCityOfBeregar.RoyalRescue, value = 2, teleportPos = Position(32611, 31513, 9)}
+local config = {
+	[1] = Position(32566, 31505, 9),
+	[2] = Position(32611, 31513, 9)
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
-	for i = 1, #wagons do
-		if player:getStorageValue(wagons[i].storage) == wagons[i].value then
-			player:teleportTo(wagons[i].teleportPos, true)
-			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-		else
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "You don't have permission to use this yet.")
-		end
+	local targetPosition = config[player:getStorageValue(Storage.hiddenCityOfBeregar.RoyalRescue)]
+	if not targetPosition then
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You don't have permission to use this yet.")
+		return true
 	end
+
+	player:teleportTo(targetPosition, true)
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 	return true
 end
