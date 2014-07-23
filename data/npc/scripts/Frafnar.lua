c2e8047be185c69ef1c293684f5b671fee914152
Skillert <peter.mollberg@gmail.com>
2014-07-23 11:08:33 +0200

    Fixes, cheers @ninjalulz
    Former-commit-id: d29bb8fdcb6198daa5978d3cbba9b1d3162fc20e [formerly 9c2d3048fdd4d80affba5e47f98ceec50e37bc99]
    Former-commit-id: f630a2e43e2d66d91a8a3299c7ffeca02e24f09e

--- data/npc/scripts/Frafnar.lua
+++ data/npc/scripts/Frafnar.lua
@@ -12,23 +12,21 @@ local function creatureSayCallback(cid, type, msg)
 		return false
 	end
 
-	local player = Player(cid)
-
 	if msgcontains(msg, "mission") then
-		if player:getStorageValue(Storage.hiddenCityOfBeregar.SweetAsChocolateCake) < 1 then
+		if Player(cid):getStorageValue(Storage.hiddenCityOfBeregar.SweetAsChocolateCake) < 1 then
 			npcHandler:say("There is indeed something you could do for me. You must know, I'm in love with Bolfana. I'm sure she'd have a beer with me if I got her a chocolate cake. Problem is that I can't leave this door as I'm on duty. Would you be so kind and help me?", cid)
 			npcHandler.topic[cid] = 1
-		elseif player:getStorageValue(Storage.hiddenCityOfBeregar.SweetAsChocolateCake) == 2 then
+		elseif Player(cid):getStorageValue(Storage.hiddenCityOfBeregar.SweetAsChocolateCake) == 2 then
 			npcHandler:say("So did you tell her that the cake came from me?", cid)
 			npcHandler.topic[cid] = 2
 		end
 	elseif msgcontains(msg, "yes") then
 		if npcHandler.topic[cid] == 1 then
-			player:setStorageValue(Storage.hiddenCityOfBeregar.SweetAsChocolateCake, 1)
+			Player(cid):setStorageValue(Storage.hiddenCityOfBeregar.SweetAsChocolateCake, 1)
 			npcHandler:say("Great! She works in the tavern of Beregar. It's situated in the western part of the city. Bring her a chocolate cake and tell her that it was me who sent it.", cid)
 			npcHandler.topic[cid] = 0
 		elseif npcHandler.topic[cid] == 2 then
-			player:setStorageValue(Storage.hiddenCityOfBeregar.SweetAsChocolateCake, 3)
+			Player(cid):setStorageValue(Storage.hiddenCityOfBeregar.SweetAsChocolateCake, 3)
 			npcHandler:say("Great! That's my breakthrough. Now she can't refuse to go out with me. I grant you access to the western part of the mine.", cid)
 			npcHandler.topic[cid] = 0
 		end
