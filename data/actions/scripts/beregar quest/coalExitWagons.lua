51c3d6f69166bc9528d2dada91645cfb7f765637
Skillert <peter.mollberg@gmail.com>
2014-07-23 19:07:01 +0200

    Removed for loop, cheers @ninjalulz

--- data/actions/scripts/beregar quest/coalExitWagons.lua
+++ data/actions/scripts/beregar quest/coalExitWagons.lua
@@ -1,20 +1,19 @@
 local wagons = {
-	{actionId = 50122, teleportPos = Position(32704, 31507, 12)}, -- small tunnel with golems 
-	{actionId = 50123, teleportPos = Position(32661, 31495, 13)}, -- mushroom quest
-	{actionId = 50124, teleportPos = Position(32687, 31470, 13)}, -- wagon maze
-	{actionId = 50125, teleportPos = Position(32690, 31495, 11)} -- room with lava that I couldn't find, setting destination to the same as mushroom quest
+	[50122] = Position(32704, 31507, 12), -- small tunnel with golems 
+	[50123] = Position(32661, 31495, 13), -- mushroom quest
+	[50124] = Position(32687, 31470, 13), -- wagon maze
+	[50125] = Position(32690, 31495, 11) -- room with lava that I couldn't find, setting destination to the same as mushroom quest
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local player = Player(cid)
-	
-	for i = 1, #wagons do
-		local table = wagons[i]
-		if item.actionid == table.actionId then
-			player:teleportTo(table.teleportPos, true)
-			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-			Item(item.actionid):remove()
-		end
-	end
-	return true
+    local targetPosition = config[item.actionid]
+    if not targetPosition then
+        return true
+    end
+
+    local player = Player(cid)
+    player:teleportTo(targetPosition)
+    player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+    Item(item.uid):remove()
+    return true
 end
