97e31ba67513626025c0cb71b8556c76d5fd7d69
Skillert <peter.mollberg@gmail.com>
2014-07-23 10:53:56 +0200

    Fixes, cheers @ninjalulz
    Former-commit-id: 34f5c6be66e0f67335a99f7968db44d9eecaa7b9

--- data/npc/scripts/Tehlim.lua
+++ data/npc/scripts/Tehlim.lua
@@ -12,16 +12,14 @@ local function creatureSayCallback(cid, type, msg)
 		return false
 	end
 
-	local player = Player(cid)
-
 	if msgcontains(msg, "mission") then
-		if player:getStorageValue(Storage.hiddenCityOfBeregar.RoyalRescue) == 3 then
+		if Player(cid):getStorageValue(Storage.hiddenCityOfBeregar.RoyalRescue) == 3 then
 			npcHandler:say("I warn you, those trolls are WAY more dangerous than the usual kind. Alone, I can't do anything for my brothers. Find a way to the trolls' hideout and rescue my brothers. Are you willing to help me?", cid)
 			npcHandler.topic[cid] = 1
 		end
 	elseif msgcontains(msg, "yes") then
 		if npcHandler.topic[cid] == 1 then
-			player:setStorageValue(Storage.hiddenCityOfBeregar.RoyalRescue, 4)
+			Player(cid):setStorageValue(Storage.hiddenCityOfBeregar.RoyalRescue, 4)
 			npcHandler:say(" Great! I hope you find my brothers. Good luck!", cid)
 			npcHandler.topic[cid] = 0
 		end
