5a692183ff0d58c33ccbdac53e2d11063eccbbca
Skillert <peter.mollberg@gmail.com>
2014-07-23 22:40:23 +0200

    Fixes, cheers @ninjalulz - Small note, looking at what I did, me, myself is wondering what the hell I've written ;p
    Former-commit-id: 1f05993e88baf78815681eecc6576b2c818b57b0 [formerly 3f93b64b9c3de6f90e6b9884f6277f29f1ff54e8]
    Former-commit-id: bac8863ad4b813ebff963375661b7401ec354a34

--- data/actions/scripts/beregar quest/coalLevers.lua
+++ data/actions/scripts/beregar quest/coalLevers.lua
@@ -1,21 +1,26 @@
-local levers = {
-	{uniqueId = 50108, actionId = 50122, wagonPos = Position(32696, 31495, 11)}
-	{uniqueId = 50109, actionId = 50123, wagonPos = Position(32694, 31495, 11)}
-	{uniqueId = 50110, actionId = 50124, wagonPos = Position(32692, 31495, 11)}
-	{uniqueId = 50111, actionId = 50125, wagonPos = Position(32690, 31495, 11)}
+local config = {
+	[50108] = {actionId = 50122, wagonPos = Position(32696, 31495, 11)},
+	[50109] = {actionId = 50123, wagonPos = Position(32694, 31495, 11)},
+	[50110] = {actionId = 50124, wagonPos = Position(32692, 31495, 11)},
+	[50111] = {actionId = 50125, wagonPos = Position(32690, 31495, 11)}
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local player = Player(cid)
-	
-	for i = 1, #levers do
-		local table = levers[i]
-		if item.uid == table.uniqueId and Tile(Position(32699, 31494, 11)):getItemById(8641):getActionId(50121) then
-			local wagon = Game.createItem(7132, table.wagonPos)
-			wagon:setActionId(table.actionId)
-			Tile(Position(32699, 31494, 11)):getItemById(8641):transform(8642)
-		end
+	local targetItem = config[item.uid]
+	if not targetItem then
+		return true
 	end
+
+	local crucible = Tile(Position(32699, 31494, 11)):getItemById(8641)
+	if crucible:getActionId() == 50121 then
+		local wagon = Game.createItem(7132, 1, targetItem.wagonPos)
+		if wagon then
+			wagon:setActionId(targetItem.actionId)
+		end
+
+		crucible:transform(8642)
+	end
+
 	Item(item.uid):transform(item.itemid == 10044 and 10045 or 10044)
 	return true
 end
