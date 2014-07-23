1c56d10bd762bb5996d12e2a69a1f6680436f80e
Skillert <peter.mollberg@gmail.com>
2014-07-23 10:16:57 +0200

    HCoB quest: - Added Justice For All
    Former-commit-id: 2fb4142a8c142bb4e55c9cac14bd5fc109843969 [formerly 1ab97d85c37008729c928f7aa6973ca5fe8d5e23]
    Former-commit-id: 72fe780d6e97eab5f58f925a4a676144f7a5a9e5

---
+++ data/actions/scripts/beregar quest/ring.lua
@@ -0,0 +1,10 @@
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	local player = Player(cid)
+
+	if player:getStorageValue(Storage.hiddenCityOfBeregar.JusticeForAll) == 3 then
+		player:addItem(14348, 1)
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You have found Rerun's ring.")
+		player:setStorageValue(Storage.hiddenCityOfBeregar.JusticeForAll, 4)
+	end
+	return true
+end
