6b67e8e5a28f9db0007acd669d3fc9bf6721ce13
Skillert <peter.mollberg@gmail.com>
2014-07-23 10:12:10 +0200

    Update: - Added script for doors
    Former-commit-id: a5716afb690dd1fde8bac0efcd23120910c58217

---
+++ data/actions/scripts/beregar quest/doors.lua
@@ -0,0 +1,20 @@
+local doors = {
+	{storage = Storage.hiddenCityOfBeregar.JusticeForAll, value = 6}
+	{storage = Storage.hiddenCityOfBeregar.SweetAsChocolateCake, value = 3}
+	{storage = Storage.hiddenCityOfBeregar.TheGoodGuard, value = 3}
+}
+
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	if item.itemid ~= 1225 then
+		return true
+	end
+
+	local player = Player(cid)
+	for i = 1, #config do
+		if player:getStorageValue(config[i].storage) == config[i].value then
+			player:teleportTo(toPosition, true)
+			Item(item.uid):transform(1226)
+		end
+	end
+	return true
+end
