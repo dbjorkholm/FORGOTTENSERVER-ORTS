88306d62d8cc01549f26351f3ec1ebdfe1fcff12
Royalot <zapf-benny@web.de>
2014-07-25 17:55:51 +0200

    add leverscript for lifering quest
    fixed hellgate leverscript
    
    
    Former-commit-id: 16046ad49360cccb6324bb4c809c404c12981caf [formerly 9e39c9d62c81945ee23a4484068616f3e2c877fc]
    Former-commit-id: 4823b0c0f34b62911e239e3ee6572b6895e02473

--- data/actions/scripts/quests/liferinglever.lua
+++ data/actions/scripts/quests/liferinglever.lua
@@ -1,11 +1,14 @@
 local config = {
 	bridgePositions = {
-		[1] = Position(32627, 31699, 10),
-		[2] = Position(32628, 31699, 10),
-		[3] = Position(32629, 31699, 10)
+		[1] = Position(32410, 32232, 10),
+		[2] = Position(32411, 32232, 10),
+		[3] = Position(32412, 32232, 10),
+		[4] = Position(32410, 32231, 10),
+		[5] = Position(32411, 32231, 10),
+		[6] = Position(32412, 32231, 10)
 	},
 	bridgeID = 5770,
-	waterID = 493
+	waterID = 4615
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
