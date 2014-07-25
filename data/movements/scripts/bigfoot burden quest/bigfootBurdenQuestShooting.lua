74b583e6fc577030f2d2769a8faf4a20b5010d5a
Daniel Björkholm <danielbjorkholm@live.se>
2014-07-25 08:18:04 +0200

    Minor fix (bigfootBurdenQuestShooting.lua)
    Former-commit-id: 2b237e6928bdddbedc1b139630a858550612ec89 [formerly 0e6bbc5cf8f95dcf7771ba0095c4a5c3ec42628f]
    Former-commit-id: 5e54440298bb655fec82634372f769597b859241

--- data/movements/scripts/bigfoot burden quest/bigfootBurdenQuestShooting.lua
+++ data/movements/scripts/bigfoot burden quest/bigfootBurdenQuestShooting.lua
@@ -1,25 +1,25 @@
 local function doCreateDummy(cid, pos)
 	local player = Player(cid)
+	if not player then
+		return true
+	end
+	
 	local tile = Tile(pos)
+	if tile then
+		local thing = tile:getTopVisibleThing()
+		if thing and isInArray({18226, 18227}, thing:getId()) then
+			thing:remove()
+		end
+	end
 
 	if player:getStorageValue(902) < 5 then
-		if tile:getItemById(18226) then
-			tile:getItemById(18226):remove()
-		elseif tile:getItemById(18227) then
-			tile:getItemById(18227):remove()
-		end
 		pos:sendMagicEffect(CONST_ME_POFF)
 		Game.createItem(math.random(18226, 18227), 1, pos)
-	elseif getPlayerStorageValue(cid, 902) == 5 then
-		if tile:getItemById(18226) then
-			tile:getItemById(18226):remove()
-		elseif tile:getItemById(18227) then
-			tile:getItemById(18227):remove()
-		end
-		-- Commenting away mayNotMove since this function no longer exists.
-		-- mayNotMove(cid, false)
+	elseif player:getStorageValue(902) == 5 then
 		player:setStorageValue(900, 9)
+		return false
 	end
+	
 	return addEvent(doCreateDummy, 2 * 1000, cid, pos)
 end
 
@@ -28,12 +28,11 @@ function onStepIn(cid, item, position, fromPosition)
 	if not player then
 		return true
 	end
-
+	
 	if player:getStorageValue(900) == 8 then
-		player:setStorageValue(902, 0)
-		-- Commenting away mayNotMove since this function no longer exists.
-		-- mayNotMove(cid, true)
-		doCreateDummy(cid, Position(player:getPosition().x, player:getPosition().y - 5, 10))
+		player:setStorageValue(902)
+		local playerPos = player:getPosition()
+		doCreateDummy(cid, Position(playerPos.x, playerPos.y - 5, 10))
 		position:sendMagicEffect(CONST_ME_MAGIC_BLUE)
 	else
 		player:teleportTo(fromPosition)
