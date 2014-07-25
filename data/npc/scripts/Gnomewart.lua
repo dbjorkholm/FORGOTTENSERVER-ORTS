17d7e16022bfd1a4cf53a3c264182437236349fa
Daniel <danielbjorkholm@live.se>
2014-07-25 13:20:32 +0200

    More typos :<

--- data/npc/scripts/Gnomewart.lua
+++ data/npc/scripts/Gnomewart.lua
@@ -15,7 +15,7 @@ local function creatureSayCallback(cid, type, msg)
 	local player = Player(cid)
 	if msgcontains(msg, "endurance") then
 		if player:getStorageValue(Storage.BigfootBurden.QuestLine) == 9 then
-			npcHandler:say({"Ah, the test is a piece of mushroomcake! Just take the teleporter over there in the south and follow the hallway. ...", npcHandler:say("You'll need to run quite a bit. It is important that you don't give up! Just keep running and running and running and ... I guess you got the idea. ...", npcHandler:say("At the end of the hallway you'll find a teleporter. Step on it and you are done! I'm sure you'll do a true gnomerun! Afterwards talk to me."}, cid)
+			npcHandler:say({"Ah, the test is a piece of mushroomcake! Just take the teleporter over there in the south and follow the hallway. ...", "You'll need to run quite a bit. It is important that you don't give up! Just keep running and running and running and ... I guess you got the idea. ...", "At the end of the hallway you'll find a teleporter. Step on it and you are done! I'm sure you'll do a true gnomerun! Afterwards talk to me."}, cid)
 			player:setStorageValue(Storage.BigfootBurden.QuestLine, 10)
 		elseif player:getStorageValue(Storage.BigfootBurden.QuestLine) == 11 then
 			npcHandler:say("You have passed the test and are ready to create your soul melody. Talk to Gnomelvis in the east about it.", cid)
