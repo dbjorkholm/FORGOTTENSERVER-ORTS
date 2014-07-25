1709a7581b834993d553d8b8cecea99636defe4b
Royalot <zapf-benny@web.de>
2014-07-25 22:43:10 +0200

    push creature from bridge
    so creature and player dont stuck in water like rl
    
    
    Former-commit-id: 3e73f8c114b29a880c0c9727bc0c3e46a2884986 [formerly e7d7c0263533a3aff62ffb474e4aac1acbaa7c2b]
    Former-commit-id: d76b009624da236f60be3cc19db9770a307f8101

--- data/actions/scripts/quests/hellgatelever.lua
+++ data/actions/scripts/quests/hellgatelever.lua
@@ -4,12 +4,19 @@ local config = {
 		[2] = Position(32628, 31699, 10),
 		[3] = Position(32629, 31699, 10)
 	},
+	removeCreaturePosition = Position(32630, 31699, 10),
 	bridgeID = 5770,
 	waterID = 493
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	for i = 1, #config.bridgePositions do
+		--if Creature on bridge then push off
+		local creature = Tile(config.bridgePositions[i]):getTopCreature()
+		if creature ~= nil then
+			creature:teleportTo(config.removeCreaturePosition)
+		end
+		--create and remove bridge
 		local tile = config.bridgePositions[i]:getTile()
 		if tile then
 			local thing = tile:getItemById(item.itemid == 1945 and config.waterID or config.bridgeID)
