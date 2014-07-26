c455e4c6e7eb0e7c942e6a83fb318d122d23392e
edumntg <edumntg@gmail.com>
2014-07-26 01:55:23 -0430

    Some scripts converted to meta. Fixed function playerExists in customfunctions.lua. It was always returning true. Fixed a bug with a nil variable Added a new creaturescript. This new script will work with the 'onKill' event and will support misc kills (Bosses, creatures quantity, etc). It is registered with the name of 'MiscKill' Added the following achievements to the game:
    Askarak Nemesis (Obtained when killing 100 Askarak Prince)
    Back from the Dead (Obtained when slaying Zanakeph)
    Back into the Abyss (Obtained when slaying Thul)
    Bibby's Bloodbath (Obtained when slaying Bibby Bloodbath)
    Blood-Red Snapper (Obtained when slaying The Snapper)
    Choking on Her Venom (Obtained when slaying The Old Widow)
    Cocoon of Doom (Obtained when slaying Bloom of Doom) [Note: Slaying or dealing damage? Idk if that creature can be killed]
    Crawling Death (Obtained when slaying Fleshcrawler
    Death Song (Obtained when killing 300 Deepling Spellsingers)
    Deer Hunt (Obtained when killing 400 White Deers)
    
    Note that the achievements were added following the description found at www.tibia.wikia.com
    More achievements will be added eventually.
    
    
    Former-commit-id: 1febb64a0475eaec9aefe7c50a76d2c046df126c

--- data/actions/scripts/bigfoot burden quest/bigfootBurdenQuestCrystal.lua
+++ data/actions/scripts/bigfoot burden quest/bigfootBurdenQuestCrystal.lua
@@ -1,15 +1,16 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(itemEx.itemid == 18307) then
-		if(getPlayerStorageValue(cid, 930) == 1) then
-			if(getPlayerStorageValue(cid, 932) < 5) then
-				setPlayerStorageValue(cid, 932, getPlayerStorageValue(cid, 932) + 1)
-				doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "You have repaired a damaged crystal.")
-				doTransformItem(itemEx.uid, 18311)
-				doSendMagicEffect(toPosition, CONST_ME_ENERGYHIT)
+	local player = Player(cid)
+	if itemEx.itemid == 18307 then
+		if player:getStorageValue(930) == 1 then
+			if player:getStorageValue(932) < 5 then
+				player:setStorageValue(932, player:getStorageValue(932) + 1)
+				player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have repaired a damaged crystal.")
+				Item(itemEx.uid):transform(18311)
+				toPosition:sendMagicEffect(CONST_ME_ENERGYHIT)
 			end
 		end
-	elseif(itemEx.itemid == 18311) then
-		doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "It's not the crystal you're looking for.")
+	elseif itemEx.itemid == 18311 then
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "It's not the crystal you're looking for.")
 	end
 	return true
 end
--- data/actions/scripts/bigfoot burden quest/bigfootBurdenQuestRepair.lua
+++ data/actions/scripts/bigfoot burden quest/bigfootBurdenQuestRepair.lua
@@ -1,10 +1,11 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.itemid == 18343 and isMonster(itemEx.uid)) then
-		if(getPlayerStorageValue(cid, 946) < 4 and getPlayerStorageValue(cid, 945) == 1 and string.lower(getCreatureName(itemEx.uid)) == "damaged crystal golem") then
-			setPlayerStorageValue(cid, 946, getPlayerStorageValue(cid, 946) + 1)
-			doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "The golem has been returned to the gnomish workshop.")
-			doRemoveCreature(itemEx.uid)
-			doSendMagicEffect(toPosition, CONST_ME_POFF)
+	local player = Player(cid)
+	if item.itemid == 18343 and isMonster(itemEx.uid) then
+		if player:getStorageValue(946) < 4 and player:getStorageValue(945) == 1 and string.lower(Creature(itemEx.uid):getName()) == "damaged crystal golem" then
+			player:setStorageValue(946, player:getStorageValue(946) + 1)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The golem has been returned to the gnomish workshop.")
+			Creature(itemEx.uid):remove()
+			toPosition:sendMagicEffect(CONST_ME_POFF)
 		end
 	end
 	return true
