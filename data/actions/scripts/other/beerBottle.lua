77e6752a6930de94b64fbdbef10f5cc97de095d8
Skillert <peter.mollberg@gmail.com>
2014-07-27 22:45:55 +0200

    Fixes, cheers @ninjalulz
    Former-commit-id: f730eeee9d41856b345cd3d0c0fb9267d8b7e3e8

--- data/actions/scripts/other/beerBottle.lua
+++ data/actions/scripts/other/beerBottle.lua
@@ -2,11 +2,11 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if itemEx.itemid ~= 8176 then
 		return false
 	end
-	local player = Player(cid)
 	
+	local player = Player(cid)
 	player:removeItem(7496, 1)
 	player:addItem(7495, 1)
 	player:say("GULP, GULP, GULP", TALKTYPE_MONSTER_SAY, false, 0, toPosition)
-	toPosition:sendMagicEffect(CONST_ME_NOTES) --should be yellow music notes
+	toPosition:sendMagicEffect(CONST_ME_SOUND_YELLOW)
 	return true
 end
