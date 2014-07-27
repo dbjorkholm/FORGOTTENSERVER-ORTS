f2819d38357dc2dd3bf10867a1663d04fb6296a7
Skillert <peter.mollberg@gmail.com>
2014-07-27 21:57:46 +0200

    Fixes, cheers @ninjalulz
    Former-commit-id: 8913151ca2169d89b4ef51e388a95c8880d00fef

--- data/actions/scripts/other/beerBottle.lua
+++ data/actions/scripts/other/beerBottle.lua
@@ -1,13 +1,12 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local player = Player(cid)
-	
-	if itemEx.itemid == 8176 then
-		player:removeItem(7496, 1)
-		player:addItem(7495, 1)
-		player:say("GULP, GULP, GULP", TALKTYPE_MONSTER_SAY, false, 0, toPosition)
-		toPosition:sendMagicEffect(CONST_ME_NOTES) --should be yellow music notes
-	else
+	if itemEx.itemid ~= 8176 then
 		return false
 	end
+	local player = Player(cid)
+	
+	player:removeItem(7496, 1)
+	player:addItem(7495, 1)
+	player:say("GULP, GULP, GULP", TALKTYPE_MONSTER_SAY, false, 0, toPosition)
+	toPosition:sendMagicEffect(CONST_ME_NOTES) --should be yellow music notes
 	return true
 end
