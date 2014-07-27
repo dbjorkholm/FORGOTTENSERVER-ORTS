53c05b6f87aecd5c7510191b822f1c7803cc429e
Skillert <peter.mollberg@gmail.com>
2014-07-27 21:54:29 +0200

    Minor
    Former-commit-id: 1d594472090cab75ccd9bcc1edf3fc799adab894 [formerly 2c97e7f143aef6e7c657b71eb4b1d030396e190a]
    Former-commit-id: 3acd187bcf7f005bd52ef1a56f7522f3267a4f89

--- data/actions/scripts/other/beerBottle.lua
+++ data/actions/scripts/other/beerBottle.lua
@@ -4,7 +4,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if itemEx.itemid == 8176 then
 		player:removeItem(7496, 1)
 		player:addItem(7495, 1)
-		Player(cid):say("GULP, GULP, GULP", TALKTYPE_MONSTER_SAY, false, 0, toPosition)
+		player:say("GULP, GULP, GULP", TALKTYPE_MONSTER_SAY, false, 0, toPosition)
 		toPosition:sendMagicEffect(CONST_ME_NOTES) --should be yellow music notes
 	else
 		return false
