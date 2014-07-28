f51731dda4b890d0a26f6159739a9afd08a3b92d
PrinterLUA <mastero_90@hotmail.com>
2014-07-28 14:04:53 +0200

    Rework annihilator & demon helmet levers
    https://github.com/PrinterLUA/FORGOTTENSERVER-ORTS/issues/937
    
    Former-commit-id: 574258757c75330a3fb683e0fdf48b2f8bb9b79e [formerly 87965effa56c411642200253f41c137b7d8bf4eb]
    Former-commit-id: ab98d924653270cb031818c9c381fa50bf94192c

--- data/actions/scripts/demon helmet quest/demonHelmetQuestLever.lua
+++ data/actions/scripts/demon helmet quest/demonHelmetQuestLever.lua
@@ -1,26 +1,34 @@
 local t = {
-	{x = 33314, y = 31592, z = 15}, -- stone position
-	{x = 33316, y = 31591, z = 15}, -- teleport creation position
-	{x = 33322, y = 31592, z = 14} -- where the teleport takes you
+	Position(33314, 31592, 15), -- stone position
+	Position(33316, 31591, 15}, -- teleport creation position
+	Position(33322, 31592, 14} -- where the teleport takes you
 }
  
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.itemid == 1945) then
-		if(getPlayerLevel(cid) > 99) then
-			local stone = getTileItemById(t[1], 1355).uid
-			if(stone > 0) then
-				doRemoveItem(stone)
+	if item.itemid == 1945 then
+		local tile = Position(t[1]):getTile()
+		if tile then
+			local stone = tile:getItemById(1355)
+			if stone and stone:isItem() then
+				stone:remove()
 			end
- 
-			doCreateTeleport(1387, t[3], t[2])
-			doSendMagicEffect(t[2], CONST_ME_TELEPORT)
-		else
-			return (doPlayerSendCancel(cid, "You cannot use this object."))
 		end
-	elseif(item.itemid == 1946) then
-		doRemoveItem(getTileItemById(t[2], 1387).uid)
-		doSendMagicEffect(t[2], CONST_ME_POFF)
-		doCreateItem(1355, 1, t[1])
+
+		local teleport = Game.createItem(1387, 1, t[2])
+		if teleport then
+			teleport:setDestination(t[3])
+			Position(t[2]):sendMagicEffect(CONST_ME_TELEPORT)
+		end
+	elseif item.itemid == 1946 then
+		local tile = Position(t[2]):getTile()
+		if tile then
+			local teleport = tile:getItemById(1387)
+			if teleport and teleport:isTeleport() then
+				teleport:remove()
+			end
+		end
+		Position(t[2]):sendMagicEffect(CONST_ME_POFF)
+		Game.createItem(1355, 1, t[1])
 	end
-	return doTransformItem(item.uid, item.itemid == 1945 and 1946 or 1945)
+	return Item(item.uid):transform(item.itemid == 1945 and 1946 or 1945)
 end
