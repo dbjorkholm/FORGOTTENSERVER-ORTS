a06522aa538b5cdf92cc12a3f89b3997415e634c
PrinterLUA <mastero_90@hotmail.com>
2014-07-28 18:18:31 +0200

    Minor fix anni and dhelm quests
    About remove player and level, that won't happen since the annih code is
    using those objects more than once. Instead of declaring
    Player(cid):stuff()
    Player(cid):stuff()
    
    You should make:
    local player = Player(cid)
    player:stuff()
    player:stuff()
    
    Else if you do it like the first example, it will create the object
    twice instead of reuse same object.
    
    Talked with @Dalkon about this.
    
    
    Former-commit-id: b72653f91445e0365a5213bd318e19c75b3ae9c1

--- data/actions/scripts/demon helmet quest/demonHelmetQuestLever.lua
+++ data/actions/scripts/demon helmet quest/demonHelmetQuestLever.lua
@@ -1,12 +1,12 @@
 local t = {
 	Position(33314, 31592, 15), -- stone position
 	Position(33316, 31591, 15}, -- teleport creation position
-	Position(33322, 31592, 14} -- where the teleport takes you
+	Position(33322, 31592, 14) -- where the teleport takes you
 }
  
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	if item.itemid == 1945 then
-		local tile = Position(t[1]):getTile()
+		local tile = t[1]:getTile()
 		if tile then
 			local stone = tile:getItemById(1355)
 			if stone and stone:isItem() then
@@ -17,17 +17,17 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		local teleport = Game.createItem(1387, 1, t[2])
 		if teleport then
 			teleport:setDestination(t[3])
-			Position(t[2]):sendMagicEffect(CONST_ME_TELEPORT)
+			t[2]:sendMagicEffect(CONST_ME_TELEPORT)
 		end
 	elseif item.itemid == 1946 then
-		local tile = Position(t[2]):getTile()
+		local tile = t[2]:getTile()
 		if tile then
 			local teleport = tile:getItemById(1387)
 			if teleport and teleport:isTeleport() then
 				teleport:remove()
 			end
 		end
-		Position(t[2]):sendMagicEffect(CONST_ME_POFF)
+		t[2]:sendMagicEffect(CONST_ME_POFF)
 		Game.createItem(1355, 1, t[1])
 	end
 	return Item(item.uid):transform(item.itemid == 1945 and 1946 or 1945)
