f4cdcc70e178ec58073466e5f0a2da6fec88a82e
Sam <sum9900@web.de>
2014-07-31 03:58:50 +0200

    Adjust scripts to #1012, fixes #1021, cleanup
    Former-commit-id: 546616d8f4537f73b28573def323fc81af46bf07

--- data/actions/scripts/dreamer challenge quest/brotherhoodOfBonesQuestDocumentsLever.lua
+++ data/actions/scripts/dreamer challenge quest/brotherhoodOfBonesQuestDocumentsLever.lua
@@ -1,27 +1,33 @@
 local config = {
-	[3045] = {Position({x = 32784, y = 32222, z = 14}), 7844},
-	[3046] = {Position({x = 32785, y = 32230, z = 14}), 7846},
-	[3047] = {Position({x = 32781, y = 32226, z = 14}), 7845}
+	[3045] = {position = Position(32784, 32222, 14), itemId = 7844},
+	[3046] = {position = Position(32785, 32230, 14), itemId = 7846},
+	[3047] = {position = Position(32781, 32226, 14), itemId = 7845}
 }
 
-function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local targetItem = config[item.uid]
-	if not targetItem then
+local function reverLever(position)
+	local leverItem = Tile(position):getItemById(1946)
+	if leverItem then
+		leverItem:transform(1945)
+	end
+end
+
+function onUse(cid, item, position, itemEx, toPosition)
+	local altar = config[item.uid]
+	if not altar then
 		return true
 	end
-	
-	if item.itemid == 1945 then
-		local tile = targetItem[1]:getTile()
-		if tile then
-			local thing = tile:getItemById(2145)
-			if thing and thing:isItem() then
-				Item(item.uid):transform(1946)
-				targetItem[1]:sendMagicEffect(CONST_ME_TELEPORT)
-				Game.createItem(targetItem[2], 1, targetItem[1])
-				thing:remove()
-				addEvent(function(toPosition) local tile = toPosition:getTile() if tile then local thing = tile:getItemById(1946) if thing and thing:isItem() then thing:transform(1945) end end end, 4 * 1000, toPosition)
-			end
-		end
+
+	if item.itemid ~= 1945 then
+		return true
+	end
+
+	local diamondItem = Tile(altar.position):getItemById(2145)
+	if diamondItem then
+		diamondItem:remove(1)
+		altar.position:sendMagicEffect(CONST_ME_TELEPORT)
+		Game.createItem(altar.itemId, 1, altar.position)
+		Item(item.uid):transform(1946)
+		addEvent(reverLever, 4 * 1000, position)
 	end
 	return true
 end
--- data/actions/scripts/tools/machete.lua
+++ data/actions/scripts/tools/machete.lua
@@ -3,17 +3,17 @@ local WILD_GROWTH = { 1499, 11099 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local targetItem = Item(itemEx.uid)
-	if(isInArray(JUNGLE_GRASS, itemEx.itemid)) then
+	if isInArray(JUNGLE_GRASS, itemEx.itemid) then
 		targetItem:transform(itemEx.itemid == 19433 and 19431 or itemEx.itemid - 1)
 		targetItem:decay()
 		return true
 	end
 
-	if(isInArray(WILD_GROWTH, itemEx.itemid)) then
+	if isInArray(WILD_GROWTH, itemEx.itemid) then
 		toPosition:sendMagicEffect(CONST_ME_POFF)
 		targetItem:remove()
 		return true
 	end
-	
+
 	return destroyItem(cid, itemEx, toPosition)
 end
