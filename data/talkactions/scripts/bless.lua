30299829d0aff8ed2e44b7dccf9a2d0f1bfbe574
EPuncker <gm.perflex@gmail.com>
2014-07-30 22:05:44 -0300

    Tried to fix #1061
    Former-commit-id: f04a327253c53ff219ac8e1424016019505de999 [formerly 2c35c1141119533769488d1f94edeea63b37d3da]
    Former-commit-id: e7165183726ede99b5f0e41967052c1420ebdbd8

--- data/talkactions/scripts/bless.lua
+++ data/talkactions/scripts/bless.lua
@@ -1,22 +1,22 @@
 function onSay(cid, words, param)
-	local p = Player(cid)
-	local cost = getBlessingsCost(getPlayerLevel(cid))
-	if(not(isPlayerPzLocked(cid))) then
-		if(p:hasBlessing(1) and p:hasBlessing(2) and p:hasBlessing(3) and p:hasBlessing(4) and p:hasBlessing(5)) then
-			p:sendCancelMessage("You have already been blessed by the gods.")
+	local player = Player(cid)
+	local totalPrice = getBlessingsCost(player:getLevel()) * 5
+	if not(isPlayerPzLocked(cid)) then
+		if player:hasBlessing(1) and player:hasBlessing(2) and player:hasBlessing(3) and player:hasBlessing(4) and player:hasBlessing(5) then
+			player:sendCancelMessage("You have already been blessed by the gods.")
 			return false
 		end
-		if(p:removeMoney(cost)) then
-			for b = 1,5 do
-				p:addBlessing(b)
+		if player:removeMoney(totalPrice) then
+			for b = 1, 5 do
+				player:addBlessing(b)
 			end
-			p:getPosition():sendMagicEffect(CONST_ME_HOLYAREA)
-			p:sendTextMessage(MESSAGE_INFO_DESCR, "You have been blessed by the 5 gods!")
+			player:getPosition():sendMagicEffect(CONST_ME_HOLYAREA)
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "You have been blessed by the five gods!")
 		else
-			p:sendCancelMessage("You need "..cost.." gold coins to buy all 5 bless.")
+			player:sendCancelMessage("You need ".. totalPrice .." gold coins to buy all 5 bless.")
 		end
 	else
-		p:sendCancelMessage("You can't buy bless, when you are in a battle.")
+		player:sendCancelMessage("You can't buy bless, when you are in a battle.")
 	end
-return false
+	return false
 end
