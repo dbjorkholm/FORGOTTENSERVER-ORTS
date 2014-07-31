5092b84f732afbcef5245ae1ee202333b277a38b
Skillert <peter.mollberg@gmail.com>
2014-07-31 17:13:46 +0200

    Postman: - Fix rest of the quest
    Former-commit-id: 7acbc6160808fd36eed86cd11ae3a0d4e8c7513c

--- data/actions/scripts/postman quest/postmanQuestWaldo.lua
+++ data/actions/scripts/postman quest/postmanQuestWaldo.lua
@@ -1,12 +1,12 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.uid == 3118) then
-		if(getPlayerStorageValue(cid, 250) == 35) then
-			setPlayerStorageValue(cid, 250, 36)
-			doPlayerAddItem(cid, 2332, 1)
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You've found a post horn.")
-		else
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "The chest is empty.")
-		end
+	local player = Player(cid)
+
+	if player:getStorageValue(Storage.postman.Mission08) == 1 then
+		player:setStorageValue(Storage.postman.Mission08, 2)
+		player:addItem(2332, 1)
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a post horn.")
+	else
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
 	end
 	return true
 end
