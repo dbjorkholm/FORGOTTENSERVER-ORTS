519417391de57d8386933d6b4bce10a7ac67ced4
PrinterLUA <mastero_90@hotmail.com>
2014-08-01 21:26:41 +0200

    Automatic add greet bubble and fixed some memory leaks
    https://github.com/PrinterLUA/FORGOTTENSERVER-ORTS/issues/1066

--- data/npc/lib/npcsystem/npchandler.lua
+++ data/npc/lib/npcsystem/npchandler.lua
@@ -352,24 +352,30 @@ if NpcHandler == nil then
 	end
 
 	-- Handles onCreatureAppear events. If you with to handle this yourself, please use the CALLBACK_CREATURE_APPEAR callback.
-	function NpcHandler:onCreatureAppear(cid)
-		if cid == getNpcCid() and next(self.shopItems) ~= nil then
-			local npc = Npc()
-			local speechBubble = npc:getSpeechBubble()
-			if speechBubble == 3 then
-				npc:setSpeechBubble(4)
-			else
-				npc:setSpeechBubble(2)
-			end
-		end
-
-		local callback = self:getCallback(CALLBACK_CREATURE_APPEAR)
-		if callback == nil or callback(cid) then
-			if self:processModuleCallback(CALLBACK_CREATURE_APPEAR, cid) then
-				--
-			end
-		end
-	end
+        function NpcHandler:onCreatureAppear(cid)
+                if cid == getNpcCid() then
+                        local npc = Npc()
+                        if next(self.shopItems) then
+                                local speechBubble = npc:getSpeechBubble()
+                                if speechBubble == 3 then
+                                        npc:setSpeechBubble(4)
+                                else
+                                        npc:setSpeechBubble(2)
+                                end
+                        else
+                                if self:getMessage(MESSAGE_GREET) then
+                                        npc:setSpeechBubble(1)
+                                end
+                        end
+                end
+ 
+                local callback = self:getCallback(CALLBACK_CREATURE_APPEAR)
+                if callback == nil or callback(cid) then
+                        if self:processModuleCallback(CALLBACK_CREATURE_APPEAR, cid) then
+                                --
+                        end
+                end
+        end
 
 	-- Handles onCreatureDisappear events. If you with to handle this yourself, please use the CALLBACK_CREATURE_DISAPPEAR callback.
 	function NpcHandler:onCreatureDisappear(cid)
