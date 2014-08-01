1a9ec27d1cbc56f69652690aca87bc8e82a6c25b
EPuncker <gm.perflex@gmail.com>
2014-08-01 11:34:31 -0300

    Small fixes
    Former-commit-id: cb312a8d92a0e7b04ce46e15770a18b47f7cf348 [formerly 850c6f5a9e12cc0f2b04c9aad0ac030e1c97111c]
    Former-commit-id: 9919fced0a775f4a0bc26a5cfccc829439f12188

--- data/actions/scripts/in service of yalahar quest/inServiceOfYalaharQuestReward.lua
+++ data/actions/scripts/in service of yalahar quest/inServiceOfYalaharQuestReward.lua
@@ -1,35 +1,31 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
-	if(player:getStorageValue(10012) < 1) then
-		doPlayerAddExp(cid, 250000, true, true)
-		player:setStorageValue(10012, 1)
-	end
 	if(item.uid == 3088) then
 		if(player:getStorageValue(Storage.InServiceofYalahar.Questline) == 53) then
 			player:setStorageValue(Storage.InServiceofYalahar.Questline, 54)
 			player:setStorageValue(Storage.InServiceofYalahar.Mission10, 5) -- StorageValue for Questlog "Mission 10: The Final Battle"
 			player:addItem(9776, 1)
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a yalahari armor.")
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found a yalahari armor.")
 		else
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The chest is empty.")
 		end
 	elseif(item.uid == 3089) then
 		if(player:getStorageValue(Storage.InServiceofYalahar.Questline) == 53) then
 			player:setStorageValue(Storage.InServiceofYalahar.Questline, 54)
 			player:setStorageValue(Storage.InServiceofYalahar.Mission10, 5) -- StorageValue for Questlog "Mission 10: The Final Battle"
 			player:addItem(9778, 1)
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a yalahari mask.")
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found a yalahari mask.")
 		else
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The chest is empty.")
 		end
 	elseif(item.uid == 3090) then
 		if(player:getStorageValue(Storage.InServiceofYalahar.Questline) == 53) then
 			player:setStorageValue(Storage.InServiceofYalahar.Questline, 54)
 			player:setStorageValue(Storage.InServiceofYalahar.Mission10, 5) -- StorageValue for Questlog "Mission 10: The Final Battle"
 			player:addItem(9777, 1)
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "You've found a yalahari leg piece.")
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found a yalahari leg piece.")
 		else
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "The chest is empty.")
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The chest is empty.")
 		end
 	end
 	return true
--- data/actions/scripts/other/premiumscroll.lua
+++ data/actions/scripts/other/premiumscroll.lua
@@ -4,7 +4,7 @@ end
 
 function onUse(cid, item, fromPosition, itemEx, toPosition) 
 	doPlayerAddPremiumPoints(cid, 30)
-	doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "You have recived 30 shop points to your account.")
+	doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "You have received 30 shop points to your account.")
 	doSendMagicEffect(getCreaturePosition(cid), 28)
 	doRemoveItem(item.uid,1)
 	return true
