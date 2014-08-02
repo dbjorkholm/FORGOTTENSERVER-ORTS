b53299aebc76eb6795afde1f1aa5b788aca3e17e
Sam <sum9900@web.de>
2014-08-02 22:40:45 +0200

    Reviewed rest in creaturescripts

--- data/creaturescripts/scripts/others/StorageConversion.lua
+++ data/creaturescripts/scripts/others/StorageConversion.lua
@@ -1,120 +1,98 @@
--- By Vinioliveirasilva --
--- contact: vinioliveirasilva@hotmail.com --
+local conversions = {
+	rashid = {
+		{101, 1},
+		{101, 2},
+		{102, 1},
+		{102, 2},
+		{102, 3},
+		{102, 4},
+		{102, 5},
+		{103, 1},
+		{103, 2},
+		{103, 3},
+		{104, 1},
+		{104, 2},
+		{104, 3},
+		{105, 1},
+		{105, 2},
+		{105, 3},
+		{106, 1},
+		{106, 2},
+		{106, 2},
+		{107, 1}
+	},
+	greenDjinn = {
+		{121, 1},
+		{122, 1},
+		{122, 2},
+		{122, 3},
+		{122, 4},
+		{123, 1},
+		{123, 2},
+		{123, 3},
+		{124, 1},
+		{124, 2},
+		{124, 3},
+		{124, 4}
+	},
+	blueDjinn = {
+		{111, 1},
+		{112, 1},
+		{112, 2},
+		{112, 3},
+		{113, 1},
+		{113, 2},
+		{113, 3},
+		{114, 1},
+		{114, 2},
+		{114, 3},
+		{114, 4}
+	}
+}
+
+local function rashidConversion(player)
+	local value = player:getStorageValue(85)
+	if value > 0 and player:getStorageValue(107) < 0 then
+		for i = 1, value do
+			player:setStorageValue(conversions.rashid[i][1], conversions.rashid[i][2])
+		end
+	end
+
+	if value == 20 then
+		player:setStorageValue(85, -1)
+	end
+end
+
+local function greenDjinnConversion(player)
+	local value = player:getStorageValue(81)
+	if value > 0 and player:getStorageValue(124) <= 4 then
+		for i = 1, value do
+			player:setStorageValue(conversions.greenDjinn[i][1], conversions.greenDjinn[i][1])
+		end
+	end
+
+	if value == 12 then
+		player:setStorageValue(81, -1)
+	end
+end
+
+local function blueDjinnConversion(player)
+	local value = player:getStorageValue(82)
+	if value > 0 and player:getStorageValue(114) <= 3 then
+		for i = 1, value do
+			player:setStorageValue(conversions.blueDjinn[i][1], conversions.blueDjinn[i][1])
+		end
+	end
+
+	if value == 11 then
+		player:setStorageValue(82, -1)
+	end
+end
 
 function onLogin(cid)
-	RashidConversion(cid)
-	GreenDjinnConversion(cid)
-	BlueDjinnConversion(cid)
-	return true
-end
-
-function RashidConversion(cid)
-local list = {
-			[1] = {101, 1},
-			[2] = {101, 2},
-			
-			[3] = {102, 1},
-			[4] = {102, 2},
-			[5] = {102, 3},
-			[6] = {102, 4},
-			[7] = {102, 5},
-			
-			[8] = {103, 1},
-			[9] = {103, 2},
-			[10] = {103, 3},
-			
-			[11] = {104, 1},
-			[12] = {104, 2},
-			[13] = {104, 3},
-			
-			[14] = {105, 1},
-			[15] = {105, 2},
-			[16] = {105, 3},
-			
-			[17] = {106, 1},
-			[18] = {106, 2},
-			[19] = {106, 2},
-			
-			[20] = {107, 1}
-}
 	local player = Player(cid)
-	local Old = 85
-	local OldValue = player:getStorageValue(85)
-	
-	if(OldValue > 0 and player:getStorageValue(107) < 0) then	
-		for i = 1, OldValue do
-			player:setStorageValue(list[i][1], list[i][2])
-		end
-	end
-	if(OldValue == 20) then
-		player:setStorageValue(Old, -1)
-	end
-	return true
-end
-
-function GreenDjinnConversion(cid)
-
-local list = {
-			[1] = {121, 1},
-			
-			[2] = {122, 1},
-			[3] = {122, 2},
-			[4] = {122, 3},
-			[5] = {122, 4},
-			
-			[6] = {123, 1},
-			[7] = {123, 2},
-			[8] = {123, 3},
-			
-			[9] = {124, 1},
-			[10] = {124, 2},
-			[11] = {124, 3},
-			[12] = {124, 4}
-}
-	local player = Player(cid)
-	local Old = 81
-	local OldValue = player:getStorageValue(81)
-	
-	if(OldValue > 0 and player:getStorageValue(124) <= 4) then	
-		for i = 1, OldValue do
-			player:setStorageValue(list[i][1], list[i][2])
-		end
-	end
-	if(OldValue == 12) then
-		player:setStorageValue(Old, -1)
-	end
-	return true
-end
-
-function BlueDjinnConversion(cid)
-
-local list = {
-			[1] = {111, 1},
-			
-			[2] = {112, 1},
-			[3] = {112, 2},
-			[4] = {112, 3},
-			
-			[5] = {113, 1},
-			[6] = {113, 2},
-			[7] = {113, 3},
-
-			[8] = {114, 1},
-			[9] = {114, 2},
-			[10] = {114, 3},
-			[11] = {114, 4}
-}
-	local player = Player(cid)
-	local Old = 82
-	local OldValue = player:getStorageValue( 82)
-	
-	if(OldValue > 0 and player:getStorageValue(114) <= 3) then	
-		for i = 1, OldValue do
-			player:setStorageValue(list[i][1], list[i][2])
-		end
-	end
-	if(OldValue == 11) then
-		player:setStorageValue(Old, -1)
-	end
+	rashidConversion(player)
+	greenDjinnConversion(player)
+	blueDjinnConversion(player)
 	return true
 end
--- data/creaturescripts/scripts/others/advance_save.lua
+++ data/creaturescripts/scripts/others/advance_save.lua
@@ -1,17 +1,26 @@
-local saveOnAdavance = true
-local healOnAdvance = true
+local config = {
+	heal = true,
+	save = true,
+	effect = true
+}
 
 function onAdvance(cid, skill, oldLevel, newLevel)
+	if skill ~= 8 or newLevel <= oldLevel then
+		return true
+	end
+
 	local player = Player(cid)
-        if skill == 8 and newLevel > oldLevel then
+	if config.effect then
 		player:getPosition():sendMagicEffect(math.random(CONST_ME_FIREWORK_YELLOW, CONST_ME_FIREWORK_BLUE))
-		player:say("LEVEL UP!", TALKTYPE_MONSTER_SAY)
-		if healOnAdvance then
-			player:addHealth(player:getMaxHealth())
-		end
-		if saveOnAdavance then
-			player:save()
-		end
+		player:say('LEVEL UP!', TALKTYPE_MONSTER_SAY)
+	end
+
+	if config.heal then
+		player:addHealth(player:getMaxHealth())
+	end
+
+	if config.save then
+		player:save()
 	end
 	return true
 end
