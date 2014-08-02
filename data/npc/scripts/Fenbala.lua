fc6c1ede819e7aa133250f6683b78575141c2a73
EPuncker <gm.perflex@gmail.com>
2014-08-02 00:18:29 -0300

    Fixed #714
    Former-commit-id: 29771e9523e82940f02d1b71a58bc97cbc43981e

--- data/npc/scripts/Fenbala.lua
+++ data/npc/scripts/Fenbala.lua
@@ -7,8 +7,27 @@ function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink()				npcHandler:onThink()					end
 
+local condition = Condition(CONDITION_FIRE)
+condition:setParameter(CONDITION_PARAM_DELAYED, 1)
+condition:addDamage(10, 1000, -10)
+
+local function creatureSayCallback(cid, type, msg)
+	if not npcHandler:isFocused(cid) then
+		return false
+	end 
+	if isInArray({"fuck", "idiot", "asshole", "ass", "fag", "stupid", "tyrant", "shit", "lunatic"}, msg) then
+		selfSay("Take this!", cid)
+		Player(cid):getPosition():sendMagicEffect(CONST_ME_EXPLOSIONAREA)
+		Player(cid):addCondition(condition)
+		npcHandler:releaseFocus(cid)
+		npcHandler:resetNpc(cid)
+	end
+	return true	
+end
+
 npcHandler:setMessage(MESSAGE_GREET, "MIND YOUR MANNERS COMMONER! Address the queen with her title!")
 npcHandler:setMessage(MESSAGE_FAREWELL, "LONG LIVE THE QUEEN!")
 npcHandler:setMessage(MESSAGE_WALKAWAY, "LONG LIVE THE QUEEN!")
 
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
