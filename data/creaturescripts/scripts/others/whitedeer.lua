36f98102b6e4d90adfc99d123946f0f0d9ed9e38
Sam <sum9900@web.de>
2014-08-02 22:40:45 +0200

    Reviewed rest in creaturescripts
    Former-commit-id: 0b8a4c7a6ca16b43c169f1381797e861db488521 [formerly b53299aebc76eb6795afde1f1aa5b788aca3e17e]
    Former-commit-id: 45f4fcfb7e67ce9d68d9f55ea4c32f93cfa4b076

--- data/creaturescripts/scripts/others/whitedeer.lua
+++ data/creaturescripts/scripts/others/whitedeer.lua
@@ -1,23 +1,25 @@
 local config = {
-	{1, 30, "Enraged White Deer", "The white deer summons all his strength and turns to fight!"},
-	{31, 100, "Desperate White Deer", "The white deer desperately tries to escape!"}
+	-- ordered by chance, the last chance being 100
+	{chance = 30,  monster = 'Enraged White Deer',   message = 'The white deer summons all his strength and turns to fight!'},
+	{chance = 100, monster = 'Desperate White Deer', message = 'The white deer desperately tries to escape!'}
 }
 
 function onDeath(cid, corpse, lasthitkiller, mostdamagekiller, lasthitunjustified, mostdamageunjustified)
-	local monster = Monster(cid)
-	if not monster or monster:getMaster() ~= nil then
+	local targetMonster = Monster(cid)
+	if not targetMonster or targetMonster:getMaster() then
 		return true
 	end
-	
-	local rand = math.random(100)
+
+	local chance = math.random(100)
 	for i = 1, #config do
-		if rand >= config[i][1] and rand <= config[i][2] then
-			local spawnMonster = Game.createMonster(config[i][3], monster:getPosition(), true, true)
+		if chance <= config[i].chance then
+			local spawnMonster = Game.createMonster(config[i].monster, targetMonster:getPosition(), true, true)
 			if spawnMonster then
 				spawnMonster:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-				monster:say(config[i][4], TALKTYPE_MONSTER_SAY)
+				targetMonster:say(config[i].message, TALKTYPE_MONSTER_SAY)
 			end
 			break
 		end
 	end
+	return true
 end
