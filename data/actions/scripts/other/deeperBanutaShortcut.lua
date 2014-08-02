efdf9f5fb896688b9c24ae5c191ecd734344109d
Royalot <zapf-benny@web.de>
2014-08-02 20:10:02 +0200

    unnatural mission 4
    Former-commit-id: 11f0a11b8ecb698ebab8da0e680ca83451aa046f [formerly 4f8a7997a27d68e59c6cf5aca2ffb82d8b9903ae]
    Former-commit-id: e38751fe9e462dc605ae1a005242cb4336edaed0

--- data/actions/scripts/other/deeperBanutaShortcut.lua
+++ data/actions/scripts/other/deeperBanutaShortcut.lua
@@ -5,7 +5,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		player:removeItem(10523, 1)
 		player:setStorageValue(Storage.DeeperBanuta.Shortcut, 1)
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You discovered a secret tunnel.")
-	else
+	elseif itemEx.actionid == 62378 then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have already discovered this secret.")
 	end
 	return true
