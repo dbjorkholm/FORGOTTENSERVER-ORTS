869bbfe2546bf167cb0154df2dc33a409a1774c3
EPuncker <gm.perflex@gmail.com>
2014-08-03 00:37:59 -0300

    Small tweaks
    Former-commit-id: 0ac26da56910f810be71e878b9a86bd54f6d0609

--- data/actions/scripts/other/premiumscroll.lua
+++ data/actions/scripts/other/premiumscroll.lua
@@ -1,11 +1,7 @@
-local function doPlayerAddPremiumPoints(cid, ppoints)
-	db.query('UPDATE znote_accounts SET points=points+'.. ppoints ..' WHERE account_id=' .. getAccountNumberByPlayerName(getCreatureName(cid)))
-end 
-
 function onUse(cid, item, fromPosition, itemEx, toPosition) 
-	doPlayerAddPremiumPoints(cid, 30)
-	doPlayerSendTextMessage(cid, MESSAGE_EVENT_ADVANCE, "You have received 30 shop points to your account.")
-	doSendMagicEffect(getCreaturePosition(cid), 28)
-	doRemoveItem(item.uid,1)
+	local player = Player(cid)
+	player:addPremiumDays(30)
+	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have received 30 premium days.")
+	Item(item.uid):remove(1)
 	return true
 end
