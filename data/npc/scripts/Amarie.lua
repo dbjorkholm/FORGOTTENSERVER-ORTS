0ef2375f5386315e1516f90963fde188d409cd29
EPuncker <gm.perflex@gmail.com>
2014-08-04 04:20:43 -0300

    Updated some Ab'Dendriel NPCs, ref #795 Updated misc reptiles and bosses Added missing boss
    Former-commit-id: 5e42fa3e748c0004f732c6b4f922273413e21210 [formerly 0ed9b928bf2e3051f151a274c03e4cd9cc53ea35]
    Former-commit-id: 5e4b29450804f84f125c31d4849e9408375b7e82

---
+++ data/npc/scripts/Amarie.lua
@@ -0,0 +1,20 @@
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+
+local rnd_sounds = 0
+function onThink()
+	if rnd_sounds < os.time() then
+		rnd_sounds = (os.time() + 5)
+		if math.random(100) < 25 then
+			Npc():say("Please leave me alone... I have to study.", TALKTYPE_SAY)
+		end
+	end
+	npcHandler:onThink()
+end
+
+npcHandler:addModule(FocusModule:new())
