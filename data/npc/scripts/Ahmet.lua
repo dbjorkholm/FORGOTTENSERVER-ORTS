9e37fee4ed1794b129191a729f8e81af59b952f8
Skillert <peter.mollberg@gmail.com>
2014-08-05 22:46:11 +0200

    Thieves Guild: - Fix typo

--- data/npc/scripts/Ahmet.lua
+++ data/npc/scripts/Ahmet.lua
@@ -14,7 +14,7 @@ local function creatureSayCallback(cid, type, msg)
 	local player = Player(cid)
 	if msgcontains(msg, 'documents') then
 		if player:getStorageValue(Storage.thievesGuild.Mission04) == 2 then
-			player:setStorageValue(Storage.thievesGuild.Mission04, 2)
+			player:setStorageValue(Storage.thievesGuild.Mission04, 3)
 			npcHandler:say({'You need some forged documents? But I will only forge something for a friend. ...', 'The nomads at the northern oasis killed someone dear to me. Go and kill at least one of them, then we talk about your document.'}, cid)
 		end
 	end
