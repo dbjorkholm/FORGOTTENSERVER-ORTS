a85e080d0b1365c080ad95ff9b3d532fb67ffeda
Sam <sum9900@web.de>
2014-08-05 13:00:27 +0200

    Reviewed some movements

--- data/movements/scripts/hidden city of beregar/elevator.lua
+++ data/movements/scripts/hidden city of beregar/elevator.lua
@@ -8,17 +8,17 @@ function onStepIn(cid, item, position, fromPosition)
 	if not player then
 		return true
 	end
-	
+
 	local teleport = config[item.actionid]
 	if not teleport then
 		return true
 	end
-	
+
 	if player:getStorageValue(Storage.hiddenCityOfBeregar.GoingDown) == 2 then
 		player:teleportTo(teleport)
 		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 	else
-		player:sendTextMessage(MESSAGE_INFO_DESCR, "You don't know how to use this yet.")
+		player:sendTextMessage(MESSAGE_INFO_DESCR, 'You don\'t know how to use this yet.')
 	end
 	return true
 end
