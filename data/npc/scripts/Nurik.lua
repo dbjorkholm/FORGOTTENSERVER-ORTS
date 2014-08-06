025a9bbe30d5cdf297d99499b3ee66f6b3e662e8
Skillert <peter.mollberg@gmail.com>
2014-08-06 14:55:45 +0200

    Thieves guild update: - Forgot to save the changes to Nurik.lua

--- data/npc/scripts/Nurik.lua
+++ data/npc/scripts/Nurik.lua
@@ -7,85 +7,49 @@ function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink()				npcHandler:onThink()					end
 
+local function greetCallback(cid)
+	if Player(cid):getOutfit({lookType = }) then -- dwarf looktype
+		if Player(cid):getStorageValue(Storage.thievesGuild.Mission04) == 6 then
+			npcHandler:say('It\'s .. It\'s YOU! At last!! So what\'s this special proposal you would like to make, my friend?', cid)
+			npcHandler.topic[cid] = 1
+		end
+	else
+		npcHandler:say('Greetings, ' .. Player(cid):getName() .. '', cid)
+	end
+	return true
+end
+
 local function creatureSayCallback(cid, type, msg)
 	if not npcHandler:isFocused(cid) then
 		return false
 	end
 	local player = Player(cid)
-	if msgcontains(msg, 'disguise') then
-		if player:getStorageValue(Storage.thievesGuild.Mission04) == 5 then
-			npcHandler:say({'Hmpf. Why should I waste my time to help some amateur? I\'m afraid I can only offer my assistance to actors that are as great as I am. ...', 'Though, your futile attempt to prove your worthiness could be amusing. Grab a copy of a script from the prop room at the theatre cellar. Then talk to me again about your test!'}, cid)
-		end
-	elseif msgcontains(msg, 'test') then
-		if player:getStorageValue(Storage.thievesGuild.Mission04) == 5 then
-			npcHandler:say('I hope you learnt your role! I\'ll tell you a line from the script and you\'ll have to answer with the corresponding line! Ready?', cid)
-			npcHandler.topic[cid] = 1
+	if msgcontains(msg, 'dwarven bridge') then
+		if npcHandler.topic[cid] == 1 then
+			npcHandler:say('Wait a minute! Do I get that right? You\'re the owner of the dwarven bridge and you are willing to sell it to me??', cid)
+			npcHandler.topic[cid] = 2
 		end
 	elseif msgcontains(msg, 'yes') then
-		if npcHandler.topic[cid] == 1 then
-			npcHandler:say('How dare you? Are you mad? I hold the princess hostage and you drop your weapons. You\'re all lost!', cid)
-			npcHandler.topic[cid] = 2
-		elseif npcHandler.topic[cid] == 3 then
-			npcHandler:say('Too late puny knight. You can\'t stop my master plan anymore!', cid)
-			npcHandler.topic[cid] = 4
-		elseif npcHandler.topic[cid] == 5 then
-			npcHandler:say('What\'s this? Behind the doctor?', cid)
-			npcHandler.topic[cid] = 6
-		elseif npcHandler.topic[cid] == 7 then
-			npcHandler:say('Grrr!', cid)
-			npcHandler.topic[cid] = 8
-		elseif npcHandler.topic[cid] == 9 then
-			npcHandler:say('You\'re such a monster!', cid)
-			npcHandler.topic[cid] = 10
-		elseif npcHandler.topic[cid] == 11 then
-			npcHandler:say('Ah well, I think you passed the test! Here is your disguise kit! Now get lost, fate awaits me!', cid)
-			player:setStorageValue(Storage.thievesGuild.Mission04, 6)
-			player:addItem(8693, 1)
-			npcHandler.topic[cid] = 0
-		end
-	elseif npcHandler.topic[cid] == 2 then
-		if msg:lower() == 'I don\'t think so, dear doctor!' then
-			npcHandler:say('Ok, ok. You\'ve got this one right! Ready for the next one?', cid)
+		if npcHandler.topic[cid] == 2 then
+			npcHandler:say({'That\'s just incredible! I\'ve dreamed about acquiring the dwarven bridge since I was a child! Now my dream will finally become true. ...', 'And you are sure you want to sell it? I mean really, really sure?'}, cid)
 			npcHandler.topic[cid] = 3
-		else
-			npcHandler:say('No no no! That is not correct!', cid)
-			npcHandler.topic[cid] = 0
-		end
-	elseif npcHandler.topic[cid] == 4 then
-		if msg:lower() == 'Watch out! It\'s a trap!' then
-			npcHandler:say('Ok, ok. You\'ve got this one right! Ready for the next one?', cid)
+		elseif npcHandler.topic[cid] == 3 then
+			npcHandler:say('How splendid! Do you have the necessary documents with you?', cid)
+			npcHandler.topic[cid] = 4
+		elseif npcHandler.topic[cid] == 4 then
+			npcHandler:say('Oh my, oh my. I\'m so excited! So let\'s seal this deal as fast as possible so I can visit my very own dwarven bridge. Are you ready for the transaction?', cid)
 			npcHandler.topic[cid] = 5
-		else
-			npcHandler:say('No no no! That is not correct!', cid)
-			npcHandler.topic[cid] = 0
-		end
-	elseif npcHandler.topic[cid] == 6 then
-		if msg:lower() == 'Look! It\'s Lucky, the wonder dog!' then
-			npcHandler:say('Ok, ok. You\'ve got this one right! Ready for the next one?', cid)
-			npcHandler.topic[cid] = 7
-		else
-			npcHandler:say('No no no! That is not correct!', cid)
-			npcHandler.topic[cid] = 0
-		end
-	elseif npcHandler.topic[cid] == 8 then
-		if msg:lower() == 'Ahhhhhh!' then
-			npcHandler:say('Ok, ok. You\'ve got this one right! Ready for the next one?', cid)
-			npcHandler.topic[cid] = 9
-		else
-			npcHandler:say('No no no! That is not correct!', cid)
-			npcHandler.topic[cid] = 0
-		end
-	elseif npcHandler.topic[cid] == 10 then
-		if msg:lower() == 'Hahaha! Now drop your weapons or else...' then
-			npcHandler:say('Ok, ok. You\'ve got this one right! Ready for the next one?', cid)
-			npcHandler.topic[cid] = 11
-		else
-			npcHandler:say('No no no! That is not correct!', cid)
-			npcHandler.topic[cid] = 0
+		elseif npcHandler.topic[cid] == 5 then
+			if player:removeItem(8694, 1) then
+				player:addItem(8699, 1)
+				player:setStorageValue(Storage.thievesGuild.Mission04, 7)
+				npcHandler:say({'Excellent! Here is the painting you requested. It\'s quite precious to my father, but imagine his joy when I tell him about my clever deal! ...', 'Now leave me alone please. I have to prepare for my departure. Now my family will not call me a squandering fool anymore!'}, cid)
+				npcHandler.topic[cid] = 0
+			end
 		end
 	end
 	return true
 end
 
-npcHandler:setMessage(MESSAGE_GREET, "Be greeted |PLAYERNAME|!")
+npcHandler:setCallback(CALLBACK_GREET, greetCallback)
 npcHandler:addModule(FocusModule:new())
