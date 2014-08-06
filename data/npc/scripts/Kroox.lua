75da05b07e52498a2b69a4eb0b5d1f9c0382f69a
EPuncker <gm.perflex@gmail.com>
2014-08-06 03:10:09 -0300

    Fixed #795
    Former-commit-id: 607713fb9d55d5574d68a13b9d0255d3b6509a1b

--- data/npc/scripts/Kroox.lua
+++ data/npc/scripts/Kroox.lua
@@ -12,10 +12,12 @@ local function creatureSayCallback(cid, type, msg)
 		return false
 	end
 	local player = Player(cid)
-	if msgcontains(msg, "sam send me") then
-		if player:getStorageValue( 330) == 1 then
-			npcHandler:say({"Oh, so its you, he wrote me about? Sadly I have no dwarven armor in stock. But I give you the permission to retrive one from the mines. ...","The problem is, some giant spiders made the tunnels where the storage is their new home. Good luck. "}, cid, 0, 1, 3000)
+	if msgcontains(msg, "sam sent me") then
+		if player:getStorageValue(330) == 1 then
+			npcHandler:say({"Oh, so its you, he wrote me about? Sadly I have no dwarven armor in stock. But I give you the permission to retrive one from the mines. ...",
+							"The problem is, some giant spiders made the tunnels where the storage is their new home. Good luck."}, cid)
 			player:setStorageValue(330, 2)
+			npcHandler.topic[cid] = 0
 		end
 	elseif msgcontains(msg, "measurements") then
 		if player:getStorageValue(Storage.postman.Mission07) >= 1 then
--- data/npc/scripts/Sam.lua
+++ data/npc/scripts/Sam.lua
@@ -2,17 +2,17 @@ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
-function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
-function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
-function onThink() npcHandler:onThink() end
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+function onThink()				npcHandler:onThink()					end
 
 local function creatureSayCallback(cid, type, msg)
-	local player = Player(cid)
 	if not npcHandler:isFocused(cid) then
 		return false
 	end
-	if msgcontains(msg, "adorn") or msgcontains(msg, "helmet") or msgcontains(msg, "outfit") or msgcontains(msg, "addon") then
+	local player = Player(cid)
+	if isInArray({"adorn", "helmet", "outfit", "addon"}, msg) then
 		if player:getStorageValue( Storage.OutfitQuest.KnightHatAddon) == 5 then
 			npcHandler:say("Oh, Gregor sent you? I see. It will be my pleasure to adorn your helmet. Your helmet is finished, I hope you like it.", cid)
 			player:setStorageValue(Storage.OutfitQuest.KnightHatAddon, 6)
@@ -21,19 +21,19 @@ local function creatureSayCallback(cid, type, msg)
 			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
 		end
 	end
- 
+
 	if msgcontains(msg, "old backpack") then
-		if player:getStorageValue( 330) < 1 then
+		if player:getStorageValue(330) < 1 then
 			npcHandler:say("What? Are you telling me you found my old adventurer's backpack that I lost years ago??", cid)
 			npcHandler.topic[cid] = 1
 		end
 	elseif msgcontains(msg, "yes") then
 		if npcHandler.topic[cid] == 1 then
-			if getPlayerItemCount(cid, 3960) >= 1 then
+			if player:getItemCount(3960) > 0 then
 				npcHandler:say({"Thank you very much! This brings back good old memories! Please, as a reward, travel to Kazordoon and ask my old friend Kroox to provide you a special dwarven armor. ...",
-								"I will mail him about you immediately. Just tell him, his old buddy Sam is sending you."}, cid, 0, 1, 3500)
-				player:setStorageValue( 330, 1)
-				player:removeItem( 3960, 1)
+								"I will mail him about you immediately. Just tell him, his old buddy Sam is sending you."}, cid)
+				player:setStorageValue(330, 1)
+				player:removeItem(3960, 1)
 			else
 				npcHandler:say("You don't have it...", cid)
 			end
@@ -47,6 +47,6 @@ local function creatureSayCallback(cid, type, msg)
 	end
 	return true
 end
- 
+
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
