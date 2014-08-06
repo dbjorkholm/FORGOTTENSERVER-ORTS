a6c07f1347b0fa300332dbbfba133401d2b316c9
EPuncker <gm.perflex@gmail.com>
2014-08-06 03:10:09 -0300

    Fixed #795
    Former-commit-id: f7d4b4ac7c76be23a35a42aeb65621027e286239 [formerly 607713fb9d55d5574d68a13b9d0255d3b6509a1b]
    Former-commit-id: 75da05b07e52498a2b69a4eb0b5d1f9c0382f69a

--- data/npc/scripts/Olrik.lua
+++ data/npc/scripts/Olrik.lua
@@ -34,4 +34,7 @@ local function creatureSayCallback(cid, type, msg)
 end
 
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:setMessage(MESSAGE_GREET, "Hello. How may I help you |PLAYERNAME|? Ask me for a {trade} if you want to buy something. I can also explain the {mail} system.")
+npcHandler:setMessage(MESSAGE_FAREWELL, "It was a pleasure to help you, |PLAYERNAME|.")
+npcHandler:setMessage(MESSAGE_SENDTRADE, "Here. Don't forget that you need to buy a label too if you want to send a parcel. Always write the name of the {receiver} in the first line.")
 npcHandler:addModule(FocusModule:new())
