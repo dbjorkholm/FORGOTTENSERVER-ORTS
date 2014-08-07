dd9d008935807f3857b63118bfa3f015b0c42c59
Skillert <peter.mollberg@gmail.com>
2014-08-07 15:34:21 +0200

    minor

--- data/actions/scripts/the thieves guild quest/dwarfDisguiseKit.lua
+++ data/actions/scripts/the thieves guild quest/dwarfDisguiseKit.lua
@@ -1,10 +1,9 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local player = Player(cid)
 	local condition = Condition(CONDITION_OUTFIT)
 	condition:setTicks(5 * 60 * 2000) -- should be 5 minutes
 	condition:addOutfit({lookType = 69}) -- dwarf looktype
 
-	player:addCondition(condition)
+	Player(cid):addCondition(condition)
 	
 	return true
 end
