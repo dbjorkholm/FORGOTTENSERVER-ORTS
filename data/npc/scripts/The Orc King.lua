7ebcf6bf77635aa5d94501499d98c6136e6af182
Daniel Björkholm <danielbjorkholm@live.se>
2014-08-07 21:17:10 +0200

    Fix #1112 & #1139
    Former-commit-id: 2d99db7c4b706a35118b0a5b9059bf6d701d5570

--- data/npc/scripts/The Orc King.lua
+++ data/npc/scripts/The Orc King.lua
@@ -7,59 +7,49 @@ function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink()				npcHandler:onThink()					end
 
-local function creatureSayCallback(cid, type, msg)
+local function greetCallback(cid)
 	local player = Player(cid)
-	-- GREET
-	if (msg == "hi" or msg == "hello") and (not npcHandler:isFocused(cid)) then
-		if player:getStorageValue(83) < 1 then
-			npcHandler:say("Arrrrgh! A dirty paleskin! To me my children! Kill them my guards!", cid)
-			player:setStorageValue(83, 1)
-			doSummonCreature("Slime", getThingPos(getNpcCid()))
-			doSummonCreature("Slime", getThingPos(getNpcCid()))
-			doSummonCreature("Slime", getThingPos(getNpcCid()))
-			doSummonCreature("Orc Warlord", getThingPos(getNpcCid()))
-			doSummonCreature("Orc Warlord", getThingPos(getNpcCid()))
-			doSummonCreature("Orc Leader", getThingPos(getNpcCid()))
-			doSummonCreature("Orc Leader", getThingPos(getNpcCid()))
-			doSummonCreature("Orc Leader", getThingPos(getNpcCid()))
-		else
-			npcHandler:say("Harrrrk! You think you are strong now? You shall never escape my wrath! I am immortal!", cid)
-			npcHandler:addFocus(cid)
+	if player:getStorageValue(83) ~= 1 then
+		player:setStorageValue(83, 1)
+		local creatures = { 'Slime', 'Slime', 'Slime', 'Orc Warlord', 'Orc Warlord', 'Orc Leader', 'Orc Leader', 'Orc Leader' }
+		for i = 1, #creatures do
+			Game.createMonster(creatures[i], Npc():getPosition())
 		end
-	end
-	-- GREET
-
-	if(not npcHandler:isFocused(cid)) then
+		selfSay('Arrrrgh! A dirty paleskin! To me my children! Kill them my guards!', TALKTYPE_SAY)
 		return false
-	end
-
-	-- Mission 3 - Orc Fortress
-	if(msgcontains(msg, "lamp")) then
-		if player:getStorageValue(GreenDjinn.MissionStart+3) == 1 or player:getStorageValue(BlueDjinn.MissionStart+3) == 1 then
-			npcHandler:say({"King: I can sense your evil intentions to imprison a djinn! You are longing for the lamp, which I still possess. ...","Who do you want to trap in this cursed lamp?"}, cid, 0, 1, 2200)
-			npcHandler.topic[cid] = 1
-		end
-	-- Mission 3 - Orc Fortress
-	elseif(msgcontains(msg, "malor")) then
-		if(npcHandler.topic[cid] == 1) then
-			if player:getStorageValue(GreenDjinn.MissionStart+3) == 1 then
-				player:setStorageValue(GreenDjinn.MissionStart+3, 2)
-			elseif player:getStorageValue(BlueDjinn.MissionStart+3) == 1 then
-				player:setStorageValue(BlueDjinn.MissionStart+3, 2)
-			end
-			npcHandler:say("I was waiting for this day! Take the lamp and let Malor feel my wrath!", cid)
-			npcHandler.topic[cid] = 0
-			doPlayerAddItem(cid, 2344, 1)
-		end
-	end
-	if (msgcontains(msg, "bye") or msgcontains(msg, "farewell")) then
-		npcHandler:say("We will meet again.", cid)
-		npcHandler.topic[cid] = 0
-		npcHandler:releaseFocus(cid)
-		npcHandler:resetNpc(cid)
+	else
+		npcHandler:setMessage(MESSAGE_GREET, 'Harrrrk! You think you are strong now? You shall never escape my wrath! I am immortal!')
 	end
 	return true
 end
 
+local function creatureSayCallback(cid, type, msg)
+	if not npcHandler:isFocused(cid) then
+		return false
+	end
+	
+	local player = Player(cid)
+
+	-- Mission 3 - Orc Fortress
+	if msgcontains(msg, 'lamp') then
+		if player:getStorageValue(GreenDjinn.MissionStart + 3) == 1 or player:getStorageValue(BlueDjinn.MissionStart + 3) == 1 then
+			npcHandler:say({'King: I can sense your evil intentions to imprison a djinn! You are longing for the lamp, which I still possess. ...', 'Who do you want to trap in this cursed lamp?'}, cid)
+			npcHandler.topic[cid] = 1
+		end
+	-- Mission 3 - Orc Fortress
+	elseif msgcontains(msg, 'malor') and npcHandler.topic[cid] == 1 then
+		if player:getStorageValue(GreenDjinn.MissionStart + 3) == 1 then
+			player:setStorageValue(GreenDjinn.MissionStart + 3, 2)
+		elseif player:getStorageValue(BlueDjinn.MissionStart + 3) == 1 then
+			player:setStorageValue(BlueDjinn.MissionStart + 3, 2)
+		end
+		player:addItem(2344, 1)
+		npcHandler:say('I was waiting for this day! Take the lamp and let Malor feel my wrath!', cid)
+		npcHandler.topic[cid] = 0
+	end
+	return true
+end
+
+npcHandler:setCallback(CALLBACK_GREET, greetCallback)
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
