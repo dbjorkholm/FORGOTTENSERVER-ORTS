b323ae779496bc618b5ae1a3481b1143bc628a9f
Sam <sum9900@web.de>
2014-08-08 14:41:47 +0200

    Fixed 'A Ghostly Woman' going crazy
    Former-commit-id: 9c145777bb08ad8204dc132390a0d9a8d537191e

--- data/npc/scripts/A Ghostly Woman.lua
+++ data/npc/scripts/A Ghostly Woman.lua
@@ -5,19 +5,23 @@ NpcSystem.parseParameters(npcHandler)
 function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
 function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
 function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
+
+local config = {delay = 20, frequency = 25, message = "Alone ... so alone. So cold."}
 function onThink()
-	local config = {delay = 20, frequency = 25, message = "Alone ... so alone. So cold."} 
-    if (os.time() - config.delay) >= config.frequency then
-        config.delay = os.time()
-        Npc():say(config.message, TALKTYPE_SAY)
-    end
-    npcHandler:onThink()    
+	if (os.time() - config.delay) >= config.frequency then
+			config.delay = os.time()
+			Npc():say(config.message, TALKTYPE_SAY)
+	end
+
+	npcHandler:onThink()
 end
 
 local function creatureSayCallback(cid, type, msg)
 	if not npcHandler:isFocused(cid) then
 		return false
-	elseif msgcontains(msg, "job") and npcHandler.topic[cid] == 0 then
+	end
+
+	if msgcontains(msg, "job") then
 		npcHandler:say("Once I was a member of the order of the nightmare knights. Now I am but a shadow who walks these cold halls.", cid)
 	end
 	return true
