2e8106615802fcb81656731bcd8f7c6f7d768abf
Daniel Björkholm <danielbjorkholm@live.se>
2014-08-09 23:02:28 +0200

    Fix #1166
    Former-commit-id: 5e90820b7e925e696fea5f11f36446da8772c1b4

--- data/npc/scripts/Morgan.lua
+++ data/npc/scripts/Morgan.lua
@@ -2,83 +2,90 @@ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
-function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
-function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
-function onThink()				npcHandler:onThink()					end
+function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
+function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
+function onThink() npcHandler:onThink() end
 
 local function creatureSayCallback(cid, type, msg)
 	if not npcHandler:isFocused(cid) then
 		return false
 	end
+
 	local player = Player(cid)
-	elseif(msgcontains(msg, 'addon')) then
-		npcHandler:say('I can forge the finest {weapons} for knights and warriors. They may wear them proudly and visible to everyone.', cid)
-		npcHandler.topic[cid] = 1
-	elseif msgcontains(msg, "firebird") then
+
+	if msgcontains(msg, 'firebird') then
 		if player:getStorageValue(Storage.OutfitQuest.PirateSabreAddon) == 4 then
-			npcHandler:say("Ahh. So Duncan sent you, eh? You must have done something really impressive. Okay, take this fine sabre from me, mate.", cid)
 			player:setStorageValue(Storage.OutfitQuest.PirateSabreAddon, 5)
 			player:addOutfitAddon(151, 1)
 			player:addOutfitAddon(155, 1)
+			player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
+			npcHandler:say('Ahh. So Duncan sent you, eh? You must have done something really impressive. Okay, take this fine sabre from me, mate.', cid)
 		end
-	elseif(msgcontains(msg, 'weapons') and npcHandler.topic[cid] == 1) then
-		npcHandler:say('Would you rather be interested in a {knight\'s sword} or in a {warrior\'s sword}?', cid)
-		npcHandler.topic[cid] = 2
-	elseif(msgcontains(msg, 'warrior\'s sword') or msgcontains(msg, 'warriors sword')) then
-		if npcHandler.topic[cid] == 2 and player:getStorageValue(Storage.OutfitQuest.WarriorSwordAddon) < 1 then 
+	elseif msgcontains(msg, 'warrior\'s sword') then
+		if player:hasOutfit(player:getSex() == 0 and 142 or 134, 2) then
+			npcHandler:say('You already have this outfit!', cid)
+			return true
+		end
+			
+		if player:getStorageValue(Storage.OutfitQuest.WarriorSwordAddon) < 1 then
+			if player:getStorageValue(Storage.OutfitQuest.DefaultStart) ~= 1 then
+				player:setStorageValue(Storage.OutfitQuest.DefaultStart, 1)
+			end
+			player:setStorageValue(Storage.OutfitQuest.WarriorSwordAddon, 1)
 			npcHandler:say('Great! Simply bring me 100 iron ore and one royal steel and I will happily {forge} it for you.', cid)
-			npcHandler.topic[cid] = 3
-		elseif npcHandler.topic[cid] == 4  and player:getStorageValue(Storage.OutfitQuest.WarriorSwordAddon) < 1 then
-			if player:removeItem(5887,1) and player:removeItem(5880,100) then
-				npcHandler:say('Alright! As a matter of fact, I have one in store. Here you go!', cid)             
-				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
-				player:setStorageValue(Storage.OutfitQuest.WarriorSwordAddon,1) 
+		elseif player:getStorageValue(Storage.OutfitQuest.WarriorSwordAddon) == 1 and npcHandler.topic[cid] == 1 then
+			if player:getItemCount(5887) > 0 and player:getItemCount(5880) > 99 then
+				player:removeItem(5887, 1)
+				player:removeItem(5880, 100)
 				player:addOutfitAddon(134, 2)
-				player:addOutfitAddon(142, 2)   
-				npcHandler.topic[cid] = 0
+				player:addOutfitAddon(142, 2)
+				player:setStorageValue(Storage.OutfitQuest.WarriorSwordAddon, 2)
+				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
+				npcHandler:say('Alright! As a matter of fact, I have one in store. Here you go!', cid)
 			else
 				npcHandler:say('You do not have all the required items.', cid)
-				npcHandler.topic[cid] = 0
 			end
-		elseif npcHandler.topic[cid] > 1 then 
-			npcHandler:say('You already have this outfit!', cid)
 			npcHandler.topic[cid] = 0
 		end
-	elseif (msgcontains(msg, 'knights sword') or msgcontains(msg, 'knight\'s sword')) then
-		if npcHandler.topic[cid] == 2 and player:getStorageValue(Storage.OutfitQuest.KnightSwordAddon) < 1 then
+	elseif msgcontains(msg, 'knight\'s sword') then
+		if player:hasOutfit(player:getSex() == 0 and 139 or 131, 1) then
+			npcHandler:say('You already have this outfit!', cid)
+			return true
+		end
+			
+		if player:getStorageValue(Storage.OutfitQuest.KnightSwordAddon) < 1 then
+			if player:getStorageValue(Storage.OutfitQuest.DefaultStart) ~= 1 then
+				player:setStorageValue(Storage.OutfitQuest.DefaultStart, 1)
+			end
+			player:setStorageValue(Storage.OutfitQuest.KnightSwordAddon, 1)
 			npcHandler:say('Great! Simply bring me 100 Iron Ore and one Crude Iron and I will happily {forge} it for you.', cid)
-			npcHandler.topic[cid] = 4
-		elseif npcHandler.topic[cid] == 4 and player:getStorageValue(Storage.OutfitQuest.KnightSwordAddon) < 1 then
-			if player:removeItem(5892,1) and player:removeItem(5880,100) then
-				npcHandler:say('Alright! As a matter of fact, I have one in store. Here you go!', cid)             
-				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
-				player:setStorageValue(Storage.OutfitQuest.KnightSwordAddon,1)
+		elseif player:getStorageValue(Storage.OutfitQuest.KnightSwordAddon) == 1 and npcHandler.topic[cid] == 1 then
+			if player:getItemCount(5892) > 0 and player:getItemCount(5880) > 99 then
+				player:removeItem(5892, 1)
+				player:removeItem(5880, 100)
 				player:addOutfitAddon(131, 1)
 				player:addOutfitAddon(139, 1)
-				npcHandler.topic[cid] = 0
+				player:setStorageValue(Storage.OutfitQuest.KnightSwordAddon, 2)
+				player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
+				npcHandler:say('Alright! As a matter of fact, I have one in store. Here you go!', cid)
 			else
 				npcHandler:say('You do not have all the required items.', cid)
-				npcHandler.topic[cid] = 0
 			end
-		elseif npcHandler.topic[cid] > 1 then 
-			npcHandler:say('You already have this outfit!', cid)
 			npcHandler.topic[cid] = 0
 		end
-	elseif(msgcontains(msg, 'forge') or msgcontains(msg, 'forge weapon')) then
+	elseif msgcontains(msg, 'forge') then
 		npcHandler:say('What would you like me to forge for you? A {knight\'s sword} or a {warrior\'s sword}?', cid)
-		npcHandler.topic[cid] = 4
+		npcHandler.topic[cid] = 1
 	end
-	
-	if (msgcontains(msg, "bye") or msgcontains(msg, "farewell")) then
-		npcHandler:say("Finally.", cid)
-		npcHandler.topic[cid] = 0
-		npcHandler:releaseFocus(cid)
-		npcHandler:resetNpc(cid)
-	end
-	
 	return true
 end
+
+keywordHandler:addKeyword({'addon'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I can forge the finest {weapons} for knights and warriors. They may wear them proudly and visible to everyone.'})
+keywordHandler:addKeyword({'weapons'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Would you rather be interested in a {knight\'s sword} or in a {warrior\'s sword}?'})
+
+npcHandler:setMessage(MESSAGE_GREET, 'Hello there.')
+npcHandler:setMessage(MESSAGE_FAREWELL, 'Good bye.')
 
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
