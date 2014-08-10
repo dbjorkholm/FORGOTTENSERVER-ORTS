2fee930ebe07e4bac05ecacbb31cecda731acee9
EPuncker <gm.perflex@gmail.com>
2014-08-10 14:42:59 -0300

    minor

--- data/npc/scripts/Yaman.lua
+++ data/npc/scripts/Yaman.lua
@@ -20,18 +20,18 @@ local list =
 		{id = 2170,		buy = 100,		sell = 50,		name = 'Silver Amulet'},
 		{id = 2161,		buy = 100,		sell = 30,		name = 'Strange Talisman'},
 		{id = 2169,		buy = 2000,		sell = 100,		name = 'Time Ring'},
-		{id = 2327,		buy = 0,		sell =  100,	name = 'Ankh'},
-		{id = 18412,	buy = 0,		sell =  6500,	name = 'Glacial Rod'},
-		{id = 2183,		buy = 0,		sell =  3000,	name = 'Hailstorm Rod'},
-		{id = 2186,		buy = 0,		sell =  200,	name = 'Moonlight Rod'},
-		{id = 18411,	buy = 0,		sell =  6000,	name = 'Muck Rod'},
-		{id = 2194,		buy = 0,		sell =  50,		name = 'Mysterious Fetish'},
-		{id = 2185,		buy = 0,		sell =  1000,	name = 'Necrotic Rod'},
-		{id = 8911,		buy = 0,		sell =  1500,	name = 'Northwind Rod'},
-		{id = 2182,		buy = 0,		sell =  100,	name = 'Snakebite Rod'},
-		{id = 8912,		buy = 0,		sell =  3600,	name = 'Springsprout Rod'},
-		{id = 2181,		buy = 0,		sell =  2000,	name = 'Terra Rod'},
-		{id = 8910,		buy = 0,		sell =  4400,	name = 'Underworld Rod'}
+		{id = 2327,		buy = 0,		sell = 100,		name = 'Ankh'},
+		{id = 18412,	buy = 0,		sell = 6500,	name = 'Glacial Rod'},
+		{id = 2183,		buy = 0,		sell = 3000,	name = 'Hailstorm Rod'},
+		{id = 2186,		buy = 0,		sell = 200,		name = 'Moonlight Rod'},
+		{id = 18411,	buy = 0,		sell = 6000,	name = 'Muck Rod'},
+		{id = 2194,		buy = 0,		sell = 50,		name = 'Mysterious Fetish'},
+		{id = 2185,		buy = 0,		sell = 1000,	name = 'Necrotic Rod'},
+		{id = 8911,		buy = 0,		sell = 1500,	name = 'Northwind Rod'},
+		{id = 2182,		buy = 0,		sell = 100,		name = 'Snakebite Rod'},
+		{id = 8912,		buy = 0,		sell = 3600,	name = 'Springsprout Rod'},
+		{id = 2181,		buy = 0,		sell = 2000,	name = 'Terra Rod'},
+		{id = 8910,		buy = 0,		sell = 4400,	name = 'Underworld Rod'}
 	}
 return list
 end
@@ -66,16 +66,16 @@ local function creatureSayCallback(cid, type, msg)
 				{ NeedItem = 2195, Ncount = 1, GiveItem = 5891, Gcount = 1}, -- Enchanted Chicken Wing
 				{ NeedItem = 2475, Ncount = 4, GiveItem = 5885, Gcount = 1}, -- Flask of Warrior's Sweat
 				{ NeedItem = 2498, Ncount = 2, GiveItem = 5884, Gcount = 1}, -- Spirit Container
-				{ NeedItem = 2392, Ncount = 3, GiveItem = 5904, Gcount = 1}, -- Magic Sulphur
-				}
+				{ NeedItem = 2392, Ncount = 3, GiveItem = 5904, Gcount = 1}  -- Magic Sulphur
+		}
 
-        if player:getItemCount(trade[npcHandler.topic[cid]].NeedItem) >= trade[npcHandler.topic[cid]].Ncount then
+		if player:getItemCount(trade[npcHandler.topic[cid]].NeedItem) >= trade[npcHandler.topic[cid]].Ncount then
 			player:removeItem(trade[npcHandler.topic[cid]].NeedItem, trade[npcHandler.topic[cid]].Ncount)
 			player:addItem(trade[npcHandler.topic[cid]].GiveItem, trade[npcHandler.topic[cid]].Gcount)
-			return npcHandler:say('Here you are.', cid)						
-        else
-            npcHandler:say('Sorry but you don\'t have the item.', cid)
-        end
+			return npcHandler:say('Here you are.', cid)
+		else
+			npcHandler:say('Sorry but you don\'t have the item.', cid)
+		end
 	elseif msgcontains(msg,'no') and (npcHandler.topic[cid] >= 1 and npcHandler.topic[cid] <= 5) then
 		 npcHandler:say('Ok then.', cid)
 		 npcHandler.topic[cid] = 0
