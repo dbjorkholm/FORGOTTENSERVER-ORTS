ab5e95fb1b8b84149c77c19b93b4ff745320a2b1
Skillert <peter.mollberg@gmail.com>
2014-08-10 23:37:52 +0200

    Thieves Guild Quest - Added actions needed for the quest

---
+++ data/actions/scripts/the thieves guild quest/note.lua
@@ -0,0 +1,23 @@
+local notePos = Position(32598, 32381, 10)
+
+local function removeNote(position)
+	local noteItem = Tile(position):getItemById(8700)
+	if noteItem then
+		noteItem:remove()
+	end
+end
+
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	local player = Player(cid)
+	if itemEx.actionid == 12509 then
+		if player:getStorageValue(Storage.thievesGuild.Mission08) == 1 then
+			player:removeItem(8701, 1)
+			Game.createItem(8700, 1, notePos)
+			player:setStorageValue(Storage.thievesGuild.Mission08, 2)
+			addEvent(removeNote, 5 * 60 * 1000, notePos)
+		end
+	else
+		return false
+	end
+	return true
+end
---
+++ data/actions/scripts/tools/lockPick.lua
@@ -0,0 +1,18 @@
+function onUse(cid, item, fromPosition, itemEx, toPosition)
+	local player = Player(cid)
+	local fail = math.random(100)
+	if itemEx.uid == 12503 then
+		if fail <= 15 then
+			if player:getStorageValue(Storage.thievesGuild.Mission02) == 1 then
+				player:addItem(8760, 1)
+				player:setStorageValue(Storage.thievesGuild.Mission02, 2)
+			end
+		elseif fail > 15 then
+			player:removeItem(8717, 1)
+			player:sendTextMessage(MESSAGE_INFO_DESCR, "Your lock pick broke.")
+		else
+			return false
+		end
+	end
+	return true
+end
