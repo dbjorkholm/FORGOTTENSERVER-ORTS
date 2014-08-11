fdad4f6ddf07abb14b35908f49368e386396ff96
Skillert <peter.mollberg@gmail.com>
2014-08-11 16:31:02 +0200

    Fixes, cheers @TheSumm

--- data/actions/scripts/the thieves guild quest/note.lua
+++ data/actions/scripts/the thieves guild quest/note.lua
@@ -9,15 +9,15 @@ end
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
-	if itemEx.actionid == 12509 then
-		if player:getStorageValue(Storage.thievesGuild.Mission08) == 1 then
-			player:removeItem(8701, 1)
-			Game.createItem(8700, 1, notePos)
-			player:setStorageValue(Storage.thievesGuild.Mission08, 2)
-			addEvent(removeNote, 5 * 60 * 1000, notePos)
-		end
-	else
+	if itemEx.actionid ~= 12509 then
 		return false
+	end
+
+	if player:getStorageValue(Storage.thievesGuild.Mission08) == 1 then
+		player:removeItem(8701, 1)
+		Game.createItem(8700, 1, notePos)
+		player:setStorageValue(Storage.thievesGuild.Mission08, 2)
+		addEvent(removeNote, 5 * 60 * 1000, notePos)
 	end
 	return true
 end
