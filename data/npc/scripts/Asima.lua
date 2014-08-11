29ef72810d736a03993d58f4ff6b17e53cfdf0d2
EPuncker <gm.perflex@gmail.com>
2014-08-11 11:27:03 -0300

    Fixed #1177 Removed some talkactions

---
+++ data/npc/scripts/Asima.lua
@@ -0,0 +1,42 @@
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+function onThink()				npcHandler:onThink()					end
+
+local function creatureSayCallback(cid, type, msg)
+	if not npcHandler:isFocused(cid) then
+		return false
+	end
+	local player = Player(cid)
+	local items = {[1] = 2190, [2] = 2182, [5] = 2190, [6] = 2182}
+	if msgcontains(msg, 'first rod') or msgcontains(msg, 'first wand') then
+		if isSorcerer(cid) or isDruid(cid) then
+			if player:getStorageValue(50079) == -1 then
+				npcHandler:say('So you ask me for a {' .. ItemType(items[player:getVocation():getId()]):getName() .. '} to begin your adventure?', cid)
+				npcHandler.topic[cid] = 1
+			else
+				npcHandler:say('What? I have already gave you one {' .. ItemType(items[player:getVocation():getId()]):getName() .. '}!', cid)
+			end
+		else
+			npcHandler:say('Sorry, you aren\'t a druid either a sorcerer.', cid)
+		end
+	elseif msgcontains(msg, 'yes') then
+		if npcHandler.topic[cid] == 1 then
+			player:addItem(items[player:getVocation():getId()], 1)
+			npcHandler:say('Here you are young adept, take care yourself.', cid)
+			player:setStorageValue(50079, 1)
+		end
+		npcHandler.topic[cid] = 0
+	elseif msgcontains(msg, 'no') and isInArray({1}, npcHandler.topic[cid]) == true then
+		npcHandler:say('Ok then.', cid)
+		npcHandler.topic[cid] = 0
+	end
+	return true
+end
+
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:addModule(FocusModule:new())
