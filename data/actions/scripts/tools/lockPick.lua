aebf6cdffee7aba1204f49a55df15f6d199fdb33
Skillert <peter.mollberg@gmail.com>
2014-08-11 16:31:02 +0200

    Fixes, cheers @TheSumm
    Former-commit-id: fdad4f6ddf07abb14b35908f49368e386396ff96

--- data/actions/scripts/tools/lockPick.lua
+++ data/actions/scripts/tools/lockPick.lua
@@ -1,18 +1,17 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
-	local fail = math.random(100)
-	if itemEx.uid == 12503 then
-		if fail <= 15 then
-			if player:getStorageValue(Storage.thievesGuild.Mission02) == 1 then
-				player:addItem(8760, 1)
-				player:setStorageValue(Storage.thievesGuild.Mission02, 2)
-			end
-		elseif fail > 15 then
-			player:removeItem(8717, 1)
-			player:sendTextMessage(MESSAGE_INFO_DESCR, "Your lock pick broke.")
-		else
-			return false
+	if itemEx.uid ~= 12503 then
+		return false
+	end
+
+	if math.random(100) <= 15 then
+		if player:getStorageValue(Storage.thievesGuild.Mission02) == 1 then
+			player:addItem(8760, 1)
+			player:setStorageValue(Storage.thievesGuild.Mission02, 2)
 		end
+	else
+		player:removeItem(8717, 1)
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "Your lock pick broke.")
 	end
 	return true
 end
