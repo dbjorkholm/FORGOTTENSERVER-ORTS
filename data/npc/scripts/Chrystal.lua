4b05cf0dbaa681d7cc4a392e86b9467fef417a19
EPuncker <gm.perflex@gmail.com>
2014-08-11 23:18:06 -0300

    more updates
    Former-commit-id: cc46ead1a610af942dd6ca70c8a8d3f4084045bd

--- data/npc/scripts/Chrystal.lua
+++ data/npc/scripts/Chrystal.lua
@@ -23,4 +23,8 @@ local function creatureSayCallback(cid, type, msg)
 end
 
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:setMessage(MESSAGE_GREET, "At your service |PLAYERNAME| and welcome to the post office.")
+npcHandler:setMessage(MESSAGE_FAREWELL, "Who is next?")
+npcHandler:setMessage(MESSAGE_WALKAWAY, "Who is next?")
+npcHandler:setMessage(MESSAGE_SENDTRADE, "Here. Don't forget that you need to buy a label too if you want to send a parcel. Always write the name of the {receiver} in the first line.")
 npcHandler:addModule(FocusModule:new())
