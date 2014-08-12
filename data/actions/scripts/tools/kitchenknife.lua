275b160ce056595e07cf75f1d76b3312ee7dcd10
Royalot <zapf-benny@web.de>
2014-08-12 21:52:07 +0200

    minor

--- data/actions/scripts/tools/kitchenknife.lua
+++ data/actions/scripts/tools/kitchenknife.lua
@@ -9,6 +9,7 @@ end
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local targetItem = Item(itemEx.uid)
+	local player = Player(cid)
 	--The Ice Islands Quest
 	if player:getStorageValue(Storage.TheIceIslands.Questline) >= 21 then
 		if itemEx.itemid == 7261 then
