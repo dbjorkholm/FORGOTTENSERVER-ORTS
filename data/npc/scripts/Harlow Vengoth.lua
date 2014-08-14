41c7466541cc802de2196b8bd1e19f61b533e5cf
Matthews <matthews.wtiger@gmail.com>
2014-08-14 14:12:29 -0300

    More updates on Boat NPCs, ref #1195
    Former-commit-id: 7f2629503991d3babd0ce35b1ef49fee7f5f5765

--- data/npc/scripts/Harlow Vengoth.lua
+++ data/npc/scripts/Harlow Vengoth.lua
@@ -8,19 +8,19 @@ function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)
 function onThink()				npcHandler:onThink()					end
 
 local function creatureSayCallback(cid, type, msg)
-	if(not npcHandler:isFocused(cid)) then
+	if not npcHandler:isFocused(cid) then
 		return false
 	end
 	
-	if(msgcontains(msg, "Yalahar")) then
-		npcHandler:say("Do you want to travel to Yalahar for 50 gold?", cid)
+	if msgcontains(msg, "Yalahar") then
+		npcHandler:say("Do you seek a passage to Yalahar for 50 gold?", cid)
 		npcHandler.topic[cid] = 1
-	elseif(msgcontains(msg, "yes")) then
-		if(npcHandler.topic[cid] == 1) then
-			if(getPlayerMoney(cid) >= 50) then
-				doPlayerRemoveMoney(cid, 50)
-				doTeleportThing(cid, {x = 32837, y = 31366, z = 7})
-				doSendMagicEffect({x = 32837, y = 31366, z = 7}, CONST_ME_TELEPORT)
+	elseif msgcontains(msg, "yes") then
+		if npcHandler.topic[cid] == 1 then
+			if player:removeMoney(110) then
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+				player:teleportTo(Position(32837, 31366, 7))
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 				npcHandler.topic[cid] = 0
 			else
 				npcHandler:say("You don't have enought money.", cid)
@@ -30,6 +30,8 @@ local function creatureSayCallback(cid, type, msg)
 	return true
 end
 
-npcHandler:setMessage(MESSAGE_GREET, "Want to go back to {Yalahar} for 50 gold? Just ask me.")
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:setMessage(MESSAGE_GREET, "Want to go back to {Yalahar} for 50 gold? Just ask me.")
+npcHandler:setMessage(MESSAGE_FAREWELL, "Good bye.")
+npcHandler:setMessage(MESSAGE_WALKAWAY, "Good bye then.")
 npcHandler:addModule(FocusModule:new())
--- data/npc/scripts/Harlow.lua
+++ data/npc/scripts/Harlow.lua
@@ -8,19 +8,19 @@ function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)
 function onThink()				npcHandler:onThink()					end
 
 local function creatureSayCallback(cid, type, msg)
-	if(not npcHandler:isFocused(cid)) then
+	if not npcHandler:isFocused(cid) then
 		return false
 	end
-	
-	if(msgcontains(msg, "Vengoth")) then
-		npcHandler:say("Do you want to travel to Vengoth for 100 gold?", cid)
+
+	if msgcontains(msg, "Vengoth") then
+		npcHandler:say("Do you seek a passage to Vengoth for 100 gold?", cid)
 		npcHandler.topic[cid] = 1
-	elseif(msgcontains(msg, "yes")) then
-		if(npcHandler.topic[cid] == 1) then
-			if(getPlayerMoney(cid) >= 100) then
-				doPlayerRemoveMoney(cid, 100)
-				doTeleportThing(cid, {x = 32858, y = 31549, z = 7})
-				doSendMagicEffect({x = 32858, y = 31549, z = 7}, CONST_ME_TELEPORT)
+	elseif msgcontains(msg, "yes") then
+		if npcHandler.topic[cid] == 1 then
+			if player:removeMoney(110) then
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+				player:teleportTo(Position(32858, 31549, 7))
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 				npcHandler.topic[cid] = 0
 			else
 				npcHandler:say("You don't have enought money.", cid)
@@ -30,6 +30,8 @@ local function creatureSayCallback(cid, type, msg)
 	return true
 end
 
-npcHandler:setMessage(MESSAGE_GREET, "Can I interest you in a trip to {Vengoth}?")
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:setMessage(MESSAGE_GREET, "Can I interest you in a trip to {Vengoth}?")
+npcHandler:setMessage(MESSAGE_FAREWELL, "Good bye.")
+npcHandler:setMessage(MESSAGE_WALKAWAY, "Good bye then.")
 npcHandler:addModule(FocusModule:new())
