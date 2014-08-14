afd1b0e94c99cb2f7adc91eb74c3f07400560e57
Matthews <matthews.wtiger@gmail.com>
2014-08-14 13:25:52 -0300

    Updated some Boat NPCs, ref #1195
    Former-commit-id: e5c0a21149b322f1f3416e47024ffd2a33552c47

--- data/npc/scripts/Captain Waverider.lua
+++ data/npc/scripts/Captain Waverider.lua
@@ -7,31 +7,41 @@ function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink()				npcHandler:onThink()					end
 
-local travelNode = keywordHandler:addKeyword({'treasure island'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Do you seek a passage to Treasure Island for free?'})
-	travelNode:addChildKeyword({'yes'}, StdModule.travel, {npcHandler = npcHandler, premium = true, level = 0, cost = 0, destination = {x = 32132, y = 32913, z = 7} })
-	travelNode:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, reset = true, text = 'We would like to serve you some time.'})
-
-keywordHandler:addKeyword({'passage'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Where do you want to go? To {Treasure Island}?'})
-keywordHandler:addKeyword({'job'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I am the captain of this ship.'})
-keywordHandler:addKeyword({'captain'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I am the captain of this ship.'})
-
 local function creatureSayCallback(cid, type, msg)
 	if not npcHandler:isFocused(cid) then
 		return false
 	end
 	local player = Player(cid)
 	if msgcontains(msg, 'peg leg') then
-		npcHandler:say('Do you seek a passage to Meriana?', cid)
+		npcHandler:say('Ohhhh. So... <lowers his voice> \'you know who\' sent you so I sail you to \'you know where\'. <wink> <wink> It will cost 50 gold to cover my expenses. Is it that what you wish?', cid)
 		npcHandler.topic[cid] = 1
 	elseif npcHandler.topic[cid] == 1 then
 		if msgcontains(msg, 'yes') then
-			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-			player:teleportTo({x = 32348, y = 32625, z = 7})
-			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			if player:removeMoney(110) then	
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+				player:teleportTo({x = 32348, y = 32625, z = 7})
+				player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+				npcHandler:say('Set the sails!', cid)
+				npcHandler.topic[cid] = 0
+			else
+				npcHandler:say("You don't have enough money.", cid)
+			end	
 		end
+		npcHandler.topic[cid] = 0
 	end
 	return true
 end      
 
+local travelNode = keywordHandler:addKeyword({'treasure island'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Do you seek a passage to Treasure Island for 200 gold?'})
+	travelNode:addChildKeyword({'yes'}, StdModule.travel, {npcHandler = npcHandler, premium = true, level = 0, cost = 200, destination = {x = 32132, y = 32913, z = 7} })
+	travelNode:addChildKeyword({'no'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, reset = true, text = 'We would like to serve you some time.'})
+
+keywordHandler:addKeyword({'passage'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'Where do you want to go? To {Treasure Island}?'})
+keywordHandler:addKeyword({'job'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I am the captain of this ship.'})
+keywordHandler:addKeyword({'captain'}, StdModule.say, {npcHandler = npcHandler, onlyFocus = true, text = 'I am the captain of this ship.'})
+
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:setMessage(MESSAGE_GREET, "Greetings, daring adventurer. If you need a {passage}, let me know.")
+npcHandler:setMessage(MESSAGE_FAREWELL, "Good bye.")
+npcHandler:setMessage(MESSAGE_WALKAWAY, "Oh well.")
 npcHandler:addModule(FocusModule:new())
