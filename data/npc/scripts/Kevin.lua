098fb1bb608fd44b96ff19f2add56369e8369889
viperthepala <timsteenwelle89@hotmail.com>
2014-08-15 15:57:40 +0200

    Update Kevin.lua

--- data/npc/scripts/Kevin.lua
+++ data/npc/scripts/Kevin.lua
@@ -87,7 +87,7 @@ local function creatureSayCallback(cid, type, msg)
 			npcHandler.topic[cid] = 13
 		end
 	elseif msgcontains(msg, "advancement") then
-		if player:getStorageValue(Storage.postman.Mission04) == 2 and player:getStorageValue(Storage.postman.rank) == 0 then
+		if player:getStorageValue(Storage.postman.Mission04) == 2 and player:getStorageValue(Storage.postman.rank) < 1 then
 			npcHandler:say("You are worthy indeed. Do you want to advance in our guild?", cid)
 			npcHandler.topic[cid] = 14
 		elseif player:getStorageValue(Storage.postman.Mission06) == 13 and player:getStorageValue(Storage.postman.Rank) == 1 then
@@ -147,7 +147,6 @@ local function creatureSayCallback(cid, type, msg)
 				doPlayerRemoveBones(cid)
 				npcHandler:say("You have collected all the 20 bones needed. Excellent! Now let's talk about further missions if you are interested.", cid)
 				player:setStorageValue(Storage.postman.Mission04, 2)
-				player:setStorageValue(Storage.postman.Rank, 0)
 				npcHandler.topic[cid] = 0
 			end
 		elseif npcHandler.topic[cid] == 14 then
