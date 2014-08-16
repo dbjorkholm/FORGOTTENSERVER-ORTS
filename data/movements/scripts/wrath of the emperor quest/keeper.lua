7421418c3367adca0678b4bb745b1db835b9c985
Royalot <zapf-benny@web.de>
2014-08-16 14:24:43 +0200

    add keeper movement script
    Former-commit-id: 0eb5ff30d7b501f4cee0e7df82b937e9d5987a2a

--- data/movements/scripts/wrath of the emperor quest/keeper.lua
+++ data/movements/scripts/wrath of the emperor quest/keeper.lua
@@ -15,7 +15,7 @@ function onStepIn(cid, item, position, fromPosition)
 		return true
 	end
 
-	doTargetCombatHealth(cid, cid, COMBAT_PHYSICALDAMAGE, 100, 400, CONST_ME_BIGPLANTS)
+	doTargetCombatHealth(0, cid, COMBAT_PHYSICALDAMAGE, -6000, -8000, CONST_ME_BIGPLANTS)
 	Item(item.uid):transform(12335)
 	addEvent(revertItem, math.random(10, 30) * 1000, position, 12335, 12334)
 	return true
