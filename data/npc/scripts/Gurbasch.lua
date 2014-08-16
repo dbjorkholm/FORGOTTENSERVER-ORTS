5326e6297e434bbb126d2d656dbc0d08693a3898
Matthews <matthews.wtiger@gmail.com>
2014-08-16 09:53:31 -0300

    Minor update on Brodrosch and Gurbasch, cheers @ninjalulz
    • added custom messages:
    Full steam ahead! (Brodrosch)
    Full speed ahead! (Gurbasch)

--- data/npc/scripts/Gurbasch.lua
+++ data/npc/scripts/Gurbasch.lua
@@ -31,21 +31,21 @@ local function creatureSayCallback(cid, type, msg)
 					player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 					player:teleportTo(Position(33025, 31553, 10))
 					player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-					npcHandler:say('Set the sails!', cid)
+					npcHandler:say('Full speed ahead!', cid)
 					npcHandler.topic[cid] = 0
 					return true
 				elseif player:getStorageValue(Storage.TheNewFrontier.Mission03) == 3 then --if The New Frontier Quest "Mission 03: Strangers in the Night" complete then Stage 2
 					player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 					player:teleportTo(Position(33025, 31553, 12))
 					player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-					npcHandler:say('Set the sails!', cid)
+					npcHandler:say('Full speed ahead!', cid)
 					npcHandler.topic[cid] = 0
 					return true
 				else --if nothing done Stage 1
 					player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 					player:teleportTo(Position(33025, 31553, 14))
 					player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-					npcHandler:say('Set the sails!', cid)
+					npcHandler:say('Full speed ahead!', cid)
 					npcHandler.topic[cid] = 0
 				end
 			else
