1de8659b1fbf422b1c7b0a3826dfea64153e2b3b
Royalot <zapf-benny@web.de>
2014-08-18 22:17:37 +0200

    minor

--- data/actions/scripts/wrath of the emperor quest/Mission08UninvitedGuestsLever.lua
+++ data/actions/scripts/wrath of the emperor quest/Mission08UninvitedGuestsLever.lua
@@ -1,11 +1,10 @@
 local config = {
-	[3184] = {destination = Position(33082, 31110, 2)},
-	[3185] = {destination = Position(33078, 31080, 13)}
+	[3184] = Position(33082, 31110, 2),
+	[3185] = Position(33078, 31080, 13)
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local Startposition = {x = toPosition.x, y = toPosition.y + 1, z = toPosition.z} -- Startposition of elevator always south of lever
-	local Elevatoruser = Tile(Position(Startposition)):getTopCreature() -- get player who stand on the Startposition elevator
+	local Elevatoruser = Tile(Position({x = toPosition.x, y = toPosition.y + 1, z = toPosition.z})):getTopCreature() -- get player who stand on the Startposition elevator
 
 	if not Elevatoruser or not Elevatoruser:isPlayer() then -- creature and player check
 		Item(item.uid):transform(item.itemid == 1945 and 1946 or 1945)
@@ -13,10 +12,13 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	end
 
 	local lever = config[item.uid]
-	if lever and item.itemid == 1945 then
+	if not lever then
+		return true
+	end
+	if item.itemid == 1945 then
 		Elevatoruser:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-		Elevatoruser:teleportTo(lever.destination)
-		lever.destination:sendMagicEffect(CONST_ME_TELEPORT)
+		Elevatoruser:teleportTo(lever)
+		lever:sendMagicEffect(CONST_ME_TELEPORT)
 	end
 	Item(item.uid):transform(item.itemid == 1945 and 1946 or 1945)
 	return true
