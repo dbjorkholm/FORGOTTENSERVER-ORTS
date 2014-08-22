7d1e3dfff732afc47220b2c146cb9971403f9873
Skillert <peter.mollberg@gmail.com>
2014-08-22 23:14:06 +0200

    Changed script to work for all myserious packages

--- data/actions/scripts/secret service/tools.lua
+++ data/actions/scripts/secret service/tools.lua
@@ -1,7 +1,18 @@
+local config = {
+	[7960] = 10515 -- TBI
+	[7961] = 10513, -- CGB
+	[7962] = 10511, -- AVIN
+}
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
+	local useItem = config[item.uid]
+	if not useItem then
+		return true
+	end
+	
 	local player = Player(cid)
 
-	player:addItem(10515, 1)
+	player:addItem(useItem)
 	player:say('You\'ve found a useful little tool for secret agents in the parcel.', TALKTYPE_MONSTER_SAY)
 	Item(item.uid):remove()
 	
