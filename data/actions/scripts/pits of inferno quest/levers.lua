e1644362c9b6a0383dd60b6ef51064578488c0fe
Skillert <peter.mollberg@gmail.com>
2014-08-22 09:48:39 +0200

    Corrections

--- data/actions/scripts/pits of inferno quest/levers.lua
+++ data/actions/scripts/pits of inferno quest/levers.lua
@@ -1,19 +1,19 @@
-local pos = {
-	[2050] = {text = "You flipped the first level. Hurry up and find the next one!", number = 1},
-	[2051] = {text = "You flipped the second level. Hurry up and find the next one!", number = 2},
-	[2052] = {text = "You flipped the third level. Hurry up and find the next one!", number = 3},
-	[2053] = {text = "You flipped the fourth level. Hurry up and find the next one!", number = 4},
-	[2054] = {text = "You flipped the fifth level. Hurry up and find the next one!", number = 5},
-	[2055] = {text = "You flipped the sixth level. Hurry up and find the next one!", number = 6},
-	[2056] = {text = "You flipped the seventh level. Hurry up and find the next one!", number = 7},
-	[2057] = {text = "You flipped the eighth level. Hurry up and find the next one!", number = 8},
-	[2058] = {text = "You flipped the ninth level. Hurry up and find the next one!", number = 9},
-	[2059] = {text = "You flipped the tenth level. Hurry up and find the next one!", number = 10},
-	[2060] = {text = "You flipped the eleventh level. Hurry up and find the next one!", number = 11},
-	[2061] = {text = "You flipped the twelfth level. Hurry up and find the next one!", number = 12},
-	[2062] = {text = "You flipped the thirteenth level. Hurry up and find the next one!", number = 13},
-	[2063] = {text = "You flipped the fourteenth level. Hurry up and find the next one!", number = 14},
-	[2064] = {text = "You flipped the fifteenth level. Hurry up and find the next one!", number = 15}
+local levers = {
+	[2050] = {text = "You flipped the first lever. Hurry up and find the next one!", number = 1},
+	[2051] = {text = "You flipped the second lever. Hurry up and find the next one!", number = 2},
+	[2052] = {text = "You flipped the third lever. Hurry up and find the next one!", number = 3},
+	[2053] = {text = "You flipped the fourth lever. Hurry up and find the next one!", number = 4},
+	[2054] = {text = "You flipped the fifth lever. Hurry up and find the next one!", number = 5},
+	[2055] = {text = "You flipped the sixth lever. Hurry up and find the next one!", number = 6},
+	[2056] = {text = "You flipped the seventh lever. Hurry up and find the next one!", number = 7},
+	[2057] = {text = "You flipped the eighth lever. Hurry up and find the next one!", number = 8},
+	[2058] = {text = "You flipped the ninth lever. Hurry up and find the next one!", number = 9},
+	[2059] = {text = "You flipped the tenth lever. Hurry up and find the next one!", number = 10},
+	[2060] = {text = "You flipped the eleventh lever. Hurry up and find the next one!", number = 11},
+	[2061] = {text = "You flipped the twelfth lever. Hurry up and find the next one!", number = 12},
+	[2062] = {text = "You flipped the thirteenth lever. Hurry up and find the next one!", number = 13},
+	[2063] = {text = "You flipped the fourteenth lever. Hurry up and find the next one!", number = 14},
+	[2064] = {text = "You flipped the fifteenth lever. Hurry up and find the next one!", number = 15}
 }
 
 local stones = {
@@ -26,7 +26,7 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 
 	if item.uid == 2065  then
 		if item.itemid == 1945 or item.itemid == 1946 then
-			if Game.getStorageValue(1000) == 14 then
+			if Game.getStorageValue(1000) == 15 then
 				for i = 1, 2 do
 					Tile(stones[i]):getItemById(1304):remove()
 					stones[i]:sendMagicEffect(CONST_ME_EXPLOSIONAREA)
@@ -42,9 +42,9 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		Game.setStorageValue(1000, 0)
 	end
 	if item.itemid == 1945 or item.itemid == 1946 then
-		if (Game.getStorageValue(1000) + 1) == pos[item.uid].number then
-			Game.setStorageValue(1000, pos[item.uid].number)
-			player:say(pos[item.uid].text, TALKTYPE_MONSTER_SAY)
+		if (Game.getStorageValue(1000) + 1) == levers[item.uid].number then
+			Game.setStorageValue(1000, levers[item.uid].number)
+			player:say(levers[item.uid].text, TALKTYPE_MONSTER_SAY)
 			Item(item.uid):transform(1946)
 		else
 			player:say("The final lever won't budge... yet.", TALKTYPE_MONSTER_SAY)
