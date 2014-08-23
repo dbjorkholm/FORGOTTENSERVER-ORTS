f258a452a4aa8077d55c317959ef770d0dcc1619
Skillert <peter.mollberg@gmail.com>
2014-08-23 11:07:15 +0200

    Added rest of Emma's CGB transcript

--- data/npc/scripts/Emma.lua
+++ data/npc/scripts/Emma.lua
@@ -37,37 +37,45 @@ local function creatureSayCallback(cid, type, msg)
 			npcHandler:say('I think the druids will be pleased to hear that the immediate threat has been averted.', cid)
 			npcHandler.topic[cid] = 0
 		elseif npcHandler.topic[cid] == 4 then
-			player:setStorageValue(Storage.secretService.AVINMission03, 3)
+			player:setStorageValue(Storage.secretService.CGBMission03, 3)
 			player:setStorageValue(Storage.secretService.Quest, 7)
-			npcHandler:say('Does it not warm up your heart if you can bring a little joy to the people while doing your job? Well, don\'t get carried away, most part of your job is not warming up hearts but tearing them out.', cid)
+			npcHandler:say('Great! This blow strikes them where it hurts most: profit.', cid)
 			npcHandler.topic[cid] = 0
 		elseif npcHandler.topic[cid] == 5 then
-			player:setStorageValue(Storage.secretService.AVINMission04, 3)
-			player:setStorageValue(Storage.secretService.Quest, 9)
-			npcHandler:say('Good work getting rid of that nuisance.', cid)
+			if player:removeItem(7702, 1) then
+				player:setStorageValue(Storage.secretService.CGBMission04, 2)
+				player:setStorageValue(Storage.secretService.Quest, 9)
+				npcHandler:say('I hope our craftsmen can do something with this stuff. For me it makes hardly any sense.', cid)
+			else
+				npcHandler:say('You need to bring me those plans!', cid)
+			end
 			npcHandler.topic[cid] = 0
 		elseif npcHandler.topic[cid] == 6 then
-			if player:removeItem(7708, 1) then
-				player:setStorageValue(Storage.secretService.AVINMission05, 2)
+			if player:removeItem(7703, 1) then
+				player:setStorageValue(Storage.secretService.CGBMission05, 2)
 				player:setStorageValue(Storage.secretService.Quest, 11)
-				npcHandler:say('Fine, fine. This will serve us quite well. Ah, don\'t give me that look... you are not that stupid, are you?', cid)
+				npcHandler:say('I will have this correspondence examined by our specialists. I am sure they are quite revealing.', cid)
 			else
-				npcHandler:say('Come back when you\'ve found the ring.', cid)
+				npcHandler:say('Bring me back some hints or something!', cid)
 			end
 			npcHandler.topic[cid] = 0
 		elseif npcHandler.topic[cid] == 7 then
-			player:setStorageValue(Storage.secretService.AVINMission06, 3)
-			player:setStorageValue(Storage.secretService.Quest, 13)
-			npcHandler:say('Even if the present has not improved our relations, the weapons will enable the barbarians to put more pressure on Svargrond and Carlin. So in any case we profited from the present.', cid)
+			if player:removeItem(7704, 1) then
+				player:setStorageValue(Storage.secretService.CGBMission06, 2)
+				player:setStorageValue(Storage.secretService.Quest, 13)
+				npcHandler:say('I hope this old book will do those researches any good. Personally I see little use to proof some bloodlines after we cut all ties to Thais.', cid)
+			else
+				npcHandler:say('You need to bring us that book of family trees!', cid)
 			npcHandler.topic[cid] = 0
 		elseif npcHandler.topic[cid] == 8 then
 			if player:removeItem(7699, 1) then
 				player:setStorageValue(Storage.secretService.Mission07, 2)
 				player:setStorageValue(Storage.secretService.Quest, 15)
-				player:addItem(7962, 1)
+				player:addItem(7961, 1)
 				npcHandler:say({
-				'You have proven yourself as very efficient. The future may hold great things for you in store ...',
-				'Take this token of gratitude. I hope you can use well what you will find inside!'
+				'Excellent. The queen was not amused about this threat. It\'s a good thing that you have saved the city ...',
+				'Unfortunately, as we are secret agents we can\'t parade for you or something like that, but let me express our gratitude for everything you have done for our city ...',
+				'Take this token of gratitude. You will know when to use it!'
 				}, cid)
 			else
 				npcHandler:say('Please bring me proof of the mad technomancers defeat!', cid)
@@ -120,66 +128,64 @@ local function creatureSayCallback(cid, type, msg)
 		elseif player:getStorageValue(Storage.secretService.CGBMission02) == 2 then
 			npcHandler:say('Have you been successful?', cid)
 			npcHandler.topic[cid] = 3
-		elseif player:getStorageValue(Storage.secretService.AVINMission02) == 2 and player:getStorageValue(Storage.secretService.Quest) == 5 then
+		elseif player:getStorageValue(Storage.secretService.CGBMission02) == 3 and player:getStorageValue(Storage.secretService.Quest) == 5 then
 			player:setStorageValue(Storage.secretService.Quest, 6)
-			player:setStorageValue(Storage.secretService.AVINMission03, 1)
-			player:addItem(7706, 1)
+			player:setStorageValue(Storage.secretService.CGBMission03, 1)
+			player:addItem(7698, 1)
 			npcHandler:say({
-				'The oppression of Carlin\'s men by their lunatic women is unbearable to some of our authorities. We see it as our honourable duty to support the male resistance in Carlin ...',
-				'The poor guys have some speakeasy in the sewers. Bring them this barrel of beer with our kind regards to strengthen their resistance.'
+				'The scheming Venoreans are a constant thorn in our side. They supply our enemies with all kind of equipment to boost the threat they pose to our freedom. It will be your task to hinder future weapon deliveries significantly ...',
+				'The druids have supplied us with some exotic bugs. They are called rust bugs and they did not receive this name for their colour ...',
+				'Take this box of rust bugs and use them on the keyhole of the smithy in the Ironhouse. These \'pets\' will ruin all metal there and it will take them a while to get rid of them.'
 			}, cid)
 			npcHandler.topic[cid] = 0
-		elseif player:getStorageValue(Storage.secretService.AVINMission03) == 2 then
-			npcHandler:say('Do you have news to make old Uncle happy?', cid)
+		elseif player:getStorageValue(Storage.secretService.CGBMission03) == 2 then
+			npcHandler:say('Have you been successful?', cid)
 			npcHandler.topic[cid] = 4
-		elseif player:getStorageValue(Storage.secretService.AVINMission03) == 3 and player:getStorageValue(Storage.secretService.Quest) == 7 then
+		elseif player:getStorageValue(Storage.secretService.CGBMission03) == 3 and player:getStorageValue(Storage.secretService.Quest) == 7 then
 			player:setStorageValue(Storage.secretService.Quest, 8)
-			player:setStorageValue(Storage.secretService.AVINMission04, 1)
+			player:setStorageValue(Storage.secretService.CGBMission04, 1)
 			npcHandler:say({
-				'As you know, our lovely city is a bastion of civilisation surrounded by numerous hazards. The nearby Plains of Havoc and the hostile elven town Shadowthorn are only a few of the obstacles we have to overcome on an almost daily basis ...',
-				'Against all odds, we managed to gain some modest profit by exploiting these circumstances in one way or the other. Recently though, one of our neighbours went too far...',
-				'In some ruin in the midst of the Green Claw Swamp, a dark knight had fancied himself as the lord of the swamp for quite a while ...',
-				'For some years, we had some sort of gentleman\'s agreement. In exchange for some supplies and luxuries, the deranged knight used his ominous influence over the local bonelord species to supply us with ... certain goods ...',
-				'However, lately the black knight has proven himself to be no gentleman at all. In a fit of unprovoked rage, he slew our emissary and almost all of his henchmen ...',
-				'Even though we can live with this loss, it becomes obvious that the knight\'s madness gets worse which makes him unbearable as a neighbour. Find him in his hideout in the Green Claw Swamp and get rid of him.'
+				'Venore has plans for a new kind of ship. It will be faster and more resilient than any other known ship. It will surely improve their dominance over the sea trade. Unless we get those plans for ourselves ...',
+				'And this is where you come into play. Find the ship plans in the Venorean shipyard or perhaps at the harbour and bring them here immediately.'
 			}, cid)
 			npcHandler.topic[cid] = 0
-		elseif player:getStorageValue(Storage.secretService.AVINMission04) == 2 then
-			npcHandler:say('Do you have news to make old Uncle happy?', cid)
+		elseif player:getStorageValue(Storage.secretService.CGBMission04) == 1 then
+			npcHandler:say('Have you been successful?', cid)
 			npcHandler.topic[cid] = 5
-		elseif player:getStorageValue(Storage.secretService.AVINMission04) == 3 and player:getStorageValue(Storage.secretService.Quest) == 9 then
+		elseif player:getStorageValue(Storage.secretService.CGBMission04) == 2 and player:getStorageValue(Storage.secretService.Quest) == 9 then
 			player:setStorageValue(Storage.secretService.Quest, 10)
-			player:setStorageValue(Storage.secretService.AVINMission05, 1)
-			npcHandler:say('I need you to locate a lost ring on the Isle of the Kings for me, get back to me once you have it.', cid)
-			npcHandler.topic[cid] = 0
-		elseif player:getStorageValue(Storage.secretService.AVINMission05) == 1 then
-			npcHandler:say('Do you have news to make old Uncle happy?', cid)
-			npcHandler.topic[cid] = 6
-		elseif player:getStorageValue(Storage.secretService.AVINMission05) == 2 and player:getStorageValue(Storage.secretService.Quest) == 11 then
-			player:setStorageValue(Storage.secretService.Quest, 12)
-			player:setStorageValue(Storage.secretService.AVINMission06, 1)
-			player:addItem(7707, 1)
+			player:setStorageValue(Storage.secretService.CGBMission05, 1)
 			npcHandler:say({
-				'We try to establish new trade agreements with various potential customers. Sometimes we have to offer some presents in advance to ensure that trade is prospering and flourishing. It will be your task to deliver one of those little presents ...',
-				'The northern barbarians are extremely hostile to us. The ones living in Svargrond are poisoned by the lies of agitators from Carlin. The barbarians that are also known as raiders are another story though ...',
-				'Of course they are extremely wild and hostile but we believe that we will sooner or later profit from it when we are able to improve our relations. Please deliver this chest of weapons to the barbarians as a sign of our good will ...',
-				'Unfortunately, most of them will attack you on sight. It will probably take some time until you find somebody that is willing to talk to you and to accept the weapons.'
+			'Ruins of some ancient cathedral are found south west of Venore. It was a project that the Thaians never finished. However, our scouts reported some suspicious activities there ...',
+			'There is a continual coming and going which hints on something big hiding there. We ask you to enter the ruins of the cathedral and to find out what all these people are doing there ...',
+			'You might find several hints there, but I am sure you will know exactly when you have found what we are looking for.'
 			}, cid)
 			npcHandler.topic[cid] = 0
-		elseif player:getStorageValue(Storage.secretService.AVINMission06) == 2 then
-			npcHandler:say('Do you have news to make old Uncle happy?', cid)
+		elseif player:getStorageValue(Storage.secretService.CGBMission05) == 1 then
+			npcHandler:say('Have you been successful?', cid)
+			npcHandler.topic[cid] = 6
+		elseif player:getStorageValue(Storage.secretService.CGBMission05) == 2 and player:getStorageValue(Storage.secretService.Quest) == 11 then
+			player:setStorageValue(Storage.secretService.Quest, 12)
+			player:setStorageValue(Storage.secretService.CGBMission06, 1)
+			npcHandler:say({
+				'As you might know, once the old aristocracy of our city shared blood-ties with the noblemen of Thais. There are many unresolved claims for titles and family heirlooms and Thais does little to help in this matter ...',
+				'Therefore, we will take matters into our own hands. There is a grave in the crypts on the Isle of the Kings in which we assume a book containing ancient family histories and family trees ...',
+				'We need this book! We will not ask how you acquired it.'
+			}, cid)
+			npcHandler.topic[cid] = 0
+		elseif player:getStorageValue(Storage.secretService.CGBMission06) == 1 then
+			npcHandler:say('Have you been successful?', cid)
 			npcHandler.topic[cid] = 7
-		elseif player:getStorageValue(Storage.secretService.AVINMission06) == 3 and player:getStorageValue(Storage.secretService.Quest) == 13 then
+		elseif player:getStorageValue(Storage.secretService.CGBMission06) == 2 and player:getStorageValue(Storage.secretService.Quest) == 13 then
 			player:setStorageValue(Storage.secretService.Quest, 14)
 			player:setStorageValue(Storage.secretService.Mission07, 1)
 			npcHandler:say({
-				'Some dwarven criminal called Blowbeard dares to blackmail our city. He threatens to destroy the whole city and demands an insane amount of gold ...',
-				'Of course we are not willing to give him a single gold coin. It will be your job to get rid of this problem. Go and kill this infamous dwarf ...',
-				'His laboratory is near the technomancer hall. Bring me his beard as proof of his demise.'
+				'I have bad news: a mad dwarf threatens to destroy our beloved city. He claims to have invented some device that enables him to destroy the whole city ...',
+				'He has a laboratory somewhere in Kazordoon, probably somewhere near the technomancer hall. Find him and kill him! Bring me his beard as a proof!'
 			}, cid)
 			npcHandler.topic[cid] = 0
-		elseif player:getStorageValue(Storage.secretService.AVINMission06) == 3 and player:getStorageValue(Storage.secretService.Mission07) == 1 then
-			npcHandler:say('Do you have news to make old Uncle happy?', cid)
+		elseif player:getStorageValue(Storage.secretService.CGBMission06) == 2 and player:getStorageValue(Storage.secretService.Mission07) == 1 then
+			npcHandler:say('Have you been successful?', cid)
 			npcHandler.topic[cid] = 8
 		end
 	end
