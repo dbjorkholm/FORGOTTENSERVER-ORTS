074fc0120c271e79356275e2738d2d4611417576
EPuncker <gm.perflex@gmail.com>
2014-08-30 14:56:02 -0300

    minor

--- data/actions/scripts/koshei amulet quest/switch.lua
+++ data/actions/scripts/koshei amulet quest/switch.lua
@@ -1,5 +1,5 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if Tile(Position(33281, 32442, 8)):getItemById(3698) and Tile(Position(33286, 32444, 8)):getItemById(3698) and Tile(Position(33276, 32444, 8)):getItemByID(3697) and Tile(Position(33278, 32450, 8)):getItemByID(3697) and Tile(Position(33284, 32450, 8)):getItemByID(3697) then
+	if Tile(Position(33281, 32442, 8)):getItemById(3698) and Tile(Position(33286, 32444, 8)):getItemById(3698) and Tile(Position(33276, 32444, 8)):getItemById(3697) and Tile(Position(33278, 32450, 8)):getItemById(3697) and Tile(Position(33284, 32450, 8)):getItemById(3697) then
 		if Tile(Position(33273, 32458, 8)):getItemById(7520) then
 			Tile(Position(33273, 32458, 8)):getItemById(7520):transform(7525)
 		else
