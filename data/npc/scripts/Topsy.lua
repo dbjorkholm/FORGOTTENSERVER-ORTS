9c89d8d19c07730547e963338a6b83782741eff3
EPuncker <gm.perflex@gmail.com>
2014-08-31 15:50:22 -0300

    Fixed #1241

---
+++ data/npc/scripts/Topsy.lua
@@ -0,0 +1,55 @@
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+
+local lastSound = 0
+function onThink()
+	if lastSound < os.time() then
+		lastSound = (os.time() + 5)
+		if math.random(100) < 25 then
+			Npc():say("Runes, wands, rods, health and mana potions! Have a look!", TALKTYPE_SAY)
+		end
+	end
+	npcHandler:onThink()
+end
+
+local function creatureSayCallback(cid, type, msg)
+	if not npcHandler:isFocused(cid) then
+		return false
+	end
+	local player = Player(cid)
+	local items = {[1] = 2190, [2] = 2182, [5] = 2190, [6] = 2182}
+	if msgcontains(msg, 'first rod') or msgcontains(msg, 'first wand') then
+		if player:isSorcerer() or player:isDruid() then
+			if player:getStorageValue(50079) == -1 then
+				npcHandler:say('So you ask me for a {' .. ItemType(items[player:getVocation():getId()]):getName() .. '} to begin your adventure?', cid)
+				npcHandler.topic[cid] = 1
+			else
+				npcHandler:say('What? I have already gave you one {' .. ItemType(items[player:getVocation():getId()]):getName() .. '}!', cid)
+			end
+		else
+			npcHandler:say('Sorry, you aren\'t a druid either a sorcerer.', cid)
+		end
+	elseif msgcontains(msg, 'yes') then
+		if npcHandler.topic[cid] == 1 then
+			player:addItem(items[player:getVocation():getId()], 1)
+			npcHandler:say('Here you are young adept, take care yourself.', cid)
+			player:setStorageValue(50079, 1)
+		end
+		npcHandler.topic[cid] = 0
+	elseif msgcontains(msg, 'no') and isInArray({1}, npcHandler.topic[cid]) == true then
+		npcHandler:say('Ok then.', cid)
+		npcHandler.topic[cid] = 0
+	end
+	return true
+end
+
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:setMessage(MESSAGE_GREET, "Hello, dear |PLAYERNAME|. How can I help you? If you need magical equipment such as runes or wands, just ask me for a trade. ")
+npcHandler:setMessage(MESSAGE_FAREWELL, "Good bye, |PLAYERNAME|. Do come again!")
+npcHandler:setMessage(MESSAGE_WALKAWAY, "Good bye, |PLAYERNAME|. Do come again!")
+npcHandler:addModule(FocusModule:new())
