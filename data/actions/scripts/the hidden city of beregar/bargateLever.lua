30b7fc20cd5c2c51a6facdcd809e748fb439db9e
Royalot <zapf-benny@web.de>
2014-09-03 18:17:29 +0200

    fix beregar gate lever
    part of #994

--- data/actions/scripts/the hidden city of beregar/bargateLever.lua
+++ data/actions/scripts/the hidden city of beregar/bargateLever.lua
@@ -3,26 +3,42 @@ local config = {
 		Position(32569, 31421, 9),
 		Position(32569, 31422, 9)
 	},
+	leverPositions = {
+		Position(32568, 31421, 9),
+		Position(32570, 31421, 9)
+	},
 	removeCreaturePosition = Position(32568, 31422, 9),
 	wallID = 3519,
+	wallID2 = 3524,
 	tileID = 3154
 }
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	local tile, thing, creature, levertile, lever
+	local tile, thing, thing2 , creature, lever, leverstatus
+	leverstatus = item.itemid
+	for i = 1, #config.leverPositions do
+		lever = Tile(config.leverPositions[i]):getItemById(leverstatus == 1945 and 1945 or 1946)
+		lever:transform(item.itemid == 1945 and 1946 or 1945)
+	end
 	for i = 1, #config.gatePositions do
 		tile = Tile(config.gatePositions[i])
 		if tile then
-			thing, creature = tile:getItemById(item.itemid == 1945 and config.tileID or config.wallID), tile:getTopCreature()
-			if thing then
-				thing:transform(item.itemid == 1945 and config.wallID or config.tileID)
+			creature = tile:getTopCreature()
+			if leverstatus == 1945 then
+				thing = tile:getItemById(config.wallID)
+				thing2 = tile:getItemById(config.wallID2)
+				if thing and thing2 then
+					thing:remove()
+					thing2:remove()
+				end
+			else
+				Game.createItem(config.wallID, 1, config.gatePositions[i])
+				Game.createItem(config.wallID2, 1, config.gatePositions[i])
 			end
-
 			if creature then
 				creature:teleportTo(config.removeCreaturePosition)
 			end
 		end
 	end
-	Item(item.uid):transform(item.itemid == 1945 and 1946 or 1945)
 	return true
 end
