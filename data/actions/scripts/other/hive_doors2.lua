87d19797c7f1f078953dadaa2c0a1fbd6afcf182
Daniel Björkholm <danielbjorkholm@live.se>
2014-09-10 17:32:23 +0200

    Tiny fix

--- data/actions/scripts/other/hive_doors2.lua
+++ data/actions/scripts/other/hive_doors2.lua
@@ -1,6 +1,11 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
-	toPosition.x = player:getPosition().x > toPosition.x and toPosition.x - 1 or toPosition.x + 1
+	local position = player:getPosition()
+	if position.x == toPosition.x then
+		return false
+	end
+
+	toPosition.x = position.x > toPosition.x and toPosition.x - 1 or toPosition.x + 1
 	player:teleportTo(toPosition)
 	toPosition:sendMagicEffect(CONST_ME_TELEPORT)
 	return true
