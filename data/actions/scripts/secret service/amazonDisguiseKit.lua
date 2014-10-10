144226f634a22b302cb8dbc44ceab3e3e42e1fd1
Daniel Björkholm <danielbjorkholm@live.se>
2014-10-10 16:22:50 +0200

    Fixed a map error caused by my last commit

--- data/actions/scripts/secret service/amazonDisguiseKit.lua
+++ data/actions/scripts/secret service/amazonDisguiseKit.lua
@@ -1,6 +1,6 @@
 local condition = Condition(CONDITION_OUTFIT)
 condition:setTicks(20 * 1000) -- should be approximately 20 seconds
-condition:setOutfit({lookType = 137}) -- amazon looktype
+condition:setOutfit({lookType = 137, lookHead = 113, lookBody = 120, lookLegs = 114, lookFeet = 132}) -- amazon looktype
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local player = Player(cid)
