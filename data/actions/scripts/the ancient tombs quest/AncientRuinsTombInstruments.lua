7ffd859533d95554cee7b0172935cbb990be523d
PrinterLUA <mastero_90@hotmail.com>
2014-12-20 15:18:40 +0100

    Minor fix Ancient Of Tomb Quest

--- data/actions/scripts/the ancient tombs quest/AncientRuinsTombInstruments.lua
+++ data/actions/scripts/the ancient tombs quest/AncientRuinsTombInstruments.lua
@@ -9,7 +9,7 @@ local config = {
 
 local storage = Storage.TheAncientTombs.VashresamunInstruments
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	local tragetTable = config[item.itemid]
+	local targetTable = config[item.itemid]
 	if not targetTable then
 		player:setStorageValue(storage, 0)
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You played them wrong, now you must begin with first again!')
